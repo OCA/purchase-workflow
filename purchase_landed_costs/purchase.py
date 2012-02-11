254be90db223aec8eada122371b5c225fdd858d7
unknown <office@chricar>
2012-02-11 12:03:01 +0100

    [ADD] purchase_landed_costs (/home/dcormier/sfl/projets/fruigor/bzr/c2c-rd-addons/ rev 167.24.2)

--- purchase_landed_costs/purchase.py
+++ purchase_landed_costs/purchase.py
@@ -24,12 +24,24 @@ from osv import osv, fields
 import decimal_precision as dp
 from tools.translate import _
         
+import sys
+
+class product_product(osv.osv):
+    _inherit = "product.product"
+
+    _columns = \
+        {
+          'landed_cost_type': fields.selection( [('value','Value'), ('quantity','Quantity'), ('none','None'), 'Distribution Type', required=True, \
+                  help="Used for Landed Costs: If landed costs are defined for purchase orders or pickings, this indicates how the costs are distributed to the lines"),
+        }
+
+landed_cost_category()
 
 class landed_cost_position(osv.osv):
     _name = "landed.cost.position"
 
     _columns = \
-      { 'product_id' : fields.many2one('product.product','Landed Cost Name', required=True, domain=[('landed_cost_type','!=', False)]),
+      { 'product_id' : fields.many2one('product.product','Landed Cost Name', required=True, domain=[(landed_cost_type),'!=', False)]),
         'amount'      : fields.float
             ( 'Amount'
             , required=True
@@ -38,21 +50,13 @@ class landed_cost_position(osv.osv):
         'amount_currency': fields.float('Amount Currency', help="The amount expressed in an optional other currency."),
         'currency_id': fields.many2one('res.currency', 'Secondary Currency', help="Optional other currency."),
         'partner_id': fields.many2one('res.partner', 'Partner', help="The supplier of this cost component ."),
-        'price_type': fields.selection( [('per_unit','Per Quantity'), ('value','Absolute Value')], 'Amount Type', required=True,  \
+        'price_type': fields.selection( [('per_unit','Per Unit'), ('value','Absolute Value'), 'Amount Type', required=True,  \
                   help="Defines if the amount is to be calculated for each quantity or an absolute value"),
-        'purchase_order_line_id': fields.many2one('purchase.order.line', 'Purchase Order Line'),
-        'purchase_order_id': fields.many2one('purchase.order', 'Purchase Order'),
-        'move_line_id': fields.many2one('stock.move', 'Picking Line'),
-        'picking_id': fields.many2one('stock.picking', 'Picking'),
+        'purchase_order_line_id': fields.many2one('purchase.order.line', 'Purchase Order Line',
+        'purchase_order_id': fields.many2one('purchase.order', 'Purchase Order',
+        'move_line_id': fields.many2one('stock.move', 'Picking Line',
+        'picking_id': fields.many2one('stock.picking', 'Picking',
       }
-
-    def onchange_product_id(self, cr, uid, ids, product_id, context=None):
-        if product_id:
-            prod_obj=self.pool.get('product.product')
-            prod=prod_obj.browse(cr,uid,[product_id])[0]
-            v = {'price_type':prod.landed_cost_type}
-            return {'value': v}
-        return {}
 
 landed_cost_position()
 
@@ -74,23 +78,13 @@ class purchase_order_line(osv.osv):
                         landed_costs += costs.amount
                     else:       
                         landed_costs += costs.amount * line.product_qty
-            result[line.id] = landed_costs
-        return result
-
-    def _landing_cost_order(self, cr, uid, ids, name, args, context):
-        if not ids : return {}
-        result = {}
-        landed_costs = 0.0
-        # landed costss for the line
-        for line in self.browse(cr, uid, ids):
-            # distrubution of landed costs of PO
+        # distrubution of landed costs of PO
             if line.order_id.landed_cost_line_ids:
                landed_costs += line.order_id.landed_cost_base_value / line.order_id.amount_total * line.price_subtotal + \
                         line.order_id.landed_cost_base_quantity / line.order_id.quantity_total * line.product_qty
             result[line.id] = landed_costs
-
+                        
         return result
-
 
     def _landed_cost(self, cr, uid, ids, name, args, context):
         if not ids : return {}
@@ -98,17 +92,15 @@ class purchase_order_line(osv.osv):
         landed_costs = 0.0
         # landed costss for the line
         for line in self.browse(cr, uid, ids):
-            landed_costs += line.price_subtotal + line.landing_costs +  line.landing_costs_order
+            landed_costs += line.price_subtotal + line.landing_costs
             result[line.id] = landed_costs
 
         return result
         
     _columns = \
-       {
-         'landed_cost_line_ids': fields.one2many('landed.cost.position', 'purchase_order_line_id', 'Landed Costs Positions'),
-         'landing_costs' : fields.function(_landing_cost, digits_compute=dp.get_precision('Account'), string='Landing Costs'),
-         'landing_costs_order' : fields.function(_landing_cost_order, digits_compute=dp.get_precision('Account'), string='Landing Costs from Order'),
-         'landed_costs' : fields.function(_landed_cost, digits_compute=dp.get_precision('Account'), string='Landed Costs'),
+         'landed_cost_line_ids': fields.one2many('account.move.line', 'purchase_order_line_id', 'Landed Costs Positions'),
+         'landing_costs' : fields.function(_landing_costs, digits_compute=dp.get_precision('Account'), string='Landing Costs'),
+         'landed_costs' : fields.function(_landed_costs, digits_compute=dp.get_precision('Account'), string='Landed Costs'),
     }
 
 purchase_order_line()
@@ -123,7 +115,7 @@ class purchase_order(osv.osv):
         for line in self.browse(cr, uid, ids):
             if line.landed_cost_line_ids:
                 for costs in line.landed_cost_line_ids:
-                    if costs.product_id.landed_cost_type == 'value':
+                    if costs.category_id.type == 'value':
                         landed_costs_base_value += costs.amount
             result[line.id] = landed_costs_base_value
         return result
@@ -135,7 +127,7 @@ class purchase_order(osv.osv):
         for line in self.browse(cr, uid, ids):
             if line.landed_cost_line_ids:
                 for costs in line.landed_cost_line_ids:
-                    if costs.product_id.landed_cost_type == 'quantity':
+                    if costs.category_id.type == 'quantity':
                          landed_costs_base_quantity += costs.amount
             result[line.id] = landed_costs_base_quantity
         return result
@@ -152,89 +144,22 @@ class purchase_order(osv.osv):
             result[line.id] = quantity_total
         return result
 
-    def _landed_cost(self, cr, uid, ids, name, args, context):
-        if not ids : return {}
-        result = {}
-        landed_costs = 0.0
-        # landed costss for the line
-        for line in self.browse(cr, uid, ids):
-            landed_costs += line.landing_cost_lines + line.amount_untaxed
-            result[line.id] = landed_costs
-
-        return result
-
-    def _landing_cost_lines(self, cr, uid, ids, name, args, context):
-        if not ids : return {}
-        result = {}
-        landed_cost_lines = 0.0
-        for line in self.browse(cr, uid, ids):
-            if line.order_line:
-                for pol in line.order_line:
-                    if pol.product_qty > 0.0:
-                         landed_cost_lines += pol.landing_costs
-            result[line.id] = landed_cost_lines
-        return result
-
 
     _columns = \
-        {
-         'landed_cost_line_ids': fields.one2many('landed.cost.position', 'purchase_order_id', 'Landed Costs'),
+         'landed_cost_line_ids': fields.one2many('account.move.line', 'purchase_order_line_id', 'Landed Costs'),
          'landed_cost_base_value' : fields.function(_landed_cost_base_value, digits_compute=dp.get_precision('Account'), string='Landed Costs Base Value'),
          'landed_cost_base_quantity' : fields.function(_landed_cost_base_quantity, digits_compute=dp.get_precision('Account'), string='Landed Costs Base Quantity'),
-         'landing_cost_lines' : fields.function(_landing_cost_lines, digits_compute=dp.get_precision('Account'), string='Landing Cost Lines'),
-         'landed_cost' : fields.function(_landed_cost, digits_compute=dp.get_precision('Account'), string='Landed Costs Total Untaxed'),
          'quantity_total' : fields.function(_quantity_total, digits_compute=dp.get_precision('Product UoM'), string='Total Quantity'),
     }
 
     def _prepare_order_line_move(self, cr, uid, order, order_line, picking_id, context=None):
         res = super(purchase_order,self)._prepare_order_line_move( cr, uid, order, order_line, picking_id, context)
-        res['price_unit_net'] =  res['price_unit']
-        res['price_unit'] = order_line.landed_costs / order_line.product_qty        
+        #price_unit_id = order_line.price_unit_id.id
+        #price_unit_pu = order_line.price_unit_pu
+        #res.update({'price_unit_id' : price_unit_id,'price_unit_pu' : price_unit_pu})
         return res
 
-    def _prepare_order_picking(self, cr, uid, order, context=None):
-        res = super(purchase_order,self)._prepare_order_picking( cr, uid, order, context)
-
-        return res
-
-    def _create_pickings(self, cr, uid, order, order_lines, picking_id=False, context=None): 
-        res =  super(purchase_order,self)._create_pickings(cr, uid, order, order_lines, picking_id, context)
-        import sys
-        pick_id = int(res[0])
-        # landing costs for PICK from PO 
-        cost_obj = self.pool.get('landed.cost.position')
-        for order_cost in order.landed_cost_line_ids:
-            vals = {}
-            vals['product_id'] = order_cost.product_id.id
-            vals['partner_id'] = order_cost.partner_id.id
-            vals['amount'] = order_cost.amount
-            vals['amount_currency'] = order_cost.amount_currency
-            vals['currency_id'] = order_cost.currency_id.id
-            vals['price_type'] = order_cost.price_type
-            vals['picking_id'] = pick_id
-            print >> sys.stderr, 'vals', vals
-            cost_obj.create(cr, uid, vals, context=None) 
-
-        #self.pool.get('landed.cost.position').create(cr, uid, cost_lines, context=None) 
-        # landing costs for PICK Lines from PO   
-        pick_obj = self.pool.get('stock.picking')
-        for pick in pick_obj.browse(cr, uid, [pick_id], context=None):
-          print >> sys.stderr, 'pick', pick
-          for line in pick.move_lines:
-           print >> sys.stderr, 'line', line
-           for order_cost in line.purchase_line_id.landed_cost_line_ids:
-            vals = {}
-            vals['product_id'] = order_cost.product_id.id
-            vals['partner_id'] = order_cost.partner_id.id
-            vals['amount'] = order_cost.amount
-            vals['amount_currency'] = order_cost.amount_currency
-            vals['currency_id'] = order_cost.currency_id.id
-            vals['price_type'] = order_cost.price_type
-            vals['move_line_id'] = line.id
-            print >> sys.stderr, 'vals', vals
-            cost_obj.create(cr, uid, vals, context=None) 
-        print >> sys.stderr, 'cost created'
-           
-        return res
+    # FIXME - must create new landed cost for picking and stock moves on confirm
+    # FIXME - must create picking for each partner on confirm
 
 purchase_order()
