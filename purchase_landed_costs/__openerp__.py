4a8a9ed6f0d2faf84ed7769b6f92c6f1cd855cb9
ferdinand <office@chricar.at>
2012-02-15 13:29:03 +0100

    merge (/home/dcormier/sfl/projets/fruigor/bzr/c2c-rd-addons/ rev 167.2.4)

---
+++ purchase_landed_costs/__openerp__.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2010-2010 Camptocamp Austria (<http://www.camptocamp.at>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Landed Costs',
+    'version': '0.7',
+    'category': 'Warehouse Management',
+    'description': """
+    This module add the possibility to iclude landed costs in the average price computation.
+    The landed costs can be defined for 
+    * purchase orders
+    * purchase order lines
+    * pickings
+    * picking lines (stock moves)
+    costs defined for purchase orders and pickings will be distributed according to the distribution type
+    defined in landed cost category
+    * value - example custom fees
+    * quantity - example freight
+    """,
+    'author': 'Camptocamp Austria',
+    'depends': ['purchase' ],
+    'update_xml': ['security/ir.model.access.csv',
+                   'purchase_view.xml',
+                   'stock_view.xml',
+                   ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
