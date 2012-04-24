41670dd784eea22e5f8b9914bc1aa2d3da7c7b1b
openerp@bazaar.camptocamp.net <openerp@bazaar.camptocamp.net>
2012-04-24 17:28:58 +0200

    merge + budget enhancements (/home/dcormier/sfl/projets/fruigor/bzr/c2c-rd-addons/ rev 242.1.6)

--- purchase_landed_costs/__init__.py
+++ purchase_landed_costs/__init__.py
@@ -3,7 +3,7 @@
 #    
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#    Copyright (C) 2010-2010 Camptocamp Austria (<http://www.camptocamp.at>)
+#    Copyright (C) 2010-2012 Camptocamp Austria (<http://www.camptocamp.at>)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
