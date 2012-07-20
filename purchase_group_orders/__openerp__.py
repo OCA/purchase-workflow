8384c946f93447c34b26c8a6dd5c8b170ecf011f
@ <@>
2012-07-20 14:20:53 +0200

    [IMP] purchase_group_orders: display information about SO shop and carrier on the PO (lp:c2c-addons/6.1  rev 28.6.31)

--- purchase_group_orders/__openerp__.py
+++ purchase_group_orders/__openerp__.py
@@ -19,7 +19,7 @@
 #
 ##############################################################################
 {'name' : 'Purchase Group Orders by Shop and Carrier',
- 'version' : '0.4',
+ 'version' : '0.3',
  'author' : 'Camptocamp',
  'maintainer': 'Camptocamp',
  'category': 'Purchase Management',
