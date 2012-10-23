cfcfa34c65b80e8acd547c72ac739828bdf5481c
@ <@>
2012-10-23 16:38:34 +0200

    [MIGR] mark modules uninstallable in 7.0 branch. Added migration_process.txt document (/home/dcormier/sfl/projets/fruigor/bzr/c2c-rd-addons/ rev 836)

--- purchase_landed_costs/__openerp__.py
+++ purchase_landed_costs/__openerp__.py
@@ -44,7 +44,7 @@
                    'stock_view.xml',
                    ],
     'demo_xml': [],
-    'installable': True,
+    'installable': False,
     'active': False,
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
