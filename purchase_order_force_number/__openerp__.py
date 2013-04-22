36d72fecabd2d4e81b56c1de0bebb3b1f66ce939
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2013-04-22 19:03:16 +0200

    [fix] description

--- purchase_order_force_number/__openerp__.py
+++ purchase_order_force_number/__openerp__.py
@@ -24,7 +24,7 @@
     'version': '0.1',
     'category': 'Purchase Management',
     'description': """This simple module allows to specify the number to use when creating purchase orders.
-If user does not change the field, the standard sequence is used.""",
+If user does not change the default value ('/'), the standard sequence is used.""",
     'author': 'Agile Business Group',
     'website': 'http://www.agilebg.com',
     'license': 'AGPL-3',
