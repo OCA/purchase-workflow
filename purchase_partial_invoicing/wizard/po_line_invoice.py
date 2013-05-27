38840acc3d3655faa917b0189ba0ad654547c364
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2013-05-27 14:39:09 +0200

    [imp] wizard

--- purchase_partial_invoicing/wizard/po_line_invoice.py
+++ purchase_partial_invoicing/wizard/po_line_invoice.py
@@ -31,6 +31,23 @@ class purchase_line_invoice(orm.TransientModel):
         'partial_invoice': fields.boolean('Partial invoicing'),
         'line_ids': fields.one2many('purchase.order.line_invoice.line','wizard_id','Lines'),
         }
+        
+    def default_get(self, cr, uid, fields, context=None):
+        po_line_obj = self.pool.get('purchase.order.line')
+        lines = []
+        for po_line in po_line_obj.browse(cr, uid, context.get('active_ids',[]), context):
+            lines.append({
+                'po_line_id': po_line.id,
+                'product_qty': po_line.product_qty,
+                'price_unit': po_line.price_unit,
+                })
+        defaults = super(purchase_line_invoice,self).default_get(
+            cr, uid, fields, context=context)
+        defaults['line_ids'] = lines
+        return defaults
+        
+    def makeInvoices(self, cr, uid, ids, context=None):
+        res=super(purchase_line_invoice,self).makeInvoices(cr, uid, ids, context=context)
 
 class purchase_line_invoice_line(orm.TransientModel):
 
@@ -39,6 +56,10 @@ class purchase_line_invoice_line(orm.TransientModel):
     _columns = {
         'po_line_id': fields.many2one('purchase.order.line',
             'Purchase order line', readonly=True),
+        'product_qty': fields.related('po_line_id', 'product_qty', type='float',
+            string='Quantity', readonly=True),
+        'price_unit': fields.related('po_line_id', 'price_unit', type='float',
+            string='Unit Price', readonly=True),
         'percentage': fields.float('Percentage'),
         'wizard_id': fields.many2one('purchase.order.line_invoice','Wizard'),
         }
