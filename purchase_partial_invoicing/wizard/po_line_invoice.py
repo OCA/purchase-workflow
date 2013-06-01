3d381011ca0ae847a739e7ce1fb0f8f95ac9d109
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2013-06-01 17:26:04 +0200

    [fix] File "/home/elbati/workspace/openerp/7.0/server/openerp/addons/purchase_partial_invoicing/wizard/po_line_invoice.py", line 53, in makeInvoices     context = {} IndexError: list index out of range

--- purchase_partial_invoicing/wizard/po_line_invoice.py
+++ purchase_partial_invoicing/wizard/po_line_invoice.py
@@ -51,27 +51,28 @@ class purchase_line_invoice(orm.TransientModel):
     def makeInvoices(self, cr, uid, ids, context=None):
         if context is None:
             context = {}
-        wizard = self.browse(cr, uid, ids[0], context)
-        if wizard.partial_invoice:
-            purchase_line_obj=self.pool.get('purchase.order.line')
-            changed_lines = {}
-            context['active_ids'] = []
-            for line in wizard.line_ids:
-                context['active_ids'].append(line.po_line_id.id)
-                changed_lines[line.po_line_id.id] = line.po_line_id.product_qty
-                invoiced_qty= line.product_qty * (line.percentage / 100.0)
-                line.po_line_id.write({
-                    'product_qty': invoiced_qty,
-                    })
-            res = super(purchase_line_invoice,self).makeInvoices(cr, uid, ids, context=context)
-            for po_line_id in changed_lines:
-                po_line = purchase_line_obj.browse(cr, uid, po_line_id, context)
-                purchase_line_obj.write(cr, uid, [po_line_id], {
-                    'product_qty': changed_lines[po_line_id],
-                    }, context=context)
-                if po_line.invoiced_qty != po_line.product_qty:
-                    po_line.write({'invoiced': False})
-            return res
+        if ids:
+            wizard = self.browse(cr, uid, ids[0], context)
+            if wizard.partial_invoice:
+                purchase_line_obj=self.pool.get('purchase.order.line')
+                changed_lines = {}
+                context['active_ids'] = []
+                for line in wizard.line_ids:
+                    context['active_ids'].append(line.po_line_id.id)
+                    changed_lines[line.po_line_id.id] = line.po_line_id.product_qty
+                    invoiced_qty= line.product_qty * (line.percentage / 100.0)
+                    line.po_line_id.write({
+                        'product_qty': invoiced_qty,
+                        })
+                res = super(purchase_line_invoice,self).makeInvoices(cr, uid, ids, context=context)
+                for po_line_id in changed_lines:
+                    po_line = purchase_line_obj.browse(cr, uid, po_line_id, context)
+                    purchase_line_obj.write(cr, uid, [po_line_id], {
+                        'product_qty': changed_lines[po_line_id],
+                        }, context=context)
+                    if po_line.invoiced_qty != po_line.product_qty:
+                        po_line.write({'invoiced': False})
+                return res
         return super(purchase_line_invoice,self).makeInvoices(cr, uid, ids, context=context)
 
 class purchase_line_invoice_line(orm.TransientModel):
