13e3c75e6e130646a94418cecf5db2d62fcf948f
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-06-26 09:38:59 +0200

    [FIX] pep8: space after commas, spaces around operators, cut long lines; use orm.Model rather than osv.Model (../generic-addons/ rev 10.2.3)

--- purchase_extended/model/purchase_order.py
+++ purchase_extended/model/purchase_order.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from openerp.osv import fields, osv
+from openerp.osv import fields, orm
 from openerp.tools.translate import _
 
 
-class PurchaseOrder(osv.Model):
+class PurchaseOrder(orm.Model):
     _inherit = "purchase.order"
     _columns = {
         'incoterm_address': fields.char(
@@ -15,27 +15,34 @@ class PurchaseOrder(osv.Model):
                  "international transactions."),
     }
 
-    def onchange_dest_address_id_mod(self, cr, uid, ids, dest_address_id, warehouse_id, context=None):
+    def onchange_dest_address_id_mod(self, cr, uid, ids, dest_address_id,
+                                     warehouse_id, context=None):
         value = self.onchange_dest_address_id(cr, uid, ids, dest_address_id)
         warehouse_obj = self.pool.get('stock.warehouse')
-        dest_ids = warehouse_obj.search(cr, uid, [('partner_id','=',dest_address_id)], context=context)
-        if len(dest_ids)>=1:
+        dest_ids = warehouse_obj.search(cr, uid,
+                                        [('partner_id', '=', dest_address_id)],
+                                        context=context)
+        if len(dest_ids) >= 1:
             warehouse_id_ret = dest_ids[0]
             for wh in dest_ids:
                 if wh == warehouse_id:
                     warehouse_id_ret = wh
         else:
             warehouse_id_ret = False
-        value['value'].update({'warehouse_id': warehouse_id_ret,})
+        value['value']['warehouse_id'] = warehouse_id_ret
         return value
 
     def onchange_dest_address_id(self, cr, uid, ids, dest_address_id):
-        return super(PurchaseOrder,self).onchange_dest_address_id(cr, uid, ids, dest_address_id)
+        return super(PurchaseOrder, self).onchange_dest_address_id(
+            cr, uid, ids, dest_address_id)
 
     def onchange_warehouse_id(self, cr, uid, ids, warehouse_id, context=None):
-        value = super(PurchaseOrder,self).onchange_warehouse_id(cr, uid, ids, warehouse_id)
+        value = super(PurchaseOrder, self).onchange_warehouse_id(
+            cr, uid, ids, warehouse_id)
         if warehouse_id:
-            dest = self.pool.get('stock.warehouse').browse(cr, uid, warehouse_id, context=context).partner_id.id
-            value['value'].update({'dest_address_id': dest,})
+            warehouse_obj = self.pool.get('stock.warehouse')
+            dest_id = warehouse_obj.browse(cr, uid, warehouse_id,
+                                           context=context).partner_id.id
+            value['value']['dest_address_id'] = dest_id
             return value
-        return False
+        return {}
