703f256933192fb6b78b121b907c069a5dd71b46
David Cormier <david.cormier@savoirfairelinux.com>
2013-07-17 10:50:16 -0400

    Adds helper method to calculate percentage of landed cost to be put on po line (/home/dcormier/sfl/projets/fruigor/bzr/c2c-rd-addons/ rev 945)

--- purchase_landed_costs/purchase.py
+++ purchase_landed_costs/purchase.py
@@ -91,6 +91,14 @@ class purchase_order_line(osv.osv):
 
         return result
 
+    def _landing_cost_factor(self, cr, uid, ids, name, args, context):
+        """
+        Calculates the percentage of landing costs that should be put on this order line
+        """
+        for line in self.browse(cr, uid, ids):
+            if line.landed_cost_line_ids:
+                pass
+
 
     def _landed_cost(self, cr, uid, ids, name, args, context):
         if not ids : return {}
@@ -198,6 +206,16 @@ class purchase_order(osv.osv):
 
         return res
 
+    def _get_product_account_expense_id(self, product):
+        """
+        Returns the product's account expense id if present
+        or it's parent categories account expense id otherwise
+        """
+        if product.property_account_expense.id:
+            return product.property_account_expense.id
+        return product.categ_id.property_account_expense_categ.id
+            
+
     def _create_pickings(self, cr, uid, order, order_lines, picking_id=False, context=None): 
         res =  super(purchase_order,self)._create_pickings(cr, uid, order, order_lines, picking_id, context)
         pick_id = int(res[0])
@@ -230,7 +248,7 @@ class purchase_order(osv.osv):
            #,'amount' : order_cost.amount
            #,'amount_currency' : order_cost.amount_currency
            #,'picking_id' : pick_id
-           ,'account_id' : order_cost.product_id.property_account_expense.id
+           ,'account_id' : self._get_product_account_expense_id(order_cost.product_id)
            ,'partner_id' : order_cost.partner_id.id
            ,'invoice_id' : inv_id
            ,'price_unit' : order_cost.amount
