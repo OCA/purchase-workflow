5c7294da35eceaceae3075dd61b0d01fcbc10683
Jacques-Etienne Baudoux <je@bcim.be>
2013-08-06 13:53:57 +0200

    [FIX]purchase, purchase_requisition: add state draft bid for registering bids in case of open call for bids: forget to add some files (../generic-addons/ rev 15.5.18)

---
+++ purchase_requisition_extended/wizard/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+import purchase_requisition_partner
---
+++ purchase_requisition_extended/wizard/purchase_requisition_partner.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+
+
+class purchase_requisition_partner(osv.TransientModel):
+    _inherit = "purchase.requisition.partner"
+
+    def create_order(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        active_id = context and context.get('active_id', [])
+        data = self.browse(cr, uid, ids, context=context)[0]
+        po_id = self.pool.get('purchase.requisition').make_purchase_order(cr,
+                    uid, [active_id], data.partner_id.id,
+                    context=context)[active_id]
+        if not context.get('draft_bid', False):
+            return {'type': 'ir.actions.act_window_close'}
+        res = self.pool.get('ir.actions.act_window').for_xml_id(cr, uid,
+                    'purchase', 'purchase_rfq', context=context)
+        res.update({'res_id': po_id,
+                    'views': [(False, 'form')],
+                    })
+        return res
