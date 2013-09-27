691fb8dc76044458530ab394b7a00d5cf2e927bc
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2013-09-27 15:18:43 +0200

    [IMP] PEP8

--- purchase_partial_invoicing/wizard/po_line_invoice.py
+++ purchase_partial_invoicing/wizard/po_line_invoice.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    Copyright (C) 2013 Agile Business Group sagl
 #    (<http://www.agilebg.com>)
 #
@@ -27,22 +27,25 @@ import openerp.addons.decimal_precision as dp
 class purchase_line_invoice(orm.TransientModel):
 
     _inherit = 'purchase.order.line_invoice'
-    
+
     _columns = {
         'partial_invoice': fields.boolean('Partial invoicing'),
-        'line_ids': fields.one2many('purchase.order.line_invoice.line','wizard_id','Lines'),
-        }
-        
+        'line_ids': fields.one2many('purchase.order.line_invoice.line',
+        'wizard_id', 'Lines'),
+    }
+
     def default_get(self, cr, uid, fields, context=None):
         po_line_obj = self.pool.get('purchase.order.line')
         lines = []
-        for po_line in po_line_obj.browse(cr, uid, context.get('active_ids',[]), context=context):
+        for po_line in po_line_obj.browse(cr, uid,
+                                          context.get('active_ids', []),
+                                          context=context):
             lines.append({
                 'po_line_id': po_line.id,
                 'product_qty': po_line.product_qty - po_line.invoiced_qty,
                 'invoiced_qty': po_line.product_qty - po_line.invoiced_qty,
                 'price_unit': po_line.price_unit,
-                })
+            })
         defaults = super(purchase_line_invoice, self).default_get(
             cr, uid, fields, context=context)
         defaults['line_ids'] = lines
@@ -54,38 +57,50 @@ class purchase_line_invoice(orm.TransientModel):
         if ids:
             wizard = self.browse(cr, uid, ids[0], context=context)
             if wizard.partial_invoice:
-                purchase_line_obj=self.pool.get('purchase.order.line')
+                purchase_line_obj = self.pool.get('purchase.order.line')
                 changed_lines = {}
                 context['active_ids'] = []
                 for line in wizard.line_ids:
                     context['active_ids'].append(line.po_line_id.id)
-                    changed_lines[line.po_line_id.id] = line.po_line_id.product_qty
+                    changed_lines[
+                        line.po_line_id.id
+                    ] = line.po_line_id.product_qty
                     line.po_line_id.write({
                         'product_qty': line.invoiced_qty,
-                        })
-                res = super(purchase_line_invoice,self).makeInvoices(cr, uid, ids, context=context)
+                    })
+                res = super(purchase_line_invoice, self).makeInvoices(
+                    cr, uid, ids, context=context)
                 for po_line_id in changed_lines:
-                    po_line = purchase_line_obj.browse(cr, uid, po_line_id, context=context)
+                    po_line = purchase_line_obj.browse(cr, uid, po_line_id,
+                                                       context=context)
                     purchase_line_obj.write(cr, uid, [po_line_id], {
                         'product_qty': changed_lines[po_line_id],
-                        }, context=context)
+                    }, context=context)
                     if po_line.invoiced_qty != po_line.product_qty:
                         po_line.write({'invoiced': False})
                 return res
-        return super(purchase_line_invoice,self).makeInvoices(cr, uid, ids, context=context)
+        return super(purchase_line_invoice, self).makeInvoices(
+            cr, uid, ids, context=context)
+
 
 class purchase_line_invoice_line(orm.TransientModel):
 
     _name = 'purchase.order.line_invoice.line'
 
     _columns = {
-        'po_line_id': fields.many2one('purchase.order.line',
-            'Purchase order line', readonly=True),
-        'product_qty': fields.float('Quantity', digits_compute=dp.get_precision(
-            'Product Unit of Measure'), readonly=True),
-        'price_unit': fields.related('po_line_id', 'price_unit', type='float',
+        'po_line_id': fields.many2one(
+            'purchase.order.line',
+            'Purchase order line', readonly=True
+        ),
+        'product_qty': fields.float(
+            'Quantity', digits_compute=dp.get_precision(
+            'Product Unit of Measure'), readonly=True
+        ),
+        'price_unit': fields.related(
+            'po_line_id', 'price_unit', type='float',
             string='Unit Price', readonly=True),
-        'invoiced_qty': fields.float('Quantity to invoice', digits_compute=dp.get_precision(
+        'invoiced_qty': fields.float(
+            'Quantity to invoice', digits_compute=dp.get_precision(
             'Product Unit of Measure')),
-        'wizard_id': fields.many2one('purchase.order.line_invoice','Wizard'),
-        }
+        'wizard_id': fields.many2one('purchase.order.line_invoice', 'Wizard'),
+    }
