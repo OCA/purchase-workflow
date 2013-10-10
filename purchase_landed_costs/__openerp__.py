bd6dda872c5b3dbd3be90634297aa0d7cd633f03
Joel Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2013-10-10 11:57:56 +0200

    [DOC] Little changes in the description after talking with business experts (thanks to Frédéric).

--- purchase_landed_costs/__openerp__.py
+++ purchase_landed_costs/__openerp__.py
@@ -29,27 +29,21 @@
 Purchase Landed Costs
 =====================
 
-This module adds the possibility to include estimated landed costs in the average price computation. To
-define those landed costs, create products for every landed costs and affect them a distribution type.
-Don't forget to as well assign them a specific financial account (the one which will record the real cost) 
-in order to compare at the end of the year the estimation with real accounting entries (see stock valuation).
+This module adds the possibility to include estimated landed costs to the average price computation. To define those landed costs, 
+create products for every landed costs and affect them a distribution type. Don't forget to as well assign them a specific 
+financial account (the one which will record the real cost) in order to compare at the end of the year the estimation with real
+accounting entries (see stock valuation). The landed costs is defined in purchase orders. These costs will be distributed 
+according to the distribution type defined in landed cost:
 
-The landed costs can be defined in purchase orders. Those costs will be distributed according to the
-distribution type defined in landed cost:
+ * value - example custom fees 
+ * quantity - example freight 
 
- * value - example custom fees
- * quantity - example freight
+Note : Products used to define landed cost must have a default "Distribution Type" set (Value/Quantity). 
 
-For each landed cost position define in a PO, a draft invoice can be created in validation of purchase
-order (an option need to be checked). Those invoice will then appear using the view invoice button of PO.
-
-The products used to define landed cost must be classified "Distribution Type" as :
-
- * "Value" (for customs) or 
- * "Quantity" (for freight)
-
-You can define landed cost relative to a whole PO or by line (or both) and the system will distribute
-them to each line according to the chosen distribution type. 
+For each landed cost position (=line) define in a PO, a draft invoice can be pre-created at PO validation (an option need to 
+be checked). Doing so will allow you to see those invoices using the view invoice button directly from the PO. You can define 
+landed cost relative to a whole PO or by PO line (or both) and the system will distribute them to each line according to the 
+chosen distribution type.
 
 Note that the landed cost is always expressed in company currency.
 
@@ -57,18 +51,18 @@ Find all landed cost here : Reporting -> Purchase -> Landed costs
 
 Stock valuation:
 ----------------
-As the Average price is also used for the stock valuation and because the computation is based on estimation
-of landed cost in the PO (done at incomming shippment reception), you will have a little difference between
-accounting and stock valuation that will need to be corrected when making the stock accounting entry. To 
-correct that amount, make a sum of estimated landed cost (landed cost position) by account and compare with 
-the real account chart value. You can acces those informations through this menu: Reporting -> Purchase -> Landed costs
+As the average price is also used for the stock valuation and because the computation is based on estimation of landed cost 
+in the PO (done at incoming shipment reception), you will have a  difference with the actual accounting bookings of landed cost. 
+Stock valuation will have to be adjusted with a manual journal entry. In order to correct that amount, make a sum of 
+estimated landed cost (landed cost position) by account and compare with the real account chart value. You can access those 
+informations through this menu: Reporting -> Purchase -> Landed costs
 
 Warning:
 --------
 
- * Average price will be computed based on the estimation made on the PO, not from
-  real cost. This is due to the way OpenERP compute average stock : it stores the updated
-  value at every input, no history, so no way to redefine the value afterwards. e.g.
+ * Average price will be computed based on the estimation made on the PO - not from real cost. This is due to the way OpenERP
+   compute average stock : it stores the updated value at every input, no history, so no way to redefine the value afterwards. 
+   i.e.
     - incomming 01: 100 product A at 50.- AVG = 50.-, stock = 100
     - incomming 02: 100 product A at 60.- AVG = 55.-, stock = 200
     - delivery 01: 50 product A AVG = 55.-, stock = 150
