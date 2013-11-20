55c0eda07b0582a2f9d04cbf6af98d33698a660d
unknown <nicolas.bessi@camptocamp.com>
2013-11-20 14:25:35 +0100

    [FIX] price list tests to support multi-currency

--- framework_agreement/tests/test_framework_agreement_price_list.py
+++ framework_agreement/tests/test_framework_agreement_price_list.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from openerp.osv import orm
 import openerp.tests.common as test_common
 from .common import BaseAgreementTestMixin
 
@@ -29,33 +30,43 @@ class TestAgreementPriceList(test_common.TransactionCase, BaseAgreementTestMixin
                                               'end_date': end_date,
                                               'delay': 5,
                                               'quantity': 1500})
+
+        pl_id = self.agreement_pl_model.create(cr, uid,
+                                               {'framework_agreement_id': agr_id,
+                                                'currency_id': self.ref('base.EUR')})
+
         self.agreement_line_model.create(cr, uid,
-                                         {'framework_agreement_id': agr_id,
+                                         {'framework_agreement_pricelist_id': pl_id,
                                           'quantity': 0,
                                           'price': 70.0})
         self.agreement_line_model.create(cr, uid,
-                                         {'framework_agreement_id': agr_id,
+                                         {'framework_agreement_pricelist_id': pl_id,
                                           'quantity': 200,
                                           'price': 60.0})
         self.agreement_line_model.create(cr, uid,
-                                         {'framework_agreement_id': agr_id,
+                                         {'framework_agreement_pricelist_id': pl_id,
                                           'quantity': 500,
                                           'price': 50.0})
         self.agreement_line_model.create(cr, uid,
-                                         {'framework_agreement_id': agr_id,
+                                         {'framework_agreement_pricelist_id': pl_id,
                                           'quantity': 1000,
                                           'price': 45.0})
         self.agreement = self.agreement_model.browse(cr, uid, agr_id)
 
     def test_00_test_qty(self):
         """Test if barem retrieval is correct"""
-        self.assertEqual(self.agreement.get_price(0), 70.0)
-        self.assertEqual(self.agreement.get_price(100), 70.0)
-        self.assertEqual(self.agreement.get_price(200), 60.0)
-        self.assertEqual(self.agreement.get_price(210), 60.0)
-        self.assertEqual(self.agreement.get_price(500), 50.0)
-        self.assertEqual(self.agreement.get_price(800), 50.0)
-        self.assertEqual(self.agreement.get_price(999), 50.0)
-        self.assertEqual(self.agreement.get_price(1000), 45.0)
-        self.assertEqual(self.agreement.get_price(10000), 45.0)
-        self.assertEqual(self.agreement.get_price(-10), 70.0)
+        self.assertEqual(self.agreement.get_price(0, currency=self.browse_ref('base.EUR')), 70.0)
+        self.assertEqual(self.agreement.get_price(100, currency=self.browse_ref('base.EUR')), 70.0)
+        self.assertEqual(self.agreement.get_price(200, currency=self.browse_ref('base.EUR')), 60.0)
+        self.assertEqual(self.agreement.get_price(210, currency=self.browse_ref('base.EUR')), 60.0)
+        self.assertEqual(self.agreement.get_price(500, currency=self.browse_ref('base.EUR')), 50.0)
+        self.assertEqual(self.agreement.get_price(800, currency=self.browse_ref('base.EUR')), 50.0)
+        self.assertEqual(self.agreement.get_price(999, currency=self.browse_ref('base.EUR')), 50.0)
+        self.assertEqual(self.agreement.get_price(1000, currency=self.browse_ref('base.EUR')), 45.0)
+        self.assertEqual(self.agreement.get_price(10000, currency=self.browse_ref('base.EUR')), 45.0)
+        self.assertEqual(self.agreement.get_price(-10, currency=self.browse_ref('base.EUR')), 70.0)
+
+    def test_01_failed_wrong_currency(self):
+        """Tests that wrong currency raise an exception"""
+        with self.assertRaises(orm.except_orm) as error:
+            self.agreement.get_price(0, currency=self.browse_ref('base.USD'))
