2790afac0676aa4e510a160a99fe91b437d5bd14
Nicolas Bessi <nbessi@gmail.com>
2014-01-08 15:07:48 +0100

    [FIX] on change product line on po line to allows manual creation of purchase order

--- purchase_extended/model/purchase_order.py
+++ purchase_extended/model/purchase_order.py
@@ -248,11 +248,11 @@ class purchase_order_line(orm.Model):
 
     def onchange_product_id(self, cr, uid, ids, pricelist_id, product_id, qty, uom_id,
             partner_id, date_order=False, fiscal_position_id=False, date_planned=False,
-            name=False, price_unit=False, context=None, state='draft', type='rfq', **kwargs):
+            name=False, price_unit=False, context=None, state='draftpo', type='purchase', **kwargs):
         res = super(purchase_order_line, self).onchange_product_id(cr, uid, ids,
                 pricelist_id, product_id, qty, uom_id, partner_id, date_order,
                 fiscal_position_id, date_planned, name, price_unit, context)
-        if state == 'draft' and type == 'bid':
+        if state == 'draft' and type == 'rfq':
             res['value'].update({'price_unit': 0})
         elif state in ('sent', 'draftbid', 'bid'):
             if 'price_unit' in res['value']:
