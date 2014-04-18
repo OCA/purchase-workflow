f4e99a3cd531289055196550fa70b852f83b0254
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-04-18 17:57:29 +0200

    [add] test merging one order

--- purchase_group_hooks/tests/test_merge_po.py
+++ purchase_group_hooks/tests/test_merge_po.py
@@ -1,4 +1,5 @@
 import unittest2
+from mock import Mock
 
 from ..purchase_group_hooks import PurchaseOrder
 
@@ -7,9 +8,16 @@ class TestGroupOrders(unittest2.TestCase):
 
     def setUp(self):
         super(TestGroupOrders, self).setUp()
+        self.order1 = Mock()
+        self.order2 = Mock()
 
     def test_no_orders(self):
         """Group an empty list of orders as an empty dictionary."""
 
         grouped = PurchaseOrder._group_orders([])
+        self.assertEquals(grouped, {})
+
+    def test_one_order(self):
+        """A single order will not be grouped."""
+        grouped = PurchaseOrder._group_orders([self.order1])
         self.assertEquals(grouped, {})
