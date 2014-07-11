629ceca37ef719f44cffb2a426ae34c0df5e37b7
Pedro M. Baeza <pedro.baeza@gmail.com>
2014-07-11 12:51:11 +0200

    [FIX] purchase_discount: Install errors.

--- purchase_discount/models/purchase_discount.py
+++ purchase_discount/models/purchase_discount.py
@@ -40,7 +40,7 @@ class PurchaseOrderLine(orm.Model):
         return res
 
     _columns = {
-        'discount': fields.float('Discount (%)', dp.get_precision('Discount'),
+        'discount': fields.float('Discount (%)', dp.get_precision('Discount')),
         'price_subtotal': fields.function(
             _amount_line, string='Subtotal',
             digits_compute=dp.get_precision('Account')),
