9327eec783484c1b3d6151b0b5f68575e423474b
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2014-09-30 16:29:55 +0200

    proof read module description

--- purchase_requisition_extended/__openerp__.py
+++ purchase_requisition_extended/__openerp__.py
@@ -26,31 +26,30 @@
  "complexity": "normal",
  "images": [],
  "description": """
-This module improves the standard Purchase Requisition module.
-==============================================================
-This module allows to make calls for bids by generating RFQ for selected
+Purchase Requisition improvements
+=================================
+
+This module allows to make calls for bids by generating an RFQ for selected
 suppliers, encode the bids, compare and select bids, generate draft POs.
 
 First, a list of products is established. The call for bid is then confirmed
-and RFQs can be generated. They are in the state 'Draft RFQ' until it is send
-to the supplier and marked as 'RFQ Sent'. The bids has to be encoded and moved
-to state 'Bid Encoded'. When closing the call for bids in order to start the
-bids selection, all RFQ that have not been sent will be canceled. However,
-send RFQs can still be encoded. Bids that are not received will remain in state
-'RFQ Sent' and can be manually canceled.
+and RFQs can be generated. They are in the state 'Draft RFQ' until sent to the
+supplier and then marked as 'RFQ Sent'. The bids have to be encoded and moved
+to state 'Bid Encoded'. When closing the call for bids, in order to start the
+bids selection, all RFQ that have not been sent will be canceled. However, sent
+RFQs can still be encoded. Bids that are not received will remain in state 'RFQ
+Sent' and can be manually canceled.
 
 Afterwards, the bids selection can be started by choosing product lines. The
 workflow has been modified to allow to mark that the selection of bids has
-occurred but without having to generate the POs yet, those can be created at a
+occurred but without having to generate the POs yet. Those can be created at a
 new later state called 'Bids Selected'.
 
-When generating POs, the are created in the state 'Draft PO' introduced by the
+When generating POs, they are created in the state 'Draft PO' introduced by the
 module purchase_extended.
-
 
 Some fields have been added to specify with more details the call for bids and
 prefill fields of the generated RFQs.
-
 
 A link has been added between a call for bids line and the corresponding line
 of each generated RFQ. This is used for the bids comparison in order to compare
