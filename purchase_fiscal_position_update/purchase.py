07b2f38e072763f7af439668e32b027038bb2c79
Alexis de Lattre <alexis@via.ecp.fr>
2014-10-09 21:12:31 +0200

    purchase_fiscal_position_update: port to v8 and new API

---
+++ purchase_fiscal_position_update/purchase.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Purchase Fiscal Position Update module for Odoo
+#    Copyright (C) 2011-2014 Julius Network Solutions SARL <contact@julius.fr>
+#    Copyright (C) 2014 Akretion (http://www.akretion.com)
+#    @author Mathieu Vatel <mathieu _at_ julius.fr>
+#    @author Alexis de Lattre <alexis.delattre@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, api, _
+
+
+class purchase_order(models.Model):
+    _inherit = "purchase.order"
+
+    @api.multi
+    def fiscal_position_change(self, fiscal_position_id, order_line):
+        '''Function executed by the on_change on the fiscal_position field
+        of a purchase order ; it updates taxes on all order lines'''
+        fp_obj = self.env['account.fiscal.position']
+        res = {'value': {}}
+        line_dict = self.resolve_2many_commands('order_line', order_line)
+        lines_without_product = []
+        if fiscal_position_id:
+            fp = fp_obj.browse(fiscal_position_id)
+        else:
+            fp = False
+        for line in line_dict:
+            # Reformat line_dict so as to be compatible with what is
+            # accepted in res['value']
+            for key, value in line.iteritems():
+                if isinstance(value, tuple) and len(value) == 2:
+                    line[key] = value[0]
+            if line.get('product_id'):
+                product = self.env['product.product'].browse(
+                    line.get('product_id'))
+                taxes = product.supplier_taxes_id
+                if fp:
+                    taxes = fp.map_tax(taxes)
+                line['taxes_id'] = [(6, 0, [tax.id for tax in taxes])]
+            else:
+                lines_without_product.append(line.get('name'))
+        res['value']['order_line'] = line_dict
+
+        if lines_without_product:
+            res['warning'] = {'title': _('Warning')}
+            if len(lines_without_product) == len(line_dict):
+                res['warning']['message'] = _(
+                    "The Purchase Order Lines were not updated to the new "
+                    "Fiscal Position because they don't have Products.\n"
+                    "You should update the Taxes of each "
+                    "Purchase Order Line manually.")
+            else:
+                res['warning']['message'] = _(
+                    "The following Purchase Order Lines were not updated "
+                    "to the new Fiscal Position because they don't have a "
+                    "Product:\n- %s\nYou should update the "
+                    "Taxes of these Purchase Order Lines manually."
+                    ) % ('\n- '.join(lines_without_product))
+        return res
