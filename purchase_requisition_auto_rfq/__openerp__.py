64522e99fcb83dd6ad38a2a79dbc847f7dba1340
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-10-20 14:35:30 +0200

    refactor tests to be more readable
    Here I take advantage of the new API and the assertions from nose.tools
    to refactor the tests and make them more readable. They are still green
    and should be otherwise unchanged.

--- purchase_requisition_auto_rfq/__openerp__.py
+++ purchase_requisition_auto_rfq/__openerp__.py
@@ -33,6 +33,9 @@ Purchase Requisition Auto RFQ
 This module adds a button on the purchase requisition form to create a RFQ
 using the suppliers from the products listed in the requisition.
 
+Note: nose is required to run the tests. It is not listed as en external
+dependency because it is not needed in production.
+
 """,
  "depends": ["purchase_requisition",
              ],
