87a867bf3258211ae131419b59a71d8f8ed18cd4
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-10-23 16:48:17 +0200

    add security, doc, demo data

--- purchase_transport_document/__openerp__.py
+++ purchase_transport_document/__openerp__.py
@@ -29,11 +29,20 @@
  "description": """
 Purchase Transport Document
 ===========================
+This module adds a new object "Transport Document" and allows to select
+Documents in Purchase Orders. Other modules can depend on this simple one to
+use the same documents.
+
+This way it is possible to specify which documents have to be supplied with the
+purchase. Possible examples (CMR, Bill of Langand and others) are provided as
+demo data.
 """,
  "depends": ["purchase",
              ],
  "data": ["view/purchase_order.xml",
           "view/transport_document.xml",
+          "security/ir.model.access.csv",
           ],
+ "demo": ["demo.xml"],
  "installable": True,
  }
