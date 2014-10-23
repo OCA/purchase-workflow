a72ac560ade2e976a604b351cc943d5bc97c0787
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-10-23 17:47:49 +0200

    propagate transport documents from tender to rfq

--- purchase_requisition_transport_document/model/purchase_requisition.py
+++ purchase_requisition_transport_document/model/purchase_requisition.py
@@ -18,7 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class PurchaseRequisition(models.Model):
@@ -28,3 +28,16 @@ class PurchaseRequisition(models.Model):
         'transport.document',
         string="Transport Documents"
     )
+
+    @api.model
+    def _prepare_purchase_order(self, requisition, supplier):
+        """Propagate transport documents from tender to RFQ"""
+
+        values = super(PurchaseRequisition, self
+                       )._prepare_purchase_order(requisition, supplier)
+        values.update({
+            'transport_document_ids': [
+                (4, doc.id) for doc in requisition.transport_document_ids
+            ],
+        })
+        return values
