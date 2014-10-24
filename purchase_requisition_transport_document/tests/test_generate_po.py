b156a2e5316268635b69c01e563a899357dff9fb
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-10-24 16:41:14 +0200

    test that zero and one documents are propagated

---
+++ purchase_requisition_transport_document/tests/test_generate_po.py
@@ -0,0 +1,34 @@
+from openerp.tests.common import TransactionCase
+
+
+class TestPropagateDocuments(TransactionCase):
+    def setUp(self):
+        super(TestPropagateDocuments, self).setUp()
+
+        self.Requisition = self.env['purchase.requisition']
+
+        self.requisition = self.Requisition.create({})
+        self.supplier = self.browse_ref('base.res_partner_1')
+
+        self.doc1 = self.browse_ref('purchase_transport_document.CMR')
+        self.doc2 = self.browse_ref(
+            'purchase_transport_document.bill_of_lading')
+
+    def test_it_propagates_no_documents(self):
+
+        order_data = self.Requisition._prepare_purchase_order(self.requisition,
+                                                              self.supplier)
+
+        self.assertFalse(order_data.get('transport_document_ids'))
+
+    def test_it_propagates_one_documents(self):
+
+        self.requisition.transport_document_ids = self.doc1
+
+        order_data = self.Requisition._prepare_purchase_order(self.requisition,
+                                                              self.supplier)
+
+        self.assertEqual(
+            order_data['transport_document_ids'],
+            [(4, self.doc1.id)]
+        )
