bc1fa34119a74f871cf5eb98cbf037fe7902c4e3
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-10-28 14:04:38 +0100

    bump version

--- framework_agreement/__openerp__.py
+++ framework_agreement/__openerp__.py
@@ -19,7 +19,7 @@
 #
 ##############################################################################
 {'name': 'Framework Agreement',
- 'version': '1.0',
+ 'version': '1.1',
  'author': 'Camptocamp',
  'maintainer': 'Camptocamp',
  'category': 'Purchase Management',
