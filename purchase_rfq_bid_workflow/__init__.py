d469e9287f9e6a662b99824e82e689c2154a7569
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-11-06 16:13:50 +0100

    use SUPERUSER_ID and PO

--- purchase_rfq_bid_workflow/__init__.py
+++ purchase_rfq_bid_workflow/__init__.py
@@ -2,11 +2,13 @@
 from . import model
 from . import wizard
 
+from openerp import SUPERUSER_ID
+
 
 def fix_inconsistent_initial_types(cr, registry):
     """A post init hook executed when the module is installed."""
     PO = registry['purchase.order']
-    approved_ids = registry['purchase.order'].search(cr, 1, [
+    approved_ids = PO.search(cr, 1, [
         ('state', '=', 'approved')
     ])
-    PO.write(cr, 1, approved_ids, {'type': 'purchase'})
+    PO.write(cr, SUPERUSER_ID, approved_ids, {'type': 'purchase'})
