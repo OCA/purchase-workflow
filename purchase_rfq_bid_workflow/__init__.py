f7d41d03d0ab9876db2d75da8c7a61578b9abfe7
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-11-06 14:02:45 +0100

    add docstring to post init hook

--- purchase_rfq_bid_workflow/__init__.py
+++ purchase_rfq_bid_workflow/__init__.py
@@ -4,6 +4,7 @@ from . import wizard
 
 
 def fix_inconsistent_initial_types(cr, registry):
+    """A post init hook executed when the module is installed."""
     PO = registry['purchase.order']
     approved_ids = registry['purchase.order'].search(cr, 1, [
         ('state', '=', 'approved')
