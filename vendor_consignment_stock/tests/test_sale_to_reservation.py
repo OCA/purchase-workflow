96170a4140db18bbc4d94a63cfcb8db0c6500d64
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2015-01-13 15:04:24 +0100

    refactor tests a bit

--- vendor_consignment_stock/tests/test_sale_to_reservation.py
+++ vendor_consignment_stock/tests/test_sale_to_reservation.py
@@ -18,48 +18,6 @@ from openerp.tests.common import TransactionCase
 
 class TestSaleToReservation(TransactionCase):
 
-    def test_sale_mto_buy_creates_procurements_and_normal_po(self):
-        self.product.route_ids = self.mto_route | self.buy_route
-
-        self.so.action_button_confirm()
-        self.Procurement.run_scheduler()
-        delivery = self.so.picking_ids
-        self.assertEqual(1, len(delivery))
-        self.assertEqual('waiting', delivery.state)
-
-        proc1 = self.sol.procurement_ids
-        self.assertEqual(1, len(proc1))
-        self.assertEqual("move", proc1.rule_id.action)
-        self.assertEqual("make_to_order", proc1.rule_id.procure_method)
-        self.assertFalse(proc1.purchase_id)
-
-        proc2 = proc1.group_id.procurement_ids - proc1
-        self.assertEqual(1, len(proc2))
-        self.assertEqual("buy", proc2.rule_id.action)
-
-        po = proc2.purchase_id
-        self.assertTrue(po)
-        self.assertIs(False, po.is_vci)
-        po.signal_workflow('purchase_confirm')
-        self.assertEqual(1, len(po.picking_ids))
-
-    def test_customer_is_owner_reserves_without_po(self):
-        self.sol.stock_owner_id = self.customer
-
-        self.so.action_button_confirm()
-        self.Procurement.run_scheduler()
-        delivery = self.so.picking_ids
-        self.assertEqual(1, len(delivery))
-        delivery.action_assign()
-        self.assertEqual('assigned', delivery.state)
-
-        proc1 = self.sol.procurement_ids
-        self.assertEqual(1, len(proc1))
-        self.assertEqual("move", proc1.rule_id.action)
-        self.assertEqual("make_to_stock", proc1.rule_id.procure_method)
-        self.assertFalse(proc1.purchase_id)
-        self.assertEqual(1, len(proc1.group_id.procurement_ids))
-
     def test_sale_vci_generates_procurements_and_special_po(self):
         self.sol.stock_owner_id = self.supplier
 
@@ -88,25 +46,63 @@ class TestSaleToReservation(TransactionCase):
         self.Procurement.run_scheduler()
         self.assertEqual('assigned', delivery.state)
 
+    def test_sale_mto_buy_creates_procurements_and_normal_po(self):
+
+        self.product.route_ids = (
+            self.env.ref('stock.route_warehouse0_mto') |
+            self.env.ref('purchase.route_warehouse0_buy')
+        )
+
+        self.so.action_button_confirm()
+        self.Procurement.run_scheduler()
+        delivery = self.so.picking_ids
+        self.assertEqual(1, len(delivery))
+        self.assertEqual('waiting', delivery.state)
+
+        proc1 = self.sol.procurement_ids
+        self.assertEqual(1, len(proc1))
+        self.assertEqual("move", proc1.rule_id.action)
+        self.assertEqual("make_to_order", proc1.rule_id.procure_method)
+        self.assertFalse(proc1.purchase_id)
+
+        proc2 = proc1.group_id.procurement_ids - proc1
+        self.assertEqual(1, len(proc2))
+        self.assertEqual("buy", proc2.rule_id.action)
+
+        po = proc2.purchase_id
+        self.assertTrue(po)
+        self.assertIs(False, po.is_vci)
+        po.signal_workflow('purchase_confirm')
+        self.assertEqual(1, len(po.picking_ids))
+
+    def test_customer_is_owner_reserves_without_po(self):
+
+        self.sol.stock_owner_id = self.customer
+
+        self.so.action_button_confirm()
+        self.Procurement.run_scheduler()
+        delivery = self.so.picking_ids
+        self.assertEqual(1, len(delivery))
+        delivery.action_assign()
+        self.assertEqual('assigned', delivery.state)
+
+        proc1 = self.sol.procurement_ids
+        self.assertEqual(1, len(proc1))
+        self.assertEqual("move", proc1.rule_id.action)
+        self.assertEqual("make_to_stock", proc1.rule_id.procure_method)
+        self.assertFalse(proc1.purchase_id)
+        self.assertEqual(1, len(proc1.group_id.procurement_ids))
+
     def setUp(self):
         super(TestSaleToReservation, self).setUp()
-        self.SO = self.env['sale.order']
-        self.SOL = self.env['sale.order.line']
-        self.Quant = self.env['stock.quant']
         self.Procurement = self.env['procurement.order']
-        self.Rule = self.env['procurement.rule']
 
         self.supplier = self.env.ref('base.res_partner_1')
         self.customer = self.env.ref('base.res_partner_2')
         self.product = self.env.ref('product.product_product_36')
         self.env.ref('stock.warehouse0').buy_vci_to_resupply = True
 
-        self.vci_route = self.env.ref(
-            'vendor_consignment_stock.route_warehouse0_buy_vci')
-        self.mto_route = self.env.ref('stock.route_warehouse0_mto')
-        self.buy_route = self.env.ref('purchase.route_warehouse0_buy')
-
-        our_quant = self.Quant.create({
+        our_quant = self.env['stock.quant'].create({
             'qty': 5000,
             'location_id': self.env.ref('stock.stock_location_stock').id,
             'product_id': self.product.id,
@@ -118,11 +114,11 @@ class TestSaleToReservation(TransactionCase):
             'owner_id': self.customer.id,
         })
 
-        self.so = self.SO.create({
+        self.so = self.env['sale.order'].create({
             'partner_id': self.customer.id,
             'picking_policy': 'direct',
         })
-        self.sol = self.SOL.create({
+        self.sol = self.env['sale.order.line'].create({
             'name': '/',
             'order_id': self.so.id,
             'product_id': self.product.id,
