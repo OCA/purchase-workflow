6588e92f545900419c2844313775239bc5982ff3
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2015-03-03 12:51:48 +0100

    Use on_changes to show the amount directly...
    ... to the user instead of after saving.
    
    Use web_context_tunnel for the pricelist_id on_change so the signature
    is not altered. Can't use it for the partner_id on_change because it has
    no context.

--- purchase_supplier_postage_free/model/purchase_order.py
+++ purchase_supplier_postage_free/model/purchase_order.py
@@ -28,21 +28,12 @@ class purchase_order(orm.Model):
 
     def _compute_free_postage(self, cr, uid, ids, prop, arg, context=None):
         res = {}
-        currency_model = self.pool['res.currency']
         for order in self.browse(cr, uid, ids, context=context):
             supplier = order.partner_id
-            amount = supplier.free_postage
-            supplier_pricelist = supplier.property_product_pricelist_purchase
-            supplier_currency = supplier_pricelist.currency_id
             order_currency = order.pricelist_id.currency_id
-            if supplier_currency != order_currency:
-                amount = currency_model.compute(
-                    cr, uid,
-                    supplier_currency.id,
-                    order_currency.id,
-                    amount,
-                    context=context)
-
+            amount = self._get_free_postage_amount(cr, uid, supplier,
+                                                   order_currency,
+                                                   context=context)
             res[order.id] = amount
         return res
 
@@ -55,3 +46,58 @@ class purchase_order(orm.Model):
                  "currency of the purchase order,"
         ),
     }
+
+    def _get_free_postage_amount(self, cr, uid, supplier, order_currency,
+                                 context=None):
+        currency_model = self.pool['res.currency']
+        amount = supplier.free_postage
+        supplier_pricelist = supplier.property_product_pricelist_purchase
+        supplier_currency = supplier_pricelist.currency_id
+        if supplier_currency != order_currency:
+            amount = currency_model.compute(
+                cr, uid,
+                supplier_currency.id,
+                order_currency.id,
+                amount,
+                context=context)
+        return amount
+
+    def onchange_partner_id(self, cr, uid, ids, partner_id, pricelist_id,
+                            context=None):
+        res = super(purchase_order, self).onchange_partner_id(cr, uid, ids,
+                                                              partner_id)
+        if not pricelist_id or not partner_id:
+            return res
+        partner_model = self.pool['res.partner']
+        pricelist_model = self.pool['product.pricelist']
+        partner = partner_model.browse(cr, uid, partner_id,
+                                       context=context)
+        pricelist = pricelist_model.browse(cr, uid, pricelist_id,
+                                           context=context)
+        amount = self._get_free_postage_amount(
+            cr, uid,
+            partner,
+            pricelist.currency_id,
+            context=context)
+        res.setdefault('value', {})['free_postage'] = amount
+        return res
+
+    def onchange_pricelist(self, cr, uid, ids, pricelist_id, context=None):
+        res = super(purchase_order, self).onchange_pricelist(cr, uid, ids,
+                                                             pricelist_id,
+                                                             context=context)
+        if not pricelist_id or not context.get('partner_id'):
+            return res
+        partner_model = self.pool['res.partner']
+        pricelist_model = self.pool['product.pricelist']
+        partner = partner_model.browse(cr, uid, context['partner_id'],
+                                       context=context)
+        pricelist = pricelist_model.browse(cr, uid, pricelist_id,
+                                           context=context)
+        amount = self._get_free_postage_amount(
+            cr, uid,
+            partner,
+            pricelist.currency_id,
+            context=context)
+        res.setdefault('value', {})['free_postage'] = amount
+        return res
