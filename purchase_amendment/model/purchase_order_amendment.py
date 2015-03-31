4830e6ff92172ee6b3219da26c3aea40482f7aac
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2015-03-31 14:26:01 +0200

    Started purchase_amendment on the model of sale_amendment

---
+++ purchase_amendment/model/purchase_order_amendment.py
@@ -0,0 +1,270 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    Authors: Guewen Baconnier
+#    Copyright 2015 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+from functools import partial
+import openerp.addons.decimal_precision as dp
+from openerp import models, fields, api, exceptions, _
+from openerp.tools import html2plaintext, float_compare
+
+
+class PurchaseOrderAmendment(models.TransientModel):
+    _name = 'purchase.order.amendment'
+
+    purchase_id = fields.Many2one(comodel_name='purchase.order',
+                                  string='Purchase Order',
+                                  required=True,
+                                  readonly=True)
+    item_ids = fields.One2many(comodel_name='purchase.order.amendment.item',
+                               inverse_name='amendment_id',
+                               string='Items')
+    reason = fields.Html()
+
+    @api.model
+    def default_get(self, fields):
+        res = super(PurchaseOrderAmendment, self).default_get(fields)
+        purchase_ids = self.env.context.get('active_ids', [])
+        active_model = self.env.context.get('active_model')
+
+        if not purchase_ids or len(purchase_ids) != 1:
+            return res
+        assert active_model == 'purchase.order', 'Bad context propagation'
+
+        purchase = self.env['purchase.order'].browse(purchase_ids)
+
+        if purchase.invoiced:
+            raise exceptions.Warning(
+                _('An invoiced order cannot be amended.')
+            )
+
+        items = []
+        for line in purchase.order_line:
+            if line.state in ('cancel', 'done'):
+                continue
+            items.append(self._prepare_item(line))
+        res['item_ids'] = items
+        return res
+
+    @api.model
+    def _prepare_item(self, purchase_line):
+        ordered = purchase_line.product_qty
+
+        shipped = 0.
+        canceled = 0.
+        for move in purchase_line.move_ids:
+            if move.state == 'done':
+                shipped += move.product_qty
+            elif move.state == 'cancel':
+                canceled += move.product_qty
+
+        amend = ordered - canceled - shipped
+        return {
+            'purchase_line_id': purchase_line.id,
+            'shipped_qty': shipped,
+            'canceled_qty': canceled,
+            'amend_qty': amend,
+        }
+
+    @api.multi
+    def _message_content(self):
+        title = _('Order amendment')
+        message = '<h3>%s</h3><ul>' % title
+        for item in self.item_ids:
+            message += (_('<li><b>%s</b>: %s Ordered, %s '
+                          'Shipped, %s Canceled, %s Amended</li>') %
+                        (item.purchase_line_id.name,
+                         item.ordered_qty, item.shipped_qty,
+                         item.canceled_qty, item.amend_qty,
+                         )
+                        )
+        message += '</ul>'
+        # if the html field is touched, it may return '<br/>' or
+        # '<p></p>' so check if it contains text at all
+        if self.reason and html2plaintext(self.reason).strip():
+            title = _('Reason for amending')
+            message += "<h3>%s</h3><p>%s</p>" % (title, self.reason)
+        return message
+
+    @api.multi
+    def do_amendment(self):
+        self.ensure_one()
+        self.item_ids.split_lines()
+        purchase = self.purchase_id
+        purchase.message_post(body=self._message_content())
+        return True
+
+    @api.multi
+    def wizard_view(self):
+        view = self.env.ref('purchase_amendment.view_purchase_order_amendment')
+
+        return {
+            'name': _('Enter the amendment details'),
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'purchase.order.amendment',
+            'views': [(view.id, 'form')],
+            'view_id': view.id,
+            'target': 'new',
+            'res_id': self.id,
+            'context': self.env.context,
+        }
+
+
+class PurchaseOrderAmendmentItem(models.TransientModel):
+    _name = 'purchase.order.amendment.item'
+
+    amendment_id = fields.Many2one(comodel_name='purchase.order.amendment',
+                                   string='Amendment',
+                                   required=True,
+                                   ondelete='cascade',
+                                   readonly=True)
+    purchase_line_id = fields.Many2one(comodel_name='purchase.order.line',
+                                       string='Line',
+                                       required=True,
+                                       readonly=True)
+    ordered_qty = fields.Float(related='purchase_line_id.product_qty',
+                               readonly=True)
+    shipped_qty = fields.Float(string='Delivered',
+                               readonly=True,
+                               digits_compute=dp.get_precision('Product UoS'))
+    canceled_qty = fields.Float(string='Canceled',
+                                readonly=True,
+                                digits_compute=dp.get_precision('Product UoS'))
+    amend_qty = fields.Float(string='Amend',
+                             digits_compute=dp.get_precision('Product UoS'))
+    product_uom_id = fields.Many2one(related='purchase_line_id.product_uom',
+                                     readonly=True)
+
+    @api.constrains('amend_qty')
+    def _constrains_amend_qty(self):
+        max_qty = self.ordered_qty - self.shipped_qty
+        min_qty = self.ordered_qty - self.shipped_qty - self.canceled_qty
+        rounding = self.purchase_line_id.product_uom.rounding
+        compare = partial(float_compare, precision_digits=rounding)
+        if (compare(min_qty, self.amend_qty) == 1 or
+                compare(self.amend_qty, max_qty) == 1):
+            raise exceptions.ValidationError(
+                _('The amended quantity for the product "%s" must be '
+                  'between the canceled quantity of %0.2f and the ordered '
+                  'quantity of %0.2f.') % (self.purchase_line_id.name,
+                                           min_qty, max_qty)
+            )
+
+    @api.multi
+    def split_lines(self):
+        """ Split the order line according to selected quantities
+
+        The shipped quantity is the quantity that will remain in the original
+        purchases order line.
+        The canceled quantity will split the original line and cancel the
+        duplicated line.
+        The amended quantity will split the original line; the
+        duplicated line will be 'confirmed' and a new picking will be created.
+        """
+        for item in self:
+            line = item.purchase_line_id
+            amend_qty = item.amend_qty
+            shipped_qty = item.shipped_qty
+            ordered_qty = item.ordered_qty
+            rounding = line.product_id.uom_id.rounding
+            # the total canceled may be different than the one displayed
+            # to the user, because the one displayed is the quantity
+            # canceled in the *pickings*, here it includes also the
+            # quantity removed when amending
+            compare = partial(float_compare, precision_digits=rounding)
+            canceled_qty = ordered_qty - shipped_qty - amend_qty
+            if compare(canceled_qty, 0) == -1:  # Means: canceled_qty < 0
+                canceled_qty = 0
+
+            if (not (shipped_qty or canceled_qty) and
+                    compare(amend_qty, ordered_qty) == 0):
+                # Means: amend_qty == ordered_qty
+                # the line is not changed
+                continue
+
+            if (not canceled_qty and
+                    compare(shipped_qty + amend_qty, ordered_qty) == 0):
+                # Means: shipped_qty + amend_qty == ordered_qty
+                # part has been shipped but there is no reason to split
+                # the lines
+                continue
+
+            procurements = line.procurement_ids
+            if shipped_qty:
+                # only keep the done procurement on the purchase line
+                proc = procurements.filtered(lambda p: p.state == 'done')
+                procurements -= proc
+                # update the current line with the shipped qty,
+                # the rest will be either canceled either amended,
+                # either both
+                line.write({
+                    'product_qty': shipped_qty,
+                    'procurement_ids': [(6, 0, proc.ids)],
+                })
+
+            if canceled_qty:
+                # only keep the canceled procurement on the purchase line
+                proc = procurements.filtered(lambda p: p.state == 'cancel')
+                procurements -= proc
+                if shipped_qty:
+                    # current line kept for the shipped quantity so
+                    # create a new one
+                    values = {'product_qty': canceled_qty,
+                              'procurement_ids': [(6, 0, proc.ids)]}
+                    canceled_line = line.copy(default=values)
+                else:
+                    # cancel the current line
+                    proc.cancel()
+                    line.write({
+                        'product_qty': canceled_qty,
+                        'procurement_ids': [(6, 0, proc.ids)],
+                    })
+                    canceled_line = line
+                canceled_line.action_cancel()
+
+            if amend_qty:
+                # link the new line with the remaining procurements
+                # (not done nor canceled)
+                values = {'product_qty': amend_qty,
+                          'procurement_ids': [(6, 0, procurements.ids)]}
+
+                original_amend_qty = (ordered_qty -
+                                      shipped_qty -
+                                      item.canceled_qty)
+                if compare(amend_qty, original_amend_qty) != 0:
+                    # Means: amend_qty != original_amend_qty
+                    # We need to change the quantity of the move
+                    # A new procurement will be generated for the line
+                    procurements.cancel()
+                    values['procurement_ids'] = False
+
+                amend_line = line.copy(default=values)
+                amend_line.button_confirm()
+                for proc in amend_line.procurement_ids:
+                    if proc.state == 'confirmed':
+                        # if a new procurement has been created,
+                        # create the move and change it to 'running'
+                        proc.run()
+
+        purchase = self.mapped('purchase_line_id.order_id')
+        # Update the pickings according to the new procurements
+        # TODO: ship recreate
+        return True
---
+++ purchase_amendment/tests/test_amendment.py
@@ -0,0 +1,320 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    Authors: Guewen Baconnier
+#    Copyright 2015 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+from openerp import exceptions
+from openerp.tests import common
+
+
+class TestAmendmentCombinations(common.TransactionCase):
+
+    def setUp(self):
+        super(TestAmendmentCombinations, self).setUp()
+        self.amendment_model = self.env['purchase.order.amendment']
+        self.purchase_model = self.env['purchase.order']
+        self.purchase_line_model = self.env['purchase.order.line']
+        self.partner1 = self.env.ref('base.res_partner_1')
+        self.product1 = self.env.ref('product.product_product_7')
+        self.product2 = self.env.ref('product.product_product_9')
+        self.product3 = self.env.ref('product.product_product_11')
+        self.location_stock = self.env.ref('stock.stock_location_stock')
+        self.purchase_pricelist = self.env.ref('purchase.list0')
+        self.purchase = self._create_purchase([(self.product1, 1000),
+                                               (self.product2, 500),
+                                               (self.product3, 800)])
+        self.purchase.signal_workflow('purchase_confirm')
+
+    def _create_purchase(self, line_products):
+        """ Create a purchase order.
+
+        ``line_products`` is a list of tuple [(product, qty)]
+        """
+        lines = []
+        for product, qty in line_products:
+            line_values = {
+                'product_id': product.id,
+                'product_qty': qty,
+                'product_uom': product.uom_id.id,
+                'price_unit': 50,
+            }
+            onchange_res = self.purchase_line_model.product_id_change(
+                self.purchase_pricelist.id,
+                product.id,
+                qty,
+                product.uom_id.id,
+                self.partner1.id)
+            line_values.update(onchange_res['value'])
+            lines.append(
+                (0, 0, line_values)
+            )
+        return self.purchase_model.create({
+            'partner_id': self.partner1.id,
+            'location_id': self.location_stock.id,
+            'pricelist_id': self.purchase_pricelist.id,
+            'order_line': lines,
+        })
+
+    def _search_picking_by_product(self, product, qty):
+        return self.purchase.picking_ids.filtered(
+            lambda p: any(move.product_id == product and
+                          move.product_qty >= qty and
+                          move.state in ('confirmed', 'assigned')
+                          for move in p.move_lines)
+        )[0]
+
+    def ship(self, products):
+        """ Ship products of a picking
+
+        products is a list of tuples [(product, qty)]
+        """
+        operations = {}
+        pickings = self.env['stock.picking'].browse()
+        for product, qty in products:
+            picking = self._search_picking_by_product(product, qty)
+            pickings |= picking
+            operations.setdefault(picking, []).append((product, qty))
+
+        pickings.do_prepare_partial()
+
+        for picking, product_qtys in operations.iteritems():
+            for (product, qty) in product_qtys:
+                pack_operation = picking.pack_operation_ids.filtered(
+                    lambda p: p.product_id == product
+                )
+                pack_operation.product_qty = qty
+        pickings.do_transfer()
+        for picking in pickings:
+            self.assertEqual(picking.state, 'done')
+
+    def split(self, products):
+        """ Split pickings
+
+        ``products`` is a list of tuples [(product, quantity)]
+        """
+        operations = {}
+        pickings = self.env['stock.picking'].browse()
+        for product, qty in products:
+            picking = self._search_picking_by_product(product, qty)
+            pickings |= picking
+            operations.setdefault(picking, []).append((product, qty))
+
+        location_stock = self.env.ref('stock.stock_location_stock')
+        location_customer = self.env.ref('stock.stock_location_customers')
+
+        for picking in pickings:
+            transfer_model = self.env['stock.transfer_details'].with_context(
+                active_model='stock.picking',
+                active_id=picking.id,
+                active_ids=picking.ids
+            )
+            items = []
+            for product_qtys in operations[picking]:
+                items.append((0, 0, {
+                    'quantity': qty,
+                    'product_id': product.id,
+                    'product_uom_id': product.uom_id.id,
+                    'sourceloc_id': location_stock.id,
+                    'destinationloc_id': location_customer.id,
+                }))
+            transfer = transfer_model.create({
+                'picking_id': picking.id,
+                'item_ids': items
+            })
+            transfer.with_context(do_only_split=True).do_detailed_transfer()
+
+    def cancel_move(self, product, qty):
+        move = self.purchase.mapped('picking_ids.move_lines').filtered(
+            lambda m: (m.product_id == product and
+                       m.product_qty == qty and
+                       m.state in ('confirmed', 'assigned'))
+        )
+        move.action_cancel()
+
+    def amend(self):
+        return self.amendment_model.with_context(
+            active_model='purchase.order',
+            active_id=self.purchase.id,
+            active_ids=self.purchase.ids,
+        ).create({'purchase_id': self.purchase.id})
+
+    def amend_product(self, amendment, product, qty):
+        item = amendment.item_ids.filtered(
+            lambda m: m.purchase_line_id.product_id == product
+        )
+        item.amend_qty = qty
+
+    def assert_amendment_quantities(self, amendment, product,
+                                    ordered_qty=0, shipped_qty=0,
+                                    canceled_qty=0, amend_qty=0):
+        item = amendment.item_ids.filtered(
+            lambda i: i.purchase_line_id.product_id == product
+        )
+        self.assertEqual(len(item), 1)
+        self.assertEqual(
+            [item.ordered_qty, item.shipped_qty,
+             item.canceled_qty, item.amend_qty],
+            [ordered_qty, shipped_qty,
+             canceled_qty, amend_qty],
+            'The quantities do not match (ordered, shipped, canceled, amended)'
+        )
+
+    def assert_purchase_lines(self, expected_lines):
+        lines = self.purchase.order_line
+        not_found = []
+        for product, qty, state in expected_lines:
+            for line in lines:
+                if ((line.product_id, line.product_qty, line.state) ==
+                        (product, qty, state)):
+                    lines -= line
+                    break
+            else:
+                not_found.append((product, qty, state))
+        message = ''
+        for product, qty, state in not_found:
+            message += ("- product: '%s', qty: '%s', state: '%s'\n" %
+                        (product.display_name, qty, state))
+        for line in lines:
+            message += ("+ product: '%s', qty: '%s', state: '%s'\n" %
+                        (line.product_id.display_name, line.product_qty,
+                         line.state))
+        if message:
+            raise AssertionError('Purchase lines do not match:\n\n%s' %
+                                 message)
+
+    def assert_procurements(self, expected_procurements):
+        procurements = self.purchase.mapped('order_line.procurement_ids')
+        not_found = []
+        for product, qty, state in expected_procurements:
+            for proc in procurements:
+                if ((proc.product_id, proc.product_qty, proc.state) ==
+                        (product, qty, state)):
+                    procurements -= proc
+                    break
+            else:
+                not_found.append((product, qty, state))
+        message = ''
+        for product, qty, state in not_found:
+            message += ("- product: '%s', qty: '%s', state: '%s'\n" %
+                        (product.display_name, qty, state))
+        for line in procurements:
+            message += ("+ product: '%s', qty: '%s', state: '%s'\n" %
+                        (line.product_id.display_name, line.product_qty,
+                         line.state))
+        if message:
+            raise AssertionError('Procurements do not match:\n\n%s' % message)
+
+    def assert_moves(self, expected_moves):
+        moves = self.purchase.mapped('picking_ids.move_lines')
+        not_found = []
+        for product, qty, state in expected_moves:
+            for move in moves:
+                if ((move.product_id, move.product_qty, move.state) ==
+                        (product, qty, state)):
+                    moves -= move
+                    break
+            else:
+                not_found.append((product, qty, state))
+        message = ''
+        for product, qty, state in not_found:
+            message += ("- product: '%s', qty: '%s', state: '%s'\n" %
+                        (product.display_name, qty, state))
+        for line in moves:
+            message += ("+ product: '%s', qty: '%s', state: '%s'\n" %
+                        (line.product_id.display_name, line.product_qty,
+                         line.state))
+        if message:
+            raise AssertionError('Moves do not match:\n\n%s' % message)
+
+    def test_cancel_one_line(self):
+        # We have 500 product2
+        # Split product2 in another picking
+        self.split([(self.product2, 500)])
+
+        # Cancel the whole product2
+        self.cancel_move(self.product2, 500)
+        self.assert_moves([
+            (self.product1, 1000, 'assigned'),
+            (self.product2, 500, 'cancel'),
+            (self.product3, 800, 'assigned'),
+        ])
+
+        # purchase is in shipping exception only if a part
+        # is done and the other part is canceled
+        self.ship([(self.product1, 1000),
+                   (self.product3, 800)])
+
+        self.assertEqual(self.purchase.state, 'except_picking')
+
+        # amend the purchase order
+        amendment = self.amend()
+        self.assert_amendment_quantities(amendment, self.product1,
+                                         ordered_qty=1000,
+                                         shipped_qty=1000,
+                                         amend_qty=0)
+        self.assert_amendment_quantities(amendment, self.product2,
+                                         ordered_qty=500,
+                                         shipped_qty=0,
+                                         canceled_qty=500)
+        self.assert_amendment_quantities(amendment, self.product3,
+                                         ordered_qty=800,
+                                         shipped_qty=800)
+        amendment.do_amendment()
+        # TODO
+        # self.assert_purchase_lines([
+        #     (self.product1, 1000, 'confirmed'),
+        #     (self.product2, 500, 'cancel'),
+        #     (self.product3, 800, 'confirmed'),
+        # ])
+        # TODO
+        # self.assert_procurements([
+        #     (self.product1, 1000, 'running'),
+        #     (self.product2, 500, 'cancel'),
+        #     (self.product3, 800, 'running'),
+        # ])
+        # self.assert_moves([
+        #     (self.product1, 1000, 'assigned'),
+        #     (self.product2, 500, 'cancel'),
+        #     (self.product3, 800, 'assigned'),
+        # ])
+
+    def test_amend_quantity(self):
+        # Ship 200 product1
+        self.ship([(self.product1, 200),
+                   (self.product2, 0),
+                   (self.product3, 0),
+                   ])
+        # 800 products remain
+        # Split 100 of product1 in another picking and cancel them
+        self.split([(self.product1, 100)])
+        self.cancel_move(self.product1, 100)
+
+        # amend the sale order
+        amendment = self.amend()
+        with self.assertRaises(exceptions.ValidationError):
+            # below min quantity (700, because on the 1000 ordered, 200
+            # have been shipped)
+            self.amend_product(amendment, self.product1, 699)
+        with self.assertRaises(exceptions.ValidationError):
+            # above max quantity (800, because on the 1000 ordered, 200
+            # have been shipped)
+            self.amend_product(amendment, self.product1, 801)
+        # between bounds
+        self.amend_product(amendment, self.product1, 700)
+        self.amend_product(amendment, self.product1, 800)
