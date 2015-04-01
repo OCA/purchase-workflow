0e3e73a88db6cd75e0b2084b1cb3b478a2041d4d
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2015-04-01 14:42:29 +0200

    extract README.rst from __openerp__.py

--- purchase_order_revision/__openerp__.py
+++ purchase_order_revision/__openerp__.py
@@ -23,16 +23,6 @@
     'name': "Purchase order revisions",
     'version': '0.1',
     'category': 'Purchase Management',
-    'description': """
-Revisions for purchase orders (and requests for quotation)
-==========================================================
-
-On canceled orders, you can click on 'new revision' and the 'old revisions'
-tab of the just created request for quotation will contain all the old
-(canceled orders) revisions.
-So that you can track every change you made to your requests for quotation and
-purchase orders.
-""",
     'author': "Agile Business Group,Odoo Community Association (OCA)",
     'website': 'http://www.agilebg.com',
     'license': 'AGPL-3',
