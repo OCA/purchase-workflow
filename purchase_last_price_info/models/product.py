2b1307c080358c52a9799cc637e86592c1d0178f
oihane <oihanecruce@gmail.com>
2015-06-19 14:23:27 +0200

    [IMP] <purchase_last_price_info> Deleted dependency

--- purchase_last_price_info/models/product.py
+++ purchase_last_price_info/models/product.py
@@ -1,19 +1,6 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
-#
+# For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
 
 from openerp import models, fields
@@ -26,12 +13,11 @@ class ProductProduct(models.Model):
         """
         Get last purchase price, last purchase date and last customer
         """
-        purchase_line_obj = self.env['purchase.order.line']
         for product in self:
             last_date = False
             last_price = False
             last_seller = False
-            lines = purchase_line_obj.search(
+            lines = self.env['purchase.order.line'].search(
                 [('product_id', '=', product.id),
                  ('state', 'in', ['confirmed', 'done'])])
             if lines:
@@ -40,7 +26,6 @@ class ProductProduct(models.Model):
                 last_date = old_lines[0].order_id.date_order
                 last_price = old_lines[0].price_unit
                 last_seller = old_lines[0].order_id.partner_id
-
             product.last_purchase_date = last_date
             product.last_purchase_price = last_price
             product.last_seller = last_seller
