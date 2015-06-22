e01ade84b818ccb62755852a840531bad38121a6
oihane <oihanecruce@gmail.com>
2015-06-22 10:57:11 +0200

    [IMP] <purchase_last_price> Changed to avoid initializing to False

--- purchase_last_price_info/models/product.py
+++ purchase_last_price_info/models/product.py
@@ -3,37 +3,29 @@
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
 
-from openerp import models, fields
+from openerp import api, fields, models
 
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
+    @api.one
     def _get_last_purchase(self):
         """
         Get last purchase price, last purchase date and last customer
         """
-        for product in self:
-            last_date = False
-            last_price = False
-            last_seller = False
-            lines = self.env['purchase.order.line'].search(
-                [('product_id', '=', product.id),
-                 ('state', 'in', ['confirmed', 'done'])])
-            if lines:
-                old_lines = sorted(lines, key=lambda l: l.order_id.date_order,
-                                   reverse=True)
-                last_date = old_lines[0].order_id.date_order
-                last_price = old_lines[0].price_unit
-                last_seller = old_lines[0].order_id.partner_id
-            product.last_purchase_date = last_date
-            product.last_purchase_price = last_price
-            product.last_seller = last_seller
+        lines = self.env['purchase.order.line'].search(
+            [('product_id', '=', self.id),
+             ('state', 'in', ['confirmed', 'done'])]).sorted(
+            key=lambda l: l.order_id.date_order, reverse=True)
+        self.last_purchase_date = lines[:1].order_id.date_order
+        self.last_purchase_price = lines[:1].price_unit
+        self.last_seller = lines[:1].order_id.partner_id
 
     last_purchase_price = fields.Float(
-        string='Last Purchase Price', compute=_get_last_purchase)
+        string='Last Purchase Price', compute='_get_last_purchase')
     last_purchase_date = fields.Date(
-        string='Last Purchase Date', compute=_get_last_purchase)
+        string='Last Purchase Date', compute='_get_last_purchase')
     last_seller = fields.Many2one(
         comodel_name='res.partner', string='Last Seller',
-        compute=_get_last_purchase)
+        compute='_get_last_purchase')
