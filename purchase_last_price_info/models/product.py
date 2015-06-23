032d50440075668f04c7d38fcbe4f42b08266b93
oihane <oihanecruce@gmail.com>
2015-06-23 13:18:31 +0200

    [IMP] <purchase_last_price_info> Minor changes

--- purchase_last_price_info/models/product.py
+++ purchase_last_price_info/models/product.py
@@ -11,21 +11,19 @@ class ProductProduct(models.Model):
 
     @api.one
     def _get_last_purchase(self):
-        """
-        Get last purchase price, last purchase date and last customer
-        """
+        """ Get last purchase price, last purchase date and last supplier """
         lines = self.env['purchase.order.line'].search(
             [('product_id', '=', self.id),
              ('state', 'in', ['confirmed', 'done'])]).sorted(
             key=lambda l: l.order_id.date_order, reverse=True)
         self.last_purchase_date = lines[:1].order_id.date_order
         self.last_purchase_price = lines[:1].price_unit
-        self.last_seller = lines[:1].order_id.partner_id
+        self.last_supplier = lines[:1].order_id.partner_id
 
     last_purchase_price = fields.Float(
         string='Last Purchase Price', compute='_get_last_purchase')
     last_purchase_date = fields.Date(
         string='Last Purchase Date', compute='_get_last_purchase')
-    last_seller = fields.Many2one(
-        comodel_name='res.partner', string='Last Seller',
+    last_supplier = fields.Many2one(
+        comodel_name='res.partner', string='Last Supplier',
         compute='_get_last_purchase')
