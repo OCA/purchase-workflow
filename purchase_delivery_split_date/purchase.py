860818e5cffec564ef0ea02411bb8c5157120e34
Lionel Sausin <ls@numerigraphe.com>
2015-06-30 13:20:12 +0200

    purchase_delivery_split_date
    Use v8 API
    Follow the OCA guidelines for manifest and README
    PEP8
    Extend _create_stock_move because the method we used to extend in v7 doesn't exist anymore.

--- purchase_delivery_split_date/purchase.py
+++ purchase_delivery_split_date/purchase.py
@@ -1,44 +1,38 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    This module is copyright (C) 2014 Numérigraphe SARL. All Rights Reserved.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2014-2016 Numérigraphe SARL
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.osv import orm
+import logging
+from itertools import groupby
+from openerp import models, api
+
+_logger = logging.getLogger(__name__)
 
 
-class PurchaseOrder(orm.Model):
+class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    def _create_pickings(self, cr, uid, order, order_lines, picking_id=False,
-                         context=None):
-        """Group the Purchase Order's receptions by expected date"""
-        picking_ids = []
-        delivery_dates = {}
-        # Triage the order lines by delivery date
-        for line in order_lines:
-            if line.date_planned in delivery_dates:
-                delivery_dates[line.date_planned].append(line)
-            else:
-                delivery_dates[line.date_planned] = [line]
-        # Process each group of lines
-        for lines in delivery_dates.itervalues():
-            picking_ids.extend(
-                super(PurchaseOrder, self)._create_pickings(
-                    cr, uid, order, lines, picking_id=picking_id,
-                    context=context))
-        return picking_ids
+    @api.model
+    def _create_stock_moves(self, order, order_lines, picking_id=False):
+        """Group the receptions in one picking per expected date"""
+
+        # Group the order lines by delivery date
+        order_lines = sorted(order_lines, key=lambda l: l.date_planned)
+        date_groups = groupby(order_lines, lambda l: l.date_planned)
+
+        # If a picking is provided, use it for the first group only
+        if picking_id:
+            delivery_date, lines = date_groups.next()
+            first_picking = self.env['stock.picking'].browse(picking_id)
+            first_picking.date = delivery_date
+            super(PurchaseOrder, self)._create_stock_moves(
+                order, list(lines), picking_id=picking_id)
+
+        for delivery_date, lines in date_groups:
+            # If a picking is provided, clone it for each date for modularity
+            if picking_id:
+                picking_id = first_picking.copy({'move_lines': [],
+                                                 'date': delivery_date}).id
+
+            super(PurchaseOrder, self)._create_stock_moves(
+                order, list(lines), picking_id=picking_id)
