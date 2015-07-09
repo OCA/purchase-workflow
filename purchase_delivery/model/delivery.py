342f15150c5fca3e93d1a878c7e0e0df054798c7
Jpoa <jpoalmeida@gmail.com>
2015-07-09 13:27:59 +0200

    Purchase Delivery Module for Odoo v8
    Module now works with Odoo 8. Removed the last view for picking as it seems to be already present (at least on my install), if later I see its needed it can be readded. At least this way some of the needed changes are already documented.

---
+++ purchase_delivery/model/delivery.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Eficent (<http://www.eficent.com/>)
+#               <contact@eficent.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, orm
+from openerp.tools.translate import _
+
+
+class delivery_carrier(orm.Model):
+    _inherit = "delivery.carrier"
+
+    def get_price(self, cr, uid, ids, field_name, arg=None, context=None):
+        if context.get('purchase_order_id', False):
+            return 0.0
+
+    def name_get(self, cr, uid, ids, context=None):
+        if not len(ids):
+            return []
+        if context is None:
+            context = {}
+        return [(r['id'], r['name']) for r in self.read(
+            cr, uid, ids, ['name'], context)]
+
+    def grid_src_dest_get(self, cr, uid, ids, src_id, dest_id, context=None):
+        dest = self.pool.get('res.partner').browse(cr, uid, dest_id,
+                                                   context=context)
+        src = self.pool.get('res.partner').browse(cr, uid, src_id,
+                                                  context=context)
+        for carrier in self.browse(cr, uid, ids, context=context):
+            for grid in carrier.grids_id:
+                get_id = lambda x: x.id
+                country_ids = map(get_id, grid.country_ids)
+                state_ids = map(get_id, grid.state_ids)
+                src_country_ids = map(get_id, grid.src_country_ids)
+                src_state_ids = map(get_id, grid.src_state_ids)
+
+                if country_ids and dest.country_id.id not in country_ids:
+                    continue
+                if state_ids and dest.state_id.id not in state_ids:
+                    continue
+                if grid.zip_from and (dest.zip or '') < grid.zip_from:
+                    continue
+                if grid.zip_to and (dest.zip or '') > grid.zip_to:
+                    continue
+                if src_country_ids and src.country_id.id not in \
+                        src_country_ids:
+                    continue
+                if src_state_ids and src.state_id.id not in src_state_ids:
+                    continue
+                if grid.src_zip_from and (src.zip or '') < grid.src_zip_from:
+                    continue
+                if grid.src_zip_to and (src.zip or '') > grid.src_zip_to:
+                    continue
+                return grid.id
+        return False
+
+
+class delivery_grid(orm.Model):
+    _inherit = "delivery.grid"
+
+    _columns = {
+        'src_country_ids': fields.many2many(
+            'res.country', 'delivery_grid_src_country_rel',
+            'grid_id', 'country_id', 'Source Countries'),
+        'src_state_ids': fields.many2many('res.country.state',
+                                          'delivery_grid_src_state_rel',
+                                          'grid_id', 'state_id',
+                                          'Source States'),
+        'src_zip_from': fields.char('Start Source Zip', size=12),
+        'src_zip_to': fields.char('To Source Zip', size=12),
+    }
+
+    def get_cost(self, cr, uid, id, order, dt, context=None):
+        total = 0
+        weight = 0
+        volume = 0
+        product_uom_obj = self.pool.get('product.uom')
+        for line in order.order_line:
+            if not line.product_id:
+                continue
+            q = product_uom_obj._compute_qty(cr, uid, line.product_uom.id,
+                                             line.product_qty,
+                                             line.product_id.uom_id.id)
+            total += line.price_subtotal or 0.0
+            weight += (line.product_id.weight or 0.0) * q
+            volume += (line.product_id.volume or 0.0) * q
+
+        return self.get_cost_from_picking(cr, uid, id, total, weight,
+                                          volume, context=context)
+
+    def get_cost_from_picking(self, cr, uid, id, total, weight, volume,
+                              context=None):
+        grid = self.browse(cr, uid, id, context=context)
+        price = 0.0
+        ok = False
+
+        for line in grid.line_ids:
+            price_dict = {'price': total, 'volume': volume, 'weight': weight,
+                          'wv': volume*weight}
+            test = eval(line.type+line.operator+str(line.max_value),
+                        price_dict)
+            if test:
+                if line.price_type == 'variable':
+                    price = line.standard_price * price_dict[
+                        line.variable_factor]
+                else:
+                    price = line.standard_price
+                ok = True
+                break
+        if not ok:
+            raise orm.except_orm(_('No cost available!'),
+                                 _('No line matched this product or order '
+                                   'in the chosen delivery grid.'))
+        return price
---
+++ purchase_delivery/model/purchase.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Eficent (<http://www.eficent.com/>)
+#               <contact@eficent.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import time
+from openerp.osv import fields, orm
+from openerp.tools.translate import _
+
+
+class purchase_order(orm.Model):
+    _inherit = 'purchase.order'
+    _columns = {
+        'carrier_id': fields.many2one("delivery.carrier", "Delivery Method",
+                                      help="Complete this field if you plan "
+                                           "to invoice the shipping based on "
+                                           "picking."),
+    }
+
+    def onchange_partner_id(self, cr, uid, ids, part, context=None):
+        result = super(purchase_order, self).onchange_partner_id(
+            cr, uid, ids, part)
+        if part:
+            dtype = self.pool.get('res.partner').browse(
+                cr, uid, part).property_delivery_carrier.id
+            result['value']['carrier_id'] = dtype
+        return result
+
+    def _prepare_order_picking(self, cr, uid, order, context=None):
+        result = super(purchase_order, self)._prepare_order_picking(
+            cr, uid, order, context=context)
+        result.update(carrier_id=order.carrier_id.id)
+        return result
+
+    def delivery_set(self, cr, uid, ids, context=None):
+        line_obj = self.pool.get('purchase.order.line')
+        grid_obj = self.pool.get('delivery.grid')
+        carrier_obj = self.pool.get('delivery.carrier')
+        acc_fp_obj = self.pool.get('account.fiscal.position')
+        for order in self.browse(cr, uid, ids, context=context):
+            src_address_id = order.partner_id
+            dest_address_id = order.dest_address_id or False
+            if (
+                not dest_address_id
+                and order.warehouse_id
+                and order.warehouse_id.partner_id
+            ):
+                dest_address_id = order.warehouse_id.partner_id
+            if not dest_address_id:
+                raise orm.except_orm(_('No destination address available!'),
+                                     _('An address must be added to the'
+                                       'purchase order or the warehouse.'))
+            grid_id = carrier_obj.grid_src_dest_get(cr, uid,
+                                                    [order.carrier_id.id],
+                                                    src_address_id.id,
+                                                    dest_address_id.id)
+            if not grid_id:
+                raise orm.except_orm(_('No Grid Available!'),
+                                     _('No grid matching for this carrier!'))
+
+            if order.state not in ('draft', 'sent'):
+                raise orm.except_orm(_('Order not in Draft State!'),
+                                     _('The order state have to be draft '
+                                       'to add delivery lines.'))
+
+            grid = grid_obj.browse(cr, uid, grid_id, context=context)
+
+            taxes = grid.carrier_id.product_id.taxes_id
+            fpos = order.fiscal_position or False
+            taxes_ids = acc_fp_obj.map_tax(cr, uid, fpos, taxes)
+            # create the purchase order line
+            line_obj.create(cr, uid, {
+                'order_id': order.id,
+                'name': grid.carrier_id.name,
+                'product_qty': 1,
+                'product_uom': grid.carrier_id.product_id.uom_id.id,
+                'product_id': grid.carrier_id.product_id.id,
+                'price_unit': grid_obj.get_cost(
+                    cr, uid, grid.id, order, time.strftime('%Y-%m-%d'),
+                    context),
+                'taxes_id': [(6, 0, taxes_ids)],
+                'date_planned': order.date_order,
+            })
+        return True
+
+    def _prepare_order_line_move(self, cr, uid, order, order_line, picking_id,
+                                 context=None):
+        res = super(purchase_order, self)._prepare_order_line_move(
+            cr, uid, order, order_line, picking_id, context=context)
+        if order.warehouse_id and not order.dest_address_id:
+            if not order.warehouse_id.partner_id:
+                raise orm.except_orm(_('Error!'),
+                                     _('The warehouse must have an address.'))
+            res['partner_id'] = order.warehouse_id.partner_id.id,
+        return res
