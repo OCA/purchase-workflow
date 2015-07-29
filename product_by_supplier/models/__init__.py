9f29929434ff8a4b1ba310f5b07e86388eee4eee
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2015-07-29 14:19:41 +0200

    [IMP]Follow coding guidelines Remove fields qty_available and virtual_available since these fields are not compatible with the product_variant concept

--- product_by_supplier/models/__init__.py
+++ product_by_supplier/models/__init__.py
@@ -1,2 +1,2 @@
 # -*- encoding: utf-8 -*-
-from . import product
+from . import product_supplierinfo
--- product_by_supplier/models/product_supplierinfo.py
+++ product_by_supplier/models/product_supplierinfo.py
@@ -3,14 +3,14 @@
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (c) 2010-2013 Elico Corp. All Rights Reserved.
-#    Author: LIN Yu <lin.yu@elico-corp.com>
+#    Author: Yannick Gouin <yannick.gouin@elico-corp.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
+#    This program is distributed in the hope that it wil    l be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU Affero General Public License for more details.
@@ -19,25 +19,10 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+from openerp import models, fields
 
-{
-    'name': 'Product by supplier info',
-    'version': '1.0.1',
-    'category': 'purchase',
-    'summary': 'Show products grouped by suppliers',
-    'author': "Elico Corp,"
-              "Odoo Community Association (OCA)",
-    'website': 'http://www.elico-corp.com, ',
-    'license': 'AGPL-3',
-    'depends': [
-        'product',
-        'stock',
-        'purchase',
-    ],
-    'data': [
-        'views/product_supplierinfo_view.xml',
-    ],
-    'installable': True,
-    'auto_install': False,
-    'application': False,
-}
+
+class ProductSupplierinfo(models.Model):
+    _inherit = 'product.supplierinfo'
+
+    delay = fields.Integer(group_operator='avg')
