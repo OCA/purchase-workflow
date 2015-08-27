6b2e3e88a405c492faecf8842aeba667623ba360
Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
2015-08-27 15:21:54 -0400

    [IMP] Description + French translation

--- purchase_internal_validation/__openerp__.py
+++ purchase_internal_validation/__openerp__.py
@@ -29,8 +29,73 @@
     "author": 'Savoir-faire Linux',
     'website': 'http://www.savoirfairelinux.com',
     "description": """
-        This module modifies the purchase workflow in order to validate
-        purchases that exceeds minimum amount set by configuration wizard.
+============================
+Purchase Internal Validation
+============================
+
+This module modifies the purchase workflow in order to validate
+purchases that exceeds the amount set in the Purchase configuration panel.
+
+Configuration
+=============
+
+* Set the limit in the Purchase configuration panel
+* Add some users to the Purchase / Validator and Purchase / User groups
+* Make sure those users have correct email addresses to receive notifications
+
+Usage
+=====
+
+* As a Purchase user, go to Purchase > Purchases > Quotations
+* Create a quotation exceeding the limit and try to confirm it
+* As a Purchase validator, validate or refuse the quotation
+* As the user, you can now confirm the PO
+
+.. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
+   :alt: Try me on Runbot
+   :target: https://runbot.odoo-community.org/runbot/142/7.0
+
+For further information, please visit:
+
+* https://www.odoo.com/forum/help-1
+
+Known issues / Roadmap
+======================
+
+* ..
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues <https://github.com/OCA/purchase-workflow/issues>`_.
+In case of trouble, please check there if your issue has already been reported.
+If you spotted it first, help us smashing it by providing a detailed and welcomed feedback
+`here <https://github.com/OCA/purchase-workflow/issues/new?body=module:%20purchase_internal_validation%0Aversion:%207.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
+* Vincent Vinet <vincent.vinet@savoirfairelinux.com>
+
+Maintainer
+----------
+
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit http://odoo-community.org.
     """,
     'data': [
         'security/groups.xml',
