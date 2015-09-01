35016276fc2ed68d46ccd290558618ec1c1ec702
Nicolas Mac Rouillon <nmr@ingadhoc.com>
2015-09-01 10:44:26 -0300

    [FIX] purchase_order_type to add policy to purchase orders
    [FIX] bugs
    
    [FIX] in purchase_order.py
    
    [FIX] string in field purchase_type

--- purchase_order_type/models/purchase_order.py
+++ purchase_order_type/models/purchase_order.py
@@ -19,15 +19,36 @@
 #
 #
 
-from openerp import models, fields
+from openerp import models, fields, api
 from openerp.addons.purchase.purchase import purchase_order
 
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
+    def _default_order_type(self):
+        return self.env['purchase.order.type'].search([], limit=1)
+
     order_type = fields.Many2one(comodel_name='purchase.order.type',
                                  readonly=False,
                                  states=purchase_order.READONLY_STATES,
                                  string='Type',
-                                 ondelete='restrict')
+                                 ondelete='restrict',
+                                 default=_default_order_type)
+
+    @api.multi
+    def onchange_partner_id(self, partner_id):
+        res = super(PurchaseOrder, self).onchange_partner_id(partner_id)
+        if partner_id:
+            partner = self.env['res.partner'].browse(partner_id)
+            if partner.purchase_type:
+                res['value'] = res.get('value', {})
+                res['value'].update({
+                    'order_type': partner.purchase_type.id,
+                })
+        return res
+
+    @api.one
+    @api.onchange('order_type')
+    def onchange_order_type(self):
+        self.invoice_method = self.order_type.invoice_method
