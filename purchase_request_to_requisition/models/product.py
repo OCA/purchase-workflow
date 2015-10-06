82cf6f8fa8eca6e690e2b53563a6ffa78526e884
jbeficent <jordi.ballester@eficent.com>
2015-10-06 11:22:10 +0200

    [IMP] purchase_request_to_requisition. We don't allow to set call for bids and purchase request at the same time in the product form.

--- purchase_request_to_requisition/models/product.py
+++ purchase_request_to_requisition/models/product.py
@@ -18,7 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp import fields, models
+from openerp import fields, models, api
 
 
 class ProductTemplate(models.Model):
@@ -30,3 +30,15 @@ class ProductTemplate(models.Model):
                                            "generating requests for "
                                            "quotation from procurement.",
                                       default=False)
+
+    @api.multi
+    def _check_request_requisition(self):
+        for product in self:
+            if product.purchase_request and product.purchase_requisition:
+                return False
+        return True
+
+    _constraints = [
+        (_check_request_requisition,
+         'Only one selection of Purchase Request or Call for Bids allowed',
+         ['purchase_request', 'purchase_requisition'])]
