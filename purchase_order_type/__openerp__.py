15860c4f7edd5536b29dda6fffcf7c84112a7c59
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 10:02:53 +0200

    [UPD] prefix versions with 8.0

--- purchase_order_type/__openerp__.py
+++ purchase_order_type/__openerp__.py
@@ -19,7 +19,7 @@
 #
 #
 {'name': 'Purchase Order Type',
- 'version': '1.0',
+ 'version': '8.0.1.0.0',
  'author': 'Camptocamp,Odoo Community Association (OCA)',
  'license': 'AGPL-3',
  'category': 'Purchase Management',
