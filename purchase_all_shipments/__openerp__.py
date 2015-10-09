0b0879d3c72fb0ac7d492fad850bba259e38cae0
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 10:02:53 +0200

    [UPD] prefix versions with 8.0

--- purchase_all_shipments/__openerp__.py
+++ purchase_all_shipments/__openerp__.py
@@ -14,7 +14,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 {'name': 'Purchase All Shipments',
- 'version': '1.0',
+ 'version': '8.0.1.0.0',
  'author': "Camptocamp,Odoo Community Association (OCA)",
  'category': 'Purchases',
  'license': 'AGPL-3',
