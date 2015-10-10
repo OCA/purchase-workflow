e1a3f03405c6219ccd0e43faa81470d52ba00b7d
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-10-10 20:50:33 +0200

    OCA Transbot updated translations from Transifex

--- purchase_order_line_stock_available/models/__init__.py
+++ purchase_order_line_stock_available/models/__init__.py
@@ -1,5 +1,5 @@
 
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
