35fcdebe511f5b77b0f7aa73c120cee572703e37
Lionel Sausin <ls@numerigraphe.com>
2015-10-29 15:17:02 +0100

    manifest conventions

--- purchase_delivery_split_date/__openerp__.py
+++ purchase_delivery_split_date/__openerp__.py
@@ -20,9 +20,11 @@
 
 {
     'name': "Purchase Deliveries split by date",
-    'version': '2.0',
+    'version': '8.0.2.0.0',
     'author': u'Numérigraphe, Odoo Community Association (OCA)',
     'category': 'Purchase Management',
     'license': 'AGPL-3',
-    "depends": ['purchase'],
+    'depends': [
+        'purchase',
+    ],
 }
