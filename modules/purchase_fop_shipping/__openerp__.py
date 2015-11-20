4885a1841755d38db8b754f9149acb63c0fcb241
Mourad <mourad.elhadj.mimoune@akretion.com>
2015-11-20 17:13:16 +0100

    Prettify and clean code

--- modules/purchase_fop_shipping/__openerp__.py
+++ modules/purchase_fop_shipping/__openerp__.py
@@ -1,26 +1,10 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Mourad EL HADJ MIOUNE
-#    Copyright 2015 Akretion
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2014-2016 Akretion (http://www.akretion.com)
+#   @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name" : "Purchase Free-Of-Paiment shipping",
+    "name": "Purchase Free-Of-Paiment shipping",
     'version': '1.1',
     'author': "Akretion,Odoo Community Association (OCA)",
     'maintainer': 'Akretion',
@@ -29,9 +13,9 @@
     'complexity': 'normal',
     'depends': ['purchase'],
     'website': 'http://www.akretion.com/',
-    'data': ['views/purchase_view.xml', 
-            'views/partner_view.xml',
-            ],
+    'data': ['views/purchase_view.xml',
+             'views/partner_view.xml',
+             ],
     'tests': [],
     'installable': True,
     'auto_install': False,
--- modules/purchase_fop_shipping/models/partner.py
+++ modules/purchase_fop_shipping/models/partner.py
@@ -1,28 +1,13 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
-#    Copyright (C) 2015-TODAY Akretion <http://www.akretion.com>.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-__author__ = 'mourad.elhadj.mimoune'
+# © 2014-2016 Akretion (http://www.akretion.com)
+#   @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
+from openerp import models, fields
+
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    fop_shipping = fields.Float('FOP shipping', help='Min order amount for FOP shipping',)
+    fop_shipping = fields.Float(
+        'FOP shipping', help='Min order amount for FOP shipping',)
