e6d4709e7da32dac350dec789dad990bbe9cb2ce
Odoo - OpenERP - Acysos S.L <ignacio@acysos.com>
2015-12-05 13:13:45 +0100

    [FIX] product_supplierinfo_discount: fix flake8

--- product_supplierinfo_discount/migrations/8.0.2.0.0/post-migration.py
+++ product_supplierinfo_discount/migrations/8.0.2.0.0/post-migration.py
@@ -4,6 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 __name__ = ("Update field price_with_disc")
 
+
 def update_price(cr):
     sql = """UPDATE pricelist_partnerinfo
              SET price_with_disc = round(price * (1 - discount/100), 4)"""
