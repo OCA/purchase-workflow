8c023380ce46370326d6a447aa5a68e773ec0836
jbeficent <jordi.ballester@eficent.com>
2016-01-26 00:53:12 +0100

    deleted

--- account_move_line_purchase_info/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L. (www.eficent.com)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from . import models
