9c6de814fdc0bd7509c251247c96e2336c20d7ca
Ronald Portier (Therp BV) <rportier@therp.nl>
2016-01-31 21:17:37 +0100

    [FIX] Get rid of the double http://.

--- procurement_purchase_no_grouping/__init__.py
+++ procurement_purchase_no_grouping/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2015 Pedro M. Baeza (http://http://www.serviciosbaeza.com).
+# © 2015 Pedro M. Baeza (http://www.serviciosbaeza.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import models
--- procurement_purchase_no_grouping/__openerp__.py
+++ procurement_purchase_no_grouping/__openerp__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2015 Pedro M. Baeza (http://http://www.serviciosbaeza.com).
+# © 2015 Pedro M. Baeza (http://www.serviciosbaeza.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Procurement Purchase No Grouping',
