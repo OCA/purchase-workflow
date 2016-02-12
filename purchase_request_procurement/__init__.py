e006dfa6fbfa36134cf9378ad49ecbb6ea4a112b
jbeficent <jordi.ballester@eficent.com>
2016-02-12 10:05:06 +0100

    [IMP] split from purchase_request_to_requisition the module purchase_request_procurement

--- purchase_request_procurement/__init__.py
+++ purchase_request_procurement/__init__.py
@@ -3,4 +3,4 @@
 # - Jordi Ballester Alomar
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from . import purchase_request_line_make_purchase_requisition
+from . import models
