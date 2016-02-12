e3a303e2e61f0565b3c9623e74313322bb761260
jbeficent <jordi.ballester@eficent.com>
2016-02-12 10:05:06 +0100

    [IMP] split from purchase_request_to_requisition the module purchase_request_procurement

--- purchase_request_procurement/models/__init__.py
+++ purchase_request_procurement/models/__init__.py
@@ -3,4 +3,6 @@
 # - Jordi Ballester Alomar
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from . import purchase_request_line_make_purchase_requisition
+from . import purchase_request
+from . import procurement
+from . import product
--- purchase_request_to_requisition/__openerp__.py
+++ purchase_request_to_requisition/__openerp__.py
@@ -1,23 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2014 Eficent (<http://www.eficent.com/>)
-#              <contact@eficent.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 Eficent Business and IT Consulting Services S.L.
+# - Jordi Ballester Alomar
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Purchase Request to Call for Bids",
@@ -26,18 +10,13 @@
               "Odoo Community Association (OCA)",
     "website": "www.eficent.com",
     "category": "Purchase Management",
-    "depends": ["purchase_request", "purchase_requisition",
-                "purchase_request_to_rfq"],
+    "depends": ["purchase_request_procurement", "purchase_requisition"],
     "data": [
         "wizard/purchase_request_line_make_purchase_requisition_view.xml",
         "views/purchase_request_view.xml",
         "views/purchase_requisition_view.xml",
-        "views/product_view.xml",
-        "views/procurement_view.xml",
     ],
-    'demo': [],
-    'test': [
-        "test/purchase_request_from_procurement.yml",
+    "test": [
         'test/purchase_request_users.yml',
         'test/purchase_request_data.yml',
         'test/purchase_request.yml',
