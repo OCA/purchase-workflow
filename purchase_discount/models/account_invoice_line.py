8a40b3b387c9f8b3d17e2afaa0c8b49fbae7f485
Jonathan Nemry (ACSONE) <jonathan.nemry@acsone.eu>
2016-03-04 12:06:30 +0100

    [MIG] from 8.0 to 9.0 * Fix xml_id as Odoo removed the referenced one (purchase.purchase_order_line_form --> purchase.purchase_order_line_form2) * Add account_invoice_line.py: datas of the invoice are managed from the invoice/invoice_line with the onchange of purchase_order_id and no more from the source model of the invoice creation * Pass _calc_line_base_price into api.multi as Odoo is now into new api * Redefine _compute_amount to add a depends on new column 'discount' * Remove  StockMove there is no more a _get_invoice_line_vals and invoice creation process from the stock.move * Fix and improve tests * Add fr translations * OCA conventions update
    [CHG] Code review
    * Add Pedro copyright
    * Fix Flake8 errors
    * Remove useless api.multi
    
    [FIX] use None instead of {} from the native signature

---
+++ purchase_discount/models/account_invoice_line.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2016 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from openerp import models
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = "account.invoice.line"
+
+    def new(self, values=None):
+        """
+        Apply the linked to a purchase.order.line.discount to the
+        account_invoice_line
+        """
+        values = {} if values is None else values
+        account_invoice_line = super(
+            AccountInvoiceLine, self).new(values=values)
+        if account_invoice_line.purchase_line_id:
+            account_invoice_line.discount =\
+                account_invoice_line.purchase_line_id.discount
+        return account_invoice_line
