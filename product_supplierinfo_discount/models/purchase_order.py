34d82f56153668b08d4ed00670e26b2fbce59792
mohamed habib challouf <ch.medhabib@yahoo.com>
2016-04-05 14:01:11 +0200

    update purchase_order.py line 69
    update onchange_product_id  standard_price when  pl_pinfo.min_quantity > qty_in_product_uom

--- product_supplierinfo_discount/models/purchase_order.py
+++ product_supplierinfo_discount/models/purchase_order.py
@@ -63,5 +63,6 @@ class PurchaseOrderLine(models.Model):
             if pl_pinfo.min_quantity <= qty_in_product_uom:
                 res['value']['discount'] = pl_pinfo.discount
             else:
-                break
+                res['value']['discount'] = 0.0
+                res['value']['price_unit'] = product.standard_price
         return res
