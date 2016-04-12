d584f73b6ccfce393af76a379a1cf97d6951552a
mike <viriyananda.michael@gmail.com>
2016-04-12 21:16:43 +0700

    [FIX]Fix errors in writing

--- purchase_rfq_number/__openerp__.py
+++ purchase_rfq_number/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Purchase RFQ(Request For Quotation) Numeration',
+    'name': 'Purchase RFQ(Request For Quotation) Numbering',
     'version': '8.0.1.0.0',
     'summary': 'Adds a sequence on purchase RFQ',
     'author': 'OpenSynergy Indonesia,Odoo Community Association (OCA)',
