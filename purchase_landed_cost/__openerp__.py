ad4e7e8428e4ece00b2646515eae9d0cfa09468b
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-04-30 13:26:07 +0200

    purchase_landed_cost: Add management of expenses from invoices

--- purchase_landed_cost/__openerp__.py
+++ purchase_landed_cost/__openerp__.py
@@ -1,35 +1,19 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
-#    Copyright (C) 2010-2012 OpenERP s.a. (<http://openerp.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2013 Joaquín Gutierrez
+# © 2014-2015 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3
 
 
 {
     'name': 'Purchase landed costs - Alternative option',
-    'version': '1.0',
+    'version': '8.0.2.0.0',
     "author": "OdooMRP team,"
               "AvanzOSC,"
               "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
+              "Tecnativa,"
               "Joaquín Gutierrez",
     'category': 'Purchase Management',
-    'website': 'http://www.gutierrezweb.es,http://www.odoomrp.com',
+    'website': 'http://www.odoomrp.com',
     'summary': 'Purchase cost distribution',
     'depends': [
         'stock',
@@ -38,6 +22,8 @@
     'data': [
         'wizard/picking_import_wizard_view.xml',
         'wizard/import_invoice_line_wizard_view.xml',
+        'wizard/import_landed_cost_pickings_wizard_view.xml',
+        'views/account_invoice_view.xml',
         'views/purchase_cost_distribution_view.xml',
         'views/purchase_expense_type_view.xml',
         'views/purchase_order_view.xml',
