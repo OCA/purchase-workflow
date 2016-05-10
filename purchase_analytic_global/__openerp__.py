76d8fea3cb29be11374a925ac17fea0af42e6a8b
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-05-10 13:53:48 +0200

    [9.0][PORT] purchase_analytic_global

--- purchase_analytic_global/__openerp__.py
+++ purchase_analytic_global/__openerp__.py
@@ -1,41 +1,17 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Guewen Baconnier
-#    Copyright 2014 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © 2014-2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {'name': 'Purchase - Analytic Account Global',
- 'version': '1.0',
- 'author': 'Camptocamp',
+ 'version': '9.0.1.0.0',
+ 'author': 'Camptocamp, Odoo Community Association (OCA)',
  'maintainer': 'Camptocamp',
  'license': 'AGPL-3',
  'category': 'Purchase Management',
  'complexity': "easy",
  'depends': ['purchase',
              ],
- 'description': """
-Purchase - Analytic Account Global
-==================================
-
-Adds an analytic account on the purchases that is applied on all the lines.
-""",
  'website': 'http://www.camptocamp.com',
- 'data': ['purchase_view.xml',
+ 'data': ['views/purchase.xml',
           ],
  'test': [],
  'installable': True,
