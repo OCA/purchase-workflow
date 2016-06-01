c7dae0affee08b38ccba3c5b566f0cbeccd18529
Jonathan Nemry (ACSONE) <jonathan.nemry@acsone.eu>
2016-06-01 09:30:43 +0200

    [CHG] restart build

--- purchase_request_procurement/models/product.py
+++ purchase_request_procurement/models/product.py
@@ -9,9 +9,7 @@ from openerp import fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    purchase_request = fields.Boolean(string='Purchase Request',
-                                      help="Check this box to generate "
-                                           "purchase request instead of "
-                                           "generating requests for "
-                                           "quotation from procurement.",
-                                      default=False)
+    purchase_request = fields.Boolean(
+        string='Purchase Request', help="Check this box to generate "
+        "purchase request instead of generating requests for "
+        "quotation from procurement.", default=False)
