973934c02e56b6c0c927a150ff8ccfebd7255566
Andrius Preimantas <andrius@versada.lt>
2016-07-14 14:58:31 +0300

    [IMP][product_supplierinfo_discount] Refactor onchange_product_id funct on POL for easier inheritance

--- product_supplierinfo_discount/models/purchase_order.py
+++ product_supplierinfo_discount/models/purchase_order.py
@@ -25,6 +25,39 @@ from openerp import models, api
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
+    @api.model
+    def _get_product_discount(self, product_id, qty, partner_id):
+        """Return discount for provided qty and supplier or None otherwise."""
+        supplierinfo_obj = self.env['product.supplierinfo']
+        product_obj = self.env['product.product']
+        product_uom_obj = self.env['product.uom']
+        pl_pinfo_obj = self.env['pricelist.partnerinfo']
+
+        to_ret = None
+        product = product_obj.browse(product_id)
+        from_uom = self.env.context.get('uom') or product.uom_id.id
+        qty_in_product_uom = qty
+        partner = self.env['res.partner'].browse(partner_id)
+        sinfos = supplierinfo_obj.search(
+            [('product_tmpl_id', '=', product.product_tmpl_id.id),
+             ('name', 'child_of', partner.commercial_partner_id.id)])
+        if not sinfos:
+            return to_ret
+        seller_uom = sinfos[:1].product_uom.id or False
+        if seller_uom and from_uom and from_uom != seller_uom:
+            qty_in_product_uom = product_uom_obj._compute_qty(
+                from_uom, qty, to_uom_id=seller_uom)
+        pl_pinfos = pl_pinfo_obj.search(
+            [('suppinfo_id', 'in', sinfos.ids)], order="min_quantity")
+
+        for pl_pinfo in pl_pinfos:
+            if pl_pinfo.min_quantity <= qty_in_product_uom:
+                to_ret = pl_pinfo.discount
+            else:
+                break
+
+        return to_ret
+
     @api.multi
     def onchange_product_id(
             self, pricelist_id, product_id, qty, uom_id, partner_id,
@@ -37,31 +70,12 @@ class PurchaseOrderLine(models.Model):
             state=state)
         if not product_id:
             return res
-        supplierinfo_obj = self.env['product.supplierinfo']
-        product_obj = self.env['product.product']
-        product_uom_obj = self.env['product.uom']
-        pl_pinfo_obj = self.env['pricelist.partnerinfo']
+
         # Look for a possible discount
-        product = product_obj.browse(product_id)
-        from_uom = self.env.context.get('uom') or product.uom_id.id
-        qty_in_product_uom = qty
-        partner = self.env['res.partner'].browse(partner_id)
-        sinfos = supplierinfo_obj.search(
-            [('product_tmpl_id', '=', product.product_tmpl_id.id),
-             ('name', 'child_of', partner.commercial_partner_id.id)])
-        if not sinfos:
-            return res
-        seller_uom = sinfos[:1].product_uom.id or False
-        if seller_uom and from_uom and from_uom != seller_uom:
-            qty_in_product_uom = product_uom_obj._compute_qty(
-                from_uom, qty, to_uom_id=seller_uom)
-        pl_pinfos = pl_pinfo_obj.search(
-            [('suppinfo_id', 'in', sinfos.ids)], order="min_quantity")
-        if 'value' not in res:
-            res['value'] = {}
-        for pl_pinfo in pl_pinfos:
-            if pl_pinfo.min_quantity <= qty_in_product_uom:
-                res['value']['discount'] = pl_pinfo.discount
-            else:
-                break
+        discount = self._get_product_discount(product_id, qty, partner_id)
+        if discount is not None:
+            if 'value' not in res:
+                res['value'] = {}
+            res['value']['discount'] = discount
+
         return res
