06c3ec87b3a4a8794e7cfe972b843e200a738416
aheficent <ahenriquez@eficent.com>
2016-08-11 10:53:54 +0200

    [UPT] Migrate code to v9

--- purchase_request/models/__init__.py
+++ purchase_request/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L.
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
 from . import purchase_request
