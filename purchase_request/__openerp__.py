0ba9a97ca0765becfc1c89f5567f8a0807ffe0c5
aheficent <ahenriquez@eficent.com>
2016-08-11 09:30:03 +0200

    [UPT] Migrate code to v9

--- purchase_request/__openerp__.py
+++ purchase_request/__openerp__.py
@@ -1,32 +1,12 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2014 Eficent (<http://www.eficent.com/>)
-#              <contact@eficent.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
 {
     "name": "Purchase Request",
-    "author": "Eficent, "
+    "author": "Eficent Business and IT Consulting Services S.L., "
               "Odoo Community Association (OCA)",
-    "version": "8.0.1.0.0",
-    "contributors": [
-        'Jordi Ballester Alomar',
-    ],
+    "version": "9.0.1.0.0",
     "category": "Purchase Management",
     "depends": [
         "purchase",
@@ -44,6 +24,6 @@
     'demo': [
         "demo/purchase_request_demo.xml",
     ],
-    "license": 'AGPL-3',
+    "license": 'LGPL-3',
     "installable": True
 }
