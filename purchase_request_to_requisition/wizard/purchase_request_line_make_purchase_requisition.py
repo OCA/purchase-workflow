8a7b5b6064c384cbcd92999f9a6e0008b2527c3e
aheficent <ahenriquez@eficent.com>
2016-08-19 12:29:46 +0200

    [UPT] Migrate to v9

--- purchase_request_to_requisition/wizard/purchase_request_line_make_purchase_requisition.py
+++ purchase_request_to_requisition/wizard/purchase_request_line_make_purchase_requisition.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L.
-# - Jordi Ballester Alomar
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
 import openerp.addons.decimal_precision as dp
 from openerp import api, exceptions, fields, models
