fad2f6505d9faa04e7f544177f95c718900b8fa0
aheficent <ahenriquez@eficent.com>
2016-08-19 12:29:46 +0200

    Migrate to v9

--- purchase_request_to_requisition/models/__init__.py
+++ purchase_request_to_requisition/models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L.
-# - Jordi Ballester Alomar
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
 from . import purchase_request
 from . import purchase_requisition
