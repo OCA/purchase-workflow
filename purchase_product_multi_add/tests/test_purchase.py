0bfea26d61cb57aeb09055f06fa4dbaa3f942657
Jonathan Nemry (ACSONE) <jonathan.nemry@acsone.eu>
2016-08-29 19:02:16 +0200

    Some improvements * Remove field_view_get and use a proper domain based on given partner_id * Some others cosmetic details

--- purchase_product_multi_add/tests/test_purchase.py
+++ purchase_product_multi_add/tests/test_purchase.py
@@ -2,44 +2,16 @@
 # © 2016  Denis Roussel, Acsone SA/NV (http://www.acsone.eu)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import openerp.tests.common as common
-import re
 
 
 class TestPurchase(common.TransactionCase):
 
     def setUp(self):
-        common.TransactionCase.setUp(self)
-
+        super(TestPurchase, self).setUp(self)
+        self.model_pip = self.env['purchase.import.products']
         self.product_35 = self.env.ref("product.product_product_35")
         self.product_36 = self.env.ref("product.product_product_36")
         self.supplier = self.env.ref("base.res_partner_8")
-
-    def test_import_product(self):
-        """ Create PO
-            Import products
-            Check products are presents
-        """
-
-        po = self.env["purchase.order"].create(
-            {"partner_id": self.supplier.id,
-             'location_id': self.env.ref("stock.stock_location_stock").id,
-             'pricelist_id': self.env.ref('purchase.list0').id})
-
-        wiz_obj = self.env['purchase.import.products']
-        wizard = wiz_obj.with_context(active_id=po.id,
-                                      active_model='purchase.order')
-
-        products = [(6, 0, [self.product_35.id, self.product_36.id])]
-
-        wizard_id = wizard.create({'products': products,
-                                   'quantity': 5.0})
-
-        wizard_id.select_products()
-
-        self.assertEqual(len(po.order_line), 2)
-
-        for line in po.order_line:
-            self.assertEqual(line.product_qty, 5.0)
 
     def test_import_product_no_quantity(self):
         """ Create PO
@@ -47,42 +19,31 @@ class TestPurchase(common.TransactionCase):
             Check products are presents
         """
 
-        po = self.env["purchase.order"].create(
-            {"partner_id": self.supplier.id,
-             'location_id': self.env.ref("stock.stock_location_stock").id,
-             'pricelist_id': self.env.ref('purchase.list0').id})
-
-        wiz_obj = self.env['purchase.import.products']
-        wizard = wiz_obj.with_context(active_id=po.id,
-                                      active_model='purchase.order')
+        po = self.env["purchase.order"].create({
+            'partner_id': self.supplier.id,
+            'location_id': self.env.ref("stock.stock_location_stock").id,
+            'pricelist_id': self.env.ref('purchase.list0').id
+        })
 
         products = [(6, 0, [self.product_35.id, self.product_36.id])]
+        wizard_id = self.model_pip.create({'products': products})
 
-        wizard_id = wizard.create({'products': products})
-
-        wizard_id.select_products()
+        self.assertEqual(
+            len(wizard_id.products), 2, 'Should have two products')
+        wizard_id.create_items()
+        qty = 8.0
+        vals = {
+            'quantity': qty
+        }
+        wizard_id.items.write(vals)
+        self.assertEqual(
+            wizard_id.items[0].quantity,
+            qty, 'Should have same quantity')
+        # wizard_id = self.model_pip.browse(wizard_id.id)
+        self.assertEqual(len(wizard_id.items), 2, 'Should have two items')
+        wizard_id.with_context(
+            active_id=po.id, active_model='purchase.order').select_products()
 
         self.assertEqual(len(po.order_line), 2)
-
         for line in po.order_line:
-            self.assertEqual(line.product_qty, 1.0)
-
-    def test_import_product_fields_view(self):
-        """ Check if domain is well affected
-        """
-
-        po = self.env["purchase.order"].create(
-            {"partner_id": self.supplier.id,
-             'location_id': self.env.ref("stock.stock_location_stock").id,
-             'pricelist_id': self.env.ref('purchase.list0').id})
-
-        wiz_obj = self.env['purchase.import.products']
-        wizard = wiz_obj.with_context(active_id=po.id,
-                                      active_model='purchase.order')
-
-        res = wizard.fields_view_get()
-        self.assertEqual('arch' in res, True)
-        match = re.compile(r'<field(.)+domain=(.)+&quot;seller_ids.name')
-        find_res = re.findall(match,
-                              res['arch'])
-        self.assertEqual(len(find_res) > 0, True)
+            self.assertEqual(line.product_qty, qty)
--- purchase_product_multi_add/wizard/purchase_import_products.py
+++ purchase_product_multi_add/wizard/purchase_import_products.py
@@ -2,9 +2,10 @@
 # © 2016 Denis Roussel, ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
+from openerp import api, fields, models
+from openerp.exceptions import Warning as UserError
+from openerp.tools.translate import _
 import openerp.addons.decimal_precision as dp
-from lxml import etree
 
 UNIT = dp.get_precision('Product Unit of Measure')
 
@@ -13,73 +14,96 @@ class PurchaseImportProducts(models.TransientModel):
     _name = 'purchase.import.products'
     _description = 'Purchase Import Products'
 
-    quantity = fields.Float(string='Quantity', digits_compute=UNIT,
-                            default=1.0)
-    products = fields.Many2many(comodel_name='product.product',
-                                string="Products")
+    products = fields.Many2many(
+        comodel_name='product.product', string="Products")
+    items = fields.One2many(
+        comodel_name='purchase.import.products.items',
+        inverse_name='wizard_id')
+    partner_id = fields.Many2one(comodel_name='res.partner', string='Supplier')
+
+    @api.multi
+    def create_items(self):
+        self.ensure_one()
+        for product in self.products:
+            self.env['purchase.import.products.items'].create({
+                'wizard_id': self.id,
+                'product_id': product.id
+            })
+        ctx = self.env.context.copy()
+        ctx['mode'] = 'quantity'
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': self._name,
+            'view_type': 'form',
+            'view_mode': 'form',
+            'target': 'new',
+            'res_id': self.id,
+            'context': ctx,
+        }
 
     @api.model
-    def fields_view_get(self, view_id=None, view_type='form', toolbar=False,
-                        submenu=False):
-        res = super(PurchaseImportProducts,
-                    self).fields_view_get(view_id=view_id, view_type=view_type,
-                                          toolbar=toolbar, submenu=False)
-        if view_type == 'form':
-            model = self.env.context.get('active_model', False)
-            model_id = self.env.context.get('active_id', False)
-            if model and model == 'purchase.order' and model_id:
-                purchase = self.env['purchase.order'].browse(model_id)
-                if purchase and purchase.partner_id:
-                    doc = etree.XML(res['arch'])
-                    nodes = doc.xpath("//field[@name='products']")
-                    for node in nodes:
-                        node.set('domain', '[("seller_ids.name", "=", ' +
-                                 str(purchase.partner_id.id) + ')]')
-                res['arch'] = etree.tostring(doc)
-        return res
+    def _get_line_values(self, purchase, item):
+        """
+        @param purchase: implied purchase order
+        @type purchase: purchase.order recordset
+        @param item: item to
+        @type item: purchase.import.products.items recordset
+        @rparam: values to create purchase order line with the product item and
+        the right quantity
+        @rtype: dict
+        """
+        quantity = 1.0
+        if item.quantity:
+            quantity = item.quantity
+        pol = self.env['purchase.order.line']
+        position = purchase.fiscal_position.id
+        date_order = purchase.date_order
+        uom = item.product_id.uom_po_id
+        suppliers = item.product_id.seller_ids.filtered(
+            lambda r: r.name == purchase.partner_id)
+        for supplier in suppliers:
+            uom = supplier.product_uom
+            break
+        onchange_f = pol._model.onchange_product_id
+        product = item.product_id
+        vals = onchange_f(
+            self.env.cr, self.env.uid, [], purchase.pricelist_id.id,
+            product.id, quantity, uom.id, purchase.partner_id.id,
+            date_order=date_order, fiscal_position_id=position,
+            date_planned=False, name=False, price_unit=False,
+            state=purchase.state, context=self.env.context)
+        if 'value' in vals:
+            taxes = vals['value']['taxes_id']
+            vals['value'].update({
+                'order_id': purchase.id,
+                'product_id': product.id,
+                'taxes_id': [(6, 0, taxes)]
+            })
+            vals = vals['value']
+        return vals
 
     @api.multi
     def select_products(self):
+        if not self.env.context.get('active_id') or\
+                not self.env.context.get('active_model'):
+            raise UserError(_('Should have an id of purchase order'))
+        self.ensure_one()
         po_obj = self.env['purchase.order']
-        for wizard in self:
-            purchase = po_obj.browse(self.env.context.get('active_id', False))
-
-            if purchase:
-                for product in wizard.products:
-                    quantity = 1.0
-                    if wizard.quantity:
-                        quantity = wizard.quantity
-
-                    pol = self.env['purchase.order.line']
-                    position = purchase.fiscal_position.id
-                    date_order = purchase.date_order
-                    uom = product.uom_po_id
-                    suppliers = product.seller_ids.filtered(
-                        lambda r: r.name == purchase.partner_id)
-                    for supplier in suppliers:
-                        uom = supplier.product_uom
-                        break
-
-                    onchange_f = pol._model.onchange_product_id
-
-                    vals = onchange_f(self.env.cr, self.env.uid, [],
-                                      purchase.pricelist_id.id,
-                                      product.id, quantity,
-                                      uom.id,
-                                      purchase.partner_id.id,
-                                      date_order=date_order,
-                                      fiscal_position_id=position,
-                                      date_planned=False,
-                                      name=False, price_unit=False,
-                                      state=purchase.state,
-                                      context=self.env.context)
-
-                    if 'value' in vals:
-                        taxes = vals['value']['taxes_id']
-                        vals['value'].update({'order_id': purchase.id,
-                                              'product_id': product.id,
-                                              'taxes_id': [(6, 0, taxes)]})
-
-                        pol.create(vals['value'])
-
+        purchase_id = po_obj.browse(self.env.context.get('active_id'))
+        for item in self.items:
+            vals = self._get_line_values(purchase_id, item)
+            self.env['purchase.order.line'].create(vals)
         return {'type': 'ir.actions.act_window_close', }
+
+
+class PurchaseImportProductsItem(models.TransientModel):
+    _name = 'purchase.import.products.items'
+
+    wizard_id = fields.Many2one(
+        comodel_name='purchase.import.products', string="Wizard",)
+    product_id = fields.Many2one(
+        comodel_name='product.product', string='Product',
+        required=True, ondelete='cascade')
+    quantity = fields.Float(
+        digits_compute=dp.get_precision('Product Unit of Measure'),
+        default=1.0, string='Quantity', required=True, ondelete='cascade')
