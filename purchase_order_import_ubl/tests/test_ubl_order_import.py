7fdbdc9681a943b3af1f0a937e42a2d1c3907244
Alexis de Lattre <alexis.delattre@akretion.com>
2016-09-04 21:31:38 +0200

    FIX copy-paste error in purchase_order_import_ubl: this module now really has unittests

--- purchase_order_import_ubl/tests/test_ubl_order_import.py
+++ purchase_order_import_ubl/tests/test_ubl_order_import.py
@@ -11,56 +11,24 @@ class TestUblOrderImport(TransactionCase):
 
     def test_ubl_order_import(self):
         tests = {
-            'UBL-Order-2.1-Example.xml': {
-                'client_order_ref': '34',
-                'date_order': '2010-01-20',
-                'partner': self.env.ref('sale_order_import_ubl.johnssons'),
-                'shipping_partner':
-                self.env.ref('sale_order_import_ubl.swedish_trucking'),
-                'currency': self.env.ref('base.SEK'),
-                },
-            'UBL-Order-2.0-Example.xml': {
-                'client_order_ref': 'AEG012345',
-                'date_order': '2010-06-20',
-                'partner': self.env.ref('sale_order_import_ubl.iyt'),
-                'shipping_partner':
-                self.env.ref('sale_order_import_ubl.fred_churchill'),
-                'currency': self.env.ref('base.GBP'),
-                },
-            'UBL-RequestForQuotation-2.0-Example.xml': {
-                'partner': self.env.ref('sale_order_import_ubl.terminus'),
-                'shipping_partner':
-                self.env.ref('sale_order_import_ubl.s_massiah'),
-                },
-            'UBL-RequestForQuotation-2.1-Example.xml': {
-                'partner':
-                self.env.ref('sale_order_import_ubl.gentofte_kommune'),
-                'currency': self.env.ref('base.DKK'),
-                'shipping_partner':
-                self.env.ref(
-                    'sale_order_import_ubl.delivery_gentofte_kommune'),
+            'quote-PO00004.pdf': {
+                'po_to_update': self.env.ref('purchase.purchase_order_4'),
+                'incoterm': self.env.ref('stock.incoterm_DDU'),
                 },
             }
+        poio = self.env['purchase.order.import']
         for filename, res in tests.iteritems():
+            po = res['po_to_update']
+
             f = file_open(
-                'sale_order_import_ubl/tests/files/' + filename, 'rb')
-            xml_file = f.read()
-            wiz = self.env['sale.order.import'].create({
-                'order_file': base64.b64encode(xml_file),
-                'order_filename': filename,
-            })
+                'purchase_order_import_ubl/tests/files/' + filename, 'rb')
+            quote_file = f.read()
+            wiz = poio.with_context(
+                active_model='purchase.order', active_id=po.id).create({
+                    'quote_file': base64.b64encode(quote_file),
+                    'quote_filename': filename,
+                })
             f.close()
-            action = wiz.import_order_button()
-            so = self.env['sale.order'].browse(action['res_id'])
-            self.assertEqual(
-                so.partner_id.commercial_partner_id,
-                res['partner'])
-            if res.get('currency'):
-                self.assertEqual(so.currency_id, res['currency'])
-            if res.get('client_order_ref'):
-                self.assertEqual(so.client_order_ref, res['client_order_ref'])
-            if res.get('date_order'):
-                self.assertEqual(so.date_order[:10], res['date_order'])
-            if res.get('shipping_partner'):
-                self.assertEqual(
-                    so.partner_shipping_id, res['shipping_partner'])
+            self.assertEqual(wiz.purchase_id, po)
+            wiz.update_rfq_button()
+            self.assertEqual(po.incoterm_id, res['incoterm'])
