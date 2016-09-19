8f828fc86fe31d95291e46ad69b375ebb9d964a8
jweste <julien.weste@akretion.com.br>
2016-09-19 18:09:12 +0200

    [REF] change version and mark as installable

--- product_supplierinfo_discount/__openerp__.py
+++ product_supplierinfo_discount/__openerp__.py
@@ -22,9 +22,10 @@
 
 {
     "name": "Discounts in product supplier info",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "author": "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
               "Antiun Ingeniería S.L., "
+              "Julien Weste"
               "Odoo Community Association (OCA)",
     "category": "Purchase Management",
     "website": "www.serviciosbaeza.com",
@@ -36,5 +37,5 @@
     "data": [
         'views/product_supplierinfo_view.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
