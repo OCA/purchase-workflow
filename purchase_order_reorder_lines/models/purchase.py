44d0bad5a9b88afc17d8833003eac2b169299674
SerpentCS <support@serpentcs.com>
2016-09-23 11:58:17 +0530

    [ADD] Migrated purchase_order_reorder_lines to v9.

--- purchase_order_reorder_lines/models/purchase.py
+++ purchase_order_reorder_lines/models/purchase.py
@@ -1,28 +1,14 @@
 # -*- coding: utf-8 -*-
-#
-#
 #    Author: Alexandre Fayolle
 #    Copyright 2013 Camptocamp SA
 #    Author: Damien Crier
 #    Copyright 2015 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# © 2015 Eficent Business and IT Consulting Services S.L. -
+# Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from openerp import models, api, fields
-from openerp.tools.safe_eval import safe_eval
+from openerp import api, fields, models
 
 
 class PurchaseOrderLine(models.Model):
@@ -32,30 +18,16 @@ class PurchaseOrderLine(models.Model):
     sequence = fields.Integer(help="Gives the sequence of this line when "
                                    "displaying the purchase order.")
 
+    @api.multi
+    def _create_stock_moves(self, picking):
+        res = super(PurchaseOrderLine, self)._create_stock_moves(picking)
+        for move, line in zip(res, self):
+            move.write({'sequence': line.sequence})
+        return res
+
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
-
-    @api.model
-    def _prepare_inv_line(self, account_id, order_line):
-        res = super(PurchaseOrder, self)._prepare_inv_line(
-            account_id,
-            order_line,
-            )
-        res['sequence'] = order_line.sequence
-        return res
-
-    @api.model
-    def _prepare_order_line_move(self, order, order_line,
-                                 picking_id, group_id):
-        res = super(PurchaseOrder, self)._prepare_order_line_move(
-            order,
-            order_line,
-            picking_id,
-            group_id)
-        if res:
-            res[0]['sequence'] = order_line.sequence
-        return res
 
     @api.one
     @api.depends('order_line')
@@ -72,34 +44,3 @@ class PurchaseOrder(models.Model):
 
     max_line_sequence = fields.Integer(string='Max sequence in lines',
                                        compute='compute_max_line_sequence')
-
-
-class PurchaseLineInvoice(models.TransientModel):
-    _inherit = 'purchase.order.line_invoice'
-
-    @api.multi
-    def makeInvoices(self):
-        invoice_line_obj = self.env['account.invoice.line']
-        purchase_line_obj = self.env['purchase.order.line']
-        ctx = self.env.context.copy()
-        res = super(PurchaseLineInvoice, self.with_context(ctx)).makeInvoices()
-        invoice_ids = []
-        for field, op, val in safe_eval(res['domain']):
-            if field == 'id':
-                invoice_ids = val
-                break
-
-        invoice_lines = invoice_line_obj.search(
-            [('invoice_id', 'in', invoice_ids)])
-        for invoice_line in invoice_lines:
-            order_line = purchase_line_obj.search(
-                [('invoice_lines', '=', invoice_line.id)],
-                limit=1
-                )
-            if not order_line:
-                continue
-
-            if not invoice_line.sequence:
-                invoice_line.write({'sequence': order_line.sequence})
-
-        return res
--- purchase_order_reorder_lines/tests/test_invoice_on_lines.py
+++ purchase_order_reorder_lines/tests/test_invoice_on_lines.py
@@ -1,83 +1,80 @@
 # -*- coding: utf-8 -*-
-#
-#
+#    Author: Alexandre Fayolle
+#    Copyright 2013 Camptocamp SA
 #    Author: Damien Crier
 #    Copyright 2015 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# © 2015 Eficent Business and IT Consulting Services S.L. -
+# Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-
+from datetime import datetime
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from openerp.tests import common
-from openerp import workflow
-from openerp.tools.safe_eval import safe_eval
 
 
-class testPurchasePartialInvoicing(common.TransactionCase):
+class TestPurchaseOrder(common.TransactionCase):
 
     def setUp(self):
-        super(testPurchasePartialInvoicing, self).setUp()
-        self.context = self.env['res.users'].context_get()
-        self.po_obj = self.env['purchase.order']
-        self.po_line_obj = self.env['purchase.order.line']
-        self.inv_line_obj = self.env['account.invoice.line']
+        super(TestPurchaseOrder, self).setUp()
+        # Useful models
+        self.PurchaseOrder = self.env['purchase.order']
+        self.PurchaseOrderLine = self.env['purchase.order.line']
+        self.AccountInvoice = self.env['account.invoice']
+        self.AccountInvoiceLine = self.env['account.invoice.line']
 
-    def common_test(self):
-        self.purchase_order = self.env.ref('purchase.purchase_order_1')
-        # I change invoice method to 'based on purchase order line'
-        self.purchase_order.invoice_method = 'manual'
-        # I change the quantity on the first line to 10
-        self.purchase_order.order_line[0].product_qty = 10
-        # I confirm the purchase order
-        workflow.trg_validate(self.uid, 'purchase.order',
-                              self.purchase_order.id, 'purchase_confirm',
-                              self.cr)
-        # I check if the purchase order is confirmed
-        self.purchase_order.invalidate_cache()
-        self.assertEqual(self.purchase_order.state, 'approved',
-                         "Purchase order's state isn't correct")
-        # I get lines to invoiced
-        purchase_lines = self.purchase_order.order_line
-        # I get menu item action
-        menu = self.env.ref('purchase.purchase_line_form_action2')
-        self.domain = safe_eval(menu.domain)
-        self.domain.extend([('order_id', '=', self.purchase_order.id),
-                            ('fully_invoiced', '=', False)])
-        purchase_line_domain = self.po_line_obj.search(self.domain)
-        # I check if all lines is on the view's result
-        self.assertEqual(purchase_line_domain, purchase_lines,
-                         "lines aren't on the menu")
+    def test_purchase_order(self):
+        self.partner_id = self.env.ref('base.res_partner_1')
+        self.product_id_1 = self.env.ref('product.product_product_8')
+        self.product_id_2 = self.env.ref('product.product_product_11')
 
-    def test_invoice_complete_po(self):
-        self.common_test()
-        # I create the wizard to invoiced lines
-        ctx = self.context.copy()
-        ctx.update({'active_ids': self.purchase_order.order_line.ids})
-        wizard = self.env['purchase.order.line_invoice']\
-            .with_context(ctx).create({})
-        # I click on make invoice button
-        wizard.with_context(ctx).makeInvoices()
-        # I check if all lines are invoiced
-        invoice_lines = self.inv_line_obj\
-            .search([('purchase_line_id', 'in', self.purchase_order.ids)])
-        self.assertEqual(len(invoice_lines), len(self.purchase_order),
-                         "All of purchase lines aren't invoiced")
-        # I check if the lines are no longer on the menu
-        purchase_line_domain = self.po_line_obj.search(self.domain)
-        self.assertEqual(len(purchase_line_domain), 0,
-                         "Lines are still present")
-        for line in invoice_lines:
-            self.assertEqual(line.sequence, line.purchase_line_id.sequence,
-                             "Not same sequence")
+        po_vals = {
+            'partner_id': self.partner_id.id,
+            'order_line': [
+                (0, 0, {
+                    'name': self.product_id_1.name,
+                    'product_id': self.product_id_1.id,
+                    'product_qty': 5.0,
+                    'product_uom': self.product_id_1.uom_po_id.id,
+                    'price_unit': 500.0,
+                    'date_planned': datetime.today().
+                    strftime(DEFAULT_SERVER_DATETIME_FORMAT),
+                }),
+                (0, 0, {
+                    'name': self.product_id_2.name,
+                    'product_id': self.product_id_2.id,
+                    'product_qty': 5.0,
+                    'product_uom': self.product_id_2.uom_po_id.id,
+                    'price_unit': 250.0,
+                    'date_planned': datetime.today().
+                    strftime(DEFAULT_SERVER_DATETIME_FORMAT),
+                })],
+        }
+        self.po = self.PurchaseOrder.create(po_vals)
+
+        self.po.button_confirm()
+        self.assertEqual(self.po.state,
+                         'purchase',
+                         'Purchase: PO state should be "Purchase"')
+        self.assertEqual(self.po.invoice_status,
+                         'to invoice',
+                         'PO invoice_status should be "Waiting Invoices"')
+        self.assertEqual(self.po.picking_count, 1,
+                         'Purchase: one picking should be created"')
+        self.picking = self.po.picking_ids[0]
+        self.picking.force_assign()
+        self.picking.pack_operation_product_ids.write({'qty_done': 5.0})
+        self.picking.do_new_transfer()
+        self.assertEqual(self.po.order_line.mapped('qty_received'),
+                         [5.0, 5.0],
+                         'Purchase: all products should be received"')
+
+        self.invoice = self.AccountInvoice.create({
+            'partner_id': self.partner_id.id,
+            'purchase_id': self.po.id,
+            'account_id': self.partner_id.property_account_payable_id.id,
+        })
+        self.invoice.purchase_order_change()
+        self.assertEqual(self.po.order_line.mapped('qty_invoiced'),
+                         [5.0, 5.0],
+                         'Purchase: all products should be invoiced"')
