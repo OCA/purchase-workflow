a37560ef84c564e423e3a827fa8527bf8ac937d3
SerpentCS <support@serpentcs.com>
2016-09-28 17:23:43 +0530

    [ADD] added dependencies and sequences

--- purchase_order_line_sequence/models/__init__.py
+++ purchase_order_line_sequence/models/__init__.py
@@ -8,5 +8,5 @@
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from . import invoice
 from . import purchase
+from . import invoice
