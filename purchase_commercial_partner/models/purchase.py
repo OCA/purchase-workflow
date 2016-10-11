cc6c6a205f0510969cb802a3a49e11e0beb7a2f8
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-12 00:11:47 +0200

    Add index=True on commercial_partner_id field

--- purchase_commercial_partner/models/purchase.py
+++ purchase_commercial_partner/models/purchase.py
@@ -10,4 +10,4 @@ class PurchaseOrder(models.Model):
 
     commercial_partner_id = fields.Many2one(
         'res.partner', related='partner_id.commercial_partner_id', store=True,
-        string='Commercial Supplier')
+        index=True, string='Commercial Supplier')
