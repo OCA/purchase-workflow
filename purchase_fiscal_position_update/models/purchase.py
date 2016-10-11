02550bc265dfc7e47231fd67a04a3ee162c275a8
Angel Moya <amoyapardo@gmail.com>
2016-10-11 18:21:32 +0200

    MIG purchase_fiscal_position_update

--- purchase_fiscal_position_update/models/purchase.py
+++ purchase_fiscal_position_update/models/purchase.py
@@ -4,4 +4,13 @@
 # Copyright 2016 - Tecnativa - Angel Moya <odoo@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import models
+from openerp import models, api, _
+
+
+class purchase_order(models.Model):
+    _inherit = "purchase.order"
+
+    @api.onchange('fiscal_position_id')
+    def onchange_fiscal_position_id(self):
+        for line in self.order_line:
+            line.onchange_product_id()
