a5e7820ebabdf0de1a2cb6ea2dd63036644aee8c
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2016-10-11 08:44:41 +0200

    prefix versions with 8.0

--- product_by_supplier/__openerp__.py
+++ product_by_supplier/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     'name': 'Product by supplier info',
-    'version': '9.0.1.0',
+    'version': '9.0.1.0.0',
     'category': 'purchase',
     'summary': 'Show products grouped by suppliers',
     'author': "Elico Corp, "
