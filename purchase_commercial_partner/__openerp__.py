07f2a536d61f6ade39492762177711c0422d773b
Alexis de Lattre <alexis@via.ecp.fr>
2016-10-14 15:04:45 +0200

    8.0 Add module purchase_commercial_partner (#278)
    [ADD] purchase_commercial_partner

---
+++ purchase_commercial_partner/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Purchase Commercial Partner',
+    'version': '8.0.1.0.0',
+    'category': 'Purchase Management',
+    'license': 'AGPL-3',
+    'summary': "Add stored related field 'Commercial Supplier' on POs",
+    'author': 'Akretion,Odoo Community Association (OCA)',
+    'website': 'http://www.akretion.com',
+    'depends': ['purchase'],
+    'data': [
+        'views/purchase.xml',
+    ],
+    'installable': True,
+}
