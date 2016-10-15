7fc3171e70af75af41c5d70f22193536e75be371
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-10-15 19:45:08 +0200

    [ADD] purchase_order_variant_mgmt
    ==================================================
    Handle easily multiple variants on Purchase Orders
    ==================================================
    
    This module allows to add/modify all the variants of a product in a direct
    screen without the need of handling them one by one.
    
    Configuration
    =============
    
    * Configure your user to have any permission from "Purchases" group.
    * Create a product with 2 attributes and several values.
    
    Usage
    =====
    
    * Go to Purchases > Purchase > Requests for Quotation
    * Create a new quotation or edit an existing one.
    * Press "Add variants" button located in the upper right corner of the
      "Order Lines" tab.
    * A new screen will appear allowing you to select the products that have
      variants.
    * Once you select the product, a 2D matrix will appear with the first
      attribute values as columns and the second one as rows.
    * If there are already order lines for the product variants, the current
      quantity will be pre-filled in the matrix.
    * Change the quantities for the variant you want and click on "Transfer to
      order"
    * Order lines for the variants will be created/removed to comply with the
      input you have done.
    
    As extra feature for saving steps, there's also a button on each existing line
    that corresponds to a variant that opens the dialog directly with the product
    selected.
    
    Known issues / Roadmap
    ======================
    
    * The inline button for modifying quantities for an existing line won't
      work correctly until these 2 PRs are merged in Odoo:
    
      * https://github.com/odoo/odoo/pull/13558
      * https://github.com/odoo/odoo/pull/13635
    
      The problems are already fixed in OCB.

---
+++ purchase_order_variant_mgmt/models/purchase_order.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    # These field names are for avoiding conflicts with any other field with
+    # the same name declared by other modules and that can be a no related one
+    product_tmpl_id_purchase_order_variant_mgmt = fields.Many2one(
+        comodel_name="product.template", related="product_id.product_tmpl_id")
+    state_purchase_order_variant_mgmt = fields.Selection(
+        related="order_id.state")
+    product_attribute_value_ids = fields.Many2many(
+        comodel_name='product.attribute.value',
+        related="product_id.attribute_value_ids")
