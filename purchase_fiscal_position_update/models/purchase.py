b26a32d4e65336c43ac3668a6623cbe6ec314837
Angel Moya <amoyapardo@gmail.com>
2016-10-19 16:27:31 +0200

    IMP Double quote instead of simple for docstring according PEP257

--- purchase_fiscal_position_update/models/purchase.py
+++ purchase_fiscal_position_update/models/purchase.py
@@ -12,8 +12,8 @@ class purchase_order(models.Model):
 
     @api.onchange('fiscal_position_id')
     def onchange_fiscal_position_id(self):
-        '''Function executed by the on_change on the fiscal_position_id field
-        of a purchase order ; it updates taxes on all order lines'''
+        """Function executed by the on_change on the fiscal_position_id field
+        of a purchase order ; it updates taxes on all order lines"""
         fp = self.fiscal_position_id
         for line in self.order_line:
             # product_id is a required field since v9
