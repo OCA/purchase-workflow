2adccf9b623e1b0ab71b049b5c7b02caa3a615d3
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-11-09 12:41:40 +0100

    default_supplierinfo_discount on res.partner model

--- product_supplierinfo_discount/__manifest__.py
+++ product_supplierinfo_discount/__manifest__.py
@@ -2,12 +2,15 @@
 # © 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
 #        Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
 # © 2016 ACSONE SA/NV (<http://acsone.eu>)
+# © 2016 GRAP (http://www.grap.coop)
+#        Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     "name": "Discounts in product supplier info",
     "version": "9.0.1.0.0",
     "author": "Tecnativa, "
+              "GRAP, "
               "Odoo Community Association (OCA)",
     "category": "Purchase Management",
     "website": "www.serviciosbaeza.com",
@@ -18,6 +21,7 @@
     ],
     "data": [
         'views/product_supplierinfo_view.xml',
+        'views/res_partner_view.xml',
     ],
     'installable': True,
 }
