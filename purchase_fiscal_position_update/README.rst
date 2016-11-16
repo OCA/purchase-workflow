b158b7324967a63717e922f1563ffa7c8f8411b9
Alexis de Lattre <alexis@via.ecp.fr>
2016-11-16 17:10:54 +0100

    10.0 Port purchase_fiscal_position_update (#280)

---
+++ purchase_fiscal_position_update/README.rst
@@ -0,0 +1,56 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+===============================
+Purchase Fiscal Position Update
+===============================
+
+With this module, when a user changes the fiscal position of a purchase
+order, the taxes on all the purchase order lines are automatically
+updated.
+
+Configuration
+=============
+
+No specific configuration needed. This module uses the standard
+configuration of the fiscal positions.
+
+Usage
+=====
+
+.. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
+   :alt: Try me on Runbot
+   :target: https://runbot.odoo-community.org/runbot/142/10.0
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/OCA/purchase-workflow/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed feedback.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Mathieu Vatel (Julius Network Solutions)
+* Alexis de Lattre <alexis.delattre@akretion.com>
+
+Maintainer
+----------
+
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit https://odoo-community.org.
--- purchase_fiscal_position_update/__manifest__.py
+++ purchase_fiscal_position_update/__manifest__.py
@@ -1,28 +1,12 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Purchase Fiscal Position Update module for Odoo
-#    Copyright (C) 2011-2014 Julius Network Solutions SARL <contact@julius.fr>
-#    Copyright (C) 2014 Akretion (http://www.akretion.com)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2011-2014 Julius Network Solutions SARL <contact@julius.fr>
+# © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 
 {
     'name': 'Purchase Fiscal Position Update',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Purchase Management',
     'license': 'AGPL-3',
     'summary': 'Changing the fiscal position of a purchase order will '
@@ -40,6 +24,5 @@ updated and a warning is displayed to the user in this case.
               "Akretion,"
               "Odoo Community Association (OCA)",
     'depends': ['purchase'],
-    'data': [],
-    'installable': False,
+    'installable': True,
 }
