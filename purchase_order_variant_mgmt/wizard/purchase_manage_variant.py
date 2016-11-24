7aaf6c11de73dab0b208a7410cb933edd5ab718b
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-11-24 23:36:37 +0100

    Update purchase_manage_variant.py

--- purchase_order_variant_mgmt/wizard/purchase_manage_variant.py
+++ purchase_order_variant_mgmt/wizard/purchase_manage_variant.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import openerp.addons.decimal_precision as dp
-from openerp import api, models, fields
+from openerp import api, fields, models
 
 
 class PurchaseManageVariant(models.TransientModel):
