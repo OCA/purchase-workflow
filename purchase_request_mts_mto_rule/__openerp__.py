2e67710b5c23b4add26ba3c2e3e84e0849857e67
Alan Ramos <alan.ramos@jarsa.com.mx>
2016-11-25 10:57:09 -0600

    [ADD] purchase_request_mts_mto_rule module to allow compatibility of purchase_request_procurement with stock_mts_mto_rule module

---
+++ purchase_request_mts_mto_rule/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Jarsa Sistemas, S.A. de C.V.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+
+{
+    "name": "Purchase Request Procurement MTS + MTO",
+    "author": "Jarsa Sistemas, S.A. de C.V., "
+              "Odoo Community Association (OCA)",
+    "version": "9.0.1.0.0",
+    "category": "Purchase Management",
+    "depends": [
+        "purchase_request_procurement",
+        "stock_mts_mto_rule",
+    ],
+    "license": 'LGPL-3',
+    "installable": True,
+    "auto_install": True,
+}
---
+++ purchase_request_mts_mto_rule/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Jarsa Sistemas, S.A. de C.V.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+from . import procurement
