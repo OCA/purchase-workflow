491962415f6a22ba4d07f2cd0aba0e32cb49630a
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-12-06 18:40:12 +0100

    [MIG] procurement_purchase_no_grouping: Migration to 9.0

--- procurement_purchase_no_grouping/tests/__init__.py
+++ procurement_purchase_no_grouping/tests/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Pedro M. Baeza
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from . import test_procurement_purchase_no_grouping
