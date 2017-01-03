4dc8ed3afd7dbabf8a0f60baa824cb6060fd3f98
Alexis de Lattre <alexis.delattre@akretion.com>
2017-01-03 23:09:33 +0100

    Port purchase_discount to v10
    Fixes #265

--- purchase_discount/models/__init__.py
+++ purchase_discount/models/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2004-2009 Tiny SPRL (<http://tiny.be>).
-# © 2015 Pedro M. Baeza
-# © 2016 ACSONE SA/NV (<http://acsone.eu>)
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
 from . import purchase_order
-from . import account_invoice_line
+from . import account_invoice
