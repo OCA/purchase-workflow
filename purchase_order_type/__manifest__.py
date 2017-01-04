18f660d26e8a9657a74476a183c956fe56e21b42
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-01-04 15:45:17 -0300

    Purchase order type menu child of purchase configuration menu

--- purchase_order_type/__manifest__.py
+++ purchase_order_type/__manifest__.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Purchase Order Type",
-    "version": "9.0.1.0.0",
+    "version": "9.0.1.1.0",
     "category": "Purchase Management",
     "website": "http://www.camptocamp.com",
     "author": "Camptocamp,"
