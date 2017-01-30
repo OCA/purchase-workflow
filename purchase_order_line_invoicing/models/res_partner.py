cdfa0d023d616c590d29749271a329380020e41e
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2017-01-30 10:54:51 +0100

    [CHG] Replace openerp by odoo

--- purchase_order_line_invoicing/models/res_partner.py
+++ purchase_order_line_invoicing/models/res_partner.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
