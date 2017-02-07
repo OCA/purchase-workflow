47094c02ee53bb2225315b58cef93ea7935e65d9
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2017-02-07 16:18:10 +0100

    [FIX] adapt code to v10.0

--- purchase_fop_shipping/__manifest__.py
+++ purchase_fop_shipping/__manifest__.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
-# © 2014-2016 Akretion (http://www.akretion.com)
+# © 2017 Akretion (http://www.akretion.com)
 #   @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Purchase Free-Of-Paiment shipping",
-    'version': '1.1',
+    'version': '10.0.0.1.0',
     'author': "Akretion,Odoo Community Association (OCA)",
     'maintainer': 'Akretion',
     'license': 'AGPL-3',
-    'category': 'Delivery',
+    'category': 'Purchase',
     'complexity': 'normal',
     'depends': ['purchase'],
     'website': 'http://www.akretion.com/',
     'data': ['views/purchase_view.xml',
              'views/partner_view.xml',
              ],
-    'tests': [],
     'installable': True,
     'auto_install': False,
 }
