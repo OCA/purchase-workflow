47afb6e4ed2cdc827393191fa4e2ac38b13640cb
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2017-02-07 16:18:10 +0100

    [FIX] adapt code to v10.0

--- purchase_fop_shipping/__init__.py
+++ purchase_fop_shipping/__init__.py
@@ -1,6 +1,3 @@
 # -*- coding: utf-8 -*-
-# © 2014-2016 Akretion (http://www.akretion.com)
-#   @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import models
