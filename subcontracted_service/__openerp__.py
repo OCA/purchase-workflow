5b5f1da27692c7618b7ce3c65aca0c9ca4ee22c9
Damien Crier <damien.crier@camptocamp.com>
2017-03-01 08:40:53 +0100

    add module 'subcontracted_service'

---
+++ subcontracted_service/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Author: Damien Crier
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": 'Subcontracted service',
+    "summary": 'Subcontracted service',
+    "version": "9.0.1.0.0",
+    "category": "Purchase",
+    "website": "https://odoo-community.org/",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "purchase",
+        "sale_service",
+    ],
+    "data": [
+        'views/product_template.xml',
+    ],
+}
