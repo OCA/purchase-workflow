2c801fab8a045be76885533946cfb61ee5541b41
Damien Crier <damien.crier@camptocamp.com>
2017-03-02 13:29:42 +0100

    flake8 + change @api.model decorator to @api.multi + self.ensure_one()

--- subcontracted_service/models/procurement.py
+++ subcontracted_service/models/procurement.py
@@ -3,14 +3,15 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api, fields, _
+from openerp import models, api
 
 
 class ProcurementOrder(models.Model):
     _inherit = "procurement.order"
 
-    @api.model
+    @api.multi
     def _is_subcontracted_service(self):
+        self.ensure_one()
         return (self.product_id.type == 'service' and
                 self.product_id.property_subcontracted_service or
                 False)
@@ -19,7 +20,9 @@ class ProcurementOrder(models.Model):
     def _find_suitable_rule(self, procurement):
         res = super(ProcurementOrder, self)._find_suitable_rule(procurement)
         if procurement._is_subcontracted_service():
-            return procurement.company_id.subcontracting_service_proc_rule_id.id
+            return (
+                procurement.company_id.subcontracting_service_proc_rule_id.id
+            )
         return res
 
     @api.model
