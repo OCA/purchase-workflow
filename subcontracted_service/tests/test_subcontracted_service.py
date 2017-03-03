d23e358d59de3bfe4a806a1c061058a9341bafde
lreficent <lois.rilo@eficent.com>
2017-03-03 14:55:07 +0100

    procurement rule moved from company to warehouse.

--- subcontracted_service/tests/test_subcontracted_service.py
+++ subcontracted_service/tests/test_subcontracted_service.py
@@ -9,6 +9,9 @@ from openerp.tests.common import TransactionCase
 class TestSubcontractedService(TransactionCase):
     def setUp(self):
         super(TestSubcontractedService, self).setUp()
+        self.procurement_obj = self.env['procurement.order']
+        self.obj_warehouse = self.env['stock.warehouse']
+
         # 1. find a supplier
         self.supplier = self.env.ref('base.res_partner_1')
 
@@ -21,66 +24,40 @@ class TestSubcontractedService(TransactionCase):
                   })]
                   }
         self.pdt_service = self.env['product.product'].create(values)
-        # 3. create procurement rules in companies
-        companies = self.env['res.company'].search([])
-        companies._set_subcontracting_service_proc_rule()
-
+        # 3. create a test warehouse
+        self.test_wh = self.obj_warehouse.create({
+            'name': 'Test WH',
+            'code': 'T',
+        })
         # 4. find a customer
         self.customer = self.env['res.partner'].search(
             [('customer', '=', True)],
             limit=1
         )
 
-    def test_01_product_no_configured(self):
-        # create a SO
-        so_vals = {
-            'partner_id': self.customer.id,
-            'partner_invoice_id': self.customer.id,
-            'partner_shipping_id': self.customer.id,
-            'order_line': [(0, 0, {
-                'name': self.pdt_service.name,
-                'product_id': self.pdt_service.id,
-                'product_uom_qty': 1,
-                'product_uom': self.pdt_service.uom_id.id,
-                'price_unit': self.pdt_service.list_price})],
-            'pricelist_id': self.env.ref('product.list0').id,
-        }
-        so = self.env['sale.order'].create(so_vals)
-        # confirm SO
-        so.action_confirm()
-        # check NO procurement exists for the SO
-        self.assertFalse(so.procurement_group_id)
+    def test_wh_procurement_rule(self):
+        """Tests if the procurement rule for subcontracting services is
+        assigned properly to the warehouse."""
+        wh = self.test_wh
+        self.assertNotEqual(wh.subcontracting_service_proc_rule_id, False,
+                            'Subcontracting Service Rule not assigned to the '
+                            'Warehouse.')
+        picking_wh = wh.subcontracting_service_proc_rule_id.\
+            picking_type_id.warehouse_id
+        self.assertEqual(picking_wh, wh, 'Rule wrongly configured.')
 
-    def test_02_product_configured(self):
-        # 1. configure product
+    def test_subcontracted_service_procurement(self):
+        """Test if the subcontracting service procurement rule is correctly
+        assigned when creating a procurement for a subcontracted service
+        product."""
         self.pdt_service.property_subcontracted_service = True
-
-        # create a SO
-        so_vals = {
-            'partner_id': self.customer.id,
-            'partner_invoice_id': self.customer.id,
-            'partner_shipping_id': self.customer.id,
-            'order_line': [(0, 0, {
-                'name': self.pdt_service.name,
-                'product_id': self.pdt_service.id,
-                'product_uom_qty': 1,
-                'product_uom': self.pdt_service.uom_id.id,
-                'price_unit': self.pdt_service.list_price})],
-            'pricelist_id': self.env.ref('product.list0').id,
-        }
-        so = self.env['sale.order'].create(so_vals)
-
-        # confirm SO
-        so.action_confirm()
-
-        # check a procurement exists for the SO
-        self.assertTrue(so.procurement_group_id)
-
-        # check a PO has been created with the product and the supplier
-        # defined in product's supplierinfo
-        proc = self.env['procurement.order'].search(
-            [('sale_line_id', 'in', [so.order_line.ids])]
-        )
-        self.assertTrue(proc.purchase_id)
-        self.assertIn(self.pdt_service,
-                      proc.purchase_id.order_line.mapped('product_id'))
+        proc = self.procurement_obj.create({
+            'name': 'Test procurement',
+            'product_id': self.pdt_service.id,
+            'product_qty': 1.0,
+            'product_uom': self.pdt_service.uom_id.id,
+            'warehouse_id': self.test_wh.id
+        })
+        self.assertEqual(
+            proc.rule_id, self.test_wh.subcontracting_service_proc_rule_id,
+            'Procurement Rule assigned incorrectly.')
