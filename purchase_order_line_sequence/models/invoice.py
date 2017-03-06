3884687730f95716f006b1497864a4e8a38f2cff
darshan-serpent <darshan.patel.serpentcs@gmail.com>
2017-03-06 19:08:14 +0530

    [IMP] test cases

--- purchase_order_line_sequence/models/invoice.py
+++ purchase_order_line_sequence/models/invoice.py
@@ -8,7 +8,7 @@
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from openerp import api, fields, models
+from openerp import models
 
 
 class AccountInvoiceLine(models.Model):
--- purchase_order_line_sequence/models/purchase.py
+++ purchase_order_line_sequence/models/purchase.py
@@ -11,12 +11,54 @@
 from openerp import api, fields, models
 
 
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    @api.depends('order_line')
+    def _compute_max_line_sequence(self):
+        """Allow to know the highest sequence
+        entered in purchase order lines.
+        Web add 1 to this value for the next sequence
+        This value is given to the context of the o2m field
+        in the view. So when we create new purchase order lines,
+        the sequence is automatically max_sequence + 1
+        """
+        self.max_line_sequence = (
+            max(self.mapped('order_line.sequence') or [0]) + 1)
+
+    max_line_sequence = fields.Integer(string='Max sequence in lines',
+                                       compute='_compute_max_line_sequence')
+
+    @api.multi
+    def _reset_sequence(self):
+        for rec in self:
+            current_sequence = 1
+            for line in rec.order_line:
+                line.write({'sequence': current_sequence})
+                current_sequence += 1
+
+    @api.multi
+    def write(self, line_values):
+        res = super(PurchaseOrder, self).write(line_values)
+        for rec in self:
+            rec._reset_sequence()
+        return res
+
+    @api.multi
+    def copy(self, default=None):
+        if not default:
+            default = {}
+        self2 = self.with_context(keep_line_sequence=True)
+        return super(PurchaseOrder, self2).copy(default)
+
+
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
     _order = 'order_id desc, sequence, id'
 
     sequence = fields.Integer(help="Gives the sequence of this line when "
-                                   "displaying the purchase order.")
+                                   "displaying the purchase order.",
+                              default=9999)
 
     sequence2 = fields.Integer(help="Shows the sequence of this line in "
                                "the purchase order.",
@@ -29,22 +71,19 @@ class PurchaseOrderLine(models.Model):
             move.write({'sequence': line.sequence})
         return res
 
+    @api.model
+    def create(self, values):
+        line = super(PurchaseOrderLine, self).create(values)
+        # We do not reset the sequence if we are copying a complete purchase
+        # order
+        if 'keep_line_sequence' not in self.env.context:
+            line.order_id._reset_sequence()
+        return line
 
-class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
-
-    @api.one
-    @api.depends('order_line')
-    def compute_max_line_sequence(self):
-        """Allow to know the highest sequence
-        entered in purchase order lines.
-        Web add 1 to this value for the next sequence
-        This value is given to the context of the o2m field
-        in the view. So when we create new purchase order lines,
-        the sequence is automatically max_sequence + 1
-        """
-        self.max_line_sequence = (
-            max(self.mapped('order_line.sequence') or [0]) + 1)
-
-    max_line_sequence = fields.Integer(string='Max sequence in lines',
-                                       compute='compute_max_line_sequence')
+    @api.multi
+    def copy(self, default=None):
+        if not default:
+            default = {}
+        if 'keep_line_sequence' not in self.env.context:
+            default['sequence'] = 9999
+        return super(PurchaseOrderLine, self).copy(default)
