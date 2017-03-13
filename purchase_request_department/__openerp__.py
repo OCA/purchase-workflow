5de77b13b60529d94b0582586662fd86280f3c0d
gmeficent <gisela.mora@eficent.com>
2017-03-13 10:59:42 +0100

    purchase_request_department

---
+++ purchase_request_department/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+
+{
+    "name": "Purchase Request Department",
+    "author": "Eficent, "
+              "Odoo Community Association (OCA)",
+    "version": "9.0.1.0.0",
+    "category": "Purchase Management",
+    "depends": [
+        "hr",
+        "purchase_request"
+    ],
+    "data": [
+        "views/purchase_request_department_view.xml"
+    ],
+    'demo': [],
+    "license": 'LGPL-3',
+    "installable": True
+}
