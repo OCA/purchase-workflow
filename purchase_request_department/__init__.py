38c4fdc088253e38ae1d34d2a2a10700d1f950e2
gmeficent <gisela.mora@eficent.com>
2017-03-13 10:59:42 +0100

    [9.0][ADD] purchase_request_department

---
+++ purchase_request_department/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+
+from . import models
+from .hooks import post_init_hook
---
+++ purchase_request_department/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+
+{
+    "name": "Purchase Request Department",
+    "author": "Eficent, "
+              "Odoo Community Association (OCA)",
+    "version": "9.0.1.0.0",
+    "category": "Purchase Management",
+    "post_init_hook": "post_init_hook",
+    "depends": [
+        "hr",
+        "purchase_request"
+    ],
+    "data": [
+        "views/purchase_request_department_view.xml"
+    ],
+    'demo': [],
+    "license": 'LGPL-3',
+    "installable": True
+}
