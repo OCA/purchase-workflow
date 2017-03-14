e46c5ba58f97471968d383cac22c666eb55e4c01
lreficent <lois.rilo@eficent.com>
2017-03-14 16:20:47 +0100

    [10.0][MIG] purchase_delivery_split_date

---
+++ purchase_delivery_split_date/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2014-2016 Numérigraphe SARL
+# © 2017 Eficent Business and IT Consulting Services, S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Purchase Delivery Split Date",
+    "version": "10.0.1.0.0",
+    "summary": "Allows Purchase Order you confirm to generate one Incoming "
+               "Shipment for each expected date indicated in the Purchase "
+               "Order Lines",
+    "author": "Numérigraphe, Eficent, Odoo Community Association (OCA)",
+    "category": "Purchase Management",
+    "license": "AGPL-3",
+    "depends": [
+        "purchase",
+    ],
+    "installable": True,
+    "application": False,
+}
