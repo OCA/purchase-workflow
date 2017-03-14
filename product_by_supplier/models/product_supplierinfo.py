1da834ca6d1aa8827523b46cdcd073db4ee20499
lreficent <lois.rilo@eficent.com>
2017-03-14 15:50:29 +0100

    [MIG] product_by_supplier

--- product_by_supplier/models/product_supplierinfo.py
+++ product_by_supplier/models/product_supplierinfo.py
@@ -1,24 +1,9 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2010-2013 Elico Corp.
-#    Author: Yannick Gouin <yannick.gouin@elico-corp.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it wil    l be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2010-2013 Elico Corp. <lin.yu@elico-corp.com>
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 from openerp import models, fields
 
 
