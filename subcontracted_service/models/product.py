346a60e82a0c9cba075728f12ebe8b7e05706cb8
Florian da Costa <florian.dacosta@akretion.com>
2017-03-15 16:19:31 +0100

    Migrate subcontracted_service to v10

--- subcontracted_service/models/product.py
+++ subcontracted_service/models/product.py
@@ -3,7 +3,7 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
