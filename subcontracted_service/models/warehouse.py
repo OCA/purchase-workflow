f1fc53260d5a2b05505cfbb6968c0a27d21b3c1c
Florian da Costa <florian.dacosta@akretion.com>
2017-03-15 16:19:31 +0100

    Migrate subcontracted_service to v10

--- subcontracted_service/models/warehouse.py
+++ subcontracted_service/models/warehouse.py
@@ -3,7 +3,7 @@
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api, fields
+from odoo import api, fields, models
 
 
 class StockWarehouse(models.Model):
