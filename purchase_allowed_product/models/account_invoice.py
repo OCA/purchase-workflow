d1ced9e197bb677689c2d3de9cfeae445e1b33f4
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2017-03-28 17:18:36 +0200

    [ADD] purchase_allowed_product
    OCA Transbot updated translations from Transifex

---
+++ purchase_allowed_product/models/account_invoice.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# © 2016 Chafique DELLI @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class AccountInvoice(models.Model):
+    _inherit = ['account.invoice', 'supplied.product.mixin']
+    _name = 'account.invoice'
