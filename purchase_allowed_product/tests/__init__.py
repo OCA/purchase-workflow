962b0046003bd4c0839aebc329a0ce338e1fbd41
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2017-03-28 17:18:36 +0200

    [ADD] purchase_allowed_product

---
+++ purchase_allowed_product/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2017 Today Mourad EL HADJ MIMOUNE @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import test_purchase_allowed_product
