97f16284e5b5d49383fd89f21e90ab044426748c
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2017-03-30 11:47:05 +0200

    [FIX] FIX oca remarks, bealdav

--- purchase_allowed_product/models/__init__.py
+++ purchase_allowed_product/models/__init__.py
@@ -3,6 +3,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import res_partner
+from . import supplied_product_mixin
 from . import account_invoice
 from . import product
 from . import purchse_order
+from . import product_supplierinfo
--- purchase_allowed_product/models/product.py
+++ purchase_allowed_product/models/product.py
@@ -11,9 +11,9 @@ class ProductProduct(models.Model):
     @api.model
     def search(self, args, offset=0, limit=None, order=None, count=False):
         restrict_supplier_id = self.env.context.get(
-            'restrict_supplier_id', False)
+            'restrict_supplier_id')
         use_only_supplied_product = self.env.context.get(
-            'use_only_supplied_product', False)
+            'use_only_supplied_product')
         if use_only_supplied_product:
             seller = self.env['res.partner'].browse(restrict_supplier_id)
             seller = seller.commercial_partner_id if seller.\
