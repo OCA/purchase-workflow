3aa4e6868bbcb6b9a306d42eadd267b54f26804a
Lois Rilo <lois.rilo@eficent.com>
2017-04-04 17:21:15 +0200

    [MIG] product_by_supplier: Migration to 10.0 (#361)

--- product_by_supplier/__manifest__.py
+++ product_by_supplier/__manifest__.py
@@ -1,28 +1,12 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2010-2013 Elico Corp.
-#    Author: LIN Yu <lin.yu@elico-corp.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2010-2013 Elico Corp. <lin.yu@elico-corp.com>
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Product by supplier info',
-    'version': '8.0.1.0',
+    'version': '10.0.1.0.0',
     'category': 'purchase',
     'summary': 'Show products grouped by suppliers',
     'author': "Elico Corp,"
@@ -36,7 +20,7 @@
     'data': [
         'views/product_supplierinfo_view.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
