d082e167fcb3b1c3fe736df9a87ba3f34b2a8b71
Giovanni Francesco Capalbo <giovanni@therp.nl>
2017-04-13 16:48:47 +0200

    uncomment fix code

--- purchase_partner_default_journal/models/purchase_order.py
+++ purchase_partner_default_journal/models/purchase_order.py
@@ -8,23 +8,19 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
 
-    """ TODO
     @api.multi
     def onchange_partner_id(self):
-        # Onchange function is ported in newAPI on 9
         for this in self:
             result = super(PurchaseOrder, this).onchange_partner_id()
             if not this.partner_id:
                 return result
             result.journal_id = self.env['res.partner'].browse().default_purchase_journal_id.id
             return result
-    """
-    """
+
     @api.model
     def _prepare_invoice_line_from_po_line(self, line):
         result = super(PurchaseOrder, self)._prepare_invoice(order, line)
-        result['journal_id'] = order.journal_id.id or\
+        result['journal_id'] = order.journal_id.id or \
             order.partner_id.default_purchase_journal_id.id
         return result
-    """ 
 
