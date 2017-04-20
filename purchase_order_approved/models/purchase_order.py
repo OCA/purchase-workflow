1cd1888ead0f299cbcb77423010badf6bee8d882
lreficent <lois.rilo@eficent.com>
2017-04-20 11:51:31 +0200

    [9.0][ADD] purchase_order_approved

---
+++ purchase_order_approved/models/purchase_order.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    state = fields.Selection(selection_add=[('approved', 'Approved')])
+    # TODO: inhterit state but adding approved state in a position after 'to
+    # aprove' state.
+
+    READONLY_STATES = {
+        'purchase': [('readonly', True)],
+        'done': [('readonly', True)],
+        'cancel': [('readonly', True)],
+        'approved': [('readonly', True)],
+    }
+
+    # Update the readonly states:
+    origin = fields.Char(states=READONLY_STATES)
+    date_order = fields.Datetime(states=READONLY_STATES)
+    partner_id = fields.Many2one(states=READONLY_STATES)
+    dest_address_id = fields.Many2one(states=READONLY_STATES)
+    currency_id = fields.Many2one(states=READONLY_STATES)
+    order_line = fields.One2many(states=READONLY_STATES)
+    company_id = fields.Many2one(states=READONLY_STATES)
+    picking_type_id = fields.Many2one(states=READONLY_STATES)
+
+    @api.multi
+    def button_release(self):
+        super(PurchaseOrder, self).button_approve()
+
+    @api.multi
+    def button_approve(self):
+        for purchase in self:
+            if purchase.company_id.purchase_approve_active:
+                purchase.write({'state': 'approved'})
+                return {}
+            else:
+                super(PurchaseOrder, purchase).button_approve()
