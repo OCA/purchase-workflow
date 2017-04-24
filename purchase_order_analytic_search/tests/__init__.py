7b48d2453dd527685d6dc20dc8e651cd85cef745
gmeficent <gisela.mora@eficent.com>
2017-04-24 14:04:55 +0200

    purchase_order_analytic_search

---
+++ purchase_order_analytic_search/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2015-17 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from . import test_analytic_search
