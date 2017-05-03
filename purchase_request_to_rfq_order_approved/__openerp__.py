d56094f0e0bcb9ef6a48239c8190309f6ab902cd
gmeficent <gisela.mora@eficent.com>
2017-05-03 10:03:09 +0200

    [ADD] purchase_request_to_rfq_order_approved

---
+++ purchase_request_to_rfq_order_approved/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+
+{
+    "name": "Purchase Request to RFQ Order Approved",
+    "author": "Eficent, "
+              "Odoo Community Association (OCA)",
+    "version": "9.0.1.0.0",
+    "website": "http://www.eficent.com/",
+    "category": "Purchase Management",
+    "depends": [
+        "purchase_request_to_rfq",
+        "purchase_order_approved"],
+    "data": [
+        "views/purchase_request_view.xml"
+    ],
+    "license": 'LGPL-3',
+    "installable": True,
+    "auto_install": True,
+}
