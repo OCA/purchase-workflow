60446c4639a372dc92b2197fb919c62898200638
Jordi Ballester <jordi.ballester@eficent.com>
2017-05-05 20:51:07 +0200

    Several fixes and added post_init_hook

--- purchase_request_to_rfq_order_approved/__init__.py
+++ purchase_request_to_rfq_order_approved/__init__.py
@@ -3,3 +3,4 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
 from . import models
+from .init_hook import post_init_hook
--- purchase_request_to_rfq_order_approved/models/purchase_request.py
+++ purchase_request_to_rfq_order_approved/models/purchase_request.py
@@ -9,14 +9,11 @@ class PurchaseRequestLine(models.Model):
     _inherit = "purchase.request.line"
 
     @api.multi
-    @api.depends('purchase_lines.state', 'purchase_lines.order_id.state')
+    @api.depends('purchase_lines', 'purchase_lines.order_id.state')
     def _compute_purchase_state(self):
-        temp_purchase_state = \
-            super(PurchaseRequestLine, self)._compute_purchase_state()
+        super(PurchaseRequestLine, self)._compute_purchase_state()
         for rec in self:
-            temp_purchase_state = False
             if rec.purchase_lines:
-                if any([po_line.state == 'approved' for po_line in
+                if any([po_line.order_id.state == 'approved' for po_line in
                         rec.purchase_lines]):
-                    temp_purchase_state = 'approved'
-            rec.purchase_state = temp_purchase_state
+                    rec.purchase_state = 'approved'
