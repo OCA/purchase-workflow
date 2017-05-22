d44c38d9ad532348ebb11d87434a1a349d3a29a5
Roberto Onnis <roberto.onnis@innoviu.com>
2017-05-22 10:04:37 +0200

    Adds copyright headers in test files

--- product_customer_code_purchase/tests/__init__.py
+++ product_customer_code_purchase/tests/__init__.py
@@ -1,1 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2017 Roberto Onnis - innoviu
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
 from . import test_product_customer_code_purchase
--- product_customer_code_purchase/tests/test_product_customer_code_purchase.py
+++ product_customer_code_purchase/tests/test_product_customer_code_purchase.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# © 2017 Roberto Onnis - innoviu
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
 
 from odoo import fields
 from odoo.tests.common import TransactionCase
