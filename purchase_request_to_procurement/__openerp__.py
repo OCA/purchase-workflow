6b2e5553870e6c3331af53a8654681101d8f496a
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-06-11 11:26:20 +0200

    [FIX] Website URI

--- purchase_request_to_procurement/__openerp__.py
+++ purchase_request_to_procurement/__openerp__.py
@@ -10,7 +10,7 @@
     'version': '9.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'www.acsone.eu',
+    'website': 'https://acsone.eu/',
     'depends': [
         # Odoo
         'procurement',
