6b8d1501abec08b7e0fda1ae1f0744abb7d1d3c8
kiko <kiko@comunitea.com>
2017-06-13 20:03:10 +0200

    [FIX] line too long

--- purchase_order_type/models/purchase_order.py
+++ purchase_order_type/models/purchase_order.py
@@ -5,7 +5,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
-from odoo.addons.purchase.models.purchase import PurchaseOrder as purchase_order
+from odoo.addons.purchase.models.purchase \
+    import PurchaseOrder as purchase_order
 
 
 class PurchaseOrder(models.Model):
