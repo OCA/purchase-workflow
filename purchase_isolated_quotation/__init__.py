22073d0e112419e6c6e9afe440563322fc65abb1
phongyanon <phongyanon@gmail.com>
2017-06-14 10:33:49 +0700

    [IMP] edit comment in header

--- purchase_isolated_quotation/__init__.py
+++ purchase_isolated_quotation/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2012 Ecosoft (<http://www.ecosoft.co.th>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from .hooks import post_init_hook, uninstall_hook
 from . import models
