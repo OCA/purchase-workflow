4683207d94e772ef0b25ee0bdbf5637e4b2fea20
phongyanon <phongyanon@gmail.com>
2017-06-14 10:33:49 +0700

    [IMP] edit comment in header

--- purchase_isolated_quotation/models/purchase.py
+++ purchase_isolated_quotation/models/purchase.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2012 Ecosoft (<http://www.ecosoft.co.th>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
