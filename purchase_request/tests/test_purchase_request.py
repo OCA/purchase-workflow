b3bac04f2c8f9298516d3585d1777b41280ef382
lreficent <lois.rilo@eficent.com>
2017-06-15 12:19:17 +0200

    improve tests to cover auto-reject

--- purchase_request/tests/test_purchase_request.py
+++ purchase_request/tests/test_purchase_request.py
@@ -12,20 +12,22 @@ class TestPurchaseRequest(common.TransactionCase):
         super(TestPurchaseRequest, self).setUp()
         self.purchase_request = self.env['purchase.request']
         self.purchase_request_line = self.env['purchase.request.line']
-
-    def test_purchase_request_status(self):
         vals = {
             'picking_type_id': self.env.ref('stock.picking_type_in').id,
             'requested_by': SUPERUSER_ID,
         }
-        purchase_request = self.purchase_request.create(vals)
+        self.purchase_request = self.purchase_request.create(vals)
         vals = {
-            'request_id': purchase_request.id,
+            'request_id': self.purchase_request.id,
             'product_id': self.env.ref('product.product_product_13').id,
             'product_uom_id': self.env.ref('product.product_uom_unit').id,
             'product_qty': 5.0,
         }
         self.purchase_request_line.create(vals)
+
+    def test_purchase_request_status(self):
+        """Tests Purchase Request status workflow."""
+        purchase_request = self.purchase_request
         self.assertEqual(
             purchase_request.is_editable, True,
             'Should be editable')
@@ -44,3 +46,25 @@ class TestPurchaseRequest(common.TransactionCase):
             purchase_request.state, 'draft',
             'Should be in state draft')
         self.purchase_request_line.unlink()
+
+    def test_auto_reject(self):
+        """Tests if a Purchase Request is autorejected when all lines are
+        cancelled."""
+        purchase_request = self.purchase_request
+        # Add a second line to the PR:
+        vals = {
+            'request_id': purchase_request.id,
+            'product_id': self.env.ref('product.product_product_14').id,
+            'product_uom_id': self.env.ref('product.product_uom_unit').id,
+            'product_qty': 5.0,
+        }
+        self.purchase_request_line.create(vals)
+        lines = purchase_request.line_ids
+        # Cancel one line:
+        lines[0].do_cancel()
+        self.assertNotEqual(purchase_request.state, 'rejected',
+                            'Purchase Request should not have been rejected.')
+        # Cancel the second one:
+        lines[1].do_cancel()
+        self.assertEqual(purchase_request.state, 'rejected',
+                         'Purchase Request should have been auto-rejected.')
