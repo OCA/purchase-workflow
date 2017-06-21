9b8c4b7b0a692d9b8cd9134ce7f42f5209dd769c
lreficent <lois.rilo@eficent.com>
2017-06-21 18:27:59 +0200

    [9.0][IMP] purchase_request: add post-migration script.

--- purchase_request/__manifest__.py
+++ purchase_request/__manifest__.py
@@ -6,7 +6,10 @@
     "name": "Purchase Request",
     "author": "Eficent Business and IT Consulting Services S.L., "
               "Odoo Community Association (OCA)",
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.0.1",
+    "summary": "Use this module to have notification of requirements of "
+               "materials and/or external services and keep track of such "
+               "requirements.",
     "category": "Purchase Management",
     "depends": [
         "purchase",
