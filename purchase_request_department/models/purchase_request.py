df884ac00232e6052ec6029611c5a1b3168f0202
mreficent <miquel.raich@eficent.com>
2017-06-22 14:28:53 +0200

    [MIG] purchase_request_department: Migration to 10.0

--- purchase_request_department/models/purchase_request.py
+++ purchase_request_department/models/purchase_request.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class PurchaseRequest(models.Model):
