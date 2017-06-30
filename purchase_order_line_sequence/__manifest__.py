62c62126d5de8bf48204c26aa13f42b75edb3507
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2017-06-30 16:07:01 +0530

    [ADD] purchase_order_line_sequence v10

--- purchase_order_line_sequence/__manifest__.py
+++ purchase_order_line_sequence/__manifest__.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
-#    Author: Alexandre Fayolle
-#    Copyright 2013 Camptocamp SA
-#    Author: Damien Crier
-#    Copyright 2015 Camptocamp SA
-# © 2015 Eficent Business and IT Consulting Services S.L. -
-# Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# Copyright 2017 Camptocamp SA - Damien Crier, Alexandre Fayolle
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Purchase order lines with sequence number',
-    'version': '9.0.1.0.1',
+    'summary': 'Adds sequence to PO lines and propagates it to'
+               'Invoice lines and Stock Moves',
+    'version': '10.0.1.0.0',
     'category': 'Purchase Management',
     'author': "Camptocamp, "
               "Eficent, "
@@ -19,12 +17,11 @@
     'website': 'http://www.camptocamp.com',
     'depends': [
         'purchase',
-        'stock_picking_line_sequence',
-        'account_invoice_line_sequence',
     ],
     'data': ['views/purchase_view.xml',
              'views/report_purchaseorder.xml',
              'views/report_purchasequotation.xml'],
+    'post_init_hook': 'post_init_hook',
     'installable': True,
     'auto_install': False,
     'license': "AGPL-3",
