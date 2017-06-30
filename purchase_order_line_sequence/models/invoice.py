b63058481d2103e31b3687a5f6d4fde8ccbe3ae6
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2017-06-30 16:07:01 +0530

    [ADD] purchase_order_line_sequence v10

--- purchase_order_line_sequence/models/invoice.py
+++ purchase_order_line_sequence/models/invoice.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
-#    Author: Alexandre Fayolle
-#    Copyright 2013 Camptocamp SA
-#    Author: Damien Crier
-#    Copyright 2015 Camptocamp SA
-# © 2015 Eficent Business and IT Consulting Services S.L. -
-# Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# Copyright 2017 Camptocamp SA - Damien Crier, Alexandre Fayolle
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import models
+from odoo import models
 
 
 class AccountInvoiceLine(models.Model):
