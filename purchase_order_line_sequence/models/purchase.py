222d6d209ec287f64e1a9db6790fbdcfe0f2279d
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2017-06-30 16:07:01 +0530

    [ADD] purchase_order_line_sequence v10

--- purchase_order_line_sequence/models/purchase.py
+++ purchase_order_line_sequence/models/purchase.py
@@ -1,30 +1,26 @@
 # -*- coding: utf-8 -*-
-#    Author: Alexandre Fayolle
-#    Copyright 2013 Camptocamp SA
-#    Author: Damien Crier
-#    Copyright 2015 Camptocamp SA
-# © 2015 Eficent Business and IT Consulting Services S.L. -
-# Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# Copyright 2017 Camptocamp SA - Damien Crier, Alexandre Fayolle
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
+    @api.multi
     @api.depends('order_line')
     def _compute_max_line_sequence(self):
-        """Allow to know the highest sequence
-        entered in purchase order lines.
-        Web add 1 to this value for the next sequence
-        This value is given to the context of the o2m field
-        in the view. So when we create new purchase order lines,
-        the sequence is automatically max_sequence + 1
+        """Allow to know the highest sequence entered in purchase order lines.
+        Then we add 1 to this value for the next sequence which is given
+        to the context of the o2m field in the view. So when we create a new
+        purchase order line, the sequence is automatically max_sequence + 1
         """
-        self.max_line_sequence = (
-            max(self.mapped('order_line.sequence') or [0]) + 1)
+        for purchase in self:
+            purchase.max_line_sequence = (
+                max(purchase.mapped('order_line.sequence') or [0]) + 1)
 
     max_line_sequence = fields.Integer(string='Max sequence in lines',
                                        compute='_compute_max_line_sequence')
@@ -34,34 +30,31 @@ class PurchaseOrder(models.Model):
         for rec in self:
             current_sequence = 1
             for line in rec.order_line:
-                line.write({'sequence': current_sequence})
+                line.sequence = current_sequence
                 current_sequence += 1
 
     @api.multi
     def write(self, line_values):
         res = super(PurchaseOrder, self).write(line_values)
-        for rec in self:
-            rec._reset_sequence()
+        self._reset_sequence()
         return res
 
     @api.multi
     def copy(self, default=None):
-        if not default:
-            default = {}
-        self2 = self.with_context(keep_line_sequence=True)
-        return super(PurchaseOrder, self2).copy(default)
+        return super(PurchaseOrder,
+                     self.with_context(keep_line_sequence=True)).copy(default)
 
 
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
-    _order = 'order_id desc, sequence, id'
+    _order = 'sequence, id'
 
-    sequence = fields.Integer(help="Gives the sequence of this line when "
+    sequence = fields.Integer(help="Gives the sequence of the line when "
                                    "displaying the purchase order.",
                               default=9999)
 
-    sequence2 = fields.Integer(help="Shows the sequence of this line in "
-                               "the purchase order.",
+    sequence2 = fields.Integer(help="Displays the sequence of the line in "
+                                    "the purchase order.",
                                related='sequence', readonly=True)
 
     @api.multi
@@ -74,16 +67,8 @@ class PurchaseOrderLine(models.Model):
     @api.model
     def create(self, values):
         line = super(PurchaseOrderLine, self).create(values)
-        # We do not reset the sequence if we are copying a complete purchase
+        # We do reset the sequence if we are copying a complete purchase
         # order
-        if 'keep_line_sequence' not in self.env.context:
+        if self.env.context.get('keep_line_sequence'):
             line.order_id._reset_sequence()
         return line
-
-    @api.multi
-    def copy(self, default=None):
-        if not default:
-            default = {}
-        if 'keep_line_sequence' not in self.env.context:
-            default['sequence'] = 9999
-        return super(PurchaseOrderLine, self).copy(default)
