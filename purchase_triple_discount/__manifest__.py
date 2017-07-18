b3ab2d3187bbd9ade9e6bdc98d1142b76ba99330
David Vidal <chienandalu@gmail.com>
2017-07-18 20:20:49 +0200

    [ADD] purchase_triple_discount: New module

---
+++ purchase_triple_discount/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Account Invoice Triple Discount',
+    'version': '10.0.1.0.0',
+    'category': 'Purchase Management',
+    'author': 'Tecnativa, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://tecnativa.com',
+    'license': 'AGPL-3',
+    'summary': 'Manage triple discount on purchase order lines',
+    'depends': [
+        'purchase_discount',
+        'account_invoice_triple_discount',
+    ],
+    'data': [
+        'views/purchase_view.xml',
+    ],
+    'installable': True,
+}
