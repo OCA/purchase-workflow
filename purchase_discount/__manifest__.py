05748aef974c47c8f95da0db5c9c0c956f8ecb49
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-07-23 00:14:52 +0200

    [IMP] purchase_discount: Respect inheratibility
    [FIX] purchase_discount: Adapt code to upstream
    
    Odoo has changed the way taxes are computed in
    https://github.com/odoo/odoo/commit/5afb5ecd04cf49be6bedb655b187b19a65e1e02d#diff-3bf7fa37ad269bad5d8df8214bd5206b
    so this the adaptation for not depending on context values

--- purchase_discount/__manifest__.py
+++ purchase_discount/__manifest__.py
@@ -10,7 +10,7 @@
               "Tecnativa, "
               "ACSONE SA/NV,"
               "Odoo Community Association (OCA)",
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.0.1",
     "category": "Purchase Management",
     "depends": ["purchase"],
     "data": [
--- purchase_discount/report/purchase_report.py
+++ purchase_discount/report/purchase_report.py
@@ -1,107 +1,61 @@
 # -*- coding: utf-8 -*-
-# © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
-# © Odoo S.A. (https://www.odoo.com/)
+# Copyright 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models, tools
+from odoo import api, fields, models, tools
+import odoo.addons.decimal_precision as dp
 
 
 class PurchaseReport(models.Model):
     _inherit = "purchase.report"
 
+    discount = fields.Float(
+        string='Discount (%)', digits=dp.get_precision('Discount'),
+        group_operator="avg",
+    )
+
+    def _select_purchase_discount(self):
+        return """
+            , l.discount AS discount
+            """
+
+    def _group_by_purchase_discount(self):
+        return ", l.discount"
+
+    def _get_discounted_price_unit_exp(self):
+        """Inheritable method for getting the SQL expression used for
+        calculating the unit price with discount(s).
+
+        :rtype: str
+        :return: SQL expression for discounted unit price.
+        """
+        return '(1 - l.discount / 100) * l.price_unit '
+
     @api.model_cr
     def init(self):
-        tools.drop_view_if_exists(self._cr, 'purchase_report')
-        self._cr.execute("""
-            create view purchase_report as (
-                WITH currency_rate as (%s)
-                select
-                    min(l.id) as id,
-                    s.date_order as date_order,
-                    s.state,
-                    s.date_approve,
-                    s.dest_address_id,
-                    spt.warehouse_id as picking_type_id,
-                    s.partner_id as partner_id,
-                    s.create_uid as user_id,
-                    s.company_id as company_id,
-                    s.fiscal_position_id as fiscal_position_id,
-                    l.product_id,
-                    p.product_tmpl_id,
-                    t.categ_id as category_id,
-                    s.currency_id,
-                    t.uom_id as product_uom,
-                    sum(l.product_qty/u.factor*u2.factor) as unit_quantity,
-                    extract(epoch from age(s.date_approve,s.date_order))/
-                        (24*60*60)::decimal(16,2) as delay,
-                    extract(epoch from age(l.date_planned,s.date_order))/
-                        (24*60*60)::decimal(16,2) as delay_pass,
-                    count(*) as nbr_lines,
-                    sum((1 - l.discount / 100) * l.price_unit /
-                    COALESCE(cr.rate, 1.0) * l.product_qty)::decimal(16,2)
-                    as price_total,
-                    avg(100.0 * ((1 - l.discount / 100) * l.price_unit /
-                    COALESCE(cr.rate,1.0) * l.product_qty) / NULLIF
-                    (ip.value_float*l.product_qty/u.factor*u2.factor, 0.0))
-                    ::decimal(16,2) as negociation,
-                    sum(ip.value_float*l.product_qty/u.factor*u2.factor)
-                    ::decimal(16,2) as price_standard,
-                    (sum((1 - l.discount / 100) * l.product_qty *
-                    l.price_unit / COALESCE(cr.rate, 1.0))/NULLIF(
-                    sum(l.product_qty/u.factor*u2.factor),0.0))::decimal(16,2)
-                    as price_average,
-                    partner.country_id as country_id,
-                    partner.commercial_partner_id as commercial_partner_id,
-                    analytic_account.id as account_analytic_id,
-                    sum(p.weight * l.product_qty/u.factor*u2.factor) as weight,
-                    sum(p.volume * l.product_qty/u.factor*u2.factor) as volume
-                from purchase_order_line l
-                    join purchase_order s on (l.order_id=s.id)
-                    join res_partner partner on s.partner_id = partner.id
-                        left join product_product p on (l.product_id=p.id)
-                            left join product_template t on
-                            (p.product_tmpl_id=t.id)
-                            LEFT JOIN ir_property ip ON
-                            (ip.name='standard_price' AND
-                            ip.res_id=CONCAT('product.template,',t.id) AND
-                            ip.company_id=s.company_id)
-                    left join product_uom u on (u.id=l.product_uom)
-                    left join product_uom u2 on (u2.id=t.uom_id)
-                    left join stock_picking_type spt on
-                    (spt.id=s.picking_type_id)
-                    left join account_analytic_account analytic_account on
-                    (l.account_analytic_id = analytic_account.id)
-                    left join currency_rate cr on
-                    (cr.currency_id = s.currency_id and
-                        cr.company_id = s.company_id and
-                        cr.date_start <= coalesce(s.date_order, now()) and
-                        (cr.date_end is null or
-                        cr.date_end > coalesce(s.date_order, now())))
-                group by
-                    s.company_id,
-                    s.create_uid,
-                    s.partner_id,
-                    u.factor,
-                    s.currency_id,
-                    l.price_unit,
-                    s.date_approve,
-                    l.date_planned,
-                    l.product_uom,
-                    s.dest_address_id,
-                    s.fiscal_position_id,
-                    l.product_id,
-                    p.product_tmpl_id,
-                    t.categ_id,
-                    s.date_order,
-                    s.state,
-                    spt.warehouse_id,
-                    u.uom_type,
-                    u.category_id,
-                    t.uom_id,
-                    u.id,
-                    u2.factor,
-                    partner.country_id,
-                    partner.commercial_partner_id,
-                    analytic_account.id
-            )
-        """ % self.env['res.currency']._select_companies_rates())
+        """Inject parts in the query with this hack, fetching the query and
+        recreating it. Query is returned all in upper case and with final ';'.
+        """
+        super(PurchaseReport, self).init()
+        self._cr.execute("SELECT pg_get_viewdef(%s, true)", (self._table,))
+        view_def = self._cr.fetchone()[0]
+        if view_def[-1] == ';':  # Remove trailing semicolon
+            view_def = view_def[:-1]
+        view_def = view_def.replace(
+            "FROM purchase_order_line",
+            "{} FROM purchase_order_line".format(
+                self._select_purchase_discount()
+            ),
+        )
+        view_def += self._group_by_purchase_discount()
+        # Replace the expression with space for avoiding to replace in the
+        # group by part
+        view_def = view_def.replace(
+            'l.price_unit ', self._get_discounted_price_unit_exp(),
+        )
+        # Re-create view
+        tools.drop_view_if_exists(self._cr, self._table)
+        self._cr.execute("create or replace view {} as ({})".format(
+            self._table, view_def,
+        ))
