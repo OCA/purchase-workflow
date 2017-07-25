441a27ae8c652d7ea0c0f9a25c5ac8bdd2c17280
mreficent <miquel.raich@eficent.com>
2017-07-25 15:17:16 +0200

    [10.0][MIG] purchase_open_qty: Migration to 10.0

--- purchase_open_qty/models/purchase_order.py
+++ purchase_open_qty/models/purchase_order.py
@@ -3,8 +3,8 @@
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
-import openerp.addons.decimal_precision as dp
+from odoo import api, fields, models
+import odoo.addons.decimal_precision as dp
 
 
 class PurchaseOrderLine(models.Model):
