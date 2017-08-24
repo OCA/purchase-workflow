4c044ee8849ca477bb7f7eed2eb89563a9285d0f
phongyanon <phongyanon@gmail.com>
2017-08-24 14:32:30 +0700

    [IMP] header license text

--- purchase_isolated_quotation/__manifest__.py
+++ purchase_isolated_quotation/__manifest__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# © 2017 Ecosoft (ecosoft.co.th).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Purchase - Separate Quote and Order',
     'version': '10.0.1.0.0',
--- purchase_isolated_quotation/models/purchase.py
+++ purchase_isolated_quotation/models/purchase.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# © 2017 Ecosoft (ecosoft.co.th).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
