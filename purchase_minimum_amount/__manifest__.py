91ce64f0c6ffe3b55ecc9af75d3aa1fcff2c0ba4
rgarnau <rgarnau20@gmail.com>
2017-08-31 12:16:18 +0200

    [ADD] Purchase Minimum Amount

---
+++ purchase_minimum_amount/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+
+{
+    "name": "Purchase Minimum Amount",
+    "author": "Eficent, Odoo Community Association (OCA)",
+    "version": "10.0.1.0.0",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "depends": [
+        'purchase',
+        'purchase_order_approval_block',
+    ],
+    "data": [
+        'data/purchase_block_reason_data.xml',
+        'views/purchase_order_view.xml',
+        'views/res_partner_view.xml',
+    ],
+    "license": 'LGPL-3',
+    "installable": True
+}
