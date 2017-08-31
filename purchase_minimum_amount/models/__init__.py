9733751b85018f062929c1172928d6b983cf414f
rgarnau <rgarnau20@gmail.com>
2017-08-31 12:16:18 +0200

    [ADD] Purchase Minimum Amount

---
+++ purchase_minimum_amount/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from . import purchase_order
+from . import partner
