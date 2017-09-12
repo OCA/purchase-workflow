f3a02cce9d85914aca15f9594f7adf2ac259b194
Denis Roussel <denis.roussel@acsone.eu>
2017-09-12 09:31:09 +0200

    [10.0][FIX] product_by_supplier: Wrong import

--- product_by_supplier/models/product_supplierinfo.py
+++ product_by_supplier/models/product_supplierinfo.py
@@ -4,7 +4,7 @@
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class ProductSupplierinfo(models.Model):
