3ee7244a4380a45e93f26d6101557341ed4119d9
Benjamin Willig <benjamin.willig@acsone.eu>
2017-11-03 10:54:32 +0100

    [FIX] Avoid negative quantities to invoice

--- purchase_order_line_invoicing/wizards/purchase_order_line_invoicing_wizard.py
+++ purchase_order_line_invoicing/wizards/purchase_order_line_invoicing_wizard.py
@@ -30,8 +30,8 @@ class PurchaseOrderLineInvoiceWizard(models.TransientModel):
             'price_subtotal': purchase_line.price_subtotal,
             'qty_received': purchase_line.qty_received,
             'qty_invoiced': purchase_line.qty_invoiced,
-            'invoice_qty':
-                purchase_line.qty_received - purchase_line.qty_invoiced,
+            'invoice_qty': max(
+                purchase_line.qty_received - purchase_line.qty_invoiced, 0),
         }
 
     @api.model
