b2de5266ec55e234a0a5474da24da7d3164bf4d2
Dave Lasley <dave@laslabs.com>
2017-11-03 17:11:53 -0700

    [ADD] purchase_line_reference: Abstract reference column * New module to add an abstract reference column to `purchase.order.line` allowing for a direct back reference to the record it represents.

---
+++ purchase_line_reference/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Purchase Line Reference',
+    'summary': 'Adds an abstract reference to purchase order lines.',
+    'version': '10.0.1.0.0',
+    'category': 'Purchase',
+    'website': 'https://laslabs.com/',
+    'author': 'LasLabs, Odoo Community Association (OCA)',
+    'license': 'LGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'purchase',
+    ],
+}
---
+++ purchase_line_reference/models/purchase_order_line.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo import api, fields, models
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    reference = fields.Reference(
+        selection='_get_references',
+        help='Link to the record that this purchase order line directly '
+             'represents.',
+    )
+
+    @api.model
+    def _get_references(self):
+        all_models = self.env['ir.model'].search([])
+        return [(m.model, m.description) for m in all_models]
