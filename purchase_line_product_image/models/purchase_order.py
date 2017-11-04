d6e17f79a1126bd00db09dfac9eea1d55dfa39a7
Lucky Kurniawan <kurniawanluckyy@gmail.com>
2017-11-04 14:19:46 +0800

    purchase_line_product_image: * fix product_image

--- purchase_line_product_image/models/purchase_order.py
+++ purchase_line_product_image/models/purchase_order.py
@@ -8,8 +8,4 @@ from odoo import fields, models
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    product_image = fields.Binary(
-        "image", related='product_id.product_tmpl_id.image_medium',
-        help="Small-sized image of the product. It is automatically "
-             "resized as a 64x64px image, with aspect ratio preserved. "
-             "Use this field anywhere a small image is required.")
+    product_image = fields.Binary(related='product_id.product_tmpl_id.image_medium')
