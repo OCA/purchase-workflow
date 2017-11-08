4fc04eaf46845ecf9f0148a770dcc7efe5609058
Vo Minh Bao Hieu <hieu.vmb@komit-consulting.com>
2017-11-08 14:25:04 +0700

    [MIG] purchase_allowed_product: Migration to 11.0

--- purchase_allowed_product/__manifest__.py
+++ purchase_allowed_product/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2016 Chafique DELLI @ Akretion
 # © 2017 Today Mourad EL HADJ MIMOUNE @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -6,9 +5,9 @@
     'name': 'Account Invoice Allowed Product',
     'summary': 'This module allows to select only products that can be '
                'supplied by the supplier',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Accounting & Finance',
-    'website': 'https://akretion.com',
+    'website': 'https://github.com/OCA/purchase-workflow',
     'author': 'Akretion, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'installable': True,
