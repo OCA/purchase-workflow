5de9a81aa8887e712020c61e5704918aecb5952d
rgarnau <rgarnau20@gmail.com>
2017-11-14 15:14:28 +0100

    [MIG] purchase_minimum_amount: Migration to 11.0

--- purchase_minimum_amount/__init__.py
+++ purchase_minimum_amount/__init__.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Eficent Business and IT Consulting Services S.L.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
 
 from . import models
