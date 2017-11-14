e53d2b7970106b87ff8643b49625255885674d17
rgarnau <rgarnau20@gmail.com>
2017-11-14 14:53:50 +0100

    [MIG] purchase_order_block: Migration to 11.0

--- purchase_order_approval_block/models/purchase_approval_block_reason.py
+++ purchase_order_approval_block/models/purchase_approval_block_reason.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
-# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
 
