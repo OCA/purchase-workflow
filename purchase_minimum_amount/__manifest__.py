d327ed9d838f9c1d1dbabdb544b28fc6b9f152f4
rgarnau <rgarnau20@gmail.com>
2017-11-14 15:14:28 +0100

    [MIG] purchase_minimum_amount: Migration to 11.0

--- purchase_minimum_amount/__manifest__.py
+++ purchase_minimum_amount/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Eficent Business and IT Consulting Services S.L.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
 
 {
     "name": "Purchase Minimum Amount",
     "author": "Eficent, Odoo Community Association (OCA)",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "website": "https://github.com/OCA/purchase-workflow",
     "depends": [
         'purchase',
