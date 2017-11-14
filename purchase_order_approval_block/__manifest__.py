57902025eaaedd492a64d7db4459766326e9040f
rgarnau <rgarnau20@gmail.com>
2017-11-14 14:53:50 +0100

    purchase_order_block: Migration to 11.0

--- purchase_order_approval_block/__manifest__.py
+++ purchase_order_approval_block/__manifest__.py
@@ -1,18 +1,18 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
-# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Purchase Order Approval Block",
-    "author": "Eficent, SerpentCS, Odoo Community Association (OCA)",
-    "version": "10.0.1.0.0",
+    "author": "Eficent, Odoo Community Association (OCA)",
+    "version": "11.0.1.0.0",
     "category": "Purchase Management",
     "website": "https://github.com/OCA/purchase-workflow",
     "depends": [
         'purchase',
+        'purchase_exception',
     ],
     "data": [
+        'data/purchase_exception_data.xml',
         'security/ir.model.access.csv',
         'security/purchase_order_approval_block_security.xml',
         'views/purchase_approval_block_reason_view.xml',
