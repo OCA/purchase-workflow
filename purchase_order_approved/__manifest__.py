19dc148949ddc371766367c555132bf24a97f7ad
Lois Rilo <lois.rilo@eficent.com>
2018-01-22 12:59:10 +0100

    [10.0][MIG] purcahse_order_approved

--- purchase_order_approved/__manifest__.py
+++ purchase_order_approved/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Purchase Order Approved",
     "summary": "Add a new state 'Approved' in purchase orders.",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Purchases",
     "website": "https://odoo-community.org/",
     "author": "Eficent, Odoo Community Association (OCA)",
@@ -16,6 +16,6 @@
     ],
     "data": [
         "views/purchase_order_view.xml",
-        "views/res_company_view.xml",
+        "views/res_config_view.xml",
     ],
 }
