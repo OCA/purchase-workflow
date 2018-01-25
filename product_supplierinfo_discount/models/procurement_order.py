36cbbf2dfd91b84faf75035c210de48b57d2709f
apesquero <apesquero@gmail.com>
2018-01-25 17:42:33 +0100

    Update product_supplierinfo_discount

--- product_supplierinfo_discount/models/procurement_order.py
+++ product_supplierinfo_discount/models/procurement_order.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import models, api
+from odoo import models, api
 
 
 class ProcurementOrder(models.Model):
