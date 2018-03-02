be5ae43ea2cfcb526ae01be8e993e38864af1d03
Sylvain Van Hoof <sylvain@okia.be>
2018-03-02 17:29:56 +0100

    Add the module purchase_cancel_reason. Copy/past from the module sale_cancel_reason (OCA/sale-workflow) developed by CampToCamp. (#438)
    * [ADD] Add the module purchase_cancel_reason. Copy/past from the module sale_cancel_reason (OCA/sale-workflow) developed by CampToCamp.

---
+++ purchase_cancel_reason/wizard/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2013 Guewen Baconnier, Camptocamp SA
+# Copyright 2017 Okia SPRL
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import purchase_cancel_reason
