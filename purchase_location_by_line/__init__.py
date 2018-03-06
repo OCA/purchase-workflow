f30540e46d728ffb360932ca3a040f27922eed14
Hizbul Bahar <hizbul.ku@gmail.com>
2018-03-06 22:41:38 +0600

    [MIG] purchase_location_by_line: Migration to 11.0

---
+++ purchase_location_by_line/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# © 2018 Hizbul Bahar <hizbul25@gmail.com>
+#   (<http://www.eficent.com>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from . import models
---
+++ purchase_location_by_line/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+#   (<http://www.eficent.com>)
+# © 2018 Hizbul Bahar <hizbul25@gmail.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from . import purchase
+from . import stock_picking
