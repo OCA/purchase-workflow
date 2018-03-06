19d224a23e35b5820bed237e2970eada674a3007
Lois Rilo <lois.rilo@eficent.com>
2018-03-06 11:26:39 +0100

    [10.0][IMP] purchase_open_qty: add filter to po lines

--- purchase_open_qty/__manifest__.py
+++ purchase_open_qty/__manifest__.py
@@ -7,7 +7,7 @@
     "name": "Purchase Open Qty",
     "summary": "Allows to identify the purchase orders that have quantities "
                "pending to invoice or to receive.",
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.1.0",
     "author": "Eficent, "
               "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
