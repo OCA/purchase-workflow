20f7c30fbafb93a2690f4186b3c2ad0aff229db1
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-03-10 17:52:01 +0100

    [IMP] purchase_discount: Switch tests to HttpCase + post_install to avoid freezes
    When performing tests in combination with other tests that prints reports,
    SavepointCase or TransactionCase tests freeze due to a block in DB.

--- purchase_discount/tests/test_purchase_discount.py
+++ purchase_discount/tests/test_purchase_discount.py
@@ -1,64 +1,68 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
-# Copyright 2015-2017 Tecnativa - Pedro M. Baeza
+# Copyright 2015-2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import odoo.tests.common as common
 from odoo import fields
 
 
-class TestPurchaseOrder(common.SavepointCase):
-    @classmethod
-    def setUpClass(cls):
-        super(TestPurchaseOrder, cls).setUpClass()
-        cls.product_1 = cls.env['product.product'].create({
+class TestPurchaseOrder(common.HttpCase):
+    at_install = False
+    post_install = False
+
+    def setUp(self):
+        super(TestPurchaseOrder, self).setUp()
+        self.company = self.env.user.company_id
+        self.company.tax_calculation_rounding_method = 'round_per_line'
+        self.product_1 = self.env['product.product'].create({
             'name': 'Test product 1',
         })
-        cls.product_2 = cls.env['product.product'].create({
+        self.product_2 = self.env['product.product'].create({
             'name': 'Test product 2',
         })
-        po_model = cls.env['purchase.order.line']
+        po_model = self.env['purchase.order.line']
         # Make sure currency is EUR for not having troubles with rates
-        cls.env.user.company_id.currency_id = cls.env.ref('base.EUR')
-        cls.purchase_order = cls.env['purchase.order'].create({
-            'partner_id': cls.env.ref('base.res_partner_3').id,
+        self.env.user.company_id.currency_id = self.env.ref('base.EUR')
+        self.purchase_order = self.env['purchase.order'].create({
+            'partner_id': self.env.ref('base.res_partner_3').id,
         })
-        cls.po_line_1 = po_model.create({
-            'order_id': cls.purchase_order.id,
-            'product_id': cls.product_1.id,
+        self.po_line_1 = po_model.create({
+            'order_id': self.purchase_order.id,
+            'product_id': self.product_1.id,
             'date_planned': fields.Datetime.now(),
             'name': 'Test',
             'product_qty': 1.0,
-            'product_uom': cls.product_1.uom_id.id,
+            'product_uom': self.product_1.uom_id.id,
             'discount': 50.0,
             'price_unit': 10.0,
         })
-        cls.tax = cls.env['account.tax'].create({
+        self.tax = self.env['account.tax'].create({
             'name': 'Sample tax 15%',
             'amount_type': 'percent',
             'type_tax_use': 'purchase',
             'amount': 15.0,
         })
-        cls.po_line_2 = po_model.create({
-            'order_id': cls.purchase_order.id,
-            'product_id': cls.product_2.id,
+        self.po_line_2 = po_model.create({
+            'order_id': self.purchase_order.id,
+            'product_id': self.product_2.id,
             'date_planned': fields.Datetime.now(),
             'name': 'Test',
             'product_qty': 10.0,
-            'product_uom': cls.product_2.uom_id.id,
+            'product_uom': self.product_2.uom_id.id,
             'discount': 30,
-            'taxes_id': [(6, 0, [cls.tax.id])],
+            'taxes_id': [(6, 0, [self.tax.id])],
             'price_unit': 230.0,
         })
-        cls.po_line_3 = po_model.create({
-            'order_id': cls.purchase_order.id,
-            'product_id': cls.product_2.id,
+        self.po_line_3 = po_model.create({
+            'order_id': self.purchase_order.id,
+            'product_id': self.product_2.id,
             'date_planned': fields.Datetime.now(),
             'name': 'Test',
             'product_qty': 1.0,
-            'product_uom': cls.product_2.uom_id.id,
+            'product_uom': self.product_2.uom_id.id,
             'discount': 0,
-            'taxes_id': [(6, 0, [cls.tax.id])],
+            'taxes_id': [(6, 0, [self.tax.id])],
             'price_unit': 10.0,
         })
 
@@ -82,3 +86,9 @@ class TestPurchaseOrder(common.SavepointCase):
         ])
         self.assertEqual(rec.price_total, 5)
         self.assertEqual(rec.discount, 50)
+
+
+class TestPurchaseOrderRoundGlobally(TestPurchaseOrder):
+    def setUp(self):
+        super(TestPurchaseOrderRoundGlobally, self).setUp()
+        self.company.tax_calculation_rounding_method = 'round_globally'
