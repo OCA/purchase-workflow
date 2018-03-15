68b5a4721dae96e618b50db60f5704d846a0a0a3
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2018-03-15 09:14:36 +0100

    [IMP] computed field implementation
    improve the search method on the field to handle the != operator
    improve the search method on the field to handle searching on account name

--- purchase_order_analytic_search/README.rst
+++ purchase_order_analytic_search/README.rst
@@ -50,6 +50,7 @@ Contributors
 
 * Gisela Mora <gisela.mora@eficent.com>
 * Serpent Consulting Services Pvt. Ltd. <support@serpentcs.com>
+* Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
 
 Maintainer
 ----------
--- purchase_order_analytic_search/models/purchase_order.py
+++ purchase_order_analytic_search/models/purchase_order.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2015 Eficent Business and IT Consulting Services S.L.
+# Copyright 2018 Camptocamp
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from odoo import api, fields, models
@@ -12,24 +13,36 @@ class PurchaseOrder(models.Model):
     @api.multi
     @api.depends('order_line.account_analytic_id')
     def _compute_analytic_accounts(self):
-        res = {}
         for purchase in self:
-            res[purchase.id] = []
-            for po_line in purchase.order_line:
-                if po_line.account_analytic_id:
-                    res[purchase.id].append(po_line.account_analytic_id.id)
-        return res
+            purchase.account_analytic_ids = purchase.mapped(
+                'order_line.account_analytic_id'
+            )
 
     @api.model
     def _search_analytic_accounts(self, operator, value):
         po_line_obj = self.env['purchase.order.line']
-        res = []
-        po_lines = po_line_obj.search(
-            [('account_analytic_id', operator, value)])
-        order_ids = [po_line.order_id and po_line.order_id.id for po_line in
-                     po_lines]
-        res.append(('id', 'in', order_ids))
-        return res
+        if not value:
+            return [('id', '=', False)]
+        if isinstance(value, (tuple, list)):
+            # we are searching on a list of ids
+            domain = [('order_id', '!=', False),
+                      ('account_analytic_id', 'in', value)]
+        else:
+            if isinstance(value, int):
+                # we are searching on the id of the analytic_account
+                domain = [('order_id', '!=', False),
+                          ('account_analytic_id', '=', value)]
+            else:
+                # assume we are searching on the analytic account name
+                domain = [('order_id', '!=', False),
+                          ('account_analytic_id.name', 'like', value),
+                          ]
+        po_lines = po_line_obj.search(domain)
+        orders = po_lines.mapped('order_id')
+        if operator in ('!=', 'not in'):
+            return [('id', 'not in', orders.ids)]
+        else:
+            return [('id', 'in', orders.ids)]
 
     account_analytic_ids = fields.Many2many(
         comodel_name='account.analytic.account',
