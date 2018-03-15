bce57c555f8461903f7fb7299b307ead29ebb8b8
aheficent <ahenriquez@eficent.com>
2018-03-15 12:04:05 +0100

    [IMP]update code to new style. Add AA to tree view

--- purchase_order_analytic_search/models/purchase_order.py
+++ purchase_order_analytic_search/models/purchase_order.py
@@ -12,22 +12,18 @@ class PurchaseOrder(models.Model):
     @api.multi
     @api.depends('order_line.account_analytic_id')
     def _compute_analytic_accounts(self):
-        res = {}
         for purchase in self:
-            res[purchase.id] = []
-            for po_line in purchase.order_line:
-                if po_line.account_analytic_id:
-                    res[purchase.id].append(po_line.account_analytic_id.id)
-        return res
+            purchase.account_analytic_ids = purchase.mapped(
+                "order_line.account_analytic_id").ids
 
     @api.model
     def _search_analytic_accounts(self, operator, value):
         po_line_obj = self.env['purchase.order.line']
         res = []
         po_lines = po_line_obj.search(
-            [('account_analytic_id', operator, value)])
-        order_ids = [po_line.order_id and po_line.order_id.id for po_line in
-                     po_lines]
+            [('order_id', '!=', False),
+             ('account_analytic_id', operator, value)])
+        order_ids = po_lines.mapped('order_id').ids
         res.append(('id', 'in', order_ids))
         return res
 
