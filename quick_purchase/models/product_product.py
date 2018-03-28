f5ea58ae999a1e98ea9deee770721ce477993b16
David Beal <david.beal@akretion.com>
2018-03-28 18:01:14 +0200

    PORT v10

--- quick_purchase/models/product_product.py
+++ quick_purchase/models/product_product.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
 class ProductProduct(models.Model):
