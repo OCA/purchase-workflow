edeb40ae4d592dc947703fac3ab61450e5ef7092
Lucky Kurniawan <kurniawanluckyy@gmail.com>
2018-03-31 10:17:28 +0800

    fix travis

--- purchase_barcode/static/src/js/purchase_barcode_handler.js
+++ purchase_barcode/static/src/js/purchase_barcode_handler.js
@@ -3,9 +3,8 @@ odoo.define('purchase_order_barcode.PurchaseBarcodeHandler', function (require) 
     var core = require('web.core');
     var Model = require('web.Model');
     var FormViewBarcodeHandler = require('barcodes.FormViewBarcodeHandler');
-    var _t = core._t;
     var PurchaseBarcodeHandler = FormViewBarcodeHandler.extend({
-        init: function (parent, context) {
+        init: function (parent) {
             if (parent.ViewManager.action) {
                 this.form_view_initial_mode = parent.ViewManager.action.context.form_view_initial_mode;
             } else if (parent.ViewManager.view_form) {
@@ -26,11 +25,9 @@ odoo.define('purchase_order_barcode.PurchaseBarcodeHandler', function (require) 
         on_barcode_scanned: function(barcode) {
             var self = this;
             var po_id = self.view.datarecord.id
-            var po_line_ids = self.view.datarecord.order_line
             self.po_model.call('po_barcode',[barcode, po_id]).then(function () {
                 self.getParent().reload();
             });
-
         },
     });
     core.form_widget_registry.add('purchase_barcode_handler', PurchaseBarcodeHandler);
