bbaf4b217a03736e98f515ef4883f9855b3f6902
Lucky Kurniawan <kurniawanluckyy@gmail.com>
2018-04-16 21:17:36 +0800

    purchase_barcode: * fix

--- purchase_barcode/models/purchase_order.py
+++ purchase_barcode/models/purchase_order.py
@@ -16,25 +16,15 @@ class PurchaseOrder(models.Model):
 
     @api.model
     def po_barcode(self, barcode, po_id):
-        purchase_order = self.search([
-            ('id', '=', po_id)
-        ])
+        purchase_order = self.search([('id', '=', po_id)])
         if not purchase_order:
             # with asumtation Purchase Order is created
-            raise UserError(_(
-                'Please Choose Your Vendor And Fix Your Purchase Order'
-            ))
-        product_id = self.env['product.product'].search([
-            ('barcode', '=', barcode)
-        ])
-        purchase_order_line = purchase_order.order_line.search([
-            ('product_id', '=', product_id.id)
-        ], limit=1)
+            raise UserError(_('Please Choose Your Vendor And Fix Your Purchase Order'))
+        product_id = self.env['product.product'].search([('barcode', '=', barcode)])
+        purchase_order_line = purchase_order.order_line.search([('product_id', '=', product_id.id)], limit=1)
         if purchase_order_line:
-            product_qty = purchase_order_line.product_qty + 1
-            purchase_order_line.product_qty = product_qty
+            purchase_order_line.product_qty = purchase_order_line.product_qty + 1
         else:
-            date = datetime.today().strftime(DEFAULT_SERVER_DATETIME_FORMAT)
             line_values = {
                 'name': product_id.name,
                 'product_id': product_id.id,
@@ -42,6 +32,5 @@ class PurchaseOrder(models.Model):
                 'product_uom': product_id.product_tmpl_id.uom_id.id,
                 'price_unit': product_id.product_tmpl_id.list_price,
                 'order_id': purchase_order.id,
-                'date_planned': date,
-            }
+                'date_planned': datetime.today().strftime(DEFAULT_SERVER_DATETIME_FORMAT)}
             purchase_order.update({'order_line': [(0, 0, line_values)]})
