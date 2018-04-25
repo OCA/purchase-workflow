0efca14720e30d99945c2d029d850aae5c325d58
hveficent <hector.villarreal@eficent.com>
2018-04-25 17:27:35 +0200

    [MIG] purchase_request: Migration to 11.0 merge with purchase_request_procurement and purchase_request_to_rfq

---
+++ purchase_request/models/product_template.py
@@ -0,0 +1,15 @@
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    purchase_request = fields.Boolean(
+        string='Purchase Request',
+        help="Check this box to generate purchase request instead of "
+             "generating requests for quotation from procurement.",
+        default=False,
+    )
