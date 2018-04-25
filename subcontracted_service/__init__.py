f098ce313be6936c90f3f94b468c5ff481fcc126
hveficent <hector.villarreal@eficent.com>
2018-04-25 15:49:51 +0200

    [MIG] subcontracted_service: Migration to 11.0

--- subcontracted_service/__init__.py
+++ subcontracted_service/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 
 from . import models
 from .init_hook import post_init_hook
--- subcontracted_service/models/product.py
+++ subcontracted_service/models/product.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Author: Damien Crier
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
@@ -12,15 +11,3 @@ class ProductTemplate(models.Model):
     property_subcontracted_service = fields.Boolean(
         string="Subcontracted Service",
         company_dependent=True)
-
-
-class ProductProduct(models.Model):
-    _inherit = 'product.product'
-
-    @api.multi
-    def _need_procurement(self):
-        for product in self:
-            if (product.type == 'service' and
-                    product.property_subcontracted_service):
-                return True
-        return super(ProductProduct, self)._need_procurement()
