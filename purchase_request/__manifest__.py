503ed13ce199da6cc87869fc2db655cb17848e33
hveficent <hector.villarreal@eficent.com>
2018-04-25 17:27:35 +0200

    [MIG] purchase_request: Migration to 11.0 merge with purchase_request_procurement and purchase_request_to_rfq

--- purchase_request/__manifest__.py
+++ purchase_request/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
 {
     "name": "Purchase Request",
-    "author": "Eficent Business and IT Consulting Services S.L., "
+    "author": "Eficent, "
               "Odoo Community Association (OCA)",
-    "version": "10.0.1.1.0",
+    "version": "11.0.1.0.0",
     "summary": "Use this module to have notification of requirements of "
                "materials and/or external services and keep track of such "
                "requirements.",
@@ -20,13 +19,17 @@
         "security/ir.model.access.csv",
         "data/purchase_request_sequence.xml",
         "data/purchase_request_data.xml",
+        "reports/report_purchase_request.xml",
         "views/purchase_request_view.xml",
-        "reports/report_purchaserequests.xml",
         "views/purchase_request_report.xml",
+        "views/product_template.xml",
+        "views/purchase_order_view.xml",
+        "wizard/purchase_request_line_make_purchase_order_view.xml",
     ],
     'demo': [
         "demo/purchase_request_demo.xml",
     ],
     "license": 'LGPL-3',
-    'installable': True
+    'installable': True,
+    'application': True,
 }
---
+++ purchase_request/readme/CONTRIBUTORS.rst
@@ -0,0 +1,6 @@
+* Jordi Ballester Alomar <jordi.ballester@eficent.com>
+* Jonathan Nemry <jonathan.nemry@acsone.eu>
+* Aaron Henriquez <ahenriquez@eficent.com>
+* Adrien Peiffer <adrien.peiffer@acsone.eu>
+* Lois Rilo <lois.rilo@eficent.com>
+* Héctor Villarreal <hector.villarreal@eficent.com>
