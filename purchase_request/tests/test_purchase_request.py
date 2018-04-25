b15e1623aa0078d8f056c8252be7651a960f1a1a
hveficent <hector.villarreal@eficent.com>
2018-04-25 17:27:35 +0200

    [MIG] purchase_request: Migration to 11.0 merge with purchase_request_procurement and purchase_request_to_rfq

--- purchase_request/tests/test_purchase_request.py
+++ purchase_request/tests/test_purchase_request.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
 from odoo.exceptions import UserError
