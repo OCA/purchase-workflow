a1bd37a3ba4e0f696689e3b21aa528a1cdb83440
hveficent <hector.villarreal@eficent.com>
2018-04-25 15:49:51 +0200

    [MIG] subcontracted_service: Migration to 11.0

--- subcontracted_service/models/procurement.py
+++ subcontracted_service/models/procurement.py
@@ -1,36 +1,26 @@
-# -*- coding: utf-8 -*-
-# Author: Damien Crier
-# Copyright 2017 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
+# # Author: Damien Crier
+# # Copyright 2017 Camptocamp SA
+# # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+#
 from odoo import api, models
 
 
-class ProcurementOrder(models.Model):
-    _inherit = "procurement.order"
+class ProcurementGroup(models.Model):
+    _inherit = "procurement.group"
 
-    @api.multi
-    def _is_subcontracted_service(self):
-        self.ensure_one()
-        return (self.product_id.type == 'service' and
-                self.product_id.property_subcontracted_service or
+    @api.model
+    def _is_subcontracted_service(self, product_id):
+        return (product_id.type == 'service' and
+                product_id.property_subcontracted_service or
                 False)
 
-    @api.multi
-    def _find_suitable_rule(self):
-        res = super(ProcurementOrder, self)._find_suitable_rule()
-        if self._is_subcontracted_service():
-            return (
-                self.warehouse_id.subcontracting_service_proc_rule_id.id
-            )
-        return res
-
-    @api.multi
-    def _assign(self):
-        res = super(ProcurementOrder, self)._assign()
+    @api.model
+    def _get_rule(self, product_id, location_id, values):
+        res = super()._get_rule(product_id, location_id, values)
         if not res:
-            rule_id = self._find_suitable_rule()
-            if rule_id:
-                self.write({'rule_id': rule_id})
-                return True
+            if self._is_subcontracted_service(product_id):
+                rule_id = location_id.get_warehouse().\
+                    subcontracting_service_proc_rule_id
+                if rule_id:
+                    return rule_id
         return res
