567ec9198831755bb339d053b8ccf03dc386f105
Lois Rilo <lois.rilo@eficent.com>
2018-05-09 17:06:30 +0200

    [11.0][MIG] purchase_tier_validation

--- purchase_tier_validation/__manifest__.py
+++ purchase_tier_validation/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Purchase Tier Validation",
     "summary": "Extends the functionality of Purchase Orders to "
                "support a tier validation process.",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Purchases",
     "website": "https://github.com/OCA/purchase-workflow",
     "author": "Eficent, Odoo Community Association (OCA)",
