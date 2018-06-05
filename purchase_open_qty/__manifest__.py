38fec383306b3fe4d23d3f5b527d6af440ebeb0c
andreas <andreasdian@portcities.net>
2018-06-05 15:25:38 +0700

    [MIG] purchase_open_qty: Migration to 11.0

--- purchase_open_qty/__manifest__.py
+++ purchase_open_qty/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -7,7 +6,7 @@
     "name": "Purchase Open Qty",
     "summary": "Allows to identify the purchase orders that have quantities "
                "pending to invoice or to receive.",
-    "version": "10.0.1.1.0",
+    "version": "11.0.1.1.0",
     "author": "Eficent, "
               "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
