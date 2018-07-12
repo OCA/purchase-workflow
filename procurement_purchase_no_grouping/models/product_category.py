4622c9fa23d9166bd6cab73cd67b57cdfa0ca1a4
Carlos Dauden <carlos.dauden@tecnativa.com>
2018-07-12 20:27:08 +0200

    [MIG] procurement_purchase_no_grouping: Migration to 11.0

--- procurement_purchase_no_grouping/models/product_category.py
+++ procurement_purchase_no_grouping/models/product_category.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 AvanzOsc (http://www.avanzosc.es)
 # Copyright 2015-2016 - Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
