d0158e5e9d6fdb369d69ce14fd466f5c21e94c68
Carlos Dauden <carlos.dauden@tecnativa.com>
2018-07-12 20:27:08 +0200

    [MIG] procurement_purchase_no_grouping: Migration to 11.0

--- procurement_purchase_no_grouping/models/purchase_order.py
+++ procurement_purchase_no_grouping/models/purchase_order.py
@@ -1,39 +1,19 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 AvanzOsc (http://www.avanzosc.es)
-# Copyright 2015-2016 - Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2015-2016 Tecnativa - Pedro M. Baeza
+# Copyright 2018 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, models
-
-
-class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
-
-    @api.model
-    def search(self, args, offset=0, limit=None, order=None, count=False):
-        make_po_conditions = {
-            'partner_id', 'state', 'picking_type_id', 'company_id',
-            'dest_address_id',
-        }
-        # Restrict the empty return for these conditions
-        if (self.env.context and
-                self.env.context.get('grouping', 'standard') == 'order' and
-                make_po_conditions.issubset(set(x[0] for x in args))):
-            return self.browse()
-        return super(PurchaseOrder, self).search(
-            args, offset=offset, limit=limit, order=order, count=count)
+from odoo import models
 
 
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    @api.model
-    def search(self, args, offset=0, limit=None, order=None, count=False):
-        # Restrict the empty return for these conditions
-        if (self.env.context and
-                self.env.context.get('grouping', 'standard') == 'line' and
-                len(args) == 1 and args[0][0] == 'order_id' and
-                args[0][1] == 'in'):
-            return self.browse()
-        return super(PurchaseOrderLine, self).search(
-            args, offset=offset, limit=limit, order=order, count=count)
+    def _merge_in_existing_line(self, product_id, product_qty, product_uom,
+                                location_id, name, origin, values):
+        grouping = product_id.categ_id.procured_purchase_grouping
+        if grouping == 'line':
+            return False
+        return super()._merge_in_existing_line(
+            product_id, product_qty, product_uom, location_id, name, origin,
+            values)
---
+++ procurement_purchase_no_grouping/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module allows to not group generated purchase orders from procurements.
+The grouping behaviour can be configurable at product category level.
