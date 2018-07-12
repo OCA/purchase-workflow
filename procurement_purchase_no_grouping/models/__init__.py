e8ddafbbb6f13ef1aa41a272aeb85d5322da7ea8
Carlos Dauden <carlos.dauden@tecnativa.com>
2018-07-12 20:27:08 +0200

    [MIG] procurement_purchase_no_grouping: Migration to 11.0

--- procurement_purchase_no_grouping/models/__init__.py
+++ procurement_purchase_no_grouping/models/__init__.py
@@ -2,5 +2,5 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from . import product_category
-from . import procurement_order
+from . import procurement_rule
 from . import purchase_order
