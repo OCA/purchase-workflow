2b260ac02bbf13fa8a603e4ef768f8ef34ff6a66
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-07-18 14:05:21 +0200

    [Fix] Use PO line procurement group on generated stock moves

--- purchase_line_procurement_group/models/purchase.py
+++ purchase_line_procurement_group/models/purchase.py
@@ -1,6 +1,6 @@
 # Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class PurchaseOrderLine(models.Model):
@@ -16,3 +16,10 @@ class PurchaseOrderLine(models.Model):
             return False
         super()._merge_in_existing_line(product_id, product_qty, product_uom,
                                         location_id, name, origin, values)
+
+    @api.multi
+    def _prepare_stock_moves(self, picking):
+        res = super()._prepare_stock_moves(picking)
+        res[0]['group_id'] = (
+            self.procurement_group_id.id or self.order_id.group_id.id)
+        return res
