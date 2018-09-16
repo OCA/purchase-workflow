860c7d2c6e1ebaee75cdd3cdae739373c3f169af
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-09-16 18:32:36 +0200

    [FIX+IMP] procurement_purchase_no_grouping: Fix tests + PO grouping + README

--- procurement_purchase_no_grouping/tests/test_procurement_purchase_no_grouping.py
+++ procurement_purchase_no_grouping/tests/test_procurement_purchase_no_grouping.py
@@ -1,6 +1,7 @@
 # Copyright 2015-2017 - Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
+from odoo import fields
 from odoo.tests import common
 
 
@@ -24,69 +25,66 @@ class TestProcurementPurchaseNoGrouping(common.SavepointCase):
                 }),
             ]}
         )
-        cls.procurement = cls.env['procurement.order'].create({
-            'name': 'Procurement test',
-            'product_id': cls.product.id,
-            'product_uom': cls.product.uom_id.id,
-            'warehouse_id': cls.env.ref('stock.warehouse0').id,
-            'location_id': cls.env.ref('stock.stock_location_stock').id,
-            'route_ids': [
-                (6, 0, [cls.env.ref('purchase.route_warehouse0_buy').id]),
-            ],
-            'product_qty': 1.0,
+        # FIXME: Core doesn't find correctly supplier if this is not set
+        cls.product.seller_ids.product_id = cls.product.id
+        cls.location = cls.env.ref('stock.stock_location_stock')
+        cls.picking_type = cls.env.ref('stock.picking_type_in')
+        cls.origin = 'Test procurement_purchase_no_grouping'
+        cls.stock_location_route = cls.env.ref('purchase.route_warehouse0_buy')
+        cls.procurement_rule = cls.stock_location_route.pull_ids[0]
+        cls.values = {
+            # FIXME: Core doesn't find correctly supplier if not recordset
+            'company_id': cls.env.user.company_id,
+            'date_planned': fields.Datetime.now(),
         }
+
+    def _run_procurement(self):
+        self.procurement_rule._run_buy(
+            self.product, 1, self.product.uom_id, self.location, False,
+            self.origin, self.values,
         )
 
     def test_procurement_grouped_purchase(self):
         self.category.procured_purchase_grouping = 'standard'
-        procurement_1 = self.procurement.copy()
-        procurement_2 = self.procurement.copy()
-        procurement_1.run()
-        procurement_2.run()
-        self.assertTrue(procurement_1.purchase_id)
-        self.assertTrue(procurement_2.purchase_id)
+        self._run_procurement()
+        self._run_procurement()
+        orders = self.env['purchase.order'].search([
+            ('origin', '=', self.origin),
+        ])
         self.assertEqual(
-            procurement_1.purchase_id, procurement_2.purchase_id,
-            'Procured purchase orders are not the same',
+            len(orders), 1, 'Procured purchase orders are not the same',
         )
         self.assertEqual(
-            procurement_1.purchase_line_id, procurement_2.purchase_line_id,
+            len(orders.order_line), 1,
             'Procured purchase orders lines are not the same',
         )
-        return True
 
     def test_procurement_no_grouping_line_purchase(self):
         self.category.procured_purchase_grouping = 'line'
-        procurement_1 = self.procurement.copy()
-        procurement_2 = self.procurement.copy()
-        procurement_1.run()
-        procurement_2.run()
-        self.assertTrue(procurement_1.purchase_id)
-        self.assertTrue(procurement_2.purchase_id)
+        self._run_procurement()
+        self._run_procurement()
+        orders = self.env['purchase.order'].search([
+            ('origin', '=', self.origin),
+        ])
         self.assertEqual(
-            procurement_1.purchase_id, procurement_2.purchase_id,
-            'Procured purchase orders are not the same',
+            len(orders), 1, 'Procured purchase orders are not the same',
         )
-        self.assertNotEqual(
-            procurement_1.purchase_line_id, procurement_2.purchase_line_id,
+        self.assertEqual(
+            len(orders.order_line), 2,
             'Procured purchase orders lines are the same',
         )
-        return True
 
     def test_procurement_no_grouping_order_purchase(self):
         self.category.procured_purchase_grouping = 'order'
-        procurement_1 = self.procurement.copy()
-        procurement_2 = self.procurement.copy()
-        procurement_1.run()
-        procurement_2.run()
-        self.assertTrue(procurement_1.purchase_id)
-        self.assertTrue(procurement_2.purchase_id)
-        self.assertNotEqual(
-            procurement_1.purchase_id, procurement_2.purchase_id,
-            'Procured purchase orders are the same',
+        self._run_procurement()
+        self._run_procurement()
+        orders = self.env['purchase.order'].search([
+            ('origin', '=', self.origin),
+        ])
+        self.assertEqual(
+            len(orders), 2, 'Procured purchase orders are the same',
         )
-        self.assertNotEqual(
-            procurement_1.purchase_line_id, procurement_2.purchase_line_id,
+        self.assertEqual(
+            len(orders.mapped('order_line')), 2,
             'Procured purchase orders lines are the same',
         )
-        return True
