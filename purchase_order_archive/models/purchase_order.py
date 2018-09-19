27fde4604adb6705c5b64504950d1d4b8677e919
Andrea <a.stirpe@onestein.nl>
2018-09-19 12:12:51 +0200

    [11.0][ADD] purchase_order_archive

---
+++ purchase_order_archive/models/purchase_order.py
@@ -0,0 +1,10 @@
+# Copyright 2017-2018 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    active = fields.Boolean(default=True)
