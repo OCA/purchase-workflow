10b8c96d33e4ddea79696fb9ded6e378a20d0880
cubells <vicent@vcubells.net>
2018-09-25 12:00:50 +0200

    [MIG] product_supplierinfo_discount: Migration to 11.0

--- product_supplierinfo_discount/__manifest__.py
+++ product_supplierinfo_discount/__manifest__.py
@@ -1,19 +1,18 @@
-# -*- coding: utf-8 -*-
-# © 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
+# Copyright 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
 #        Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-# © 2016 ACSONE SA/NV (<http://acsone.eu>)
-# © 2016 GRAP (http://www.grap.coop)
+# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2016 GRAP (http://www.grap.coop)
 #        Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     "name": "Discounts in product supplier info",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Tecnativa, "
               "GRAP, "
               "Odoo Community Association (OCA)",
     "category": "Purchase Management",
-    "website": "http://www.serviciosbaeza.com/",
+    "website": "https://github.com/OCA/purchase-workflow",
     "license": "AGPL-3",
     "depends": [
         'product',
