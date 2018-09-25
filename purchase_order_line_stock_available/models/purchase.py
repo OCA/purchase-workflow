498e33f22612e597130c760514eca26e81b4b933
cubells <vicent@vcubells.net>
2018-09-25 12:43:42 +0200

    [MIG] purchase_order_line_stock_available: Migration to 11.0

--- purchase_order_line_stock_available/models/purchase.py
+++ purchase_order_line_stock_available/models/purchase.py
@@ -1,8 +1,10 @@
-
-from openerp import models, fields
+from odoo import fields, models
 
 
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    virtual_available = fields.Float(related='product_id.virtual_available')
+    virtual_available = fields.Float(
+        related='product_id.virtual_available',
+        readonly=True,
+    )
