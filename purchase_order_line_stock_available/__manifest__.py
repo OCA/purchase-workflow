be145c03bf9126a5bebcfc1b2b4ee2b6e414606c
cubells <vicent@vcubells.net>
2018-09-25 12:43:42 +0200

    [MIG] purchase_order_line_stock_available: Migration to 11.0

---
+++ purchase_order_line_stock_available/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2015 AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase order line stock available",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "OdooMRP team,"
+              "AvanzOSC,"
+              "Tecnativa",
+    'website': "https://github.com/OCA/purchase-workflow",
+    "depends": [
+        'purchase',
+    ],
+    "category": "Custom Modules",
+    "data": [
+        'views/purchase_view.xml',
+    ],
+    "installable": True
+}
