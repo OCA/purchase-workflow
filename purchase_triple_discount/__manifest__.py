59e6a30796fff852109c279c221a10eab306df63
Stefaan <s.ferket@open2bizz.nl>
2018-10-11 10:06:12 +0200

    [MIG] purchase_triple_discount: Migration to 11.0 and correct module name

--- purchase_triple_discount/__manifest__.py
+++ purchase_triple_discount/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Account Invoice Triple Discount',
-    'version': '10.0.1.1.0',
+    'name': 'Purchase Triple Discount',
+    'version': '11.0.1.1.0',
     'category': 'Purchase Management',
     'author': 'Tecnativa, '
               'Odoo Community Association (OCA)',
