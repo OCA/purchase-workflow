afe206d4ea5b445c816f985ec8000a34761f4f19
hveficent <hector.villarreal@eficent.com>
2018-10-22 17:05:38 +0200

    [MIG] purchase_request_department: Migration to 11.0

--- purchase_request_department/models/purchase_request.py
+++ purchase_request_department/models/purchase_request.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
-
 from odoo import api, fields, models
 
 
@@ -10,9 +8,8 @@ class PurchaseRequest(models.Model):
 
     def _get_my_department(self):
         employees = self.env.user.employee_ids
-
         return (employees[0].department_id if employees
-                else self.env['hr.department'])
+                else self.env['hr.department'] or False)
 
     department_id = fields.Many2one('hr.department', 'Department',
                                     default=_get_my_department)
@@ -21,14 +18,13 @@ class PurchaseRequest(models.Model):
     def onchange_requested_by(self):
         employees = self.requested_by.employee_ids
         self.department_id = (employees[0].department_id if employees
-                              else self.env['hr.department'])
+                              else self.env['hr.department'] or False)
 
 
 class PurchaseRequestLine(models.Model):
     _inherit = 'purchase.request.line'
 
-    department_id = fields.Many2one(
-        comodel_name='hr.department',
-        related='request_id.department_id',
-        store=True,
-        string='Department', readonly=True)
+    department_id = fields.Many2one(comodel_name='hr.department',
+                                    related='request_id.department_id',
+                                    store=True,
+                                    string='Department', readonly=True)
