e26f9ae1abbd3eceedcb97a79ef9ca77e26bf1e5
hveficent <hector.villarreal@eficent.com>
2018-10-22 17:05:38 +0200

    [MIG] purchase_request_department: Migration to 11.0

--- purchase_request_department/hooks.py
+++ purchase_request_department/hooks.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
-from openerp import api, SUPERUSER_ID
+from odoo import api, SUPERUSER_ID
 import logging
 
 _logger = logging.getLogger(__name__)
