4a48919a1d1a2c047e0672bcd1f0752d77e9c635
hveficent <hector.villarreal@eficent.com>
2018-10-24 16:02:44 +0200

    [MIG+RNM] purchase_request_to_rfq_order_approved: Migration to 11.0 -> purchase_request_order_approved

---
+++ purchase_request_order_approved/models/purchase_request.py
@@ -0,0 +1,20 @@
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+
+from odoo import api, models
+
+
+class PurchaseRequestLine(models.Model):
+    _inherit = "purchase.request.line"
+
+    @api.multi
+    @api.depends('purchase_lines', 'purchase_lines.order_id.state')
+    def _compute_purchase_state(self):
+        super(PurchaseRequestLine, self)._compute_purchase_state()
+        for rec in self:
+            if rec.purchase_lines:
+                if any([po_line.state == 'approved' for po_line in
+                        rec.purchase_lines]) and not any(
+                    [po_line.state in ['purchase', 'done'] for po_line in
+                     rec.purchase_lines]):
+                    rec.purchase_state = 'approved'
