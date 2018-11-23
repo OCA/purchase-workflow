82536a40eb1064da39f3900f8b4e538c39156bed
Sudhir Arya <sudhir@erpharbor.com>
2018-11-23 14:45:31 +0530

    Merged product_supplierinfo_discount module's functionality in purchase_discount module

--- purchase_discount/models/purchase_order.py
+++ purchase_discount/models/purchase_order.py
@@ -8,6 +8,21 @@ from odoo import api, fields, models
 import odoo.addons.decimal_precision as dp
 
 
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    @api.multi
+    def _add_supplier_to_product(self):
+        """ Insert a mapping of products to discounts to be picked up
+        in supplierinfo's create() """
+        self.ensure_one()
+        discount_map = dict(
+            [(line.product_id.product_tmpl_id.id, line.discount)
+             for line in self.order_line.filtered('discount')])
+        return super(PurchaseOrder, self.with_context(
+            discount_map=discount_map))._add_supplier_to_product()
+
+
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
@@ -57,3 +72,22 @@ class PurchaseOrderLine(models.Model):
         if price_unit:
             self.price_unit = price_unit
         return price
+
+    @api.onchange('product_qty', 'product_uom')
+    def _onchange_quantity(self):
+        """
+        Check if a discount is defined into the supplier info and if so then
+        apply it to the current purchase order line
+        """
+        res = super(PurchaseOrderLine, self)._onchange_quantity()
+        if self.product_id:
+            date = None
+            if self.order_id.date_order:
+                date = fields.Date.to_string(
+                    fields.Date.from_string(self.order_id.date_order))
+            product_supplierinfo = self.product_id._select_seller(
+                partner_id=self.partner_id, quantity=self.product_qty,
+                date=date, uom_id=self.product_uom)
+            if product_supplierinfo:
+                self.discount = product_supplierinfo.discount
+        return res
