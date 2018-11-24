23cb9aac2473ef6404ae810810b248be743755bf
Krunal <krunal267@bitbucket.org>
2018-11-24 14:40:01 +0530

    [IMP]Improved Module name

--- purchase_triple_discount/__manifest__.py
+++ purchase_triple_discount/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Account Invoice Triple Discount',
+    'name': 'Purchase Triple Discount',
     'version': '11.0.0.0.0',
     'category': 'Purchase Management',
     'author': 'Tecnativa, '
