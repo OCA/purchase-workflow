b2207d1482cc87a560aa120e8d41c9cfc6e25837
Beñat Jimenez <benat.jimenez@eficent.com>
2018-11-26 12:02:26 +0100

    [MIG] purchase_order_line_sequence to version 12

--- purchase_order_line_sequence/tests/test_po_lines_sequence.py
+++ purchase_order_line_sequence/tests/test_po_lines_sequence.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Camptocamp SA - Damien Crier, Alexandre Fayolle
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
@@ -15,12 +14,36 @@ class TestPurchaseOrder(common.TransactionCase):
         super(TestPurchaseOrder, self).setUp()
         # Useful models
         self.PurchaseOrder = self.env['purchase.order']
-        self.Invoice = self.env['account.invoice']
         self.PurchaseOrderLine = self.env['purchase.order.line']
-        self.InvoiceLine = self.env['account.invoice.line']
         self.partner_id = self.env.ref('base.res_partner_1')
         self.product_id_1 = self.env.ref('product.product_product_8')
         self.product_id_2 = self.env.ref('product.product_product_11')
+
+        self.AccountInvoice = self.env['account.invoice']
+        self.AccountInvoiceLine = self.env['account.invoice.line']
+
+        self.category = self.env.ref('product.product_category_1').copy(
+            {'name': 'Test category', 'property_valuation': 'real_time',
+             'property_cost_method': 'fifo'})
+
+        account_type = self.env['account.account.type'].create(
+            {'name': 'RCV type', 'type': 'other'})
+        self.account_expense = self.env['account.account'].create(
+            {'name': 'Expense', 'code': 'EXP00',
+             'user_type_id': account_type.id, 'reconcile': True})
+        self.account_payable = self.env['account.account'].create(
+            {'name': 'Payable', 'code': 'PAY00',
+             'user_type_id': account_type.id, 'reconcile': True})
+
+        self.category.property_account_expense_categ_id = self.account_expense
+
+        self.category.property_stock_account_payable_id = self.account_payable
+        self.category.property_stock_journal = self.env[
+            'account.journal'].create(
+            {'name': 'Stock journal', 'type': 'sale', 'code': 'STK00'})
+        self.product_id_1.categ_id = self.category
+        self.product_id_2.categ_id = self.category
+        self.partner_id.property_account_payable_id = self.account_payable
 
     def _create_purchase_order(self):
         po_vals = {
@@ -79,16 +102,18 @@ class TestPurchaseOrder(common.TransactionCase):
 
         po = self._create_purchase_order()
         po.button_confirm()
-        po.picking_ids.do_transfer()
-
-        invoice = self.Invoice.create({'partner_id': self.partner_id.id})
-        invoice.purchase_id = po.id
-        invoice.currency_id = self.env.ref('base.INR').id,
-        invoice.purchase_order_change()
-
+        po.picking_ids.action_done()
+        self.invoice = self.AccountInvoice.create({
+            'partner_id': self.partner_id.id,
+            'purchase_id': po.id,
+            'account_id': self.partner_id.property_account_payable_id.id,
+            'type': 'in_invoice',
+        })
+        self.invoice.purchase_order_change()
+        self.invoice.action_invoice_open()
         self.assertEqual(po.order_line[0].sequence,
-                         invoice.invoice_line_ids[0].sequence,
+                         self.invoice.invoice_line_ids[0].sequence,
                          'The Sequence is not copied properly')
         self.assertEqual(po.order_line[1].sequence,
-                         invoice.invoice_line_ids[1].sequence,
+                         self.invoice.invoice_line_ids[1].sequence,
                          'The Sequence is not copied properly')
