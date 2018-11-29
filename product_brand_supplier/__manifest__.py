50715248dca107befcdb07406b514f489f9f46a0
Lluís Rovira @PlanetaTIC <lrovira@planetatic.com>
2018-11-29 01:18:45 +0100

    [ADD] TH181127/0009 product_brand_supplier: Add module

---
+++ product_brand_supplier/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 PlanetaTIC - Lluís Rovira <lrovira@planetatic.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Product Brand Supplier',
+    'summary': 'Supplier of brand on buy procurements',
+    'version': '10.0.1.0.0',
+    'category': 'Procurements',
+    'author': 'PlanetaTIC',
+    'website': 'https://www.planetatic.com',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'purchase',
+        'product_brand',
+    ],
+    'data': [
+    ],
+}
---
+++ product_brand_supplier/models/purchase.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 PlanetaTIC - Lluís Rovira <lrovira@planetatic.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api, _
+
+
+class ProcurementOrder(models.Model):
+    _inherit = 'procurement.order'
+
+    @api.multi
+    def make_po(self):
+        cache = {}
+        res = []
+        for procurement in self:
+            suppliers = procurement.product_id.seller_ids\
+                .filtered(lambda r: (not r.company_id or r.company_id == procurement.company_id) and (not r.product_id or r.product_id == procurement.product_id))
+            if not suppliers:
+                if procurement.product_id.product_brand_id.partner_id and procurement.product_id.product_brand_id.partner_id.supplier:
+                    new_supplierinfo = self.env['product.supplierinfo'].create({'product_tmpl_id': procurement.product_id.product_tmpl_id.id, 'name': procurement.product_id.product_brand_id.partner_id.id})
+                    suppliers = [new_supplierinfo]
+                else:
+                    procurement.message_post(body=_('No vendor associated to product %s. Please set one to fix this procurement.') % (procurement.product_id.name))
+                    continue
+            supplier = procurement._make_po_select_supplier(suppliers)
+            partner = supplier.name
+
+            domain = procurement._make_po_get_domain(partner)
+
+            if domain in cache:
+                po = cache[domain]
+            else:
+                po = self.env['purchase.order'].search([dom for dom in domain])
+                po = po[0] if po else False
+                cache[domain] = po
+            if not po:
+                vals = procurement._prepare_purchase_order(partner)
+                po = self.env['purchase.order'].create(vals)
+                name = (procurement.group_id and (procurement.group_id.name + ":") or "") + (procurement.name != "/" and procurement.name or "")
+                message = _("This purchase order has been created from: <a href=# data-oe-model=procurement.order data-oe-id=%d>%s</a>") % (procurement.id, name)
+                po.message_post(body=message)
+                cache[domain] = po
+            elif not po.origin or procurement.origin not in po.origin.split(', '):
+                # Keep track of all procurements
+                if po.origin:
+                    if procurement.origin:
+                        po.write({'origin': po.origin + ', ' + procurement.origin})
+                    else:
+                        po.write({'origin': po.origin})
+                else:
+                    po.write({'origin': procurement.origin})
+                name = (self.group_id and (self.group_id.name + ":") or "") + (self.name != "/" and self.name or "")
+                message = _("This purchase order has been modified from: <a href=# data-oe-model=procurement.order data-oe-id=%d>%s</a>") % (procurement.id, name)
+                po.message_post(body=message)
+            if po:
+                res += [procurement.id]
+
+            # Create Line
+            po_line = False
+            for line in po.order_line:
+                if line.product_id == procurement.product_id and line.product_uom == procurement.product_id.uom_po_id:
+                    procurement_uom_po_qty = procurement.product_uom._compute_quantity(procurement.product_qty, procurement.product_id.uom_po_id)
+                    seller = procurement.product_id._select_seller(
+                        partner_id=partner,
+                        quantity=line.product_qty + procurement_uom_po_qty,
+                        date=po.date_order and po.date_order[:10],
+                        uom_id=procurement.product_id.uom_po_id)
+
+                    price_unit = self.env['account.tax']._fix_tax_included_price_company(seller.price, line.product_id.supplier_taxes_id, line.taxes_id, self.company_id) if seller else 0.0
+                    if price_unit and seller and po.currency_id and seller.currency_id != po.currency_id:
+                        price_unit = seller.currency_id.compute(price_unit, po.currency_id)
+
+                    po_line = line.write({
+                        'product_qty': line.product_qty + procurement_uom_po_qty,
+                        'price_unit': price_unit,
+                        'procurement_ids': [(4, procurement.id)]
+                    })
+                    break
+            if not po_line:
+                vals = procurement._prepare_purchase_order_line(po, supplier)
+                self.env['purchase.order.line'].create(vals)
+            new_supplierinfo.unlink()
+        return res
