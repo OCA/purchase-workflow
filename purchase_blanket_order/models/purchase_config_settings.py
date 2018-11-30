048f6bb38629ef99ea330cd6d386862caf5e03c4
Adrià Gil Sorribes <adria.gil@eficent.com>
2018-11-30 13:45:15 +0100

    [11.0][ADD] Add the module purchase_blanket_order

---
+++ purchase_blanket_order/models/purchase_config_settings.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2018 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class PurchaseConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    group_purchase_blanket_disable_adding_lines = fields.Boolean(
+        string='Disable adding more lines to SOs',
+        implied_group='purchase_blanket_order.'
+                      'purchase_blanket_orders_disable_adding_lines')
