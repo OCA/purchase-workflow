402eaaa0486fc8ea86ab1d281bdd742e9a3ea51c
Adrià Gil Sorribes <adria.gil@eficent.com>
2018-11-30 13:45:15 +0100

    [11.0][ADD] Add the module purchase_blanket_order

---
+++ purchase_blanket_order/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2018 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Purchase Blanket Orders',
+    'category': 'Purchase',
+    'license': 'AGPL-3',
+    'author': 'Eficent S.L.,Odoo Community Association (OCA)',
+    'version': '11.0.1.0.0',
+    'website': 'https://github.com/OCA/purchase-workflow',
+    'summary': "Purchase Blanket Orders",
+    'depends': [
+        'purchase',
+        'web_action_conditionable',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/security.xml',
+        'data/sequence.xml',
+        'data/ir_cron.xml',
+        'wizard/create_purchase_orders.xml',
+        'views/purchase_config_settings.xml',
+        'views/blanket_orders.xml',
+        'views/purchase_orders.xml',
+        'report/templates.xml',
+        'report/report.xml',
+    ],
+    'installable': True,
+}
