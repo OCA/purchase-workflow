b11397eb8f7a39b50d91c37d9db257072b6e52e5
Beñat Jimenez <benat.jimenez@eficent.com>
2018-12-04 09:56:56 +0100

    [MIG] purchase_open_qty: Migration to v12

--- purchase_open_qty/models/purchase_order.py
+++ purchase_open_qty/models/purchase_order.py
@@ -3,27 +3,31 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models
+from odoo.tools import float_compare
+from odoo.tools.safe_eval import safe_eval
 import odoo.addons.decimal_precision as dp
 
 
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    @api.depends('product_qty', 'qty_invoiced',
-                 'invoice_lines.invoice_id.state',
-                 'order_id.state', 'move_ids.state', 'qty_received')
+    @api.depends('product_qty', 'qty_invoiced', 'product_id.purchase_method',
+                 'qty_received')
     def _compute_qty_to_invoice(self):
+        precision = self.env['decimal.precision'].precision_get(
+            'Product Unit of Measure')
         for line in self:
             if line.product_id.purchase_method == 'receive':
                 qty = line.qty_received - line.qty_invoiced
-                if qty >= 0.0:
+                if float_compare(qty, 0.0, precision_digits=precision):
                     line.qty_to_invoice = qty
                 else:
                     line.qty_to_invoice = 0.0
             else:
                 line.qty_to_invoice = line.product_qty - line.qty_invoiced
 
-    @api.depends('order_id.state', 'move_ids.state', 'product_qty')
+    @api.depends('move_ids.state', 'move_ids.product_uom',
+                 'move_ids.product_uom_qty')
     def _compute_qty_to_receive(self):
         for line in self:
             total = 0.0
@@ -61,29 +65,39 @@ class PurchaseOrder(models.Model):
 
     @api.model
     def _search_qty_to_invoice(self, operator, value):
-        po_line_obj = self.env['purchase.order.line']
         res = []
-        po_lines = po_line_obj.search(
-            [('qty_to_invoice', operator, value)])
-        order_ids = po_lines.mapped('order_id')
-        res.append(('id', 'in', order_ids.ids))
+        order_ids = []
+        # To evaluate the operation 'is equal to', change the operator value
+        if operator == '=':
+            operator = '=='
+        for po in self.search([]):
+            qty_to_invoice = sum(po.mapped('order_line.qty_to_invoice'))
+            flag = safe_eval(str(qty_to_invoice) + operator +
+                             str(value))
+            if flag:
+                order_ids.append(po.id)
+        res.append(('id', 'in', order_ids))
         return res
 
     @api.model
     def _search_qty_to_receive(self, operator, value):
-        po_line_obj = self.env['purchase.order.line']
         res = []
-        po_lines = po_line_obj.search(
-            [('qty_to_receive', operator, value)])
-        order_ids = po_lines.mapped('order_id')
-        res.append(('id', 'in', order_ids.ids))
+        order_ids = []
+        # To evaluate the operation 'is equal to', change the operator value
+        if operator == '=':
+            operator = '=='
+        for po in self.search([]):
+            qty_to_receive = sum(po.mapped('order_line.qty_to_receive'))
+            flag = safe_eval(str(qty_to_receive) + operator +
+                             str(value))
+            if flag:
+                order_ids.append(po.id)
+        res.append(('id', 'in', order_ids))
         return res
 
     qty_to_invoice = fields.Float(compute='_compute_qty_to_invoice',
                                   search='_search_qty_to_invoice',
-                                  string="Qty to Bill",
-                                  default=0.0)
+                                  string="Qty to Bill")
     qty_to_receive = fields.Float(compute='_compute_qty_to_receive',
                                   search='_search_qty_to_receive',
-                                  string="Qty to Receive",
-                                  default=0.0)
+                                  string="Qty to Receive")
