9f50eb71bc90b1c3d37f76c30e958abef52d7f11
cubells <vicent@vcubells.net>
2018-12-10 08:38:37 +0100

    purchase_order_line_description: Migration to 11.0

--- purchase_order_line_description/__manifest__.py
+++ purchase_order_line_description/__manifest__.py
@@ -1,13 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Alex Comba - Agile Business Group
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': "Purchase order line description",
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Purchase Management',
-    'author': "Agile Business Group, Odoo Community Association (OCA)",
-    'website': 'http://www.agilebg.com',
+    'author': "Agile Business Group, "
+              "Odoo Community Association (OCA)",
+    'website': 'https://github.com/OCA/purchase-workflow',
     'license': 'AGPL-3',
     "depends": [
         'purchase',
---
+++ purchase_order_line_description/readme/USAGE.rst
@@ -0,0 +1,6 @@
+#. Go to **Setting > Users & Companies > Users** and check you have checked
+   the "Use only product purchase description on order lines" access right.
+#. Add or modify a purchase description to any of your products.
+#. Create a purchase order and add a purchase line with this product and check
+   out that now purchase order line description only contains the description
+   you are created.
