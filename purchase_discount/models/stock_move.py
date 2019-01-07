b3fbd898e3254a12318c506e5a54b8f2cb75eb4f
Pedro M. Baeza <pedro.baeza@gmail.com>
2019-01-07 12:20:41 +0100

    [FIX] purchase_discount: Fix stock.move._get_price_unit
    This method is used for getting price cost and is using another logic when coming
    from purchase instead of getting stock.move `price_unit` field. This commit fixes
    it and also includes a unit test for checking properly the cost price update.
    
    Closes #562

---
+++ purchase_discount/models/stock_move.py
@@ -0,0 +1,26 @@
+# Copyright 2018 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def _get_price_unit(self):
+        """Get correct price with discount replacing current price_unit
+        value before calling super and restoring it later for assuring
+        maximum inheritability.
+        """
+        price_unit = False
+        po_line = self.purchase_line_id
+        if po_line and self.product_id == po_line.product_id:
+            price = po_line._get_discounted_price_unit()
+            if price != po_line.price_unit:
+                # Only change value if it's different
+                price_unit = self.price_unit
+                po_line.price_unit = price
+        res = super()._get_price_unit()
+        if price_unit:
+            po_line.price_unit = price_unit
+        return res
