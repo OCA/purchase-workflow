e71e8c66f01738e838fa55dd13432d9a929e5415
Nikul-Chaudhary <nikulchaudhary2112@gmail.com>
2019-01-27 18:27:19 +0530

    [MIG] purchase_order_secondary_unit v11 to v12

--- purchase_order_secondary_unit/tests/test_purchase_order_secondary_unit.py
+++ purchase_order_secondary_unit/tests/test_purchase_order_secondary_unit.py
@@ -1,17 +1,18 @@
 # Copyright 2018 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import fields
-from odoo.tests import SavepointCase
+from odoo.tests.common import SavepointCase, tagged
 
 
+@tagged('post_install', '-at_install')
 class TestPurchaseOrderSecondaryUnit(SavepointCase):
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.product_uom_kg = cls.env.ref('product.product_uom_kgm')
-        cls.product_uom_gram = cls.env.ref('product.product_uom_gram')
-        cls.product_uom_unit = cls.env.ref('product.product_uom_unit')
+        cls.product_uom_kg = cls.env.ref('uom.product_uom_kgm')
+        cls.product_uom_gram = cls.env.ref('uom.product_uom_gram')
+        cls.product_uom_unit = cls.env.ref('uom.product_uom_unit')
         cls.product = cls.env['product.product'].create({
             'name': 'test',
             'uom_id': cls.product_uom_kg.id,
@@ -31,9 +32,19 @@ class TestPurchaseOrderSecondaryUnit(SavepointCase):
             'name': 'test - partner',
             'supplier': True,
         })
-        po = cls.env['purchase.order'].new({
+        type_obj = cls.env['stock.picking.type']
+        company_id = cls.env.user.company_id.id
+        picking_type_id = type_obj.search([
+            ('code', '=', 'incoming'),
+            ('warehouse_id.company_id', '=', company_id)
+        ], limit=1)
+        if not picking_type_id:
+            picking_type_id = cls.env.ref('stock.picking_type_in')
+        cls.purchase_order_obj = cls.env['purchase.order']
+        po_val = {
             'partner_id': cls.partner.id,
             'company_id': cls.env.user.company_id.id,
+            'picking_type_id': picking_type_id.id,
             'order_line': [(0, 0, {
                 'name': cls.product.name,
                 'product_id': cls.product.id,
@@ -42,10 +53,10 @@ class TestPurchaseOrderSecondaryUnit(SavepointCase):
                 'price_unit': 1000.00,
                 'date_planned': fields.Datetime.now(),
             })],
-        })
+        }
+        po = cls.purchase_order_obj.new(po_val)
         po.onchange_partner_id()
-        cls.order = cls.env['purchase.order'].create(
-            po._convert_to_write(po._cache))
+        cls.order = cls.purchase_order_obj.create(po_val)
 
     def test_onchange_secondary_uom(self):
         self.order.order_line.write({
