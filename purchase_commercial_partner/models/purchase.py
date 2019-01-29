72b3a145f2872a74de875e28d492d8e2c1bcfbbf
Alexis de Lattre <alexis.delattre@akretion.com>
2019-01-29 10:43:03 +0100

    [MIG] purchase_commercial_partner from v10 to v12

--- purchase_commercial_partner/models/purchase.py
+++ purchase_commercial_partner/models/purchase.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
-# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2019 Akretion France (https://akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class PurchaseOrder(models.Model):
@@ -10,5 +10,4 @@ class PurchaseOrder(models.Model):
 
     commercial_partner_id = fields.Many2one(
         'res.partner', related='partner_id.commercial_partner_id', store=True,
-        index=True, string='Commercial Vendor', compute_sudo=True,
-        readonly=True)
+        index=True, string='Commercial Vendor', readonly=True)
