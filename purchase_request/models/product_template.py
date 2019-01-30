255aff32a2b7bc46b67124b0a454818d1d667076
CaiBen <ben.cai@elico-corp.com>
2019-01-30 13:24:43 +0800

    purchase_request moigration V11 to V12

--- purchase_request/models/product_template.py
+++ purchase_request/models/product_template.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+# Copyright 2018-2019 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
 from odoo import fields, models
@@ -8,7 +8,6 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     purchase_request = fields.Boolean(
-        string='Purchase Request',
-        help="Check this box to generate purchase request instead of "
-             "generating requests for quotation from procurement."
+        help="Check this box to generate Purchase Request instead of "
+             "generating Requests For Quotation from procurement."
     )
