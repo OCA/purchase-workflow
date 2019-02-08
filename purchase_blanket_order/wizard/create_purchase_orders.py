323d86165aed52eabc0e3b6caaa0de776a2c2ed1
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-02-08 13:58:06 +0100

    [ADD] Blanket Order Lines View and ability to create PO from BO lines

--- purchase_blanket_order/wizard/create_purchase_orders.py
+++ purchase_blanket_order/wizard/create_purchase_orders.py
@@ -1,6 +1,8 @@
 # Copyright (C) 2018 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import fields, models, api, _
+from odoo.tools import float_is_zero
+from collections import defaultdict
 from odoo.exceptions import UserError
 
 
@@ -14,37 +16,79 @@ class BlanketOrderWizard(models.TransientModel):
         self.env['purchase.blanket.order'].expire_orders()
         if not self.env.context.get('active_id'):
             return False
-        blanket_order = self.env['purchase.blanket.order'].browse(
-            self.env.context['active_id'])
+        blanket_order = self.env['purchase.blanket.order'].search(
+            [('id', '=', self.env.context['active_id'])], limit=1)
         if blanket_order.state == 'expired':
             raise UserError(_('You can\'t create a purchase order from '
                               'an expired blanket order!'))
         return blanket_order
 
     @api.model
+    def _check_valid_blanket_order_line(self, bo_lines):
+        precision = self.env['decimal.precision'].precision_get(
+            'Product Unit of Measure')
+        company_id = False
+
+        for line in bo_lines:
+
+            if line.order_id.state != 'open':
+                raise UserError(
+                    _('Purchase Blanket Order %s is not open') %
+                    line.order_id.name)
+
+            if float_is_zero(line.remaining_qty, precision_digits=precision):
+                raise UserError(
+                    _('The purchase has already been completed.'))
+
+            line_company_id = line.company_id \
+                              and line.company_id.id or False
+            if company_id is not False \
+                    and line_company_id != company_id:
+                raise UserError(
+                    _('You have to select lines '
+                      'from the same company.'))
+            else:
+                company_id = line_company_id
+
+    @api.model
     def _default_lines(self):
-        blanket_order = self._default_order()
+        blanket_order_line_obj = self.env['purchase.blanket.order.line']
+        blanket_order_line_ids = self.env.context.get('active_ids', False)
+        active_model = self.env.context.get('active_model', False)
+
+        if active_model == 'purchase.blanket.order':
+            bo_lines = self._default_order().line_ids
+        else:
+            bo_lines = blanket_order_line_obj.browse(blanket_order_line_ids)
+
+        self._check_valid_blanket_order_line(bo_lines)
+
         lines = [(0, 0, {
             'blanket_line_id': l.id,
             'product_id': l.product_id.id,
             'date_schedule': l.date_schedule,
             'remaining_qty': l.remaining_qty,
+            'price_unit': l.price_unit,
             'qty': l.remaining_qty,
-        }) for l in blanket_order.lines_ids]
+            'partner_id': l.partner_id,
+        }) for l in bo_lines]
         return lines
 
     blanket_order_id = fields.Many2one(
-        'purchase.blanket.order', default=_default_order, readonly=True)
-    lines_ids = fields.One2many(
+        'purchase.blanket.order', readonly=True)
+    purchase_order_id = fields.Many2one(
+        'purchase.order',
+        string='Purchase Order',
+        domain=[('state', '=', 'draft')])
+    line_ids = fields.One2many(
         'purchase.blanket.order.wizard.line', 'wizard_id',
         string='Lines', default=_default_lines)
 
     @api.multi
     def create_purchase_order(self):
-        self.ensure_one()
 
-        order_lines = []
-        for line in self.lines_ids:
+        order_lines_by_supplier = defaultdict(list)
+        for line in self.line_ids:
             if line.qty == 0.0:
                 continue
 
@@ -61,34 +105,43 @@ class BlanketOrderWizard(models.TransientModel):
                     'product_uom': line.blanket_line_id.product_uom.id,
                     'sequence': line.blanket_line_id.sequence,
                     'price_unit': line.blanket_line_id.price_unit,
-                    'blanket_line_id': line.blanket_line_id.id,
+                    'blanket_order_line': line.blanket_line_id.id,
                     'product_qty': line.qty}
-            order_lines.append((0, 0, vals))
+            order_lines_by_supplier[line.partner_id.id].append((0, 0, vals))
 
-        if not order_lines:
+        if not order_lines_by_supplier:
             raise UserError(_('An order can\'t be empty'))
 
-        order_vals = {
-            'partner_id': self.blanket_order_id.partner_id.id,
-        }
-        order_vals.update(self.env['purchase.order'].onchange(
-            order_vals, 'partner_id', {'partner_id': 'true'})['value'])
-        order_vals.update({
-            'origin': self.blanket_order_id.name,
-            'currency_id': self.blanket_order_id.currency_id.id,
-            'order_line': order_lines,
-            'payment_term_id': (self.blanket_order_id.payment_term_id.id
-                                if self.blanket_order_id.payment_term_id
-                                else False),
-        })
-
-        purchase_order = self.env['purchase.order'].create(order_vals)
+        res = []
+        for supplier in order_lines_by_supplier:
+            order_vals = {
+                'partner_id': int(supplier),
+            }
+            order_vals.update(self.env['purchase.order'].onchange(
+                order_vals, 'partner_id', {'partner_id': 'true'})['value'])
+            if self.blanket_order_id:
+                order_vals.update({
+                    'origin': self.blanket_order_id.name,
+                    'currency_id': self.blanket_order_id.currency_id.id,
+                    'payment_term_id': (
+                        self.blanket_order_id.payment_term_id.id
+                        if self.blanket_order_id.payment_term_id
+                        else False),
+                })
+            order_vals.update({
+                'order_line': order_lines_by_supplier[supplier],
+            })
+            purchase_order = self.env['purchase.order'].create(order_vals)
+            res.append(purchase_order.id)
         return {
-            'type': 'ir.actions.act_window',
+            'domain': [('id', 'in', res)],
+            'name': _('RFQ'),
             'view_type': 'form',
-            'view_mode': 'form',
+            'view_mode': 'tree,form',
             'res_model': 'purchase.order',
-            'res_id': purchase_order.id,
+            'view_id': False,
+            'context': {'from_purchase_order': True},
+            'type': 'ir.actions.act_window'
         }
 
 
@@ -107,3 +160,10 @@ class BlanketOrderWizardLine(models.TransientModel):
     remaining_qty = fields.Float(
         related='blanket_line_id.remaining_qty', readonly=True)
     qty = fields.Float(string='Quantity to Order', required=True)
+    price_unit = fields.Float(
+        related='blanket_line_id.price_unit', readonly=True)
+    partner_id = fields.Many2one(
+        'res.partner',
+        related='blanket_line_id.partner_id',
+        string='Vendor', readonly=True,
+    )
