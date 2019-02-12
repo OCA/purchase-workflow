90c37390f61ca87186eafea1176482fe24e5602c
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-02-12 16:25:59 +0100

    [MIG] purchase_order_type: Migration to 11.0 * remove coding info and fix reference * added tests * add sequence is mandatory

--- purchase_order_type/models/__init__.py
+++ purchase_order_type/models/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from . import purchase_order_type
 from . import purchase_order
 from . import res_partner
--- purchase_order_type/models/purchase_order.py
+++ purchase_order_type/models/purchase_order.py
@@ -1,26 +1,8 @@
-# -*- coding: utf-8 -*-
-#
-#
-#    Authors: Guewen Baconnier
-#    Copyright 2015 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# Copyright (C) 2015 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
-from openerp.addons.purchase.purchase import purchase_order
+from odoo import api, fields, models
+from odoo.addons.purchase.models.purchase import PurchaseOrder
 
 
 class PurchaseOrder(models.Model):
@@ -31,24 +13,34 @@ class PurchaseOrder(models.Model):
 
     order_type = fields.Many2one(comodel_name='purchase.order.type',
                                  readonly=False,
-                                 states=purchase_order.READONLY_STATES,
+                                 states=PurchaseOrder.READONLY_STATES,
                                  string='Type',
                                  ondelete='restrict',
                                  default=_default_order_type)
 
     @api.multi
-    def onchange_partner_id(self, partner_id):
-        res = super(PurchaseOrder, self).onchange_partner_id(partner_id)
-        if partner_id:
-            partner = self.env['res.partner'].browse(partner_id)
-            if partner.purchase_type:
-                res['value'] = res.get('value', {})
-                res['value'].update({
-                    'order_type': partner.purchase_type.id,
-                })
-        return res
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        super(PurchaseOrder, self).onchange_partner_id()
+        purchase_type = (self.partner_id.purchase_type or
+                         self.partner_id.commercial_partner_id.purchase_type)
+        if purchase_type:
+            self.order_type = purchase_type
 
-    @api.one
+    @api.multi
     @api.onchange('order_type')
     def onchange_order_type(self):
-        self.invoice_method = self.order_type.invoice_method
+        for order in self:
+            if order.order_type.payment_term_id:
+                order.payment_term_id = order.order_type.payment_term_id.id
+            if order.order_type.incoterm_id:
+                order.incoterm_id = order.order_type.incoterm_id.id
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name', '/') == '/' and vals.get('order_type'):
+            purchase_type = self.env['purchase.order.type'].browse(
+                vals['order_type'])
+            if purchase_type.sequence_id:
+                vals['name'] = purchase_type.sequence_id.next_by_id()
+        return super(PurchaseOrder, self).create(vals)
