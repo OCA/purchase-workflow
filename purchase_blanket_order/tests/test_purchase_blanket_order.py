a094a142879473e903f1bbad9e7fe18d68c11945
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-02-28 11:47:38 +0100

    add taxes and total price to blanket order and extend tests

--- purchase_blanket_order/tests/test_purchase_blanket_order.py
+++ purchase_blanket_order/tests/test_purchase_blanket_order.py
@@ -48,7 +48,7 @@ class TestPurchaseBlanketOrders(common.TransactionCase):
         self.yesterday = date.today() - timedelta(days=1)
         self.tomorrow = date.today() + timedelta(days=1)
 
-    def test_create_blanket_order(self):
+    def test_01_create_blanket_order_flow(self):
         """ We create a blanket order and check constrains to confirm BO """
         blanket_order = self.blanket_order_obj.create({
             'partner_id': self.partner.id,
@@ -63,9 +63,14 @@ class TestPurchaseBlanketOrders(common.TransactionCase):
         })
         blanket_order.sudo().onchange_partner_id()
         blanket_order.line_ids[0].sudo().onchange_product()
+        blanket_order._compute_line_count()
+        blanket_order._compute_uom_qty()
 
         self.assertEqual(blanket_order.state, 'draft')
         self.assertEqual(blanket_order.line_ids[0].price_unit, 30.0)
+        self.assertEqual(blanket_order.original_uom_qty, 20.0)
+        self.assertEqual(blanket_order.ordered_uom_qty, 0.0)
+        self.assertEqual(blanket_order.remaining_uom_qty, 20.0)
 
         # date in the past
         with self.assertRaises(UserError):
@@ -74,9 +79,23 @@ class TestPurchaseBlanketOrders(common.TransactionCase):
         blanket_order.validity_date = fields.Date.to_string(self.tomorrow)
         blanket_order.sudo().action_confirm()
 
-        self.assertEqual(blanket_order.state, 'open')
+        blanket_order.sudo().action_cancel()
+        self.assertEqual(blanket_order.state, 'expired')
+        blanket_order.sudo().set_to_draft()
+        self.assertEqual(blanket_order.state, 'draft')
+        blanket_order.sudo().action_confirm()
 
-    def test_create_purchase_orders_from_blanket_order(self):
+        self.assertEqual(blanket_order.state, 'open')
+        blanket_order.action_view_purchase_blanket_order_line()
+
+        # Search view check
+        blanket_order._search_original_uom_qty('>=', 0.0)
+        blanket_order._search_ordered_uom_qty('>=', 0.0)
+        blanket_order._search_invoiced_uom_qty('>=', 0.0)
+        blanket_order._search_received_uom_qty('>=', 0.0)
+        blanket_order._search_remaining_uom_qty('>=', 0.0)
+
+    def test__02_create_purchase_orders_from_blanket_order(self):
         """ We create a blanket order and create two purchase orders """
         blanket_order = self.blanket_order_obj.create({
             'partner_id': self.partner.id,
@@ -90,11 +109,22 @@ class TestPurchaseBlanketOrders(common.TransactionCase):
             })],
         })
         blanket_order.sudo().onchange_partner_id()
+
+        with self.assertRaises(UserError):
+            # Blanket order is not confirmed
+            self.blanket_order_wiz_obj.with_context(
+                active_id=blanket_order.id,
+                active_model='purchase.blanket.order').create({})
         blanket_order.sudo().action_confirm()
 
         wizard1 = self.blanket_order_wiz_obj.with_context(
             active_id=blanket_order.id,
             active_model='purchase.blanket.order').create({})
+        wizard1.line_ids[0].write({'qty': 30.0})
+
+        with self.assertRaises(UserError):
+            # Wizard quantity greater than remaining quantity
+            wizard1.sudo().create_purchase_order()
         wizard1.line_ids[0].write({'qty': 10.0})
         wizard1.sudo().create_purchase_order()
 
@@ -104,6 +134,12 @@ class TestPurchaseBlanketOrders(common.TransactionCase):
         wizard2.line_ids[0].write({'qty': 10.0})
         wizard2.sudo().create_purchase_order()
 
+        with self.assertRaises(UserError):
+            # Blanket order already completed
+            self.blanket_order_wiz_obj.with_context(
+                active_id=blanket_order.id,
+                active_model='purchase.blanket.order').create({})
+
         self.assertEqual(blanket_order.state, 'done')
 
         self.assertEqual(blanket_order.purchase_count, 2)
@@ -112,7 +148,7 @@ class TestPurchaseBlanketOrders(common.TransactionCase):
         domain_ids = view_action['domain'][0][2]
         self.assertEqual(len(domain_ids), 2)
 
-    def test_create_purchase_orders_from_blanket_order_line(self):
+    def test_03_create_purchase_orders_from_blanket_order_line(self):
         """ We create a blanket order and create two purchase orders
             from the blanket order lines """
         blanket_order = self.blanket_order_obj.create({
