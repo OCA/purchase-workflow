2e341a6a146a3e4497ff231d14d51b1e194c33d6
ernesto <ernesto.tejeda87@gmail.com>
2019-03-08 12:21:00 -0500

    [MIG] purchase_landed_cost: Migration to 12.0

--- purchase_landed_cost/models/purchase_order.py
+++ purchase_landed_cost/models/purchase_order.py
@@ -14,18 +14,4 @@ class PurchaseOrder(models.Model):
         line_obj = self.env['purchase.cost.distribution.line']
         lines = line_obj.search([('purchase_id', '=', self.id)])
         if lines:
-            mod_obj = self.env['ir.model.data']
-            model, action_id = tuple(
-                mod_obj.get_object_reference(
-                    'purchase_landed_cost',
-                    'action_purchase_cost_distribution'))
-            action = self.env[model].browse(action_id).read()[0]
-            ids = set([x.distribution.id for x in lines])
-            if len(ids) == 1:
-                res = mod_obj.get_object_reference(
-                    'purchase_landed_cost', 'purchase_cost_distribution_form')
-                action['views'] = [(res and res[1] or False, 'form')]
-                action['res_id'] = list(ids)[0]
-            else:
-                action['domain'] = "[('id', 'in', %s)]" % list(ids)
-            return action
+            return lines.get_action_purchase_cost_distribution()
