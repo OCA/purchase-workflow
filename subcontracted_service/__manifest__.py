000b818bee8b0159af6dfa3d6dd8256268aa4f8e
Ruben Dario Bravo <rubenred18@gmail.com>
2019-04-30 13:28:21 -0400

    [MIG] subcontracted_service: Migration to 12.0

--- subcontracted_service/__manifest__.py
+++ subcontracted_service/__manifest__.py
@@ -1,11 +1,12 @@
 # Author: Damien Crier
 # Copyright 2017 Camptocamp SA
+# Copyright 2019 Rubén Bravo <rubenred18@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": 'Subcontracted service',
     "summary": 'Subcontracted service',
-    "version": "11.0.1.0.1",
+    "version": "12.0.1.0.0",
     "category": "Purchase",
     "website": "https://odoo-community.org/",
     "author": "Camptocamp, Odoo Community Association (OCA)",
@@ -13,7 +14,7 @@
     "application": False,
     "installable": True,
     "depends": [
-        "purchase",
+        "purchase_stock",
     ],
     "data": [
         'views/product_template.xml',
---
+++ subcontracted_service/readme/CONTRIBUTORS.rst
@@ -0,0 +1,4 @@
+* Damien Crier <damien.crier@camptocamp.com>
+* Jordi Ballester Alomar <jordi.ballester@eficent.com>
+* Lois Rilo <lois.rilo@eficent.com>
+* Rubén Bravo <rubenred18@gmail.com>
