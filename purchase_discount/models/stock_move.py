6628698e1017cbdc8ac6a9a3ae06e415ea1a4593
nicomacr <nmr@adhoc.com.ar>
2019-06-11 10:32:37 -0300

    [FIX] purchase_discount: Use sudo in purchase lines

--- purchase_discount/models/stock_move.py
+++ purchase_discount/models/stock_move.py
@@ -13,7 +13,10 @@ class StockMove(models.Model):
         maximum inheritability.
         """
         price_unit = False
-        po_line = self.purchase_line_id
+        # We do it with sudo to avoid permission errors in case
+        # the user validates the order and does not have permission to
+        # modifiy purchase lines
+        po_line = self.purchase_line_id.sudo()
         if po_line and self.product_id == po_line.product_id:
             price = po_line._get_discounted_price_unit()
             if price != po_line.price_unit:
