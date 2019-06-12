9d5c983ff71ec7867e6f7a24b9a576cd5cc2f550
Kitti U <kittiu@gmail.com>
2019-06-12 22:05:11 +0700

    [12.0][ADD] purchase_deposit

---
+++ purchase_deposit/models/purchase.py
@@ -0,0 +1,28 @@
+# Copyright 2019 Elico Corp, Dominique K. <dominique.k@elico-corp.com.sg>
+# Copyright 2019 Ecosoft Co., Ltd., Kitti U. <kittiu@ecosoft.co.th>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    @api.multi
+    def copy_data(self, default=None):
+        if default is None:
+            default = {}
+        default['order_line'] = \
+            [(0, 0, line.copy_data()[0])
+             for line in self.order_line.filtered(lambda l: not l.is_deposit)]
+        return super(PurchaseOrder, self).copy_data(default)
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    is_deposit = fields.Boolean(
+        string='Is a deposit payment',
+        help="Deposit payments are made when creating invoices from a purhcase"
+        " order. They are not copied when duplicating a purchase order.",
+    )
