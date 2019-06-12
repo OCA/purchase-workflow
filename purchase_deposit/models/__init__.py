17d87b2bcd71c5fa941b4507a18bc7843192bac2
Kitti U <kittiu@gmail.com>
2019-06-12 22:05:11 +0700

    [12.0][ADD] purchase_deposit

---
+++ purchase_deposit/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2019 Elico Corp, Dominique K. <dominique.k@elico-corp.com.sg>
+# Copyright 2019 Ecosoft Co., Ltd., Kitti U. <kittiu@ecosoft.co.th>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import res_config_settings
+from . import purchase
+from . import invoice
---
+++ purchase_deposit/readme/CONTRIBUTORS.rst
@@ -0,0 +1,2 @@
+* Dominique K. <dominique.k@elico-corp.com.sg>
+* Kitti Upariphutthiphong <kittiu@ecosoft.co.th>
