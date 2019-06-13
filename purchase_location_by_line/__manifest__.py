ff9158b4ee3b93a49823319ed8cde01f2877eef5
mreficent <miquel.raich@eficent.com>
2019-06-13 18:34:00 +0200

    [MIG] purchase_location_by_line: Migration to 12.0

--- purchase_location_by_line/__manifest__.py
+++ purchase_location_by_line/__manifest__.py
@@ -7,13 +7,13 @@
     "name": "Purchase Location by Line",
     "summary": "Allows to define a specific destination location on each PO "
                "line",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent, "
               "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Purchase Management",
     "depends": [
-        "purchase",
+        "purchase_stock",
         "purchase_delivery_split_date"
     ],
     "license": "AGPL-3",
