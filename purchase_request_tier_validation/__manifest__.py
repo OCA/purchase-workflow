2cc2a50d98f5bdb1d0164f9569c9d15ad4786f10
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-06-13 11:56:00 +0200

    [11.0][FIX] purchase_request_tier_validation

--- purchase_request_tier_validation/__manifest__.py
+++ purchase_request_tier_validation/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Purchase Request Tier Validation",
     "summary": "Extends the functionality of Purchase Requests to "
                "support a tier validation process.",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.1.0",
     "category": "Purchase Management",
     "website": "https://github.com/OCA/purchase-workflow",
     "author": "Eficent, Odoo Community Association (OCA)",
