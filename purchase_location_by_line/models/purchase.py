c3b8a46821ed1887a4079b3497fcf2b3e7340595
mreficent <miquel.raich@eficent.com>
2019-06-13 18:34:00 +0200

    [MIG] purchase_location_by_line: Migration to 12.0

--- purchase_location_by_line/models/purchase.py
+++ purchase_location_by_line/models/purchase.py
@@ -10,16 +10,16 @@ class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
     location_dest_id = fields.Many2one(
-        'stock.location', 'Destination', domain=[('usage', 'in',
-                                                  ['internal', 'transit'])])
+        comodel_name='stock.location', string='Destination',
+        domain=[('usage', 'in', ['internal', 'transit'])])
 
     @api.model
     def _first_picking_copy_vals(self, key, lines):
         """The data to be copied to new pickings is updated with data from the
         grouping key.  This method is designed for extensibility, so that
         other modules can store more data based on new keys."""
-        vals = super(PurchaseOrderLine, self)._first_picking_copy_vals(key,
-                                                                       lines)
+        vals = super(PurchaseOrderLine, self)._first_picking_copy_vals(
+            key, lines)
         for key_element in key:
             if 'location_dest_id' in key_element.keys():
                 vals['location_dest_id'] = key_element['location_dest_id'].id
@@ -32,8 +32,8 @@ class PurchaseOrderLine(models.Model):
         dictionary element with the field that you want to group by. This
         method is designed for extensibility, so that other modules can add
         additional keys or replace them by others."""
-        key = super(PurchaseOrderLine, self)._get_group_keys(order, line,
-                                                             picking=picking)
+        key = super(PurchaseOrderLine, self)._get_group_keys(
+            order, line, picking=picking)
         default_picking_location_id = line.order_id._get_destination_location()
         default_picking_location = self.env['stock.location'].browse(
             default_picking_location_id)
