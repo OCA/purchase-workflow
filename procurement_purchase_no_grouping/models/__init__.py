1dbd92d9e948e8d62575e280d59e380594f4773f
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-06-17 19:46:00 +0200

    [MIG] procurement_purchase_no_grouping: Migration to 12.0

--- procurement_purchase_no_grouping/models/__init__.py
+++ procurement_purchase_no_grouping/models/__init__.py
@@ -1,5 +1,3 @@
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-
 from . import product_category
-from . import procurement_rule
+from . import stock_rule
 from . import purchase_order
--- procurement_purchase_no_grouping/models/stock_rule.py
+++ procurement_purchase_no_grouping/models/stock_rule.py
@@ -6,15 +6,15 @@
 from odoo import api, models
 
 
-class ProcurementRule(models.Model):
-    _inherit = 'procurement.rule'
+class StockRule(models.Model):
+    _inherit = 'stock.rule'
 
     @api.multi
     def _run_buy(self, product_id, product_qty, product_uom, location_id,
                  name, origin, values):
         grouping = product_id.categ_id.procured_purchase_grouping
         self_wc = self.with_context(grouping=grouping)
-        return super(ProcurementRule, self_wc)._run_buy(
+        return super(StockRule, self_wc)._run_buy(
             product_id, product_qty, product_uom, location_id, name,
             origin, values)
 
