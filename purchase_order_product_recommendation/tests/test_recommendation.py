6f5094e3895264d194a646932d361a521387875b
David <david.vidal@tecnativa.com>
2019-06-18 13:07:14 +0200

    [IMP] purchase_order_product_recommendation: warehouse filter
    - If you have multiple warehouses, you can also constrain the recommendations to
    the deliveries of specific ones.

--- purchase_order_product_recommendation/tests/test_recommendation.py
+++ purchase_order_product_recommendation/tests/test_recommendation.py
@@ -25,12 +25,17 @@ class RecommendationCase(SavepointCase):
             'name': 'Test Product 3',
             'seller_ids': [(0, 0, {'name': cls.partner.id, 'price': 7})],
         })
+        # Warehouses
+        cls.wh1 = cls.env['stock.warehouse'].create({
+            'name': 'TEST WH1',
+            'code': 'TST1',
+        })
+        cls.wh2 = cls.env['stock.warehouse'].create({
+            'name': 'TEST WH2',
+            'code': 'TST2',
+        })
         # Locations
         location_obj = cls.env['stock.location']
-        cls.internal_loc = location_obj.create({
-            'name': 'Test internal location',
-            'usage': 'internal',
-        })
         cls.supplier_loc = location_obj.create({
             'name': 'Test supplier location',
             'usage': 'supplier',
@@ -39,39 +44,62 @@ class RecommendationCase(SavepointCase):
             'name': 'Test customer location',
             'usage': 'customer',
         })
-        # Create delivered and received lines to have a history
+        # Create deliveries and receipts orders to have a history
+        cls.picking_obj = cls.env['stock.picking']
+        cls.picking_1 = cls.picking_obj.create({
+            'location_id': cls.wh1.lot_stock_id.id,
+            'location_dest_id': cls.customer_loc.id,
+            'partner_id': cls.partner.id,
+            'picking_type_id': cls.wh1.out_type_id.id,
+        })
+        cls.picking_2 = cls.picking_obj.create({
+            'location_id': cls.wh2.lot_stock_id.id,
+            'location_dest_id': cls.customer_loc.id,
+            'partner_id': cls.partner.id,
+            'picking_type_id': cls.wh2.out_type_id.id,
+        })
+        cls.picking_3 = cls.picking_obj.create({
+            'location_id': cls.supplier_loc.id,
+            'location_dest_id': cls.wh1.lot_stock_id.id,
+            'partner_id': cls.partner.id,
+            'picking_type_id': cls.wh1.in_type_id.id,
+        })
         cls.move_line = cls.env['stock.move.line']
         cls.move_line |= cls.move_line.create({
             'product_id': cls.prod_1.id,
             'product_uom_id': cls.prod_1.uom_id.id,
             'qty_done': 1,
             'date': '2018-01-11 15:05:00',
-            'location_id': cls.internal_loc.id,
+            'location_id': cls.wh1.lot_stock_id.id,
             'location_dest_id': cls.customer_loc.id,
+            'picking_id': cls.picking_1.id,
         })
         cls.move_line |= cls.move_line.create({
             'product_id': cls.prod_2.id,
             'product_uom_id': cls.prod_2.uom_id.id,
             'qty_done': 38,
             'date': '2019-02-01 00:05:00',
-            'location_id': cls.internal_loc.id,
+            'location_id': cls.wh1.lot_stock_id.id,
             'location_dest_id': cls.customer_loc.id,
+            'picking_id': cls.picking_1.id,
         })
         cls.move_line |= cls.move_line.create({
             'product_id': cls.prod_2.id,
             'product_uom_id': cls.prod_2.uom_id.id,
             'qty_done': 4,
             'date': '2019-02-01 00:05:00',
-            'location_id': cls.internal_loc.id,
+            'location_id': cls.wh2.lot_stock_id.id,
             'location_dest_id': cls.customer_loc.id,
+            'picking_id': cls.picking_2.id,
         })
         cls.move_line |= cls.move_line.create({
             'product_id': cls.prod_3.id,
             'product_uom_id': cls.prod_3.uom_id.id,
             'qty_done': 13,
             'date': '2019-02-01 00:06:00',
-            'location_id': cls.internal_loc.id,
+            'location_id': cls.wh2.lot_stock_id.id,
             'location_dest_id': cls.customer_loc.id,
+            'picking_id': cls.picking_2.id,
         })
         cls.move_line |= cls.move_line.create({
             'product_id': cls.prod_3.id,
@@ -79,7 +107,8 @@ class RecommendationCase(SavepointCase):
             'qty_done': 7,
             'date': '2019-02-01 00:00:00',
             'location_id': cls.supplier_loc.id,
-            'location_dest_id': cls.internal_loc.id,
+            'location_dest_id': cls.wh1.lot_stock_id.id,
+            'picking_id': cls.picking_3.id,
         })
         cls.move_line.write({
             'state': 'done',
@@ -118,3 +147,43 @@ class RecommendationCase(SavepointCase):
         wizard.line_amount = 1
         wizard._generate_recommendations()
         self.assertEqual(len(wizard.line_ids), 1)
+
+    def test_recommendations_by_warehouse(self):
+        """We can split recommendations by delivery warehouse"""
+        wizard = self.wizard()
+        wizard.date_begin = wizard.date_end = '2019-02-01'
+        # Just delivered to WH2
+        wizard.warehouse_ids = self.wh2
+        wizard._generate_recommendations()
+        self.assertEqual(wizard.line_ids[0].times_delivered, 1)
+        self.assertEqual(wizard.line_ids[0].units_delivered, 4)
+        self.assertEqual(wizard.line_ids[0].units_included, 4)
+        self.assertEqual(wizard.line_ids[0].product_id, self.prod_2)
+        self.assertEqual(wizard.line_ids[1].times_delivered, 1)
+        self.assertEqual(wizard.line_ids[1].units_delivered, 13)
+        self.assertEqual(wizard.line_ids[1].units_included, 13)
+        self.assertEqual(wizard.line_ids[1].product_id, self.prod_3)
+        # Just delivered to WH1
+        wizard.warehouse_ids = self.wh1
+        wizard._generate_recommendations()
+        self.assertEqual(wizard.line_ids[0].times_delivered, 1)
+        self.assertEqual(wizard.line_ids[0].units_delivered, 38)
+        self.assertEqual(wizard.line_ids[0].units_included, 38)
+        self.assertEqual(wizard.line_ids[0].product_id, self.prod_2)
+        self.assertEqual(wizard.line_ids[1].times_delivered, 0)
+        self.assertEqual(wizard.line_ids[1].units_delivered, 0)
+        self.assertEqual(wizard.line_ids[1].units_received, 7)
+        self.assertEqual(wizard.line_ids[1].units_included, 0)
+        self.assertEqual(wizard.line_ids[1].product_id, self.prod_3)
+        self.assertEqual(len(wizard.line_ids), 2)
+        # Delivered to both warehouses
+        wizard.warehouse_ids |= self.wh2
+        wizard._generate_recommendations()
+        self.assertEqual(wizard.line_ids[0].times_delivered, 2)
+        self.assertEqual(wizard.line_ids[0].units_delivered, 42)
+        self.assertEqual(wizard.line_ids[0].units_included, 42)
+        self.assertEqual(wizard.line_ids[0].product_id, self.prod_2)
+        self.assertEqual(wizard.line_ids[1].times_delivered, 1)
+        self.assertEqual(wizard.line_ids[1].units_delivered, 13)
+        self.assertEqual(wizard.line_ids[1].units_included, 13)
+        self.assertEqual(wizard.line_ids[1].product_id, self.prod_3)
