1a3ccb73474ace4d3c478eada9d9cdc7b5f42d62
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-07-02 09:21:32 +0200

    [IMP] add fields to product template form view and update readme
    [ADD] post-init hook
    
    [ADD] migration script

--- purchase_last_price_info/models/product.py
+++ purchase_last_price_info/models/product.py
@@ -6,22 +6,66 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     last_purchase_price = fields.Float(
-        string='Last Purchase Price', compute='_compute_last_purchase')
+        string='Last Purchase Price')
     last_purchase_date = fields.Date(
-        string='Last Purchase Date', compute='_compute_last_purchase')
+        string='Last Purchase Date')
     last_supplier_id = fields.Many2one(
-        comodel_name='res.partner', string='Last Supplier',
-        compute='_compute_last_purchase')
+        comodel_name='res.partner', string='Last Supplier')
 
     @api.multi
-    def _compute_last_purchase(self):
+    def set_product_last_purchase(self, order_id=False):
         """ Get last purchase price, last purchase date and last supplier """
         PurchaseOrderLine = self.env['purchase.order.line']
+        if not self.check_access_rights('write', raise_exception=False):
+            return
         for product in self:
-            lines = PurchaseOrderLine.search(
-                [('product_id', '=', product.id),
-                 ('state', 'in', ['purchase', 'done'])]).sorted(
-                key=lambda l: l.order_id.date_order, reverse=True)
-            product.last_purchase_date = lines[:1].order_id.date_order
-            product.last_purchase_price = lines[:1].price_unit
-            product.last_supplier_id = lines[:1].order_id.partner_id
+            date_order = False
+            price_unit_uom = 0.0
+            last_supplier = False
+
+            # Check if Order ID was passed, to speed up the search
+            if order_id:
+                lines = PurchaseOrderLine.search([
+                    ('order_id', '=', order_id),
+                    ('product_id', '=', product.id)], limit=1)
+            else:
+                lines = PurchaseOrderLine.search(
+                    [('product_id', '=', product.id),
+                     ('state', 'in', ['purchase', 'done'])]).sorted(
+                    key=lambda l: l.order_id.date_order, reverse=True)
+
+            if lines:
+                # Get most recent Purchase Order Line
+                last_line = lines[:1]
+
+                date_order = last_line.order_id.date_order
+                # Compute Price Unit in the Product base UoM
+                price_unit_uom = product.product_tmpl_id.uom_id.\
+                    _compute_quantity(last_line.price_unit,
+                                      last_line.product_uom)
+                last_supplier = last_line.order_id.partner_id
+
+            # Assign values to record
+            product.last_purchase_date = date_order
+            product.last_purchase_price = price_unit_uom
+            product.last_supplier_id = last_supplier
+            # Set related product template values
+            product.product_tmpl_id.set_product_template_last_purchase(
+                date_order, price_unit_uom, last_supplier)
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    last_purchase_price = fields.Float(
+        string='Last Purchase Price')
+    last_purchase_date = fields.Date(
+        string='Last Purchase Date')
+    last_supplier_id = fields.Many2one(
+        comodel_name='res.partner', string='Last Supplier')
+
+    def set_product_template_last_purchase(self, date_order, price_unit,
+                                           partner_id):
+        self.last_purchase_date = date_order
+        self.last_purchase_price = price_unit
+        self.last_supplier_id = partner_id
---
+++ purchase_last_price_info/readme/USAGE.rst
@@ -0,0 +1,7 @@
+In order to use this module, go to:
+
+* Products -> Tab Purchase (see screenshot)
+
+.. image:: ../static/description/purchase_last_price.png
+   :alt: Purchase Last Price Info
+   :width: 400 px
