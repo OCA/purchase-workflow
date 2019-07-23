76738053dc42a9b950bb700b88660fe0f734a9d9
hparfr <raph@hpar.fr>
2019-07-23 17:35:02 +0200

    Fix move link.
    We want to link moves but not procurement because cancellation/unlink is
    managed in purchase reques

--- purchase_request_to_rfq/models/purchase_order.py
+++ purchase_request_to_rfq/models/purchase_order.py
@@ -3,6 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
 from odoo import _, api, exceptions, fields, models
+from odoo.tools.float_utils import float_compare
 
 
 class PurchaseOrder(models.Model):
@@ -103,3 +104,62 @@ class PurchaseOrderLine(models.Model):
                 'view_type': 'form',
                 'view_mode': 'tree,form',
                 'domain': domain}
+
+    @api.multi
+    def _prepare_stock_moves(self, picking):
+        """Link move_dest_id when purchase request
+
+        If the POL is linked to a purchase request, there is no
+        self.procurement_ids but self.purchase_request_lines.procurement_id
+        This function is mainly a copy past from product/_prepare_stock_moves
+        """
+        self.ensure_one()
+        res = super(PurchaseOrderLine, self)._prepare_stock_moves(picking)
+        if len(res) == 0:
+            # super return [] if nothing to do
+            return res
+        qty = 0.0
+        for move in self.move_ids.filtered(lambda x: x.state != 'cancel'):
+            qty += move.product_uom_qty
+        diff_quantity = self.product_qty - qty
+        # Fullfill all related procurements with this po line
+        if self.purchase_request_lines:
+            template = res.pop()
+            for proc in self.purchase_request_lines.mapped(
+                'procurement_id'
+            ).filtered(lambda p: p.state != 'cancel'):
+                # If the procurement has some moves already,
+                # we should deduct their quantity
+                sum_existing_moves = sum(
+                    x.product_qty for x in proc.move_ids
+                    if x.state != 'cancel')
+                existing_proc_qty = proc.product_id.uom_id._compute_quantity(
+                    sum_existing_moves, proc.product_uom)
+                procurement_qty = proc.product_uom._compute_quantity(
+                    proc.product_qty, self.product_uom) - existing_proc_qty
+                if (
+                    float_compare(
+                        procurement_qty, 0.0,
+                        precision_rounding=proc.product_uom.rounding
+                    ) > 0 and
+                    float_compare(
+                        diff_quantity, 0.0,
+                        precision_rounding=self.product_uom.rounding) > 0
+                ):
+                    tmp = template.copy()
+                    tmp.update({
+                        'product_uom_qty': min(procurement_qty, diff_quantity),
+                        'move_dest_id': proc.move_dest_id.id,
+                        # move destination is same as procurement destination
+                        'procurement_id': proc.id,
+                        'propagate': proc.rule_id.propagate,
+                    })
+                    res.append(tmp)
+                    diff_quantity -= min(procurement_qty, diff_quantity)
+            if float_compare(
+                diff_quantity, 0.0,
+                precision_rounding=self.product_uom.rounding
+            ) > 0:
+                template['product_uom_qty'] = diff_quantity
+                res.append(template)
+        return res
