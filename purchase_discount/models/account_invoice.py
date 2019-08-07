c30f75c7487357242ba909608b3488d81931d243
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-08-07 12:14:17 +0200

    [FIX+IMP] purchase_discount: several things
    * hook to allow better overload for purchase_triple_discount
    * simple form of call of super()
    * restauring GRAP in author list
    * readme images paths

--- purchase_discount/models/account_invoice.py
+++ purchase_discount/models/account_invoice.py
@@ -7,7 +7,6 @@ class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
     def _prepare_invoice_line_from_po_line(self, line):
-        vals = super(AccountInvoice, self)._prepare_invoice_line_from_po_line(
-            line)
+        vals = super()._prepare_invoice_line_from_po_line(line)
         vals['discount'] = line.discount
         return vals
--- purchase_discount/models/purchase_order.py
+++ purchase_discount/models/purchase_order.py
@@ -28,10 +28,10 @@ class PurchaseOrderLine(models.Model):
     # adding discount to depends
     @api.depends('discount')
     def _compute_amount(self):
-        return super(PurchaseOrderLine, self)._compute_amount()
+        return super()._compute_amount()
 
     def _prepare_compute_all_values(self):
-        vals = super(PurchaseOrderLine, self)._prepare_compute_all_values()
+        vals = super()._prepare_compute_all_values()
         vals.update({'price_unit': self._get_discounted_price_unit()})
         return vals
 
@@ -68,7 +68,7 @@ class PurchaseOrderLine(models.Model):
             # Only change value if it's different
             price_unit = self.price_unit
             self.price_unit = price
-        price = super(PurchaseOrderLine, self)._get_stock_move_price_unit()
+        price = super()._get_stock_move_price_unit()
         if price_unit:
             self.price_unit = price_unit
         return price
@@ -79,15 +79,22 @@ class PurchaseOrderLine(models.Model):
         Check if a discount is defined into the supplier info and if so then
         apply it to the current purchase order line
         """
-        res = super(PurchaseOrderLine, self)._onchange_quantity()
+        res = super()._onchange_quantity()
         if self.product_id:
             date = None
             if self.order_id.date_order:
                 date = fields.Date.to_string(
                     fields.Date.from_string(self.order_id.date_order))
-            product_supplierinfo = self.product_id._select_seller(
+            seller = self.product_id._select_seller(
                 partner_id=self.partner_id, quantity=self.product_qty,
                 date=date, uom_id=self.product_uom)
-            if product_supplierinfo:
-                self.discount = product_supplierinfo.discount
+            self._apply_value_from_seller(seller)
         return res
+
+    @api.model
+    def _apply_value_from_seller(self, seller):
+        """Overload this function to prepare other data from seller,
+        like in purchase_triple_discount module"""
+        if not seller:
+            return
+        self.discount = seller.discount
