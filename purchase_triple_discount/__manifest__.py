826e5b01b1268f2087401bffc2ac76654e4c0404
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-08-08 09:44:41 +0200

    [MIG] purchase_triple_discount
    - remove dependency to obsolete module product_supplierinfo_discount
    - rename procurement.rule into stock.rule
    - Use new hook introduced in purchase_discount V12 to add discount2 and discount3
    - Introduce discount2 and discount3 in purchase.report model

--- purchase_triple_discount/__manifest__.py
+++ purchase_triple_discount/__manifest__.py
@@ -1,16 +1,17 @@
 # Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Account Invoice Triple Discount',
-    'version': '11.0.1.0.0',
+    'name': 'Purchase Order Triple Discount',
+    'version': '12.0.1.0.0',
     'category': 'Purchase Management',
-    'author': 'Tecnativa, '
+    'author': 'Tecnativa,'
+              'GRAP,'
               'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/purchase-workflow',
     'license': 'AGPL-3',
     'summary': 'Manage triple discount on purchase order lines',
     'depends': [
-        'product_supplierinfo_discount',
+        'purchase_discount',
         'account_invoice_triple_discount',
     ],
     'data': [
