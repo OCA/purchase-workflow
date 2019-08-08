2f03697031ba8c6bd86b216ca450f3ed2c580680
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-08-08 09:44:41 +0200

    [MIG] purchase_triple_discount
    - remove dependency to obsolete module product_supplierinfo_discount
    - rename procurement.rule into stock.rule
    - Use new hook introduced in purchase_discount V12 to add discount2 and discount3
    - Introduce discount2 and discount3 in purchase.report model

--- purchase_triple_discount/models/account_invoice.py
+++ purchase_triple_discount/models/account_invoice.py
@@ -8,8 +8,7 @@ class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
     def _prepare_invoice_line_from_po_line(self, line):
-        vals = super(AccountInvoice,
-                     self)._prepare_invoice_line_from_po_line(line)
+        vals = super()._prepare_invoice_line_from_po_line(line)
         vals['discount2'] = line.discount2
         vals['discount3'] = line.discount3
         return vals
