7d091a0e79733cad756328cf61f7240e4341460c
mreficent <miquel.raich@eficent.com>
2019-08-09 15:11:17 +0200

    [MIG] purchase_fiscal_position_update: Migration to 11.0

--- purchase_fiscal_position_update/models/purchase.py
+++ purchase_fiscal_position_update/models/purchase.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2011-2014 Julius Network Solutions SARL <contact@julius.fr>
 # @author Mathieu Vatel <mathieu _at_ julius.fr>
 # © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
@@ -12,8 +11,8 @@ class PurchaseOrder(models.Model):
 
     @api.onchange('fiscal_position_id')
     def fiscal_position_change(self):
-        '''Function executed by the on_change on the fiscal_position_id field
-        of a purchase order ; it updates taxes on all order lines'''
+        """Function executed by the onchange on the fiscal_position_id field
+        of a purchase order; it updates taxes on all order lines"""
         fp = self.fiscal_position_id
         for line in self.order_line:
             # product_id is a required field since v9
