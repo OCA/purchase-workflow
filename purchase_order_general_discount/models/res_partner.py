33106bd8dea06104be4734f3466e9998d2c24aed
David <david.vidal@tecnativa.com>
2019-08-27 14:14:16 +0200

    [ADD][11.0] purchase_order_general_discount: New Module

---
+++ purchase_order_general_discount/models/res_partner.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+from odoo.addons import decimal_precision as dp
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    purchase_general_discount = fields.Float(
+        digits=dp.get_precision('Discount'),
+        string='Purchase General Discount (%)',
+        company_dependent=True,
+    )
---
+++ purchase_order_general_discount/readme/USAGE.rst
@@ -0,0 +1,10 @@
+To set a partner default general discount you need to:
+
+#. Go to a partner and set the general discount in *Sales & Purchases* tab.
+
+To set a general discount in a purchase order you need to:
+
+#. Create a purchase order.
+#. Either select a partner with a purchase general discount defined or set one
+   in the summary section of the order.
+#. This discount will be applied to every line.
