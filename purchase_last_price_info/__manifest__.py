64dddbced1ebccf532b93982eb72d1a93f972a54
Lois Rilo <lois.rilo@eficent.com>
2019-09-23 16:38:24 +0200

    [12.0][MIG] purchase_last_price_info

--- purchase_last_price_info/__manifest__.py
+++ purchase_last_price_info/__manifest__.py
@@ -1,23 +1,17 @@
-
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    "name": "Product Last Price Info - Purchase",
-    "version": "11.0.1.0.0",
+    "name": "Purchase Product Last Price Info",
+    "version": "12.0.1.0.0",
     "category": "Purchase Management",
     "license": "AGPL-3",
     "author": "OdooMRP team, "
               "AvanzOSC, "
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+              "Tecnativa - Pedro M. Baeza, "
               "Odoo Community Association (OCA)",
+    "development_status": "Beta",
+    "maintainers": ['lreficent'],
     "website": "https://github.com/OCA/purchase-workflow",
-    "contributors": [
-        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
-        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
-        "Ana Juaristi <anajuaristi@avanzosc.es>",
-        "Carlos Lopez Mite <celm1990@hotmail.com>",
-        "Adria Gil Sorribes <adria.gil@eficent.com>",
-    ],
     "depends": [
         "purchase",
     ],
