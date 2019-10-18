15acc05bb0be851f60c4098f6fb3e8345759540c
hveficent <hector.villarreal@eficent.com>
2019-10-18 12:06:44 +0200

    [REFACTOR] s/procurement_rule/stock_rule across repo

--- purchase_discount/models/__init__.py
+++ purchase_discount/models/__init__.py
@@ -1,6 +1,6 @@
 from . import account_invoice
 from . import purchase_order
 from . import stock_move
-from . import procurement_rule
+from . import stock_rule
 from . import product_supplierinfo
 from . import res_partner
