5b646996acea3f933c09c09e100490406d465c67
Sergio Teruel <sergio.teruel@tecnativa.com>
2019-10-23 17:04:30 +0200

    [MIG] purchase_order_line_deep_sort: Migration to v12.0

--- purchase_order_line_deep_sort/__manifest__.py
+++ purchase_order_line_deep_sort/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Purchase Order Line Deep Sort',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     "author": "Tecnativa,"
               "Odoo Community Association (OCA)",
     'license': 'AGPL-3',
