4528274ffff03391f834c4e446bfc49970e960e2
Sergio Teruel <sergio.teruel@tecnativa.com>
2019-10-23 17:04:30 +0200

    [MIG] purchase_order_line_deep_sort: Migration to v12.0

--- purchase_order_line_deep_sort/models/res_config_settings.py
+++ purchase_order_line_deep_sort/models/res_config_settings.py
@@ -1,17 +1,26 @@
 # Copyright 2018 Tecnativa - Vicent Cubells <vicent.cubells@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    default_po_line_order = fields.Selection(
+    # In v12.0 is forbidden use field names like default_xxx
+    po_line_order_default = fields.Selection(
         related='company_id.default_po_line_order',
         string="Line Order",
+        readonly=False,
     )
-    default_po_line_direction = fields.Selection(
+    po_line_direction_default = fields.Selection(
         related='company_id.default_po_line_direction',
         string="Sort Direction",
+        readonly=False,
     )
+
+    @api.onchange('po_line_order_default')
+    def onchange_po_line_order_default(self):
+        """ Reset direction line order when user remove order field value """
+        if not self.po_line_order_default:
+            self.po_line_direction_default = False
