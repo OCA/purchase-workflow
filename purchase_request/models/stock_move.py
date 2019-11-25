744c0eb95d7d47c1ead000d8b6f49b84cd4aadcb
Cas Vissers <c.vissers@brahoo.nl>
2019-11-25 08:27:47 +0100

    [FIX] Incorrect mapping in compute

--- purchase_request/models/stock_move.py
+++ purchase_request/models/stock_move.py
@@ -67,7 +67,7 @@ class StockMove(models.Model):
     def _compute_purchase_request_ids(self):
         for rec in self:
             rec.purchase_request_ids = rec.\
-                purchase_request_allocation_ids.mapped('purchase_request_id')
+            purchase_request_allocation_ids.purchase_request_line_id.mapped('request_id')
 
     def _merge_moves_fields(self):
         res = super(StockMove, self)._merge_moves_fields()
