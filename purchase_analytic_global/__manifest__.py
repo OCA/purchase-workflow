221dc549fce6a2251770fcd9dadaf920ce74f9d9
Iryna Vushnevska <i.vyshnevska@mobilunity.com>
2019-11-26 12:16:45 +0200

    [MIG][REF] 12.0 purchase_analytic_global

---
+++ purchase_analytic_global/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2014-2019 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Purchase - Analytic Account Global",
+    "version": "12.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainer": "Camptocamp",
+    "license": "AGPL-3",
+    "category": "Purchase Management",
+    "complexity": "easy",
+    "depends": ["purchase"],
+    "website": "https://github.com/OCA/purchase-workflow",
+    "data": ["views/purchase.xml"],
+    "installable": True,
+    "auto_install": False,
+}
