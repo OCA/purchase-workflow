aef9cd919e344c9e1fea176eb283a2199eae1386
Iryna Vushnevska <i.vyshnevska@mobilunity.com>
2019-11-26 12:16:45 +0200

    [MIG][REF] 12.0 purchase_analytic_global

--- purchase_analytic_global/models/purchase.py
+++ purchase_analytic_global/models/purchase.py
@@ -1,26 +1,32 @@
-# -*- coding: utf-8 -*-
-# © 2014-2016 Camptocamp SA
+# Copyright 2014-2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+    _inherit = "purchase.order"
 
     account_analytic_id = fields.Many2one(
-        'account.analytic.account',
-        string='Analytic Account',
-        states={'confirmed': [('readonly', True)],
-                'approved': [('readonly', True)],
-                'done': [('readonly', True)]},
-        help="The analytic account that will be set on all the lines. "
-             "If you want to use different accounts, leave this field "
-             "empty and set the accounts individually on the lines.")
+        "account.analytic.account",
+        string="Analytic Account",
+        compute="_compute_analytic_account",
+        inverse="_inverse_analytic_account",
+        help="This account will be propagated to all lines, if you need "
+        "to use different accounts, define the account at line level.",
+    )
 
-    @api.onchange('account_analytic_id')
-    def onchange_default_analytic_id(self):
-        if not self.order_line:
-            return
-        for line in self.order_line:
-            line.account_analytic_id = self.account_analytic_id
+    @api.depends("order_line.account_analytic_id")
+    def _compute_analytic_account(self):
+        for rec in self:
+            account = rec.mapped("order_line.account_analytic_id")
+            if len(account) == 1:
+                rec.account_analytic_id = account
+            else:
+                rec.account_analytic_id = False
+
+    def _inverse_analytic_account(self):
+        for rec in self:
+            if rec.account_analytic_id:
+                for line in rec.order_line:
+                    line.account_analytic_id = rec.account_analytic_id
---
+++ purchase_analytic_global/readme/CONTRIBUTORS.rst
@@ -0,0 +1,3 @@
+* Guewen Baconnier <guewen.baconnier@camptocamp.com>
+* Yannick Vaucher <yannick.vaucher@camptocamp.com>
+* Iryna Vyshnevska <i.vyshnevska@mobilunity.com>
