7c269087feb19c4a8ccf9a441cf89a488f342072
pedro.gonzalez@pesol.es <pedro.gonzalez@pesol.es>
2019-11-28 13:06:46 +0100

    [IMP] purchase_tier_validation: black, isort

--- purchase_tier_validation/models/purchase_order.py
+++ purchase_tier_validation/models/purchase_order.py
@@ -6,6 +6,6 @@ from odoo import models
 
 class PurchaseOrder(models.Model):
     _name = "purchase.order"
-    _inherit = ['purchase.order', 'tier.validation']
-    _state_from = ['draft', 'sent', 'to approve']
-    _state_to = ['purchase', 'approved']
+    _inherit = ["purchase.order", "tier.validation"]
+    _state_from = ["draft", "sent", "to approve"]
+    _state_to = ["purchase", "approved"]
