597332ebc6999e3ef4cc3aed0cea5d28a6b07263
pedro.gonzalez@pesol.es <pedro.gonzalez@pesol.es>
2019-11-28 13:07:37 +0100

    [MIG] 13.0 purchase_tier_validation

--- purchase_tier_validation/__manifest__.py
+++ purchase_tier_validation/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Purchase Tier Validation",
     "summary": "Extends the functionality of Purchase Orders to "
     "support a tier validation process.",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Purchases",
     "website": "https://github.com/OCA/purchase-workflow",
     "author": "Eficent, Odoo Community Association (OCA)",
