96c5c9c30f7aba550b7f0ba0bce47e90177ce22a
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2019-12-16 15:28:52 +0100

    [12.0][FIX] purchase_open_qty

--- purchase_open_qty/models/purchase_order.py
+++ purchase_open_qty/models/purchase_order.py
@@ -29,14 +29,14 @@ class PurchaseOrderLine(models.Model):
                  'move_ids.product_uom_qty')
     def _compute_qty_to_receive(self):
         for line in self:
-            total = 0.0
+            total = line.product_uom_qty
             for move in line.move_ids.filtered(
-                    lambda m: m.state not in ('cancel', 'done')):
+                    lambda m: m.state == 'done'):
                 if move.product_uom != line.product_uom:
-                    total += move.product_uom._compute_quantity(
+                    total -= move.product_uom._compute_quantity(
                         move.product_uom_qty, line.product_uom)
                 else:
-                    total += move.product_uom_qty
+                    total -= move.product_uom_qty
             line.qty_to_receive = total
 
     qty_to_invoice = fields.Float(compute='_compute_qty_to_invoice',
