539f3bb55d46676b10d90655821764f41d2c6125
kongrattapong <rattapong.chok@gmail.com>
2019-12-18 09:23:57 +0700

    [IMP] purchase_force_invoiced: black, isort

--- purchase_force_invoiced/__manifest__.py
+++ purchase_force_invoiced/__manifest__.py
@@ -3,18 +3,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Purchase Force Invoiced',
-    'summary': 'Allows to force the billing status of the purchase order to '
-               '"Invoiced"',
-    'version': '12.0.1.0.0',
-    "author": "Eficent,"
-              "Odoo Community Association (OCA)",
-    'category': 'Purchase Management',
-    'license': 'AGPL-3',
-    'website': "https://github.com/OCA/purchase-workflow",
-    'depends': ['purchase'],
-    'data': [
-        'view/purchase_view.xml'
-    ],
-    'installable': True,
+    "name": "Purchase Force Invoiced",
+    "summary": "Allows to force the billing status of the purchase order to "
+    '"Invoiced"',
+    "version": "12.0.1.0.0",
+    "author": "Eficent," "Odoo Community Association (OCA)",
+    "category": "Purchase Management",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "depends": ["purchase"],
+    "data": ["view/purchase_view.xml"],
+    "installable": True,
 }
