01c37c2f26d4ed4b2ef55c807354f8b041d55a89
kongrattapong <rattapong.chok@gmail.com>
2019-12-18 16:38:25 +0700

    [IMP] purchase_product_usage: black, isort

--- purchase_product_usage/__manifest__.py
+++ purchase_product_usage/__manifest__.py
@@ -3,21 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0).
 
 {
-    'name': "Purchase Product Usage",
-    'version': '12.0.1.0.0',
-    'category': 'Purchase Management',
-    'author': "Eficent, "
-              "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/purchase-workflow',
-    'license': 'AGPL-3',
-    "depends": [
-        'purchase',
-    ],
+    "name": "Purchase Product Usage",
+    "version": "12.0.1.0.0",
+    "category": "Purchase Management",
+    "author": "Eficent, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "license": "AGPL-3",
+    "depends": ["purchase"],
     "data": [
-        'security/purchase_product_usage.xml',
-        'security/ir.model.access.csv',
-        'views/purchase_order_line_view.xml',
-        'views/purchase_product_usage_view.xml',
+        "security/purchase_product_usage.xml",
+        "security/ir.model.access.csv",
+        "views/purchase_order_line_view.xml",
+        "views/purchase_product_usage_view.xml",
     ],
-    "installable": True
+    "installable": True,
 }
