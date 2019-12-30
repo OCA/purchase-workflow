0ae216cf4c36a6136dd59722092436e64cae5455
ahenriquez <ahenriquez@eficent.com>
2019-12-30 12:26:40 +0100

    [MIG] product_supplier_code_purchase to v12

--- product_supplier_code_purchase/__manifest__.py
+++ product_supplier_code_purchase/__manifest__.py
@@ -1,14 +1,12 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015-17 Eficent Business and IT Consulting Services, S.L.
-#           (http://www.eficent.com)
+# Copyright 2015-20 ForgeFlow, S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Product Supplier Code in Purchase",
     "summary": """This module adds to the purchase order line the supplier
                 code defined in the product.""",
-    "version": "10.0.1.0.0",
-    "author": "Eficent, Odoo Community Association (OCA)",
+    "version": "12.0.1.0.0",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Purchase Management",
     "license": "AGPL-3",
