58db86c37cb4003e77646142e2ef7277ff8fc211
ahenriquez <ahenriquez@eficent.com>
2019-12-30 12:26:40 +0100

    [MIG] product_supplier_code_purchase to v12

--- product_supplier_code_purchase/models/__init__.py
+++ product_supplier_code_purchase/models/__init__.py
@@ -1,6 +1,1 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015-17 Eficent Business and IT Consulting Services, S.L.
-#           (http://www.eficent.com)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from . import purchase_order
---
+++ product_supplier_code_purchase/tests/__init__.py
@@ -0,0 +1,1 @@
+from . import test_product_supplier_code_purchase
