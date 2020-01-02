6c73b6f7ab30b700d657948eda60c44c9be1bac0
kongrattapong <rattapong.chok@gmail.com>
2020-01-02 14:51:52 +0700

    [IMP] purchase_request; black, isort

--- purchase_request/models/product_template.py
+++ purchase_request/models/product_template.py
@@ -9,5 +9,5 @@ class ProductTemplate(models.Model):
 
     purchase_request = fields.Boolean(
         help="Check this box to generate Purchase Request instead of "
-             "generating Requests For Quotation from procurement."
+        "generating Requests For Quotation from procurement."
     )
