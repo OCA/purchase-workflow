7c200186e765238c5ec622c6b97fcd15d8ab9050
kongrattapong <rattapong.chok@gmail.com>
2020-01-02 15:02:48 +0700

    [MIG] purchase_request: Migration to 13.0

--- purchase_request/models/__init__.py
+++ purchase_request/models/__init__.py
@@ -1,3 +1,5 @@
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
+
 from . import purchase_request_allocation
 from . import orderpoint
 from . import purchase_request
--- purchase_request/models/product_template.py
+++ purchase_request/models/product_template.py
@@ -1,5 +1,5 @@
 # Copyright 2018-2019 Eficent Business and IT Consulting Services S.L.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
 
 from odoo import fields, models
 
