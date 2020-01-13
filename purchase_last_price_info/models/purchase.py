4d25881ff834e0db3facd22ed31328f77587c42c
kongrattapong <rattapong.chok@gmail.com>
2020-01-13 11:42:38 +0700

    [MIG] purchase_last_price_info: Migration to 13.0

--- purchase_last_price_info/models/purchase.py
+++ purchase_last_price_info/models/purchase.py
@@ -2,20 +2,18 @@
 # Copyright 2019 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 
-from odoo import api, models
+from odoo import models
 
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    @api.multi
     def button_approve(self, force=False):
         res = super().button_approve(force)
         for rec in self:
             rec.order_line.mapped("product_id").set_product_last_purchase(rec.id)
         return res
 
-    @api.multi
     def button_cancel(self):
         res = super().button_cancel()
         for rec in self:
