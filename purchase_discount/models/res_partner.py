a2259929ca8382f110a3a44928e0254c879a2df7
Samuel Piquet <samuel.piquet@gmail.com>
2020-01-14 12:12:33 +0100

    [IMP] : black, isort

--- purchase_discount/models/res_partner.py
+++ purchase_discount/models/res_partner.py
@@ -2,7 +2,8 @@
 #        Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import fields, models
+
 from odoo.addons import decimal_precision as dp
 
 
@@ -10,7 +11,8 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     default_supplierinfo_discount = fields.Float(
-        string='Default Supplier Discount (%)',
-        digits=dp.get_precision('Discount'),
+        string="Default Supplier Discount (%)",
+        digits=dp.get_precision("Discount"),
         help="This value will be used as the default one, for each new"
-        " supplierinfo line depending on that supplier.")
+        " supplierinfo line depending on that supplier.",
+    )
