0e327072ce19221a018cf3bf48128dd440f3ed8e
Samuel Piquet <samuel.piquet@gmail.com>
2020-01-14 15:00:45 +0100

    Standard migration

--- purchase_discount/models/stock_rule.py
+++ purchase_discount/models/stock_rule.py
@@ -6,19 +6,21 @@ from odoo import api, models
 class StockRule(models.Model):
     _inherit = "stock.rule"
 
-    @api.multi
     def _prepare_purchase_order_line(
-        self, product_id, product_qty, product_uom, values, po, partner
+        self, product_id, product_qty, product_uom, company_id, values, po
     ):
         """Apply the discount to the created purchase order"""
         res = super()._prepare_purchase_order_line(
-            product_id, product_qty, product_uom, values, po, partner
+            product_id, product_qty, product_uom, company_id, values, po
         )
         date = None
         if po.date_order:
             date = po.date_order.date()
         seller = product_id._select_seller(
-            partner_id=partner, quantity=product_qty, date=date, uom_id=product_uom
+            partner_id=values["supplier"].name,
+            quantity=product_qty,
+            date=date,
+            uom_id=product_uom,
         )
         res.update(self._prepare_purchase_order_line_from_seller(seller))
         return res
