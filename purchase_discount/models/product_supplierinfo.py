3192d3316bd092434fab001761e94b8680a3d399
Samuel Piquet <samuel.piquet@gmail.com>
2020-01-14 15:00:45 +0100

    Standard migration

--- purchase_discount/models/product_supplierinfo.py
+++ purchase_discount/models/product_supplierinfo.py
@@ -4,16 +4,13 @@
 
 from odoo import api, fields, models
 
-import odoo.addons.decimal_precision as dp
-
 
 class ProductSupplierInfo(models.Model):
     _inherit = "product.supplierinfo"
 
-    discount = fields.Float(string="Discount (%)", digits=dp.get_precision("Discount"))
+    discount = fields.Float(string="Discount (%)", digits="Discount")
 
     @api.onchange("name")
-    @api.multi
     def onchange_name(self):
         """ Apply the default supplier discount of the selected supplier """
         for supplierinfo in self.filtered("name"):
