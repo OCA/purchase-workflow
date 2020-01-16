3e43bb298c6871fdcff14f7e25678264a1cdaf11
hveficent <hector.villarreal@forgeflow.com>
2020-01-16 11:58:56 +0100

    [MIG] purchase_request_tier_validation: Migration to 13.0

--- purchase_request_tier_validation/__manifest__.py
+++ purchase_request_tier_validation/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Purchase Request Tier Validation",
     "summary": "Extends the functionality of Purchase Requests to "
     "support a tier validation process.",
-    "version": "12.0.1.1.0",
+    "version": "13.0.1.1.0",
     "category": "Purchase Management",
     "website": "https://github.com/OCA/purchase-workflow",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
