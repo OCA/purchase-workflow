c7d0452444295c06f57da0a7bf24d621c66da91a
hveficent <hector.villarreal@forgeflow.com>
2020-01-16 11:31:28 +0100

    [MIG] purchase_request_department: Migration to 13.0

--- purchase_request_department/__manifest__.py
+++ purchase_request_department/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-2020 Forgeflow S.L.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
 {
     "name": "Purchase Request Department",
-    "author": "Eficent, " "Odoo Community Association (OCA)",
-    "version": "12.0.1.0.0",
+    "author": "ForgeFlow, " "Odoo Community Association (OCA)",
+    "version": "13.0.1.0.0",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Purchase Management",
     "post_init_hook": "post_init_hook",
