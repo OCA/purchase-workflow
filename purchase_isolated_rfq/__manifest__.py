cd8cdb07145b452ca900162041b346fd4d037a90
kongrattapong <rattapong.chok@gmail.com>
2020-01-28 16:02:31 +0700

    [13.0][ADD] purchase_isolated_rfq

---
+++ purchase_isolated_rfq/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Ecosoft Co., Ltd (https://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+{
+    "name": "Purchase Isolated RFQ",
+    "version": "13.0.1.0.0",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "category": "Purchases",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "depends": ["purchase"],
+    "license": "AGPL-3",
+    "data": ["data/ir_sequence_data.xml", "views/purchase_views.xml"],
+    "installable": True,
+    "post_init_hook": "post_init_hook",
+}
