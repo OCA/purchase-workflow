2dd7b95cd8dde68363d93fd32ca587270900d253
Kevin Khao <kevinkhao@gmail.com>
2020-01-31 14:06:57 +0100

    [MIG][12.0] purchase_picking_state: Migration to 12.0

--- purchase_picking_state/models/purchase.py
+++ purchase_picking_state/models/purchase.py
@@ -6,39 +6,43 @@ from odoo.tools.translate import _
 
 
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+    _inherit = "purchase.order"
 
     @api.model
     def get_picking_state(self):
         return [
-            ('draft', ''),
-            ('cancel', _('Cancelled')),
-            ('not_received', _('Not Received')),
-            ('partially_received', _('Partially Received')),
-            ('done', _('Transferred')),
+            ("draft", ""),
+            ("cancel", _("Cancelled")),
+            ("not_received", _("Not Received")),
+            ("partially_received", _("Partially Received")),
+            ("done", _("Transferred")),
         ]
 
-    @api.multi
-    @api.depends('picking_ids', 'picking_ids.state')
+    @api.depends("picking_ids", "picking_ids.state")
     def _compute_picking_state(self):
         for purchase in self:
             if purchase.picking_ids:
                 pickings_state = set(
-                    [picking.state for picking in purchase.picking_ids])
-                if pickings_state == set(['cancel']):
-                    purchase.picking_state = 'cancel'
-                elif (pickings_state == set(['cancel', 'done']) or
-                      pickings_state == set(['done'])):
-                    purchase.picking_state = 'done'
-                elif 'done' in pickings_state:
-                    purchase.picking_state = 'partially_received'
+                    [picking.state for picking in purchase.picking_ids]
+                )
+                if pickings_state == set(["cancel"]):
+                    purchase.picking_state = "cancel"
+                elif pickings_state == set(
+                    ["cancel", "done"]
+                ) or pickings_state == set(["done"]):
+                    purchase.picking_state = "done"
+                elif "done" in pickings_state:
+                    purchase.picking_state = "partially_received"
                 else:
-                    purchase.picking_state = 'not_received'
+                    purchase.picking_state = "not_received"
             else:
-                purchase.picking_state = 'draft'
+                purchase.picking_state = "draft"
 
     picking_state = fields.Selection(
-        string="Picking status", readonly=True,
-        compute='_compute_picking_state', store=True,
-        selection='get_picking_state',
-        help="Overall status based on all pickings")
+        string="Picking status",
+        readonly=True,
+        compute="_compute_picking_state",
+        store=True,
+        selection="get_picking_state",
+        help="Overall status based on all pickings",
+    )
---
+++ purchase_picking_state/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module adds a "picking status" field on Purchase Orders, displaying the general state of the pickings related to that Order.
