976d921cbd4f5c543be307b97dadab354505c65a
hveficent <hector.villarreal@forgeflow.com>
2020-01-31 12:20:19 +0100

    [FIX] Proper Run Buy implementation

--- purchase_request/__manifest__.py
+++ purchase_request/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2018-2019 Eficent Business and IT Consulting Services S.L.
+# Copyright 2018-2019 ForgeFlow, S.L.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
 {
     "name": "Purchase Request",
-    "author": "Eficent, " "Odoo Community Association (OCA)",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
     "version": "13.0.1.0.0",
     "summary": "Use this module to have notification of requirements of "
     "materials and/or external services and keep track of such "
