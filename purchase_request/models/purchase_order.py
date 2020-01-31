508237c41814eb9391aa8c6c3e257c49db12a64d
hveficent <hector.villarreal@forgeflow.com>
2020-01-31 12:20:19 +0100

    [FIX] Proper Run Buy implementation

--- purchase_request/models/purchase_order.py
+++ purchase_request/models/purchase_order.py
@@ -1,4 +1,4 @@
-# Copyright 2018-2019 Eficent Business and IT Consulting Services S.L.
+# Copyright 2018-2019 ForgeFlow, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
 
 from odoo import _, api, exceptions, fields, models
