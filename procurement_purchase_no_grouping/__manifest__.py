3788856702020a6422304ffe8f7eebe09d528a2d
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-02-12 15:50:07 +0100

    [IMP] procurement_purchase_no_grouping: black, isort

--- procurement_purchase_no_grouping/__manifest__.py
+++ procurement_purchase_no_grouping/__manifest__.py
@@ -4,19 +4,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Procurement Purchase No Grouping',
-    'version': '12.0.1.0.0',
-    'author': 'AvanzOSC,'
-              'Tecnativa,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/purchase-workflow',
-    'category': 'Procurements',
-    'depends': [
-        'purchase_stock',
-    ],
-    'data': [
-        'views/product_category_view.xml',
-    ],
-    'installable': True,
-    'license': 'AGPL-3',
+    "name": "Procurement Purchase No Grouping",
+    "version": "13.0.1.0.0",
+    "author": "AvanzOSC," "Tecnativa," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "category": "Procurements",
+    "depends": ["purchase_stock"],
+    "data": ["views/product_category_view.xml"],
+    "installable": True,
+    "license": "AGPL-3",
 }
