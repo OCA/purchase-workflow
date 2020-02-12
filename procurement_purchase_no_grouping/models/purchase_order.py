befb12065b23297bee844fdb2be838f9bfb2fc2d
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-02-12 15:50:07 +0100

    [IMP] procurement_purchase_no_grouping: black, isort

--- procurement_purchase_no_grouping/models/purchase_order.py
+++ procurement_purchase_no_grouping/models/purchase_order.py
@@ -7,13 +7,14 @@ from odoo import models
 
 
 class PurchaseOrderLine(models.Model):
-    _inherit = 'purchase.order.line'
+    _inherit = "purchase.order.line"
 
-    def _merge_in_existing_line(self, product_id, product_qty, product_uom,
-                                location_id, name, origin, values):
+    def _merge_in_existing_line(
+        self, product_id, product_qty, product_uom, location_id, name, origin, values
+    ):
         grouping = product_id.categ_id.procured_purchase_grouping
-        if grouping == 'line':
+        if grouping == "line":
             return False
         return super()._merge_in_existing_line(
-            product_id, product_qty, product_uom, location_id, name, origin,
-            values)
+            product_id, product_qty, product_uom, location_id, name, origin, values
+        )
