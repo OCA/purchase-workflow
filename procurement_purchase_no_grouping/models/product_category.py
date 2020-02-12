bb3dec3f2f9c19aa347849fe1dad8c6dc01ca56f
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-02-12 15:50:07 +0100

    [IMP] procurement_purchase_no_grouping: black, isort

--- procurement_purchase_no_grouping/models/product_category.py
+++ procurement_purchase_no_grouping/models/product_category.py
@@ -2,24 +2,28 @@
 # Copyright 2015-2016 - Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ProductCategory(models.Model):
-    _inherit = 'product.category'
+    _inherit = "product.category"
 
     procured_purchase_grouping = fields.Selection(
-        [('standard', 'Standard grouping'),
-         ('line', 'No line grouping'),
-         ('order', 'No order grouping')],
-        string='Procured purchase grouping', default='standard',
+        [
+            ("standard", "Standard grouping"),
+            ("line", "No line grouping"),
+            ("order", "No order grouping"),
+        ],
+        string="Procured purchase grouping",
+        default="standard",
         help="Select the behaviour for grouping procured purchases for the "
-             "the products of this category:\n"
-             "* Standard grouping (default): Procurements will generate "
-             "purchase orders as always, grouping lines and orders when "
-             "possible.\n"
-             "* No line grouping: If there are any open purchase order for "
-             "the same supplier, it will be reused, but lines won't be "
-             "merged.\n"
-             "* No order grouping: This option will prevent any kind of "
-             "grouping.")
+        "the products of this category:\n"
+        "* Standard grouping (default): Procurements will generate "
+        "purchase orders as always, grouping lines and orders when "
+        "possible.\n"
+        "* No line grouping: If there are any open purchase order for "
+        "the same supplier, it will be reused, but lines won't be "
+        "merged.\n"
+        "* No order grouping: This option will prevent any kind of "
+        "grouping.",
+    )
