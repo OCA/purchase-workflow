172d995bb79aea6d60dc23562fc4329a3c7ab7ca
kongrattapong <rattapong.chok@gmail.com>
2020-02-21 17:46:13 +0700

    [IMP] purchase_deposit: black, isort

--- purchase_deposit/__manifest__.py
+++ purchase_deposit/__manifest__.py
@@ -3,21 +3,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Purchase Deposit',
-    'version': '12.0.1.0.0',
-    'summary': 'Option to create deposit from purchase order',
-    'author': 'Elico Corp, Ecosoft, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/purchase-workflow',
-    'category': 'Purchase Management',
-    'license': 'AGPL-3',
-    'depends': [
-        'purchase',
+    "name": "Purchase Deposit",
+    "version": "12.0.1.0.0",
+    "summary": "Option to create deposit from purchase order",
+    "author": "Elico Corp, Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "category": "Purchase Management",
+    "license": "AGPL-3",
+    "depends": ["purchase"],
+    "data": [
+        "wizard/purchase_make_invoice_advance_views.xml",
+        "views/res_config_settings_views.xml",
+        "views/purchase_view.xml",
     ],
-    'data': [
-        'wizard/purchase_make_invoice_advance_views.xml',
-        'views/res_config_settings_views.xml',
-        'views/purchase_view.xml',
-    ],
-    'installable': True,
-    'auto_install': False,
+    "installable": True,
+    "auto_install": False,
 }
