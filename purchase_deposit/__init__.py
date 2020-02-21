ae6a4db5692048da54b2b37ab7596ed26f757812
kongrattapong <rattapong.chok@gmail.com>
2020-02-21 17:46:13 +0700

    [IMP] purchase_deposit: black, isort

--- purchase_deposit/__init__.py
+++ purchase_deposit/__init__.py
@@ -2,5 +2,5 @@
 # Copyright 2019 Ecosoft Co., Ltd., Kitti U. <kittiu@ecosoft.co.th>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import models 
+from . import models
 from . import wizard
