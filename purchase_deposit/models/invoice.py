c784aa44c2e82df7ddd0a7e0ffe971dda01553f1
kongrattapong <rattapong.chok@gmail.com>
2020-02-21 17:46:13 +0700

    [IMP] purchase_deposit: black, isort

--- purchase_deposit/models/invoice.py
+++ purchase_deposit/models/invoice.py
@@ -6,11 +6,10 @@ from odoo import models
 
 
 class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
     def _prepare_invoice_line_from_po_line(self, line):
-        res = super(AccountInvoice, self).\
-            _prepare_invoice_line_from_po_line(line)
+        res = super(AccountInvoice, self)._prepare_invoice_line_from_po_line(line)
         if line.is_deposit:
-            res['quantity'] = -1 * line.qty_invoiced
+            res["quantity"] = -1 * line.qty_invoiced
         return res
