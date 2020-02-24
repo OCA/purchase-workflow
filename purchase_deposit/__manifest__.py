322c2eeffff6dfab1817fc62c389466b71f32e8a
kongrattapong <rattapong.chok@gmail.com>
2020-02-24 11:24:50 +0700

    [MIG] purchase_deposit: Migration to 13.0

--- purchase_deposit/__manifest__.py
+++ purchase_deposit/__manifest__.py
@@ -1,10 +1,10 @@
 # Copyright 2019 Elico Corp, Dominique K. <dominique.k@elico-corp.com.sg>
 # Copyright 2019 Ecosoft Co., Ltd., Kitti U. <kittiu@ecosoft.co.th>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Purchase Deposit",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "summary": "Option to create deposit from purchase order",
     "author": "Elico Corp, Ecosoft, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
