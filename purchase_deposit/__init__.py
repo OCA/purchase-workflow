cf331ace547c87a0d994b1912e97b649376f1b60
kongrattapong <rattapong.chok@gmail.com>
2020-02-24 11:24:50 +0700

    [MIG] purchase_deposit: Migration to 13.0

--- purchase_deposit/__init__.py
+++ purchase_deposit/__init__.py
@@ -1,6 +1,6 @@
 # Copyright 2019 Elico Corp, Dominique K. <dominique.k@elico-corp.com.sg>
 # Copyright 2019 Ecosoft Co., Ltd., Kitti U. <kittiu@ecosoft.co.th>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import models
 from . import wizard
