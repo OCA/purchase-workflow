362590222ffcb2ed12ee0f9db74832ef365ef289
Manuel Calero <manuel.calero@tecnativa.com>
2020-02-28 18:15:18 +0100

    [WIP] purchase_order_uninvoiced_amount 12.0

---
+++ purchase_order_uninvoiced_amount/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Tecnativa - Manuel Calero
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Purchase Order Univoiced Amount",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "version": "12.0.1.0.0",
+    'development_status': 'Beta',
+    "website": "https://github.com/OCA/purchase-workflow",
+    'category': 'Purchase',
+    'license': 'AGPL-3',
+    'application': False,
+    "installable": True,
+    'depends': [
+        'purchase',
+    ],
+    "data": [
+        'views/purchase_order_view.xml',
+    ],
+}
