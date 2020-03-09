b22a9256779e89f6459802840569c8070d72062e
ps-tubtim <pimolnats@ecosoft.co.th>
2020-03-09 14:03:19 +0700

    [IMP] purchase_requisition_tier_validation: black, isort

--- purchase_requisition_tier_validation/tests/test_purchase_requisition_tier_validation.py
+++ purchase_requisition_tier_validation/tests/test_purchase_requisition_tier_validation.py
@@ -7,8 +7,10 @@ from odoo.tests import common
 class TestHrExpenseTierValidation(common.TransactionCase):
     def setUp(self):
         super(TestHrExpenseTierValidation, self).setUp()
-        self.tier_definition = self.env['tier.definition']
+        self.tier_definition = self.env["tier.definition"]
 
     def test_get_tier_validation_model_names(self):
-        self.assertIn('purchase.requisition',
-                      self.tier_definition._get_tier_validation_model_names())
+        self.assertIn(
+            "purchase.requisition",
+            self.tier_definition._get_tier_validation_model_names(),
+        )
