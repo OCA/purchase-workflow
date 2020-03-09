984db455ef8d7d93096eac3275c8ff9802949e78
ps-tubtim <pimolnats@ecosoft.co.th>
2020-03-09 15:55:30 +0700

    [IMP] purchase_work_acceptance: black, isort

--- purchase_work_acceptance/models/res_config_settings.py
+++ purchase_work_acceptance/models/res_config_settings.py
@@ -5,25 +5,25 @@ from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     group_enable_wa_on_po = fields.Boolean(
         string="Enable WA on Purchase Order",
-        implied_group='purchase_work_acceptance.group_enable_wa_on_po',
+        implied_group="purchase_work_acceptance.group_enable_wa_on_po",
     )
     group_enable_wa_on_in = fields.Boolean(
         string="Enable WA on Goods Receipt",
-        implied_group='purchase_work_acceptance.group_enable_wa_on_in',
+        implied_group="purchase_work_acceptance.group_enable_wa_on_in",
     )
     group_enforce_wa_on_in = fields.Boolean(
         string="Enforce WA on Goods Receipt",
-        implied_group='purchase_work_acceptance.group_enforce_wa_on_in',
+        implied_group="purchase_work_acceptance.group_enforce_wa_on_in",
     )
     group_enable_wa_on_invoice = fields.Boolean(
         string="Enable WA on Vendor Bill",
-        implied_group='purchase_work_acceptance.group_enable_wa_on_invoice',
+        implied_group="purchase_work_acceptance.group_enable_wa_on_invoice",
     )
     group_enforce_wa_on_invoice = fields.Boolean(
         string="Enforce WA on Vendor Bill",
-        implied_group='purchase_work_acceptance.group_enforce_wa_on_invoice',
+        implied_group="purchase_work_acceptance.group_enforce_wa_on_invoice",
     )
