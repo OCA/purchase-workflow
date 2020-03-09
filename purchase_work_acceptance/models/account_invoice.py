3f6b97a04309fb62703c50e3699d668ceb86f123
ps-tubtim <pimolnats@ecosoft.co.th>
2020-03-09 15:55:30 +0700

    [IMP] purchase_work_acceptance: black, isort

--- purchase_work_acceptance/models/account_invoice.py
+++ purchase_work_acceptance/models/account_invoice.py
@@ -1,42 +1,41 @@
 # Copyright 2019 Ecosoft Co., Ltd. (http://ecosoft.co.th)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
-    require_wa = fields.Boolean(
-        compute='_compute_require_wa',
-    )
+    require_wa = fields.Boolean(compute="_compute_require_wa")
     wa_id = fields.Many2one(
-        comodel_name='work.acceptance',
-        string='WA Reference',
+        comodel_name="work.acceptance",
+        string="WA Reference",
         copy=False,
         domain=lambda self: [
-            ('state', '=', 'accept'),
-            ('purchase_id', '=', self._context.get('active_id'))],
-        help='To control quantity and unit price of the vendor bill, to be '
-             'according to the quantity and unit price of the work acceptance.',
+            ("state", "=", "accept"),
+            ("purchase_id", "=", self._context.get("active_id")),
+        ],
+        help="To control quantity and unit price of the vendor bill, to be "
+        "according to the quantity and unit price of the work acceptance.",
     )
 
     @api.multi
     def _compute_require_wa(self):
         self.require_wa = self.env.user.has_group(
-            'purchase_work_acceptance.group_enforce_wa_on_invoice')
+            "purchase_work_acceptance.group_enforce_wa_on_invoice"
+        )
 
     def _prepare_invoice_line_from_po_line(self, line):
         res = super()._prepare_invoice_line_from_po_line(line)
-        wa_line = self.wa_id.wa_line_ids.filtered(
-            lambda l: l.purchase_line_id == line)
+        wa_line = self.wa_id.wa_line_ids.filtered(lambda l: l.purchase_line_id == line)
         if wa_line:
-            res['quantity'] = wa_line.product_qty
-            res['uom_id'] = wa_line.product_uom
+            res["quantity"] = wa_line.product_qty
+            res["uom_id"] = wa_line.product_uom
         return res
 
-    @api.onchange('purchase_id')
+    @api.onchange("purchase_id")
     def purchase_order_change(self):
         res = super().purchase_order_change()
         if self.wa_id:
@@ -51,7 +50,8 @@ class AccountInvoice(models.Model):
                 wa_line = {}
                 for line in rec.wa_id.wa_line_ids:
                     qty = line.product_uom._compute_quantity(
-                        line.product_qty, line.product_id.uom_id)
+                        line.product_qty, line.product_id.uom_id
+                    )
                     if qty > 0.0:
                         if line.product_id.id in wa_line.keys():
                             qty_old = wa_line[line.product_id.id]
@@ -61,7 +61,8 @@ class AccountInvoice(models.Model):
                 invoice_line = {}
                 for line in rec.invoice_line_ids:
                     qty = line.uom_id._compute_quantity(
-                        line.quantity, line.product_id.uom_id)
+                        line.quantity, line.product_id.uom_id
+                    )
                     if qty > 0.0:
                         if line.product_id.id in invoice_line.keys():
                             qty_old = invoice_line[line.product_id.id]
@@ -69,6 +70,10 @@ class AccountInvoice(models.Model):
                         else:
                             invoice_line[line.product_id.id] = qty
                 if wa_line != invoice_line:
-                    raise ValidationError(_('You cannot validate a bill if '
-                                          'Quantity not equal accepted quantity'))
+                    raise ValidationError(
+                        _(
+                            "You cannot validate a bill if "
+                            "Quantity not equal accepted quantity"
+                        )
+                    )
         return super().action_invoice_open()
