e52b83eb4acd65d9aceb87ac175dcf62e799a10c
ps-tubtim <pimolnats@ecosoft.co.th>
2020-03-11 16:43:32 +0700

    [MIG] purchase_work_acceptance: Migration to 13.0

--- purchase_work_acceptance/models/__init__.py
+++ purchase_work_acceptance/models/__init__.py
@@ -1,6 +1,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import account_invoice
+from . import account_move
 from . import purchase
 from . import res_config_settings
 from . import stock_picking
