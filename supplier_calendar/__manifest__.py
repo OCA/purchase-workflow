97fecb0c3cbee493734e146bbed9fabc2c22671a
Núria Martín Xifré <nuria.martin@eficent.com>
2020-03-19 11:45:35 +0100

    [12.0][ADD]supplier_calendar

---
+++ supplier_calendar/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2020 ForgeFlow
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Supplier Calendar",
+    "summary": "Supplier Calendar",
+    'version': '12.0.1.0.0',
+    "category": "Purchase Management",
+    "author": "ForgeFlow,"
+               "Odoo Community Association (OCA)",
+    "maintainers": ["NuriaMForgeFlow"],
+    "license": "LGPL-3",
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    "depends": [
+        "purchase_stock",
+        "resource",
+    ],
+    "data": [
+        'views/res_partner_view.xml',
+        'views/product_view.xml'
+    ],
+}
