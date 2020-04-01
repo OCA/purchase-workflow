d52f0e6448206c1681e458ba5b166a40e6798538
Manuel Calero <manuel.calero@tecnativa.com>
2020-04-01 17:51:43 +0200

    [MIG] purchase_allowed_product: Migration to 12.0

--- purchase_allowed_product/__manifest__.py
+++ purchase_allowed_product/__manifest__.py
@@ -1,11 +1,12 @@
 # © 2016 Chafique DELLI @ Akretion
 # © 2017 Today Mourad EL HADJ MIMOUNE @ Akretion
+# 2020 Manuel Calero - Tecnativa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Account Invoice Allowed Product',
     'summary': 'This module allows to select only products that can be '
                'supplied by the supplier',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Accounting & Finance',
     'website': 'https://github.com/OCA/purchase-workflow',
     'author': 'Akretion, Odoo Community Association (OCA)',
