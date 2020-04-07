84ad4bc1030214a248ec9e54f4d527f3d82ba947
HviorForgeFlow <hector.villarreal@forgeflow.com>
2020-04-07 09:19:35 +0200

    [13.0][ADD] purchase_warn_message

---
+++ purchase_warn_message/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 ForgeFlow S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Purchase Warn Message",
+    "summary": """
+        Add a popup warning on purchase to ensure warning is populated""",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "www.github.com/OCA/stock-logistics-workflow.git",
+    "depends": ["purchase"],
+    "data": ["views/purchase_order_views.xml"],
+}
