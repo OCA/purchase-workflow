f0c39fa97cc1bd68f26594dd1695ad1487d092d3
Lluís Rovira @PlanetaTIC <lrovira@planetatic.com>
2020-04-22 18:48:58 +0200

    [FIX] Flake8

--- product_brand_supplier/models/purchase.py
+++ product_brand_supplier/models/purchase.py
@@ -2,7 +2,7 @@
 # Copyright 2018 PlanetaTIC - Lluís Rovira <lrovira@planetatic.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api, _
+from odoo import models, api, _
 
 
 class ProcurementOrder(models.Model):
