217d1b06db0880cd754d5d9e23c05e2472685c88
ps-tubtim <pimolnats@ecosoft.co.th>
2020-04-22 11:28:32 +0700

    [IMP] purchase_order_type: black, isort, prettier

--- purchase_order_type/models/res_partner.py
+++ purchase_order_type/models/res_partner.py
@@ -1,11 +1,12 @@
 # Copyright 2015 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     purchase_type = fields.Many2one(
-        comodel_name='purchase.order.type', string='Purchase Order Type')
+        comodel_name="purchase.order.type", string="Purchase Order Type"
+    )
