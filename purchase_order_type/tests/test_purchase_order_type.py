6cbad7b1548a302a6fc08ae619ff4d96ad06202f
ps-tubtim <pimolnats@ecosoft.co.th>
2020-04-22 11:28:32 +0700

    [IMP] purchase_order_type: black, isort, prettier

--- purchase_order_type/tests/test_purchase_order_type.py
+++ purchase_order_type/tests/test_purchase_order_type.py
@@ -2,38 +2,37 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 import time
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+
 from odoo.tests import common
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
 
 class TestPurchaseOrderType(common.SavepointCase):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.po_obj = cls.env['purchase.order']
+        cls.po_obj = cls.env["purchase.order"]
         # Partner
-        cls.partner1 = cls.env.ref('base.res_partner_1')
+        cls.partner1 = cls.env.ref("base.res_partner_1")
         # Products
-        cls.product1 = cls.env.ref('product.product_product_7')
-        cls.product2 = cls.env.ref('product.product_product_9')
-        cls.product3 = cls.env.ref('product.product_product_11')
+        cls.product1 = cls.env.ref("product.product_product_7")
+        cls.product2 = cls.env.ref("product.product_product_9")
+        cls.product3 = cls.env.ref("product.product_product_11")
         # Purchase Type
-        cls.type1 = cls.env.ref('purchase_order_type.po_type_regular')
-        cls.type2 = cls.env.ref('purchase_order_type.po_type_planned')
+        cls.type1 = cls.env.ref("purchase_order_type.po_type_regular")
+        cls.type2 = cls.env.ref("purchase_order_type.po_type_planned")
         # Payment Term
-        cls.payterm = cls.env.ref('account.account_payment_term_immediate')
+        cls.payterm = cls.env.ref("account.account_payment_term_immediate")
         # Incoterm
-        cls.incoterm = cls.env.ref('account.incoterm_EXW')
+        cls.incoterm = cls.env.ref("account.incoterm_EXW")
         cls.type2.payment_term_id = cls.payterm
         cls.type2.incoterm_id = cls.incoterm
         cls.partner1.purchase_type = cls.type2
 
     def test_purchase_order_type(self):
         purchase = self._create_purchase(
-            [(self.product1, 1),
-             (self.product2, 5),
-             (self.product3, 8)])
+            [(self.product1, 1), (self.product2, 5), (self.product3, 8)]
+        )
         self.assertEquals(purchase.order_type, self.type1)
         self.assertFalse(purchase.incoterm_id)
         self.assertFalse(purchase.payment_term_id)
@@ -50,16 +49,15 @@ class TestPurchaseOrderType(common.SavepointCase):
         lines = []
         for product, qty in line_products:
             line_values = {
-                'name': product.name,
-                'product_id': product.id,
-                'product_qty': qty,
-                'product_uom': product.uom_id.id,
-                'price_unit': 100,
-                'date_planned': time.strftime(DEFAULT_SERVER_DATETIME_FORMAT),
+                "name": product.name,
+                "product_id": product.id,
+                "product_qty": qty,
+                "product_uom": product.uom_id.id,
+                "price_unit": 100,
+                "date_planned": time.strftime(DEFAULT_SERVER_DATETIME_FORMAT),
             }
             lines.append((0, 0, line_values))
-        purchase = self.po_obj.create({
-            'partner_id': self.partner1.id,
-            'order_line': lines,
-        })
+        purchase = self.po_obj.create(
+            {"partner_id": self.partner1.id, "order_line": lines}
+        )
         return purchase
