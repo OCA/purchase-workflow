203ecc8f7c878fddd236ecdfc4386801273b1670
ps-tubtim <pimolnats@ecosoft.co.th>
2020-04-22 11:28:32 +0700

    [IMP] purchase_order_type: black, isort, prettier

--- purchase_order_type/models/purchase_order.py
+++ purchase_order_type/models/purchase_order.py
@@ -2,33 +2,38 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models
+
 from odoo.addons.purchase.models.purchase import PurchaseOrder as Purchase
 
 
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+    _inherit = "purchase.order"
 
     def _default_order_type(self):
-        return self.env['purchase.order.type'].search([], limit=1)
+        return self.env["purchase.order.type"].search([], limit=1)
 
-    order_type = fields.Many2one(comodel_name='purchase.order.type',
-                                 readonly=False,
-                                 states=Purchase.READONLY_STATES,
-                                 string='Type',
-                                 ondelete='restrict',
-                                 default=_default_order_type)
+    order_type = fields.Many2one(
+        comodel_name="purchase.order.type",
+        readonly=False,
+        states=Purchase.READONLY_STATES,
+        string="Type",
+        ondelete="restrict",
+        default=_default_order_type,
+    )
 
     @api.multi
-    @api.onchange('partner_id')
+    @api.onchange("partner_id")
     def onchange_partner_id(self):
         super().onchange_partner_id()
-        purchase_type = (self.partner_id.purchase_type or
-                         self.partner_id.commercial_partner_id.purchase_type)
+        purchase_type = (
+            self.partner_id.purchase_type
+            or self.partner_id.commercial_partner_id.purchase_type
+        )
         if purchase_type:
             self.order_type = purchase_type
 
     @api.multi
-    @api.onchange('order_type')
+    @api.onchange("order_type")
     def onchange_order_type(self):
         for order in self:
             if order.order_type.payment_term_id:
@@ -38,9 +43,8 @@ class PurchaseOrder(models.Model):
 
     @api.model
     def create(self, vals):
-        if vals.get('name', '/') == '/' and vals.get('order_type'):
-            purchase_type = self.env['purchase.order.type'].browse(
-                vals['order_type'])
+        if vals.get("name", "/") == "/" and vals.get("order_type"):
+            purchase_type = self.env["purchase.order.type"].browse(vals["order_type"])
             if purchase_type.sequence_id:
-                vals['name'] = purchase_type.sequence_id.next_by_id()
+                vals["name"] = purchase_type.sequence_id.next_by_id()
         return super().create(vals)
