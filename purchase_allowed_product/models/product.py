f6c668effdcd28c43b2b494df299200e0c85941a
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-04-28 17:34:31 +0200

    [MIG] purchase_allowed_product: Refine migration
    [UPD] Update purchase_allowed_product.pot

--- purchase_allowed_product/models/product.py
+++ purchase_allowed_product/models/product.py
@@ -1,5 +1,6 @@
-# © 2017 Today Mourad EL HADJ MIMOUNE @ Akretion
-# 2020 Manuel Calero - Tecnativa
+# Copyright 2017 Today Mourad EL HADJ MIMOUNE @ Akretion
+# Copyright 2020 Tecnativa - Manuel Calero
+# Copyright 2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, models
@@ -9,32 +10,23 @@ class ProductProduct(models.Model):
     _inherit = "product.product"
 
     @api.model
-    def search(self, args, offset=0, limit=None, order=None, count=False):
+    def _search(self, args, offset=0, limit=None, order=None, count=False,
+                access_rights_uid=None):
         use_only_supplied_product = self.env.context.get(
             'use_only_supplied_product')
         if use_only_supplied_product:
             restrict_supplier_id = self.env.context.get('restrict_supplier_id')
             seller = self.env['res.partner'].browse(restrict_supplier_id)
-            seller = seller.commercial_partner_id \
-                if seller.commercial_partner_id else seller
+            seller = seller.commercial_partner_id
             supplierinfos = self.env['product.supplierinfo'].search(
                 [('name', '=', seller.id)])
             args += [
                 '|',
                 ('product_tmpl_id', 'in',
-                    [x.product_tmpl_id.id for x in supplierinfos]),
+                 supplierinfos.mapped('product_tmpl_id').ids),
                 ('id', 'in',
-                    [x.product_id.id for x in supplierinfos])]
-
-        return super(ProductProduct, self).search(
-            args, offset=offset, limit=limit, order=order, count=count)
-
-    @api.model
-    def name_search(self, name, args=None, operator='ilike', limit=100):
-        args = args or []
-        if name:
-            args += [
-                ('name', operator, name)
+                 supplierinfos.mapped('product_id').ids)
             ]
-        tasks = self.search(args, limit=limit)
-        return tasks.name_get()
+        return super()._search(
+            args, offset=offset, limit=limit, order=order, count=count,
+            access_rights_uid=access_rights_uid)
---
+++ purchase_allowed_product/readme/DESCRIPTION.rst
@@ -0,0 +1,7 @@
+This module adds a restriction in purchase and supplier invoices that has the
+mark "Order and invoice only supplied products" checked for allowing to select
+only products that can be supplied by the defined vendor.
+
+The restriction can be set by default for each partner, and it's propagated
+to their supplier invoices and purchases, but it can be changed for each
+invoice and purchase order if you want.
