b9d2de9a6addf8b95fa411c25c74a82fc93466cb
ps-tubtim <pimolnats@ecosoft.co.th>
2020-04-29 10:37:26 +0700

    [IMP] purchase_delivery_split_date: black, isort, prettier

--- purchase_delivery_split_date/__manifest__.py
+++ purchase_delivery_split_date/__manifest__.py
@@ -4,17 +4,15 @@
 
 {
     "name": "Purchase Delivery Split Date",
-    "version": "12.0.2.0.0",
+    "version": "13.0.1.0.0",
     "summary": "Allows Purchase Order you confirm to generate one Incoming "
-               "Shipment for each expected date indicated in the Purchase "
-               "Order Lines",
+    "Shipment for each expected date indicated in the Purchase "
+    "Order Lines",
     "author": "Numerigraphe, Eficent, Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Purchase Management",
     "license": "AGPL-3",
-    "depends": [
-        "purchase_stock",
-    ],
+    "depends": ["purchase_stock"],
     "installable": True,
     "application": False,
 }
