d35e858e231e28520f22ba8f8f6ee5457f7fb570
Valentin Vinagre Urteaga <valentin.vinagre@sygel.es>
2020-05-06 12:36:42 +0200

    [FIX] Invoice tax calculation

--- purchase_product_usage/models/account_move.py
+++ purchase_product_usage/models/account_move.py
@@ -2,12 +2,22 @@
 # Copyright 2019 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0).
 
-from odoo import models
+from odoo import api, models
 
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
+    @api.onchange(
+        "amount_currency",
+        "currency_id",
+        "debit",
+        "credit",
+        "tax_ids",
+        "account_id",
+        "analytic_account_id",
+        "analytic_tag_ids",
+    )
     def _onchange_mark_recompute_taxes(self):
         for line in self:
             if line.purchase_line_id.usage_id.account_id:
