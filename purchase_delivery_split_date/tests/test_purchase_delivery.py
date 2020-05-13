5991aa525e0c7f4cac1fa25fe124f9283bddef04
ps-tubtim <pimolnats@ecosoft.co.th>
2020-05-13 12:03:16 +0700

    [MIG] purchase_delivery_split_date: Migration to 13.0

--- purchase_delivery_split_date/tests/test_purchase_delivery.py
+++ purchase_delivery_split_date/tests/test_purchase_delivery.py
@@ -1,6 +1,6 @@
 # Copyright 2014-2016 Numérigraphe SARL
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests.common import TransactionCase
 
