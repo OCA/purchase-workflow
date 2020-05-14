a6b901dab6e99a581d00a5b2c8efb967eee6ef11
Harald Panten <harald.panten@sygel.es>
2020-05-14 19:53:53 +0200

    [IMP] purchase_location_by_line: black, isort, prettier

--- purchase_location_by_line/models/purchase.py
+++ purchase_location_by_line/models/purchase.py
@@ -7,22 +7,23 @@ from odoo import api, fields, models
 
 
 class PurchaseOrderLine(models.Model):
-    _inherit = 'purchase.order.line'
+    _inherit = "purchase.order.line"
 
     location_dest_id = fields.Many2one(
-        comodel_name='stock.location', string='Destination',
-        domain=[('usage', 'in', ['internal', 'transit'])])
+        comodel_name="stock.location",
+        string="Destination",
+        domain=[("usage", "in", ["internal", "transit"])],
+    )
 
     @api.model
     def _first_picking_copy_vals(self, key, lines):
         """The data to be copied to new pickings is updated with data from the
         grouping key.  This method is designed for extensibility, so that
         other modules can store more data based on new keys."""
-        vals = super(PurchaseOrderLine, self)._first_picking_copy_vals(
-            key, lines)
+        vals = super(PurchaseOrderLine, self)._first_picking_copy_vals(key, lines)
         for key_element in key:
-            if 'location_dest_id' in key_element.keys():
-                vals['location_dest_id'] = key_element['location_dest_id'].id
+            if "location_dest_id" in key_element.keys():
+                vals["location_dest_id"] = key_element["location_dest_id"].id
         return vals
 
     @api.model
@@ -33,25 +34,28 @@ class PurchaseOrderLine(models.Model):
         method is designed for extensibility, so that other modules can add
         additional keys or replace them by others."""
         key = super(PurchaseOrderLine, self)._get_group_keys(
-            order, line, picking=picking)
+            order, line, picking=picking
+        )
         default_picking_location_id = line.order_id._get_destination_location()
-        default_picking_location = self.env['stock.location'].browse(
-            default_picking_location_id)
+        default_picking_location = self.env["stock.location"].browse(
+            default_picking_location_id
+        )
 
         location = line.location_dest_id or default_picking_location
-        return key + ({'location_dest_id': location},)
+        return key + ({"location_dest_id": location},)
 
     @api.multi
     def _create_stock_moves(self, picking):
         res = super(PurchaseOrderLine, self)._create_stock_moves(picking)
         for line in self:
-            default_picking_location_id = \
-                line.order_id._get_destination_location()
-            default_picking_location = self.env['stock.location'].browse(
-                default_picking_location_id)
+            default_picking_location_id = line.order_id._get_destination_location()
+            default_picking_location = self.env["stock.location"].browse(
+                default_picking_location_id
+            )
 
             location = line.location_dest_id or default_picking_location
             if location:
-                line.move_ids.filtered(lambda m: m.state != 'done').write(
-                    {'location_dest_id': location.id})
+                line.move_ids.filtered(lambda m: m.state != "done").write(
+                    {"location_dest_id": location.id}
+                )
         return res
