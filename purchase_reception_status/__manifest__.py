257eb96c68bd337b79cb1771b50b9365f3f86231
Radovan Skolnik <radovan@skolnik.info>
2020-05-23 20:40:46 +0200

    [IMP] purchase_reception_status: black, isort, prettier

--- purchase_reception_status/__manifest__.py
+++ purchase_reception_status/__manifest__.py
@@ -3,17 +3,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Purchase Reception Status',
-    'version': '12.0.1.0.0',
-    'category': 'Purchases',
-    'license': 'AGPL-3',
-    'summary': 'Add reception status on purchase orders',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'maintainers': ['alexis-via'],
-    'website': 'https://github.com/OCA/purchase-workflow',
-    'depends': ['purchase'],
-    'data': [
-        'views/purchase_order.xml',
-    ],
-    'installable': True,
+    "name": "Purchase Reception Status",
+    "version": "12.0.1.0.0",
+    "category": "Purchases",
+    "license": "AGPL-3",
+    "summary": "Add reception status on purchase orders",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/purchase-workflow",
+    "depends": ["purchase"],
+    "data": ["views/purchase_order.xml",],
+    "installable": True,
 }
