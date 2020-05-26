96fdee196730b6745b1966725fd0ac6bf003da87
Tony Galmiche <tony.galmiche@infosaone.com>
2020-05-26 13:42:40 +0200

    [IMP] purchase_order_line_deep_sort: black, isort, prettier

--- purchase_order_line_deep_sort/models/res_company.py
+++ purchase_order_line_deep_sort/models/res_company.py
@@ -3,32 +3,31 @@
 
 from odoo import fields, models
 
-
 SORTING_CRITERIA = [
-    ('name', 'By name'),
-    ('product_id.name', 'By product name'),
-    ('product_id.default_code', 'By product reference'),
-    ('date_planned', 'By date planned'),
-    ('price_unit', 'By price'),
-    ('product_qty', 'By quantity'),
+    ("name", "By name"),
+    ("product_id.name", "By product name"),
+    ("product_id.default_code", "By product reference"),
+    ("date_planned", "By date planned"),
+    ("price_unit", "By price"),
+    ("product_qty", "By quantity"),
 ]
 
 SORTING_DIRECTION = [
-    ('asc', 'Ascending'),
-    ('desc', 'Descending'),
+    ("asc", "Ascending"),
+    ("desc", "Descending"),
 ]
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
     default_po_line_order = fields.Selection(
         selection=SORTING_CRITERIA,
-        string='Line Order',
-        help='Select a sorting criteria for purchase order lines.'
+        string="Line Order",
+        help="Select a sorting criteria for purchase order lines.",
     )
     default_po_line_direction = fields.Selection(
         selection=SORTING_DIRECTION,
-        string='Sort Direction',
-        help='Select a sorting direction for purchase order lines.'
+        string="Sort Direction",
+        help="Select a sorting direction for purchase order lines.",
     )
