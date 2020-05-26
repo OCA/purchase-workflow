6edef3dcfaf77d9c7c183cedf44531e6ab48efae
Tony Galmiche <tony.galmiche@infosaone.com>
2020-05-26 13:42:40 +0200

    [IMP] purchase_order_line_deep_sort: black, isort, prettier

--- purchase_order_line_deep_sort/models/purchase_order.py
+++ purchase_order_line_deep_sort/models/purchase_order.py
@@ -4,25 +4,25 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3
 
 from odoo import api, fields, models
+
 from .res_company import SORTING_CRITERIA, SORTING_DIRECTION
 
 
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+    _inherit = "purchase.order"
 
     line_order = fields.Selection(
         selection=SORTING_CRITERIA,
-        string='Sort Lines By',
+        string="Sort Lines By",
         default=lambda self: self.env.user.company_id.default_po_line_order,
     )
     line_direction = fields.Selection(
         selection=SORTING_DIRECTION,
-        string='Sort Direction',
-        default=lambda self:
-            self.env.user.company_id.default_po_line_direction,
+        string="Sort Direction",
+        default=lambda self: self.env.user.company_id.default_po_line_direction,
     )
 
-    @api.onchange('line_order')
+    @api.onchange("line_order")
     def onchange_line_order(self):
         if not self.line_order:
             self.line_direction = False
@@ -30,23 +30,22 @@ class PurchaseOrder(models.Model):
     @api.multi
     def _sort_purchase_line(self):
         def resolve_subfields(obj, line_order):
-            subfields = line_order.split('.')
+            subfields = line_order.split(".")
             res = obj
-            str_fields = ('text', 'varchar', 'timestamp', 'date')
+            str_fields = ("text", "varchar", "timestamp", "date")
             for subfield in subfields:
                 if res._fields[subfield].column_type[0] in str_fields:
-                    res = getattr(res, subfield) or ''
+                    res = getattr(res, subfield) or ""
                 else:
                     res = getattr(res, subfield)
             return res
 
         if not self.line_order and not self.line_direction:
             return
-        reverse = self.line_direction == 'desc'
+        reverse = self.line_direction == "desc"
         sequence = 0
         sorted_lines = self.order_line.sorted(
-            key=lambda p: resolve_subfields(p, self.line_order),
-            reverse=reverse,
+            key=lambda p: resolve_subfields(p, self.line_order), reverse=reverse,
         )
         for line in sorted_lines:
             sequence += 10
@@ -57,8 +56,11 @@ class PurchaseOrder(models.Model):
     @api.multi
     def write(self, values):
         res = super(PurchaseOrder, self).write(values)
-        if 'order_line' in values or 'line_order' in values or \
-                'line_direction' in values:
+        if (
+            "order_line" in values
+            or "line_order" in values
+            or "line_direction" in values
+        ):
             self._sort_purchase_line()
         return res
 
@@ -70,7 +72,7 @@ class PurchaseOrder(models.Model):
 
 
 class PurchaseOrderLine(models.Model):
-    _inherit = 'purchase.order.line'
+    _inherit = "purchase.order.line"
 
     @api.model
     def create(self, vals):
