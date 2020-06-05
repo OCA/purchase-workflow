74b593c32f5496eb3d0446cea56816a38fb68020
Benjamin Willig <benjamin.willig@acsone.eu>
2020-06-05 12:11:30 +0200

    [MIG] purchase_fop_shipping migration to v13

--- purchase_fop_shipping/models/partner.py
+++ purchase_fop_shipping/models/partner.py
@@ -1,13 +1,13 @@
-# -*- coding: utf-8 -*-
 # © 2014-2016 Akretion (http://www.akretion.com)
 #   @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     fop_shipping = fields.Float(
-        'FOP shipping', help='Min purchase order amount for FOP shipping',)
+        string="FOP shipping", help="Min purchase order amount for FOP shipping",
+    )
--- purchase_fop_shipping/tests/test_fop_shipping.py
+++ purchase_fop_shipping/tests/test_fop_shipping.py
@@ -1,49 +1,71 @@
-# -*- coding: utf-8 -*-
 # © 2017 Akretion (http://www.akretion.com)
 #   @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import openerp.tests.common as common
-from openerp.exceptions import Warning as UserError
-from openerp import fields
+from odoo import fields
+from odoo.exceptions import UserError
+from odoo.tests.common import SavepointCase
 
 
-class TestPurchaseOrder(common.TransactionCase):
+class TestPurchaseOrder(SavepointCase):
 
-    def setUp(self):
-        super(TestPurchaseOrder, self).setUp()
-        self.product_1 = self.env.ref('product.product_product_4')
-        self.product_2 = self.env.ref('product.product_product_5b')
-        self.partner_3 = self.env.ref('base.res_partner_3')
-        self.partner_3.fop_shipping = 250
-        po_model = self.env['purchase.order.line']
-        self.purchase_order1 = self.env['purchase.order'].create(
-            {'partner_id': self.partner_3.id})
-        self.po_line_1 = po_model.create(
-            {'order_id': self.purchase_order1.id,
-             'product_id': self.product_1.id,
-             'date_planned': fields.Datetime.now(),
-             'name': 'Test',
-             'product_qty': 1.0,
-             'product_uom': self.product_1.uom_id.id,
-             'price_unit': 100.0})
-        self.purchase_order2 = self.env['purchase.order'].create(
-            {'partner_id': self.partner_3.id})
-        self.po_line_2 = po_model.create(
-            {'order_id': self.purchase_order2.id,
-             'product_id': self.product_2.id,
-             'date_planned': fields.Datetime.now(),
-             'name': 'Test',
-             'product_qty': 10.0,
-             'product_uom': self.product_2.uom_id.id,
-             'price_unit': 45.0})
+    @classmethod
+    def setUpClass(cls):
+        super(TestPurchaseOrder, cls).setUpClass()
+        cls.Product = cls.env['product.product']
+        cls.Purchase = cls.env['purchase.order']
+        cls.PurchaseLine = cls.env['purchase.order.line']
 
-    def test_purchase_order_vals(self):
-        with self.assertRaises(UserError):
-            self.purchase_order1.button_approve()
-        self.purchase_order2.button_approve()
-        self.assertEqual(self.purchase_order1.state, 'draft')
-        self.assertEqual(self.purchase_order2.state, 'purchase')
-        self.purchase_order1.force_order_under_fop = True
-        self.purchase_order1.button_approve()
-        self.assertEqual(self.purchase_order1.state, 'purchase')
+        cls.product_1 = cls.Product.create({
+            'name': 'Product',
+            'type': 'consu',
+        })
+
+        cls.partner_3 = cls.env.ref('base.res_partner_3')
+        cls.partner_3.fop_shipping = 250
+
+    def test_fop_shipping_reached1(self):
+        po = self.Purchase.create({
+            'partner_id': self.partner_3.id,
+        })
+        self.PurchaseLine.create({
+            'order_id': po.id,
+            'product_id': self.product_1.id,
+            'date_planned': fields.Datetime.now(),
+            'name': 'Test',
+            'product_qty': 1.0,
+            'product_uom': self.product_1.uom_id.id,
+            'price_unit': 100.0,
+        })
+
+        self.assertFalse(po.fop_reached)
+
+        with self.assertRaises(UserError) as e, self.env.cr.savepoint():
+            po.button_approve()
+        self.assertTrue(
+            "You cannot confirm a purchase order with amount under "
+            "FOP shipping"
+            in e.exception.name
+        )
+
+        po.force_order_under_fop = True
+        po.button_approve()
+        self.assertEqual(po.state, 'purchase')
+
+    def test_fop_shipping_reached2(self):
+        po = self.Purchase.create({
+            'partner_id': self.partner_3.id,
+        })
+        self.PurchaseLine.create({
+            'order_id': po.id,
+            'product_id': self.product_1.id,
+            'date_planned': fields.Datetime.now(),
+            'name': 'Test',
+            'product_qty': 10,
+            'product_uom': self.product_1.uom_id.id,
+            'price_unit': 45,
+        })
+
+        self.assertTrue(po.fop_reached)
+        po.button_approve()
+        self.assertEqual(po.state, 'purchase')
