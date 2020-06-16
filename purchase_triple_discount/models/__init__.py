13c9bf40da760b35056741652ae2d91704bad3c9
Benjamin Willig <benjamin.willig@acsone.eu>
2020-06-16 10:13:39 +0200

    [MIG] purchase_triple_discount to v13

--- purchase_triple_discount/models/__init__.py
+++ purchase_triple_discount/models/__init__.py
@@ -1,5 +1,4 @@
-from . import account_invoice
-from . import stock_rule
 from . import product_supplierinfo
-from . import purchase_order
+from . import purchase_order_line
 from . import res_partner
+from . import stock_rule
