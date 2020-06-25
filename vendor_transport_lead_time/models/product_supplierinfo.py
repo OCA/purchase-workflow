2cc0272b6dcc5fdbcc25882e02ec5b3d6e3854bf
Matthieu Méquignon <matthieu.mequignon@camptocamp.com>
2020-06-25 16:37:55 +0200

    fixup! fixup! fixup! [ADD] purchase_delays

--- vendor_transport_lead_time/models/product_supplierinfo.py
+++ vendor_transport_lead_time/models/product_supplierinfo.py
@@ -1,16 +1,22 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ProductSupplierinfo(models.Model):
 
     _inherit = "product.supplierinfo"
 
-    delay = fields.Integer(string="Lead Time", compute="_compute_delay",)
+    delay = fields.Integer(
+        string="Lead Time",
+        compute="_compute_delay",
+        inverse="_inverse_delay",
+        readonly=True,
+    )
     supplier_delay = fields.Integer(
-        string="Supplier Lead Time", default=1, required=True
+        string="Supplier Lead Time", default=0, required=True
     )
     transport_delay = fields.Integer(
         string="Transport Lead Time", default=0, required=True
@@ -20,3 +26,13 @@ class ProductSupplierinfo(models.Model):
     def _compute_delay(self):
         for record in self:
             record.delay = record.supplier_delay + record.transport_delay
+
+    def _inverse_delay(self):
+        for record in self:
+            diff = record.delay - record.transport_delay
+            if diff < 0:
+                raise ValidationError(
+                    _("You can't set a delay inferior to the transport delay.")
+                )
+            else:
+                record.supplier_delay = diff
