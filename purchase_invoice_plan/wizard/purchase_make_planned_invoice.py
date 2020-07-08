4cf768402f4802eb44494e7e3f22b2e0bdaa9b2d
Kitti U <kittiu@gmail.com>
2020-07-08 14:32:01 +0700

    purchase_invoice_plan
    :100644 000000 9b90b773 00000000 D	purchase_invoice_plan/README.rst
    :100644 100644 9a68d442 051bb1da M	purchase_invoice_plan/__manifest__.py
    :100644 100644 8bb4c5d6 3da440c9 M	purchase_invoice_plan/models/purchase.py
    :100644 100644 d90eaa42 1c1deabc M	purchase_invoice_plan/wizard/purchase_create_invoice_plan.py
    :100644 100644 326c1a0b dab7a93c M	purchase_invoice_plan/wizard/purchase_create_invoice_plan_view.xml
    :100644 100644 95a5d1f1 0883bce1 M	purchase_invoice_plan/wizard/purchase_make_planned_invoice.py
    :100644 100644 1c6f8548 0bbbcd59 M	purchase_invoice_plan/wizard/purchase_make_planned_invoice_view.xml
    :000000 120000 00000000 71b680a3 A	setup/purchase_invoice_plan/odoo/addons/purchase_invoice_plan
    :000000 100644 00000000 28c57bb6 A	setup/purchase_invoice_plan/setup.py

--- purchase_invoice_plan/wizard/purchase_make_planned_invoice.py
+++ purchase_invoice_plan/wizard/purchase_make_planned_invoice.py
@@ -1,14 +1,13 @@
 # Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo import api, models
+from odoo import models
 
 
 class PurchaseAdvancePaymentInv(models.TransientModel):
     _name = "purchase.make.planned.invoice"
     _description = "Wizard when create invoice by plan"
 
-    @api.multi
     def create_invoices_by_plan(self):
         purchase = self.env["purchase.order"].browse(self._context.get("active_id"))
         purchase.ensure_one()
