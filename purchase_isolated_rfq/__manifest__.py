b82888d2a485924d53a59782bdc1a3a934b054f2
Kitti U <kittiu@gmail.com>
2020-07-16 17:22:42 +0700

    [FIX] purchase_isolated_rfq, add uninstall_hook to restore original action domain/context

--- purchase_isolated_rfq/__manifest__.py
+++ purchase_isolated_rfq/__manifest__.py
@@ -9,6 +9,8 @@
     "depends": ["purchase"],
     "license": "AGPL-3",
     "data": ["data/ir_sequence_data.xml", "views/purchase_views.xml"],
+    "maintainers": ["kittiu"],
     "installable": True,
+    "uninstall_hook": "uninstall_hook",
     "post_init_hook": "post_init_hook",
 }
