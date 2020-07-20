a453841ae1bd3608390592e72fe5c9967d3278f3
niaisoh <niaisoh457@gmail.com>
2020-07-20 09:54:45 +0700

    [13.0][ADD] purchase_work_acceptance_invoice_plan

---
+++ purchase_work_acceptance_invoice_plan/models/purchase.py
@@ -0,0 +1,37 @@
+# Copyright 2020 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    def _get_product_qty(self):
+        installment_id = self._context.get("installment_id", False)
+        if installment_id:
+            installment = self.env["purchase.invoice.plan"].browse(installment_id)
+            return self.product_qty * (installment.percent / 100)
+        return super()._get_product_qty()
+
+
+class PurchaseInvoicePlan(models.Model):
+    _inherit = "purchase.invoice.plan"
+
+    def name_get(self):
+        result = []
+        for rec in self:
+            result.append(
+                (
+                    rec.id,
+                    "%s %s : %s -- %d %s"
+                    % (
+                        "Invoice Plan",
+                        rec.installment,
+                        rec.plan_date,
+                        rec.percent,
+                        "%",
+                    ),
+                )
+            )
+        return result
