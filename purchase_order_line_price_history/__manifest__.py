af0932c23472475c591fba68df58ad966542c1bd
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-07-28 15:44:46 -0400

    [MIG] purchase_order_line_price_history: Migration to 13.0

--- purchase_order_line_price_history/__manifest__.py
+++ purchase_order_line_price_history/__manifest__.py
@@ -2,12 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Purchase order line price history",
-    "version": "12.0.1.0.2",
+    "version": "13.0.1.0.0",
     "category": "Purchase Management",
-    "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow/",
     "license": "AGPL-3",
-    "depends": ["purchase",],
+    "depends": ["purchase"],
     "data": [
         "wizards/purchase_order_line_price_history.xml",
         "views/purchase_views.xml",
