14d1e160e457812c0a293e76b1d8da57acbe90ab
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2020-07-31 09:40:41 +0200

    [FIX] purchase_warn_message website key

--- purchase_warn_message/__manifest__.py
+++ purchase_warn_message/__manifest__.py
@@ -5,10 +5,10 @@
     "name": "Purchase Warn Message",
     "summary": """
         Add a popup warning on purchase to ensure warning is populated""",
-    "version": "13.0.1.0.1",
+    "version": "13.0.1.0.2",
     "license": "AGPL-3",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
-    "website": "www.github.com/OCA/stock-logistics-workflow.git",
+    "website": "https://github.com/OCA/purchase-workflow",
     "depends": ["purchase"],
     "data": ["views/purchase_order_views.xml"],
 }
