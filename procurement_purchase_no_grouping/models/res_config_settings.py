dbc19ebbf7abb021a17b5c844cc093cfd79d5a64
Radovan Skolnik <radovan@skolnik.info>
2020-08-09 19:12:54 +0200

    [IMP] procurement_purchase_no_grouping: Add company level policy

---
+++ procurement_purchase_no_grouping/models/res_config_settings.py
@@ -0,0 +1,15 @@
+# Copyright 2020 - Radovan Skolnik <radovan@skolnik.info>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    """Configuration of default value for procurement purchase grouping."""
+
+    _inherit = "res.config.settings"
+    _description = "Procurement purchase grouping settings"
+
+    procured_purchase_grouping = fields.Selection(
+        related="company_id.procured_purchase_grouping", readonly=False,
+    )
