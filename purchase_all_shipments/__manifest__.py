f59aac95d1b6198ddebc386b0a54735f0ccb6788
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-08-25 16:22:23 -0300

    [MIG] purchase_all_shipments: migrate to v13

--- purchase_all_shipments/__manifest__.py
+++ purchase_all_shipments/__manifest__.py
@@ -1,25 +1,12 @@
-#    Author: Leonardo Pistone
-#    Copyright 2015 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     "name": "Purchase All Shipments",
-    "version": "8.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "category": "Purchases",
     "license": "AGPL-3",
-    "depends": ["purchase"],
-    "data": ["view/purchase_order.xml"],
-    "installable": False,
+    "depends": ["purchase_stock"],
+    "installable": True,
+    "data": ["views/purchase_order_views.xml"],
 }
