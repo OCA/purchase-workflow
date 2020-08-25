e66037340aeb37edeeabb70b501aafbe47c10b59
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-08-25 15:24:10 -0300

    [IMP] purchase_all_shipments : black, isort, prettier

--- purchase_all_shipments/model/purchase_order.py
+++ purchase_all_shipments/model/purchase_order.py
@@ -13,44 +13,41 @@
 #
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from openerp import models, fields, api
+from openerp import api, fields, models
 
 
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+    _inherit = "purchase.order"
 
     def _all_shipment_count(self):
         self.all_shipment_count = len(self.all_picking_ids)
 
     def _all_pickings(self):
-        groups = self.mapped('picking_ids.move_lines.group_id')
+        groups = self.mapped("picking_ids.move_lines.group_id")
 
-        all_moves = self.env['stock.move'].search(
-            [('group_id', 'in', groups.ids)]
-        )
-        self.all_picking_ids = all_moves.mapped('picking_id')
+        all_moves = self.env["stock.move"].search([("group_id", "in", groups.ids)])
+        self.all_picking_ids = all_moves.mapped("picking_id")
 
     @api.multi
     def view_all_picking(self):
         """Similar to the view_picking method in the purchase module"""
-        action_data = self.env.ref('stock.action_picking_tree').read()[0]
-        pickings = self.mapped('all_picking_ids')
+        action_data = self.env.ref("stock.action_picking_tree").read()[0]
+        pickings = self.mapped("all_picking_ids")
 
         # override the context to get rid of the default filtering on
         # picking type
-        action_data['context'] = {}
+        action_data["context"] = {}
 
         # choose the view_mode accordingly
         if len(pickings) > 1:
-            action_data['domain'] = [('id', 'in', pickings.ids)]
+            action_data["domain"] = [("id", "in", pickings.ids)]
         else:
-            form_view = self.env.ref('stock.view_picking_form')
-            action_data['views'] = [(form_view.id, 'form')]
-            action_data['res_id'] = pickings.id
+            form_view = self.env.ref("stock.view_picking_form")
+            action_data["views"] = [(form_view.id, "form")]
+            action_data["res_id"] = pickings.id
         return action_data
 
-    all_picking_ids = fields.One2many('stock.picking',
-                                      string='All Shipments',
-                                      compute='_all_pickings')
-    all_shipment_count = fields.Integer('All Shipments',
-                                        compute='_all_shipment_count')
+    all_picking_ids = fields.One2many(
+        "stock.picking", string="All Shipments", compute="_all_pickings"
+    )
+    all_shipment_count = fields.Integer("All Shipments", compute="_all_shipment_count")
