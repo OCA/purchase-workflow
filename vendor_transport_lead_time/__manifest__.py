264869616b9e082367181858d40ca7c5f587879a
sebalix <seb@usr-src.org>
2020-08-28 12:40:05 +0200

    [IMP] vendor_transport_lead_time: PO integration

--- vendor_transport_lead_time/__manifest__.py
+++ vendor_transport_lead_time/__manifest__.py
@@ -3,11 +3,15 @@
 {
     "name": "Vendor transport lead time",
     "summary": "Purchase delay based on transport and supplier delays",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.2.0",
     "website": "https://odoo-community.org/",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "depends": ["product"],
-    "data": ["views/product_supplierinfo.xml"],
+    "depends": ["product", "purchase"],
+    "data": [
+        "views/product_supplierinfo.xml",
+        "views/purchase_order_line.xml",
+        "report/purchase_order_template.xml",
+    ],
     "installable": True,
 }
