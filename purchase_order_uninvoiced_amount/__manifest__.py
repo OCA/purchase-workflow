d58c3112a369e477968ba7daef434563f418a666
João Marques <joao.marques@tecnativa.com>
2020-09-14 08:30:35 +0200

    [IMP] purchase_order_uninvoiced_amount: black, isort, prettier

--- purchase_order_uninvoiced_amount/__manifest__.py
+++ purchase_order_uninvoiced_amount/__manifest__.py
@@ -5,16 +5,12 @@
     "name": "Purchase Order Univoiced Amount",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "version": "12.0.1.0.1",
-    'development_status': 'Beta',
+    "development_status": "Beta",
     "website": "https://github.com/OCA/purchase-workflow",
-    'category': 'Purchase',
-    'license': 'AGPL-3',
-    'application': False,
+    "category": "Purchase",
+    "license": "AGPL-3",
+    "application": False,
     "installable": True,
-    'depends': [
-        'purchase',
-    ],
-    "data": [
-        'views/purchase_order_view.xml',
-    ],
+    "depends": ["purchase",],
+    "data": ["views/purchase_order_view.xml",],
 }
