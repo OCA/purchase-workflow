ea8627bca48e92e772546852a7bddc00035b32e7
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-09-15 10:08:22 +0200

    [FIX] procurement_purchase_no_grouping: Don't group by domain
    When purchase grouping is set to no order grouping, the previously returned domain
    ensured no other PO is found, but being the same domain for all will make that they
    are grouped together in a batch operation, like the creation of all procurements
    from a confirmed sales order.
    
    Steps to reproduce:
    
    * Create 2 products with MTO and different vendors.
    * Add both in a sales order.
    * Confirm the sales order.
    * One purchase order is created for both products instead of 2, more even in this
      case that they have different vendors.
    
    Fixes #975

--- procurement_purchase_no_grouping/models/stock_rule.py
+++ procurement_purchase_no_grouping/models/stock_rule.py
@@ -4,6 +4,8 @@
 # Copyright 2020 Radovan Skolnik
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
+import random
+
 from odoo import models
 
 
@@ -19,6 +21,17 @@ class StockRule(models.Model):
         return super()._run_buy(procurements)
 
     def _make_po_get_domain(self, company_id, values, partner):
+        """Inject an impossible domain for not getting match in case of no order
+        grouping.
+
+        We try to make it the more unique possible for avoiding coincidences for not
+        overlapping in a batch procurement run (like a sales order with multiple MTO
+        lines confirmation).
+        """
+        domain = super()._make_po_get_domain(company_id, values, partner)
         if values.get("grouping") == "order":
-            return (("id", "=", 0),)
-        return super()._make_po_get_domain(company_id, values, partner)
+            if values.get("move_dest_ids"):
+                domain += (("id", "=", -values["move_dest_ids"][:1].id),)
+            # The minimum is imposed by PG int4 limit
+            domain += (("id", "=", random.randint(-2147483648, 0)),)
+        return domain
