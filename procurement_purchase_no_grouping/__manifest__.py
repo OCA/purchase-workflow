10a909f13a7d2e01294c403fee93c0eaebd24d48
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-09-15 10:08:22 +0200

    [FIX] procurement_purchase_no_grouping: Don't group by domain
    When purchase grouping is set to no order grouping, the previously returned domain
    ensured no other PO is found, but being the same domain for all will make that they
    are grouped together in a batch operation, like the creation of all procurements
    from a confirmed sales order.
    
    Steps to reproduce:
    
    * Create 2 products with MTO and different vendors.
    * Add both in a sales order.
    * Confirm the sales order.
    * One purchase order is created for both products instead of 2, more even in this
      case that they have different vendors.
    
    Fixes #975

--- procurement_purchase_no_grouping/__manifest__.py
+++ procurement_purchase_no_grouping/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Procurement Purchase No Grouping",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "author": "AvanzOSC," "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Procurements",
