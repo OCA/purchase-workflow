620b4bcd46cda7932b08fc130b2bb9f0db44ae2d
sergio-teruel <sergio.teruel@tecnativa.com>
2020-09-17 16:16:46 +0200

    [MIG] purchase_stock_price_unit_sync: Migration to v13.0
    [UPD] Update purchase_stock_price_unit_sync.pot

--- purchase_stock_price_unit_sync/tests/test_purchase_stock_price_unit_sync.py
+++ purchase_stock_price_unit_sync/tests/test_purchase_stock_price_unit_sync.py
@@ -9,16 +9,20 @@ class TestProductCostPriceAvcoSync(SavepointCase):
     @classmethod
     def setUpClass(cls):
         super(TestProductCostPriceAvcoSync, cls).setUpClass()
-        cls.partner = cls.env["res.partner"].create(
-            {"customer": True, "supplier": True, "name": "Test Partner",}
+        cls.partner = cls.env["res.partner"].create({"name": "Test Partner"})
+        cls.product_category = cls.env["product.category"].create(
+            {
+                "name": "Category property_cost_method average",
+                "property_cost_method": "average",
+            }
         )
         cls.product = cls.env["product.product"].create(
             {
                 "name": "Product for test",
                 "type": "product",
                 "tracking": "none",
-                "property_cost_method": "average",
-                "standard_price": 7.0,
+                "categ_id": cls.product_category.id,
+                "standard_price": 1,
             }
         )
 
@@ -48,6 +52,6 @@ class TestProductCostPriceAvcoSync(SavepointCase):
         move = picking.move_lines[:1]
         move.quantity_done = move.product_uom_qty
         picking.action_done()
-        self.assertAlmostEqual(move.price_unit, 8.0, 2)
+        self.assertAlmostEqual(move.stock_valuation_layer_ids[:1].unit_cost, 8.0, 2)
         self.order.order_line[:1].price_unit = 6.0
-        self.assertAlmostEqual(move.price_unit, 6.0, 2)
+        self.assertAlmostEqual(move.stock_valuation_layer_ids[:1].unit_cost, 6.0, 2)
