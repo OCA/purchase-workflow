c79bb45f4d51fcf6b55e49c4dc9b0fd9f41d2fcb
sergio-teruel <sergio.teruel@tecnativa.com>
2020-09-17 16:16:45 +0200

    [IMP] purchase_stock_price_unit_sync: black, isort

--- purchase_stock_price_unit_sync/tests/test_purchase_stock_price_unit_sync.py
+++ purchase_stock_price_unit_sync/tests/test_purchase_stock_price_unit_sync.py
@@ -6,34 +6,41 @@ from odoo.tests.common import SavepointCase
 
 
 class TestProductCostPriceAvcoSync(SavepointCase):
-
     @classmethod
     def setUpClass(cls):
         super(TestProductCostPriceAvcoSync, cls).setUpClass()
-        cls.partner = cls.env['res.partner'].create({
-            'customer': True,
-            'supplier': True,
-            'name': 'Test Partner',
-        })
-        cls.product = cls.env['product.product'].create({
-            'name': 'Product for test',
-            'type': 'product',
-            'tracking': 'none',
-            'property_cost_method': 'average',
-            'standard_price': 7.0,
-        })
+        cls.partner = cls.env["res.partner"].create(
+            {"customer": True, "supplier": True, "name": "Test Partner",}
+        )
+        cls.product = cls.env["product.product"].create(
+            {
+                "name": "Product for test",
+                "type": "product",
+                "tracking": "none",
+                "property_cost_method": "average",
+                "standard_price": 7.0,
+            }
+        )
 
-        cls.order = cls.env['purchase.order'].create(
-            {'partner_id': cls.partner.id,
-             'order_line': [
-                 (0, 0, {
-                     'name': 'Test line',
-                     'product_qty': 10.0,
-                     'product_id': cls.product.id,
-                     'product_uom': cls.product.uom_id.id,
-                     'date_planned': fields.Date.today(),
-                     'price_unit': 8.0}),
-             ]})
+        cls.order = cls.env["purchase.order"].create(
+            {
+                "partner_id": cls.partner.id,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": "Test line",
+                            "product_qty": 10.0,
+                            "product_id": cls.product.id,
+                            "product_uom": cls.product.uom_id.id,
+                            "date_planned": fields.Date.today(),
+                            "price_unit": 8.0,
+                        },
+                    ),
+                ],
+            }
+        )
 
     def test_sync_cost_price(self):
         self.order.button_confirm()
