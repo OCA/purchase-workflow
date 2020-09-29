c57047cb9b344d1d7704fb1d86d68c274f360c66
Carlos Roca <carlos.roca@tecnativa.com>
2020-09-29 11:39:10 +0200

    [MIG] purchase_stock_return_request: Migration to v12

--- purchase_stock_return_request/__manifest__.py
+++ purchase_stock_return_request/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Purchase Stock Return Request",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Stock",
     "website": "https://github.com/OCA/purchase-workflow",
     "author": "Tecnativa, "
