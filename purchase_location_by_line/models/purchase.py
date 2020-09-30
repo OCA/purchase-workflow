8b9c1cc2f8c9ebf514b388f7e3afcbb92723454f
mreficent <miquel.raich@eficent.com>
2020-09-30 11:47:21 +0200

    [UPD] Eficent -> ForgeFlow

--- purchase_location_by_line/models/purchase.py
+++ purchase_location_by_line/models/purchase.py
@@ -1,5 +1,5 @@
-# © 2016 Eficent Business and IT Consulting Services S.L.
-#   (<http://www.eficent.com>)
+# © 2016 ForgeFlow S.L.
+#   (<http://www.forgeflow.com>)
 # © 2018 Hizbul Bahar <hizbul25@gmail.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
