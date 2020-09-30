024f5af15de4ee6bf0d030f5b66051fbf6e79b89
François Honoré <francois.honore@acsone.eu>
2020-09-30 11:34:10 +0200

    [FIX] Add a glue module in case of the sale module is installed to fix onchanges who update the fiscal position, to keep the one set on the PO

---
+++ account_fiscal_position_allowed_journal_sale_purchase/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Fiscal Position Allowed Journal Purchase",
+    "summary": """Glue module between Sale + purchase +
+    account fiscal position allowed journal purchase""",
+    "version": "13.0.1.0.0",
+    "development_status": "Beta",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "depends": ["sale_purchase", "account_fiscal_position_allowed_journal_purchase"],
+    "auto_install": True,
+}
