7a9ce17e8e91147359e79c287513c0d42eb4443e
mreficent <miquel.raich@eficent.com>
2020-09-30 11:47:21 +0200

    [UPD] Eficent -> ForgeFlow

--- purchase_location_by_line/tests/test_purchase_delivery.py
+++ purchase_location_by_line/tests/test_purchase_delivery.py
@@ -1,5 +1,5 @@
-# © 2016 Eficent Business and IT Consulting Services S.L.
-#   (<http://www.eficent.com>)
+# © 2016 ForgeFlow S.L.
+#   (<http://www.forgeflow.com>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 import time
