94fe59238046e0770429c17b996d5cb92c9da94e
Darius Martinkus <odooims@gmail.com>
2020-11-04 08:57:16 +0200

    [IMP] purchase_exception: black, isort, prettier

--- purchase_exception/models/exception_rule.py
+++ purchase_exception/models/exception_rule.py
@@ -2,16 +2,16 @@
 # Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ExceptionRule(models.Model):
-    _inherit = 'exception.rule'
+    _inherit = "exception.rule"
 
-    purchase_ids = fields.Many2many(
-        comodel_name="purchase.order", string="Purchases")
+    purchase_ids = fields.Many2many(comodel_name="purchase.order", string="Purchases")
     model = fields.Selection(
         selection_add=[
-            ('purchase.order', 'Purchase order'),
-            ('purchase.order.line', 'Purchase order line'),
-        ])
+            ("purchase.order", "Purchase order"),
+            ("purchase.order.line", "Purchase order line"),
+        ]
+    )
