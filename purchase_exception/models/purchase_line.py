ff7b0711780b037072eee547ad24c98a75f91034
Darius Martinkus <odooims@gmail.com>
2020-11-04 08:57:16 +0200

    [IMP] purchase_exception: black, isort, prettier

--- purchase_exception/models/purchase_line.py
+++ purchase_exception/models/purchase_line.py
@@ -3,32 +3,32 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 import logging
-from odoo import api, models, fields
+
+from odoo import api, fields, models
 
 logger = logging.getLogger(__name__)
 
 
 class PurchaseOrderLine(models.Model):
-    _inherit = ['purchase.order.line', 'base.exception.method']
-    _name = 'purchase.order.line'
+    _inherit = ["purchase.order.line", "base.exception.method"]
+    _name = "purchase.order.line"
 
     ignore_exception = fields.Boolean(
-        related='order_id.ignore_exception',
-        store=True,
-        string="Ignore Exceptions")
+        related="order_id.ignore_exception", store=True, string="Ignore Exceptions"
+    )
 
     @api.multi
     def _get_main_records(self):
-        return self.mapped('order_id')
+        return self.mapped("order_id")
 
     @api.model
     def _reverse_field(self):
-        return 'purchase_ids'
+        return "purchase_ids"
 
     @api.multi
     def _detect_exceptions(self, rule):
         records = super()._detect_exceptions(rule)
-        return records.mapped('order_id')
+        return records.mapped("order_id")
 
     @api.model
     def _exception_rule_eval_context(self, rec):
@@ -38,10 +38,12 @@ class PurchaseOrderLine(models.Model):
         # code during v13 migration.
         res = super()._exception_rule_eval_context(rec)
         if res.get("purchase_line"):
-            logger.warning("""
+            logger.warning(
+                """
                 For a full compatibility with future versions of this module,
                 please use 'self' instead of 'purchase_line' in your
                 custom exceptions rules.
-                """)
-        res['purchase_line'] = rec
+                """
+            )
+        res["purchase_line"] = rec
         return res
