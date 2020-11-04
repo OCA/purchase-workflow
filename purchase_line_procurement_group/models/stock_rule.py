5e8edf4b111d06d0d77e341b237b577403d70229
Tran Thanh Phuc <phuc@trobz.com>
2020-11-05 00:46:34 +0700

    [IMP] purchase_line_procurement_group: black, isort, prettier

--- purchase_line_procurement_group/models/stock_rule.py
+++ purchase_line_procurement_group/models/stock_rule.py
@@ -1,19 +1,21 @@
 # Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import models, api
+from odoo import api, models
 
 
 class StockRule(models.Model):
 
-    _inherit = 'stock.rule'
+    _inherit = "stock.rule"
 
     @api.multi
-    def _prepare_purchase_order_line(self, product_id, product_qty,
-                                     product_uom, values, po, supplier):
+    def _prepare_purchase_order_line(
+        self, product_id, product_qty, product_uom, values, po, supplier
+    ):
         """Add procurement group to values"""
         res = super()._prepare_purchase_order_line(
-            product_id, product_qty, product_uom, values, po, supplier)
-        procurement_group = values.get('group_id')
+            product_id, product_qty, product_uom, values, po, supplier
+        )
+        procurement_group = values.get("group_id")
         if procurement_group:
-            res['procurement_group_id'] = procurement_group.id
+            res["procurement_group_id"] = procurement_group.id
         return res
