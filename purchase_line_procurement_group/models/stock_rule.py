2ce3c972700a4b4fc8e294622b213951e11a2fe4
Tran Thanh Phuc <phuc@trobz.com>
2020-11-11 21:38:04 +0700

    [MIG] purchase_line_procurement_group: Migration to 13.0

--- purchase_line_procurement_group/models/stock_rule.py
+++ purchase_line_procurement_group/models/stock_rule.py
@@ -1,19 +1,18 @@
 # Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, models
+from odoo import models
 
 
 class StockRule(models.Model):
 
     _inherit = "stock.rule"
 
-    @api.multi
     def _prepare_purchase_order_line(
-        self, product_id, product_qty, product_uom, values, po, supplier
+        self, product_id, product_qty, product_uom, company_id, values, po
     ):
         """Add procurement group to values"""
         res = super()._prepare_purchase_order_line(
-            product_id, product_qty, product_uom, values, po, supplier
+            product_id, product_qty, product_uom, company_id, values, po
         )
         procurement_group = values.get("group_id")
         if procurement_group:
