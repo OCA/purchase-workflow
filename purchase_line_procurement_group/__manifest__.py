d7751e4ee3813e6114251b1930ee24f173e3c75c
Tran Thanh Phuc <phuc@trobz.com>
2020-11-11 21:38:04 +0700

    [MIG] purchase_line_procurement_group: Migration to 13.0

--- purchase_line_procurement_group/__manifest__.py
+++ purchase_line_procurement_group/__manifest__.py
@@ -3,11 +3,11 @@
 {
     "name": "Purchase Line Procurement Group",
     "summary": "Group purchase order line according to procurement group",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.1",
     "category": "Purchase",
     "website": "https://github.com/OCA/purchase-workflow",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "depends": ["purchase_stock",],
-    "data": ["views/purchase.xml",],
+    "depends": ["purchase_stock"],
+    "data": ["views/purchase.xml"],
 }
