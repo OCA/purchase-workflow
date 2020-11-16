aa819c2fbafd85d6591e46aec7aae783e43027a0
Darius Z <darius.zh@gmail.com>
2020-11-16 15:38:00 +0200

    [13.0][MIG] purchase_allowed_product: migration to 13.0

--- purchase_allowed_product/tests/test_purchase_allowed_product.py
+++ purchase_allowed_product/tests/test_purchase_allowed_product.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import Form, TransactionCase
 
 
 class TestPurchaseAllowedProduct(TransactionCase):
@@ -15,7 +15,7 @@ class TestPurchaseAllowedProduct(TransactionCase):
         self.supplierinfo = self.supplierinfo_model.search(
             [("name", "=", self.partner_4.id)]
         )
-        self.prtner_4_supplied_products = self.product_model.search(
+        self.partner_4_supplied_products = self.product_model.search(
             [
                 (
                     "product_tmpl_id",
@@ -25,76 +25,51 @@ class TestPurchaseAllowedProduct(TransactionCase):
             ]
         )
 
-    def new_purchase(self):
-        """Create an empty purchase."""
-        new = self.env["purchase.order"].new()
-        return new
-
-    def new_invoice(self):
-        """Create an empty purchase."""
-        new = self.env["account.invoice"].new()
-        return new
-
-    def set_purchase_partner(self, partner):
-        self.purchase.partner_id = partner
-
-        # It triggers onchange
-        self.purchase.partner_id_change()
-
-        # Ensure the use_only_supplied_product is set
-        self.assertEqual(
-            self.purchase.use_only_supplied_product, partner.use_only_supplied_product
-        )
-
-    def set_invoice_partner(self, partner):
-        self.invoice.partner_id = partner
-
-        # It triggers onchange
-        self.invoice.partner_id_change()
-
-        # Ensure the use_only_supplied_product is set
-        self.assertEqual(
-            self.invoice.use_only_supplied_product, partner.use_only_supplied_product
-        )
-
     def test_purchase_onchange(self):
         """A user creates a purchase from the form."""
         self.partner_4.use_only_supplied_product = True
+        with Form(
+            self.env["purchase.order"], view="purchase.purchase_order_form"
+        ) as purchase_form:
+            purchase_form.partner_id = self.partner_4
 
-        with self.env.do_in_onchange():
-            # User presses ``new``
-            self.purchase = self.new_purchase()
+            # Ensure the use_only_supplied_product is set
+            self.assertEqual(
+                purchase_form.use_only_supplied_product,
+                self.partner_4.use_only_supplied_product,
+            )
 
-            # User changes fields
-            self.set_purchase_partner(self.partner_4)
-
-            self.assertEqual(self.purchase.use_only_supplied_product, True)
-        context = {
-            "restrict_supplier_id": self.purchase.partner_id.id,
-            "use_only_supplied_product": self.purchase.use_only_supplied_product,
-        }
+            self.assertEqual(purchase_form.use_only_supplied_product, True)
+            context = {
+                "restrict_supplier_id": purchase_form.partner_id.id,
+                "use_only_supplied_product": purchase_form.use_only_supplied_product,
+            }
         supplied_product = self.product_model.with_context(context)._search([])
         self.assertEqual(
-            set(supplied_product), set(self.prtner_4_supplied_products.ids)
+            set(supplied_product), set(self.partner_4_supplied_products.ids)
         )
 
     def test_invoice_onchange(self):
         """A user creates a invoice from the form."""
         self.partner_4.use_only_supplied_product = True
+        with Form(
+            self.env["account.move"].with_context(default_type="out_invoice"),
+            view="account.view_move_form",
+        ) as invoice_form:
+            invoice_form.partner_id = self.partner_4
 
-        with self.env.do_in_onchange():
-            # User presses ``new``
-            self.invoice = self.new_invoice()
+            # Ensure the use_only_supplied_product is set
+            self.assertEqual(
+                invoice_form.use_only_supplied_product,
+                self.partner_4.use_only_supplied_product,
+            )
 
-            # User changes fields
-            self.set_invoice_partner(self.partner_4)
-
-            self.assertEqual(self.invoice.use_only_supplied_product, True)
-        context = {
-            "restrict_supplier_id": self.invoice.partner_id.id,
-            "use_only_supplied_product": self.invoice.use_only_supplied_product,
-        }
+            self.assertEqual(invoice_form.use_only_supplied_product, True)
+            context = {
+                "restrict_supplier_id": invoice_form.partner_id.id,
+                "use_only_supplied_product": invoice_form.use_only_supplied_product,
+            }
         supplied_product = self.product_model.with_context(context)._search([])
         self.assertEqual(
-            set(supplied_product), set(self.prtner_4_supplied_products.ids)
+            set(supplied_product), set(self.partner_4_supplied_products.ids)
         )
