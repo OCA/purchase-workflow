9ef012012332472285bba6501dc25260923ebd0e
Darius Z <darius.zh@gmail.com>
2020-11-16 11:55:23 +0200

    [IMP] purchase_allowed_product: black, isort, prettier

--- purchase_allowed_product/models/supplied_product_mixin.py
+++ purchase_allowed_product/models/supplied_product_mixin.py
@@ -5,18 +5,20 @@ from odoo import api, fields, models
 
 
 class SuppliedProductMixin(models.AbstractModel):
-    _name = 'supplied.product.mixin'
+    _name = "supplied.product.mixin"
     _description = "Mixin for documents that want to restrict products"
 
     use_only_supplied_product = fields.Boolean(
         string="Use only allowed products",
         help="If checked, only the products provided by this supplier "
-             "will be shown.")
+        "will be shown.",
+    )
 
-    @api.onchange('partner_id')
+    @api.onchange("partner_id")
     def partner_id_change(self):
         only_supp_prod = self.partner_id.use_only_supplied_product
         if not only_supp_prod:
-            only_supp_prod = self.partner_id.commercial_partner_id.\
-                use_only_supplied_product
+            only_supp_prod = (
+                self.partner_id.commercial_partner_id.use_only_supplied_product
+            )
         self.use_only_supplied_product = only_supp_prod
