cb7eee5a4f1c7892c523ed2c8a17763b1b07140d
Darius Z <darius.zh@gmail.com>
2020-11-16 11:55:23 +0200

    [IMP] purchase_allowed_product: black, isort, prettier

--- purchase_allowed_product/tests/test_purchase_allowed_product.py
+++ purchase_allowed_product/tests/test_purchase_allowed_product.py
@@ -7,18 +7,23 @@ from odoo.tests.common import TransactionCase
 
 
 class TestPurchaseAllowedProduct(TransactionCase):
-
     def setUp(self):
         super(TestPurchaseAllowedProduct, self).setUp()
-        self.supplierinfo_model = self.env['product.supplierinfo']
-        self.product_model = self.env['product.product']
-        self.partner_4 = self.env.ref('base.res_partner_4')
+        self.supplierinfo_model = self.env["product.supplierinfo"]
+        self.product_model = self.env["product.product"]
+        self.partner_4 = self.env.ref("base.res_partner_4")
         self.supplierinfo = self.supplierinfo_model.search(
-            [('name', '=', self.partner_4.id)]
+            [("name", "=", self.partner_4.id)]
         )
         self.prtner_4_supplied_products = self.product_model.search(
-            [('product_tmpl_id', 'in',
-              [x.product_tmpl_id.id for x in self.supplierinfo])])
+            [
+                (
+                    "product_tmpl_id",
+                    "in",
+                    [x.product_tmpl_id.id for x in self.supplierinfo],
+                )
+            ]
+        )
 
     def new_purchase(self):
         """Create an empty purchase."""
@@ -38,8 +43,8 @@ class TestPurchaseAllowedProduct(TransactionCase):
 
         # Ensure the use_only_supplied_product is set
         self.assertEqual(
-            self.purchase.use_only_supplied_product,
-            partner.use_only_supplied_product)
+            self.purchase.use_only_supplied_product, partner.use_only_supplied_product
+        )
 
     def set_invoice_partner(self, partner):
         self.invoice.partner_id = partner
@@ -49,8 +54,8 @@ class TestPurchaseAllowedProduct(TransactionCase):
 
         # Ensure the use_only_supplied_product is set
         self.assertEqual(
-            self.invoice.use_only_supplied_product,
-            partner.use_only_supplied_product)
+            self.invoice.use_only_supplied_product, partner.use_only_supplied_product
+        )
 
     def test_purchase_onchange(self):
         """A user creates a purchase from the form."""
@@ -63,15 +68,15 @@ class TestPurchaseAllowedProduct(TransactionCase):
             # User changes fields
             self.set_purchase_partner(self.partner_4)
 
-            self.assertEqual(
-                self.purchase.use_only_supplied_product, True)
+            self.assertEqual(self.purchase.use_only_supplied_product, True)
         context = {
-            'restrict_supplier_id': self.purchase.partner_id.id,
-            'use_only_supplied_product':
-            self.purchase.use_only_supplied_product}
+            "restrict_supplier_id": self.purchase.partner_id.id,
+            "use_only_supplied_product": self.purchase.use_only_supplied_product,
+        }
         supplied_product = self.product_model.with_context(context)._search([])
         self.assertEqual(
-            set(supplied_product), set(self.prtner_4_supplied_products.ids))
+            set(supplied_product), set(self.prtner_4_supplied_products.ids)
+        )
 
     def test_invoice_onchange(self):
         """A user creates a invoice from the form."""
@@ -84,12 +89,12 @@ class TestPurchaseAllowedProduct(TransactionCase):
             # User changes fields
             self.set_invoice_partner(self.partner_4)
 
-            self.assertEqual(
-                self.invoice.use_only_supplied_product, True)
+            self.assertEqual(self.invoice.use_only_supplied_product, True)
         context = {
-            'restrict_supplier_id': self.invoice.partner_id.id,
-            'use_only_supplied_product':
-            self.invoice.use_only_supplied_product}
+            "restrict_supplier_id": self.invoice.partner_id.id,
+            "use_only_supplied_product": self.invoice.use_only_supplied_product,
+        }
         supplied_product = self.product_model.with_context(context)._search([])
         self.assertEqual(
-            set(supplied_product), set(self.prtner_4_supplied_products.ids))
+            set(supplied_product), set(self.prtner_4_supplied_products.ids)
+        )
