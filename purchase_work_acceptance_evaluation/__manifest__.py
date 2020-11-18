4c3bb077e045e749b6ac2a655074947364d89216
Prapassorn Sornkaew <prapassorn.s@prothaitechnology.com>
2020-11-18 08:43:36 +0000

    [MIG] purchase_work_acceptance_evaluation: Migration to 14.0

--- purchase_work_acceptance_evaluation/__manifest__.py
+++ purchase_work_acceptance_evaluation/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Purchase Work Acceptance Evaluation",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Purchase Management",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "license": "AGPL-3",
