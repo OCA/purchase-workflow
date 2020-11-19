536df5373e052d293020b9d5cbf78f7dff14d727
Surachet Saejueng <surachet.s@prothaitechnology.com>
2020-11-19 05:09:57 +0000

    [MIG] purchase_open_qty: Migration to 14.0

--- purchase_open_qty/__manifest__.py
+++ purchase_open_qty/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Purchase Open Qty",
     "summary": "Allows to identify the purchase orders that have quantities "
     "pending to invoice or to receive.",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Purchases",
