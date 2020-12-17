8d4916dfc9da4a95ef6a06e8e852e0c01f9b8462
Pierrick Brun <pierrick.brun@akretion.com>
2020-12-17 14:58:34 +0100

    [MIG] purchase_quick: Migration to 14.0

--- purchase_quick/__manifest__.py
+++ purchase_quick/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Quick Purchase order",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "license": "AGPL-3",
