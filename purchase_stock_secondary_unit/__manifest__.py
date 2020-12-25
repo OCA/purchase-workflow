881148058ee28bcbfd3c119ab756495f044c1ba2
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-12-25 09:29:25 -0600

    [ADD] purchase_stock_secondary_unit: add new module that allows to pass the secondary unit from a purchase order to the related pickings

---
+++ purchase_stock_secondary_unit/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 Jarsa Sistemas
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Purchase Stock Secondary Unit",
+    "summary": "Get product quantities in a secondary unit",
+    "version": "13.0.1.0.0",
+    "development_status": "Beta",
+    "category": "stock",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "author": "Jarsa, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["purchase_order_secondary_unit", "stock_secondary_unit"],
+    "auto_install": True,
+}
