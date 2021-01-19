13ab09a0ac8352244e9296ac09765c434621ec9f
Kitti U <kittiu@gmail.com>
2021-01-19 10:51:06 +0700

    [13.0][FIX] purchase_order_secondary_unit, error selecting 2nd_uom when no product_id

--- purchase_order_secondary_unit/__manifest__.py
+++ purchase_order_secondary_unit/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Purchase Order Secondary Unit",
     "summary": "Purchase product in a secondary unit",
-    "version": "13.0.1.1.0",
+    "version": "13.0.1.1.1",
     "development_status": "Beta",
     "category": "Purchase",
     "website": "https://github.com/OCA/purchase-workflow",
