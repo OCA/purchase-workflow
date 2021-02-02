15afbfb000f2c0972f3bbcbdd45279ecd9566610
Surachet Saejueng <surachet.s@prothaitechnology.com>
2021-02-02 09:03:00 +0000

    [MIG] purchase_isolated_rfq: Migration to 14.0

--- purchase_isolated_rfq/tests/test_purchase_isolated_rfq.py
+++ purchase_isolated_rfq/tests/test_purchase_isolated_rfq.py
@@ -1,7 +1,10 @@
 # Copyright 2020 Ecosoft Co., Ltd (https://ecosoft.co.th)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 from odoo.exceptions import UserError
+from odoo.modules import registry
 from odoo.tests.common import TransactionCase
+
+from ..hooks import ACTIONS, uninstall_hook
 
 
 class TestPurchaseIsolatedRFQ(TransactionCase):
@@ -30,4 +33,31 @@ class TestPurchaseIsolatedRFQ(TransactionCase):
         with self.assertRaises(UserError) as e:
             po.action_convert_to_order()
         error_message = "Only quotation can convert to order"
-        self.assertEqual(e.exception.name, error_message)
+        self.assertEqual(e.exception.args[0], error_message)
+
+    def test_uninstall_hook(self):
+        """Test if related actions are reset to state
+        before this module was installed
+        """
+        uninstall_hook(self.cr, registry)
+        for xml_action_id in ACTIONS:
+            action = self.env.ref(xml_action_id)
+            # check context
+            check_ctx = any(
+                [
+                    x in action.context
+                    for x in ["order_sequence", "default_order_sequence"]
+                ]
+            )
+            self.assertFalse(
+                check_ctx,
+                "context order_sequence/default_order_sequence"
+                + " should be removed from action (%s)" % (action.name),
+            )
+            # check domain
+            check_dom = any([x in action.domain for x in ["order_sequence"]])
+            self.assertFalse(
+                check_dom,
+                "domain order_sequence should be removed from action (%s)"
+                % (action.name),
+            )
