b3df19414082976753451e195b0e6cf0623325fd
Cecile Jallais <cjallais@archeti.ca>
2021-02-03 13:58:35 -0500

    [IMP] purchase_order_line_sequence: black, isort, prettier

--- purchase_order_line_sequence/hooks.py
+++ purchase_order_line_sequence/hooks.py
@@ -11,5 +11,5 @@ def post_init_hook(cr, pool):
     Fetches all the PO and resets the sequence of the purchase order lines.
     """
     env = Environment(cr, SUPERUSER_ID, {})
-    purchase = env['purchase.order'].search([])
+    purchase = env["purchase.order"].search([])
     purchase._reset_sequence()
