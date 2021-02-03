f1a526b18ecdfd0bb14246130e8f94cb93d7d0a9
Cecile Jallais <cjallais@archeti.ca>
2021-02-03 13:58:35 -0500

    [IMP] purchase_order_line_sequence: black, isort, prettier

--- purchase_order_line_sequence/__manifest__.py
+++ purchase_order_line_sequence/__manifest__.py
@@ -4,24 +4,27 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Purchase Order Line Sequence',
-    'summary': 'Adds sequence to PO lines and propagates it to'
-               'Invoice lines and Stock Moves',
-    'version': '12.0.1.0.1',
-    'category': 'Purchase Management',
-    'author': "Camptocamp, "
-              "Eficent, "
-              "Serpent CS, "
-              "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/purchase-workflow',
-    'depends': [
-        'purchase_stock', 'stock_picking_line_sequence',
+    "name": "Purchase Order Line Sequence",
+    "summary": "Adds sequence to PO lines and propagates it to"
+    "Invoice lines and Stock Moves",
+    "version": "14.0.1.0.0",
+    "category": "Purchase Management",
+    "author": "Camptocamp, "
+    "Eficent, "
+    "Serpent CS, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "depends": [
+        "purchase_stock",
+        "stock_picking_line_sequence",
     ],
-    'data': ['views/purchase_view.xml',
-             'views/report_purchaseorder.xml',
-             'views/report_purchasequotation.xml'],
-    'post_init_hook': 'post_init_hook',
-    'installable': True,
-    'auto_install': False,
-    'license': "AGPL-3",
+    "data": [
+        "views/purchase_view.xml",
+        "views/report_purchaseorder.xml",
+        "views/report_purchasequotation.xml",
+    ],
+    "post_init_hook": "post_init_hook",
+    "installable": True,
+    "auto_install": False,
+    "license": "AGPL-3",
 }
