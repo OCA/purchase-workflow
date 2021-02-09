4300037149f2387dc07e0a41eda2ec70a2470e7b
Freni-OSI <fpatel@opensourceintegrators.com>
2021-02-09 14:47:05 +0530

    [14.0][ADD]Added Module purchase_order_line_menu

---
+++ purchase_order_line_menu/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2021 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Purchase Order Line Menu",
+    "version": "14.0.1.0.0",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "summary": "Adds Purchase Order Lines Menu",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "license": "AGPL-3",
+    "depends": ["purchase"],
+    "category": "Inventory/Purchase",
+    "data": [
+        "views/purchase_order_line_views.xml",
+    ],
+    "installable": True,
+    "maintainer": "dreispt",
+    "development_status": "Beta",
+}
