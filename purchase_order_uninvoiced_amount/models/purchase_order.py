877eed8248a4f88ae2135c21bd1233e5a95b1475
Víctor Martínez <victor.martinez@tecnativa.com>
2021-02-10 13:34:41 +0100

    [FIX] purchase_order_uninvoiced_amount: Allow amount_uninvoiced negative

--- purchase_order_uninvoiced_amount/models/purchase_order.py
+++ purchase_order_uninvoiced_amount/models/purchase_order.py
@@ -1,10 +1,10 @@
 # Copyright 2020 Tecnativa - Manuel Calero
 # Copyright 2020 Tecnativa - Pedro M. Baeza
 # Copyright 2020 Tecnativa - João Marques
+# Copyright 2021 Tecnativa - Víctor Martínez
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from odoo import api, fields, models
-from odoo.tools.float_utils import float_compare
 
 
 class PurchaseOrder(models.Model):
@@ -28,9 +28,6 @@ class PurchaseOrder(models.Model):
                     qty = line.product_qty - line.qty_invoiced
                 else:
                     qty = line.qty_received - line.qty_invoiced
-                rounding = line.product_uom.rounding
-                if float_compare(qty, 0.0, precision_rounding=rounding) <= 0:
-                    qty = 0.0
                 # we use this way for being compatible with purchase_discount
                 price_unit = (
                     line.product_qty
