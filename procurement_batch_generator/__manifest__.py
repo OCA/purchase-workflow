a9c2d8a1f793a56c043e51d5e2d62d0bde91b7a0
Alexis de Lattre <alexis.delattre@akretion.com>
2021-02-17 10:30:52 +0100

    procurement_batch_generator: black, isort, etc...

--- procurement_batch_generator/__manifest__.py
+++ procurement_batch_generator/__manifest__.py
@@ -3,18 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Procurement Batch Generator',
-    'version': '14.0.1.0.0',
-    'category': 'Warehouse Management',
-    'license': 'AGPL-3',
-    'summary': 'Wizard to replenish from product tree view',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'maintainers': ['alexis-via'],
-    'website': 'https://github.com/OCA/purchase-workflow',
-    'depends': ['stock'],
-    'data': [
-        'security/ir.model.access.csv',
-        'wizard/procurement_batch_generator_view.xml',
-        ],
-    'installable': True,
+    "name": "Procurement Batch Generator",
+    "version": "14.0.1.0.0",
+    "category": "Warehouse Management",
+    "license": "AGPL-3",
+    "summary": "Wizard to replenish from product tree view",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/purchase-workflow",
+    "depends": ["stock"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/procurement_batch_generator_view.xml",
+    ],
+    "installable": True,
 }
