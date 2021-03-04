f06a679a4eae9ea16ce5ec5d4532b70a7ea0b60b
Alexis de Lattre <alexis.delattre@akretion.com>
2021-03-04 10:15:21 +0100

    procurement_batch_generator: add a test on product.template scenario

--- procurement_batch_generator/tests/test_batch_generator.py
+++ procurement_batch_generator/tests/test_batch_generator.py
@@ -3,11 +3,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests.common import TransactionCase
-from odoo.tools import float_compare
 
 
 class TestProcurementBatchGenerator(TransactionCase):
-    def test_wizard(self):
+    def test_product_product_wizard(self):
         """Create procurement requests, validate and check result"""
         pbgo = self.env["procurement.batch.generator"]
         ctx = self.env.context.copy()
@@ -21,5 +20,23 @@ class TestProcurementBatchGenerator(TransactionCase):
             }
         )
         wiz = pbgo.with_context(ctx).create({})
+        self.assertEqual(len(wiz.line_ids), 2)
         wiz.line_ids.write({"procurement_qty": 42})
-        action = wiz.validate()
+        wiz.validate()
+
+    def test_product_template_wizard(self):
+        pbgo = self.env["procurement.batch.generator"]
+        ctx = self.env.context.copy()
+        ctx.update(
+            {
+                "active_ids": [
+                    self.env.ref("product.product_product_9").product_tmpl_id.id,
+                    self.env.ref("product.product_product_12").product_tmpl_id.id,
+                ],
+                "active_model": "product.template",
+            }
+        )
+        wiz = pbgo.with_context(ctx).create({})
+        self.assertEqual(len(wiz.line_ids), 2)
+        wiz.line_ids.write({"procurement_qty": 12})
+        wiz.validate()
