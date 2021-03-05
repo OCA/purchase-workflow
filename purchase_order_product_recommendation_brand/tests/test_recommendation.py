1e97e2adbb4362fcd638919be2a51430b37a2339
Manuel Calero <manuel.calero@tecnativa.com>
2021-03-05 10:13:58 +0100

    [IMP] purchase_order_product_recommendation_brand: black, isort, prettier

--- purchase_order_product_recommendation_brand/tests/test_recommendation.py
+++ purchase_order_product_recommendation_brand/tests/test_recommendation.py
@@ -1,20 +1,15 @@
 # Copyright 2019 Tecnativa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo.addons.purchase_order_product_recommendation.tests import (
-    test_recommendation)
+from odoo.addons.purchase_order_product_recommendation.tests import test_recommendation
 
 
 class BrandRecommendationCase(test_recommendation.RecommendationCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.brand_obj = cls.env['product.brand']
-        cls.brand_1 = cls.brand_obj.create({
-            'name': 'OCA Cola',
-        })
-        cls.brand_2 = cls.brand_obj.create({
-            'name': 'Play-Odoo',
-        })
+        cls.brand_obj = cls.env["product.brand"]
+        cls.brand_1 = cls.brand_obj.create({"name": "OCA Cola",})
+        cls.brand_2 = cls.brand_obj.create({"name": "Play-Odoo",})
         cls.prod_1.product_brand_id = cls.brand_1
         cls.prod_2.product_brand_id = cls.brand_2
         cls.prod_3.product_brand_id = cls.brand_2
@@ -22,7 +17,7 @@ class BrandRecommendationCase(test_recommendation.RecommendationCase):
     def test_recommendations_by_brand(self):
         """We can filter by brand"""
         wizard = self.wizard()
-        wizard.date_begin = wizard.date_end = '2019-02-01'
+        wizard.date_begin = wizard.date_end = "2019-02-01"
         # Just delivered from brand 1
         wizard.product_brand_ids = self.brand_1
         wizard.show_all_partner_products = True
@@ -45,14 +40,14 @@ class BrandRecommendationCase(test_recommendation.RecommendationCase):
     def test_recommendations_by_brand_all_products(self):
         """Brand filters also apply to all products filter"""
         wizard = self.wizard()
-        wizard.date_begin = wizard.date_end = '2019-02-01'
+        wizard.date_begin = wizard.date_end = "2019-02-01"
         # First we show all purchasable products
         wizard.show_all_products = True
         wizard.line_amount = 0
         wizard._generate_recommendations()
-        purchase_products_number = self.product_obj.search_count([
-            ('purchase_ok', '!=', False),
-        ])
+        purchase_products_number = self.product_obj.search_count(
+            [("purchase_ok", "!=", False),]
+        )
         self.assertEqual(len(wizard.line_ids), purchase_products_number)
         # Then we filter by brand
         wizard.product_brand_ids = self.brand_2
