f8caaf14ceb56c72d9b953ad8ed2085e1763a914
Jordi Ballester <jordi.ballester@forgeflow.com>
2021-03-14 14:11:33 +0100

    fixup!

--- purchase_order_line_schedule_stock/models/purchase_order_line.py
+++ purchase_order_line_schedule_stock/models/purchase_order_line.py
@@ -10,15 +10,22 @@ class PurchaseOrderLine(models.Model):
     def _prepare_stock_moves(self, picking):
         new_vals = []
         vals = super(PurchaseOrderLine, self)._prepare_stock_moves(picking)
+        schedule_line_id = self.env.context.get("schedule_line_id", False)
+
         for val in vals:
             if "purchase_line_id" in val:
                 pol = self.browse(val["purchase_line_id"])
-                if not pol.schedule_line_ids:
+                schedule_lines = pol.schedule_line_ids
+                if schedule_line_id:
+                    schedule_lines = pol.schedule_line_ids.filtered(
+                        lambda l: l.id == schedule_line_id
+                    )
+                if not schedule_lines:
                     new_vals.append(val)
                     continue
                 po_line_uom = self.product_uom
                 quant_uom = pol.product_id.uom_id
-                for sl in pol.schedule_line_ids:
+                for sl in schedule_lines:
                     new_val = val.copy()
                     remaining_qty = sl.product_qty - sl.qty_received
                     new_val["date_expected"] = sl.date_planned
--- purchase_order_line_schedule_stock/models/purchase_order_line_schedule.py
+++ purchase_order_line_schedule_stock/models/purchase_order_line_schedule.py
@@ -9,16 +9,55 @@ class PurchaseOrderLineSchedule(models.Model):
 
     @api.depends("order_line_id.qty_received")
     def _compute_qty_received(self):
-        # We really don't care about the relation between the stock move
-        # and the schedule line. We allocate all the received quantity
-        # from stock moves into the schedule lines by allocating first the older
-        # schedule lines.
         super(PurchaseOrderLineSchedule, self)._compute_qty_received()
         for ol in self.mapped("order_line_id").filtered(
-            lambda line: line.qty_received_method == "stock_moves"
+            lambda l: l.qty_received_method == "stock_moves"
         ):
-            ol._compute_qty_received()
-            qty_to_allocate = ol.qty_received
-            for sl in ol.schedule_line_ids.sorted(lambda x: x.date_planned):
-                sl.qty_received = min(sl.product_qty, qty_to_allocate)
-                qty_to_allocate -= sl.qty_received
+            for sl in ol.schedule_line_ids:
+                sl.qty_received = 0.0
+            for move in ol.move_ids.filtered(
+                lambda m: m.product_id == ol.product_id
+            ).sorted(lambda m: m.date_expected):
+                total = 0.0
+                ret_move = move.origin_returned_move_id
+                if move.state == "done":
+                    if move.location_dest_id.usage == "supplier":
+                        if move.to_refund:
+                            total -= move.product_uom._compute_quantity(
+                                move.product_uom_qty, ol.product_uom
+                            )
+                    elif (
+                        ret_move
+                        and ret_move._is_dropshipped()
+                        and not move._is_dropshipped_returned()
+                    ):
+                        pass
+                    elif (
+                        move.location_dest_id.usage == "internal"
+                        and move.to_refund
+                        and move.location_dest_id
+                        not in self.env["stock.location"].search(
+                            [("id", "child_of", move.warehouse_id.view_location_id.id)]
+                        )
+                    ):
+                        total -= move.product_uom._compute_quantity(
+                            move.product_uom_qty, ol.product_uom
+                        )
+                    else:
+                        total += move.product_uom._compute_quantity(
+                            move.product_uom_qty, ol.product_uom
+                        )
+                to_allocate = total
+                # Try to allocate first to the schedule lines that match
+                # exactly in the date
+                for sl in ol.schedule_line_ids.filtered(
+                    lambda l: l.date_planned.date() == move.date_expected.date()
+                ):
+                    qty = min(to_allocate, sl.product_qty - sl.qty_received)
+                    sl.qty_received += qty
+                    to_allocate -= qty
+                # Now try to allocate the remaining qty
+                for sl in ol.schedule_line_ids.sorted(lambda l: l.date_planned):
+                    qty = min(to_allocate, sl.product_qty - sl.qty_received)
+                    sl.qty_received += qty
+                    to_allocate -= qty
