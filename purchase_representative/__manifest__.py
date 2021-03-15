f698995ed8aba1bff9a027e8358d9cfe35c32ab5
Lois Rilo <lois.rilo@forgeflow.com>
2021-03-15 16:34:02 +0100

    [13.0][MIG] purchase_representative
    This moudule is needed again after upstrean change: https://github.com/odoo/odoo/commit/ab737229acee801f1a79066bd534a4b5f3be2db2.
    
    It ensures that the purchase representative is propagated from
    the user launching the procurement. This is is some scenarios desired.

--- purchase_representative/__manifest__.py
+++ purchase_representative/__manifest__.py
@@ -4,15 +4,12 @@
     "name": "Purchase Representative",
     "summary": "Purchase Representatives will be the point of contact for "
     "RFQ's and PO's",
-    "version": "11.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Purchases",
     "website": "https://github.com/OCA/purchase-workflow",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": ["purchase",],
-    "data": ["views/purchase_order_view.xml",],
-    "post_init_hook": "post_init_hook",
-    "uninstall_hook": "uninstall_hook",
+    "depends": ["purchase_stock"],
 }
