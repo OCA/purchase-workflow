a62e1aa567f2176ca8a5c7d3737ee9fb98d2378f
Prapassorn Sornkaew <prapassorn.s@prothaitechnology.com>
2021-03-24 06:57:59 +0000

    [IMP] purchase_cancel_reason: black, isort, prettier

--- purchase_cancel_reason/model/purchase_order.py
+++ purchase_cancel_reason/model/purchase_order.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2013 Guewen Baconnier, Camptocamp SA
 # Copyright 2017 Okia SPRL
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -6,17 +5,18 @@ from odoo import fields, models
 
 
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+    _inherit = "purchase.order"
 
     cancel_reason_id = fields.Many2one(
-        'purchase.order.cancel.reason',
+        "purchase.order.cancel.reason",
         string="Reason for cancellation",
         readonly=True,
-        ondelete="restrict")
+        ondelete="restrict",
+    )
 
 
 class PurchaseOrderCancelReason(models.Model):
-    _name = 'purchase.order.cancel.reason'
-    _description = 'Purchase Order Cancel Reason'
+    _name = "purchase.order.cancel.reason"
+    _description = "Purchase Order Cancel Reason"
 
-    name = fields.Char('Reason', required=True, translate=True)
+    name = fields.Char("Reason", required=True, translate=True)
