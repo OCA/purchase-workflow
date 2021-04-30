19a33a436dbeb154d7a69adb185157618cd9a136
Kitti U <kittiu@gmail.com>
2021-04-30 18:13:32 +0700

    [14.0][MIG] purchase_request_to_requisition

--- purchase_request_to_requisition/models/purchase_request.py
+++ purchase_request_to_requisition/models/purchase_request.py
@@ -1,20 +1,18 @@
 # Copyright 2016 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
-from openerp import _, api, exceptions, fields, models
+from odoo import _, api, exceptions, fields, models
 
 
 class PurchaseRequestLine(models.Model):
     _inherit = "purchase.request.line"
 
-    @api.multi
     @api.depends("purchase_lines")
     def _compute_is_editable(self):
         super(PurchaseRequestLine, self)._compute_is_editable()
         for rec in self.filtered(lambda p: p in self and p.requisition_lines):
             rec.is_editable = False
 
-    @api.multi
     def _compute_requisition_qty(self):
         for rec in self:
             requisition_qty = 0.0
@@ -23,7 +21,6 @@ class PurchaseRequestLine(models.Model):
                     requisition_qty += requisition_line.product_qty
             rec.requisition_qty = requisition_qty
 
-    @api.multi
     @api.depends("requisition_lines.requisition_id.state")
     def _compute_requisition_state(self):
         for rec in self:
@@ -64,11 +61,10 @@ class PurchaseRequestLine(models.Model):
         "purchase_request_purchase_requisition_line_rel",
         "purchase_request_line_id",
         "purchase_requisition_line_id",
-        string="Purchase Requisition Lines",
+        string="Purchase Agreement Lines",
         readonly=True,
         copy=False,
     )
-
     requisition_qty = fields.Float(
         compute="_compute_requisition_qty", string="Quantity in a Bid"
     )
@@ -77,18 +73,16 @@ class PurchaseRequestLine(models.Model):
         string="Bid Status",
         type="selection",
         selection=lambda self: self.env["purchase.requisition"]
-        ._columns["state"]
+        ._fields["state"]
         .selection,
         store=True,
     )
-
     is_editable = fields.Boolean(compute="_compute_is_editable", string="Is editable")
 
-    @api.multi
     def unlink(self):
         for line in self:
             if line.requisition_lines:
                 raise exceptions.Warning(
-                    _("You cannot delete a record that refers to purchase " "lines!")
+                    _("You cannot delete a record that refers to purchase lines!")
                 )
         return super(PurchaseRequestLine, self).unlink()
