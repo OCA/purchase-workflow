af2b7af13ff78dab36cd5c90fa3ed192b868866d
Matthieu Méquignon <matthieu.mequignon@camptocamp.com>
2021-05-05 10:07:48 +0200

    [MIG] purchase_propagate_qty: Migration to 13.0

--- purchase_propagate_qty/__manifest__.py
+++ purchase_propagate_qty/__manifest__.py
@@ -1,5 +1,6 @@
 # Copyright 2014-2016 Numérigraphe SARL
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2021 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
@@ -7,7 +8,7 @@
     "version": "13.0.1.0.0",
     "summary": "Quantity decrease on purchase line are propagated "
     "to the corresponding stock.move",
-    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "author": "Numerigraphe, Eficient, Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Purchase Management",
     "license": "AGPL-3",
--- purchase_propagate_qty/models/purchase.py
+++ purchase_propagate_qty/models/purchase.py
@@ -1,15 +1,15 @@
 # Copyright 2014-2016 Numérigraphe SARL
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2021 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import models
 from odoo.tools import float_is_zero
 
 
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
-    @api.multi
     def write(self, values):
         res = super().write(values)
         if "product_qty" in values or "product_uom" in values:
