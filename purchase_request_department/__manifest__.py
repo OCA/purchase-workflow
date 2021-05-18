4a9bf7d84688867b3d8ef86ae5fcbc71f41146ed
Daylin <daylinglez@gmail.com>
2021-05-18 09:35:02 -0400

    [MIG] purchase_request_department: Migration to 14.0

--- purchase_request_department/__manifest__.py
+++ purchase_request_department/__manifest__.py
@@ -1,15 +1,15 @@
 # Copyright 2017-2020 Forgeflow S.L.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "Purchase Request Department",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Purchase Management",
     "post_init_hook": "post_init_hook",
     "depends": ["hr", "purchase_request"],
     "data": ["views/purchase_request_department_view.xml"],
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "installable": True,
 }
