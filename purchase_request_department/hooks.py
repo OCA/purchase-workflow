c2f77c0aa26cc8fb43f8aea63c883ffae259b2e3
Daylin <daylinglez@gmail.com>
2021-05-18 09:35:02 -0400

    [MIG] purchase_request_department: Migration to 14.0

--- purchase_request_department/hooks.py
+++ purchase_request_department/hooks.py
@@ -1,5 +1,5 @@
 # Copyright 2017-2020 Forgeflow S.L.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 import logging
 
 from odoo import SUPERUSER_ID, api
