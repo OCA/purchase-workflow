6e101a9640d7cebf779ad9ad4393e2cd894b37cb
Daylin <daylinglez@gmail.com>
2021-05-18 09:35:02 -0400

    [MIG] purchase_request_department: Migration to 14.0

--- purchase_request_department/tests/test_purchase_request_department.py
+++ purchase_request_department/tests/test_purchase_request_department.py
@@ -1,5 +1,5 @@
 # Copyright 2017-2020 Forgeflow S.L.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo.tests.common import TransactionCase
 
