d2d0dae007c45b2d1399b56e86659d06b595d894
sbejaoui <souheil.bejaoui@acsone.eu>
2021-06-03 16:53:00 +0200

    [14.0][MIG] - purchase_fop_shipping

--- purchase_fop_shipping/__manifest__.py
+++ purchase_fop_shipping/__manifest__.py
@@ -1,10 +1,10 @@
-# © 2017 Akretion (http://www.akretion.com)
+# © 2017 Akretion (http://www.akretion.com) Payment
 #   @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Purchase Free-Of-Paiment shipping",
-    "version": "13.0.1.0.0",
+    "name": "Purchase Free-Of-Payment shipping",
+    "version": "14.0.1.0.0",
     "author": "Akretion,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "maintainer": "Akretion",
