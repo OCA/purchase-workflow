14a811f047b480079227d63cf1e14f6a30adff90
sbejaoui <souheil.bejaoui@acsone.eu>
2021-06-10 10:25:38 +0200

    [14.0][MIG] - purchase_order_line_invoicing

--- purchase_order_line_invoicing/models/res_partner.py
+++ purchase_order_line_invoicing/models/res_partner.py
@@ -1,19 +1,19 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     purchase_order_lines_count = fields.Float(
-        compute='_compute_purchase_order_lines_count')
+        compute="_compute_purchase_order_lines_count"
+    )
 
-    @api.multi
     def _compute_purchase_order_lines_count(self):
-
-        for partner in self.filtered(lambda x: x.supplier):
-            domain = [('partner_id', 'child_of', partner.id)]
-            partner.purchase_order_lines_count = self.env[
-                'purchase.order.line'].search_count(domain)
+        purchase_order_line_model = self.env["purchase.order.line"]
+        for partner in self:
+            domain = [("partner_id", "child_of", partner.id)]
+            partner.purchase_order_lines_count = purchase_order_line_model.search_count(
+                domain
+            )
