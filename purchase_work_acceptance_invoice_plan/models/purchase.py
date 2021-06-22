f98ac2d8cc81b246e7fa507fb96a53a683e82f7b
Kitti U <kittiu@gmail.com>
2021-06-22 22:28:41 +0700

    [14.0][IMP] purchase_work_acceptance_invoice_plan, invisible Create Bill by Plan button

--- purchase_work_acceptance_invoice_plan/models/purchase.py
+++ purchase_work_acceptance_invoice_plan/models/purchase.py
@@ -8,10 +8,17 @@ class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
     def _get_product_qty(self):
-        installment_id = self._context.get("installment_id", False)
+        installment_id = self.env.context.get("installment_id", False)
+        wa_qty_line_ids = self.env.context.get("wa_qty_line_ids", [])
         if installment_id:
-            installment = self.env["purchase.invoice.plan"].browse(installment_id)
-            return self.product_qty * (installment.percent / 100)
+            if wa_qty_line_ids:
+                qty = self.env["select.work.acceptance.invoice.plan.qty"].search(
+                    [("id", "in", wa_qty_line_ids), ("order_line_id", "=", self.id)]
+                )
+                return qty[:1].quantity
+            else:
+                installment = self.env["purchase.invoice.plan"].browse(installment_id)
+                return self.product_qty * (installment.percent / 100)
         return super()._get_product_qty()
 
 
@@ -35,3 +42,16 @@ class PurchaseInvoicePlan(models.Model):
                 )
             )
         return result
+
+    def _no_edit(self):
+        no_edit = super()._no_edit()
+        return no_edit or self.env["work.acceptance"].search_count(
+            [
+                ("installment_id", "=", self.id),
+            ]
+        )
+
+    def _compute_new_invoice_quantity(self, invoice_move):
+        if self.env.context.get("wa_id"):
+            return
+        return super()._compute_new_invoice_quantity(invoice_move)
