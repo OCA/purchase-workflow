467ae3e3865f0e0f2042b216032c4d24273828c6
Kitti U <kittiu@gmail.com>
2021-06-28 16:31:19 +0700

    [14.0][MIG] purchase_substate

--- purchase_substate/__manifest__.py
+++ purchase_substate/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Purchase Sub State",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Tools",
     "author": "Akretion,Ecosoft,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
