a5c667a3dbceb310721df94eeaabb3221021ce38
Víctor Martínez <victor.martinez@tecnativa.com>
2021-07-27 10:21:37 +0200

    [13.0][ADD] purchase_stock_picking_show_currency_rate: Show currency rate in purchase stock pickings

---
+++ purchase_stock_picking_show_currency_rate/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Stock Picking Show Currency Rate",
+    "summary": "Show currency rate in purchase stock picking.",
+    "version": "13.0.1.0.0",
+    "category": "Purchases",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["purchase_stock"],
+    "maintainers": ["victoralmau"],
+    "data": ["views/stock_picking_view.xml"],
+}
---
+++ purchase_stock_picking_show_currency_rate/readme/CONFIGURE.rst
@@ -0,0 +1,9 @@
+Enable the option for multiple currencies in your instance:
+
+#. Go to Invoicing > Configuration > Settings > Currencies > Multi-Currencies
+
+Rates for non-company currencies must be available and be distinct to 1.0. For example, for EUR:
+
+#. Go to Invoicing > Configuration > Currencies and go to EUR
+#. Go to Rates smart-button
+#. Update 01/01/2010 record and change rate to 1.5
