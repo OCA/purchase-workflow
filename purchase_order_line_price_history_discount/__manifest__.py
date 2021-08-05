32993366522f48f2e522c6d7c06adb3abeb79a3f
João Marques <joao.marques@tecnativa.com>
2021-08-05 09:00:40 +0100

    [IMP] *: pre-commit execution
    Fix pre-commit errors by addind the correct `website` key

--- purchase_order_line_price_history_discount/__manifest__.py
+++ purchase_order_line_price_history_discount/__manifest__.py
@@ -5,7 +5,7 @@
     "version": "13.0.1.0.0",
     "category": "Purchase Management",
     "author": "Tecnativa, Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/purchase-workflow/",
+    "website": "https://github.com/OCA/purchase-workflow",
     "license": "AGPL-3",
     "depends": ["purchase_order_line_price_history", "purchase_discount"],
     "data": ["wizards/purchase_order_line_price_history.xml"],
