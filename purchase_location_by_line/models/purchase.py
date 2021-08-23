2f25441c4a29dafdb0f3011347f277258c5077c5
Jacques-Etienne Baudoux <je@bcim.be>
2021-08-23 09:43:39 +0200

    fixup! [REF] purchase_split_by_date: Support new PO line on a confirmed PO

--- purchase_location_by_line/models/purchase.py
+++ purchase_location_by_line/models/purchase.py
@@ -19,14 +19,18 @@ class PurchaseOrderLine(models.Model):
         res = super()._is_valid_picking(picking)
         if not res:
             return res
-        location = self.location_dest_id or self.order_id._get_destination_location()
+        location = self.location_dest_id or self.env["stock.location"].browse(
+            self.order_id._get_destination_location()
+        )
         return picking.location_dest_id == location
 
     def _prepare_stock_moves(self, picking):
         # When the first move of the picking is prepared, ensure the
         # destination of the picking to make it a valid candidate
         if not picking.move_lines:
-            location = self.location_dest_id or self.order_id._get_destination_location()
+            location = self.location_dest_id or self.env["stock.location"].browse(
+                self.order_id._get_destination_location()
+            )
             if picking.location_dest_id != location:
                 picking.location_dest_id = location
         return super()._prepare_stock_moves(picking)
