34d4cc6cad0a908bb565fed75e588b5d65e19d9f
Víctor Martínez <victor.martinez@tecnativa.com>
2021-08-30 08:32:28 +0200

    [ADD] purchase_order_qty_change_no_recompute: Prevent recompute if only quantity has changed in sale order line

---
+++ purchase_order_qty_change_no_recompute/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Order Qty change no recompute",
+    "summary": "Prevent recompute if only quantity has changed in purchase order line",
+    "version": "13.0.1.0.0",
+    "category": "Purchases",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["purchase"],
+    "maintainers": ["victoralmau"],
+}
