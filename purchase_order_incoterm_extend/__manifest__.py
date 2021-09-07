91ec38e003bb9e862e41c4e3918ce8bd027a75ee
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-09-07 17:09:31 +0800

    edit manifest for pass flake8

--- purchase_order_incoterm_extend/__manifest__.py
+++ purchase_order_incoterm_extend/__manifest__.py
@@ -4,10 +4,14 @@
     "name": "Purchase Order Incoterm Extend",
     "version": "12.0.1.0.0",
     "website": "https://github.com/OCA/purchase-workflow",
-    "author": "Quartile Limited, " "Odoo Community Association (OCA)",
+    "author": "Quartile Limited, "
+              "Odoo Community Association (OCA)",
     "category": "Purchase",
     "license": "LGPL-3",
     "depends": ["purchase_stock"],
-    "data": ["views/purchase_order_views.xml", "report/purchase_order_report_templates.xml"],
+    "data": [
+        "views/purchase_order_views.xml",
+        "report/purchase_order_report_templates.xml"
+    ],
     "installable": True,
 }
