999b21f7c18f6b164c3f2bea5073948631e77002
xavier-bouquiaux <xavier.bouquiaux@acsone.eu>
2021-09-20 10:46:10 +0200

    [14.0][FIX] purchase_fop_shipping
    some view feature lost in migrations

--- purchase_fop_shipping/__manifest__.py
+++ purchase_fop_shipping/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Purchase Free-Of-Payment shipping",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "author": "Akretion,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "maintainer": "Akretion",
