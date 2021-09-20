5e0c7a23dc652ffc5768899c12315cc2748895bf
Reyes4711 <carlos.rg.92.47@gmail.com>
2021-09-20 16:46:04 +0200

    [14.0][MIG] purchase_force_invoiced: Migration to 14.0

--- purchase_force_invoiced/model/purchase.py
+++ purchase_force_invoiced/model/purchase.py
@@ -13,7 +13,8 @@ class PurchaseOrder(models.Model):
         copy=False,
         help="When you set this field, the purchase order will be "
         "considered as fully billed, even when there may be ordered "
-        "or delivered quantities pending to bill.",
+        "or delivered quantities pending to bill. To use this field, "
+        "the order must be in 'Locked' state",
     )
 
     @api.depends("force_invoiced")
@@ -23,15 +24,3 @@ class PurchaseOrder(models.Model):
             lambda po: po.force_invoiced and po.invoice_status == "to invoice"
         ):
             order.invoice_status = "invoiced"
-
-
-class PurchaseOrderLine(models.Model):
-    _inherit = "purchase.order.line"
-
-    def _prepare_account_move_line(self, line):
-        res = super(PurchaseOrderLine, self)._prepare_account_move_line(line)
-        if res.get("purchase_line_id", False):
-            pol = self.browse(res["purchase_line_id"])
-            if pol.order_id.force_invoiced:
-                res["quantity"] = 0.0
-        return res
