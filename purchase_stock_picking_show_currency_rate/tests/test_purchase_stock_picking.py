e0efed0bfeca8b13527893eaa1c99d4ae8bddfea
Víctor Martínez <victor.martinez@tecnativa.com>
2021-09-28 14:04:28 +0200

    [IMP] purchase_stock_picking_show_currency_rate: Improve tests according to different currencies

--- purchase_stock_picking_show_currency_rate/tests/test_purchase_stock_picking.py
+++ purchase_stock_picking_show_currency_rate/tests/test_purchase_stock_picking.py
@@ -8,8 +8,10 @@ class TestPurchaseStockPicking(common.SavepointCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.currency_usd = cls.env.ref("base.USD")
-        cls.currency_eur = cls.env.ref("base.EUR")
+        usd = cls.env.ref("base.USD")
+        eur = cls.env.ref("base.EUR")
+        cls.currency = cls.env.ref("base.main_company").currency_id
+        cls.currency_extra = eur if cls.currency == usd else usd
         cls.account_tax = cls.env["account.tax"].create(
             {"name": "0%", "amount_type": "fixed", "type_tax_use": "sale", "amount": 0}
         )
@@ -25,9 +27,9 @@ class TestPurchaseStockPicking(common.SavepointCase):
                 "taxes_id": [(6, 0, [cls.account_tax.id])],
             }
         )
-        # Create custom rates to USD + EUR
-        cls._create_currency_rate(cls, cls.currency_usd, fields.Date.today(), 1.0)
-        cls._create_currency_rate(cls, cls.currency_eur, fields.Date.today(), 2.0)
+        # Create custom rates to currency + currency_extra
+        cls._create_currency_rate(cls, cls.currency, fields.Date.today(), 1.0)
+        cls._create_currency_rate(cls, cls.currency_extra, fields.Date.today(), 2.0)
 
     def _create_currency_rate(self, currency_id, name, rate):
         self.env["res.currency.rate"].create(
@@ -45,19 +47,19 @@ class TestPurchaseStockPicking(common.SavepointCase):
         purchase.button_confirm()
         return purchase
 
-    def test_01_purchase_usd(self):
-        purchase = self._create_purchase_order(self.currency_usd)
+    def test_01_purchase_currency(self):
+        purchase = self._create_purchase_order(self.currency)
         picking = purchase.picking_ids[0]
-        self.assertEqual(picking.purchase_currency_id, self.currency_usd)
+        self.assertEqual(picking.purchase_currency_id, self.currency)
         self.assertEqual(picking.currency_rate_amount, 1.0)
         picking.move_ids_without_package.quantity_done = 1
         picking.button_validate()
         self.assertEqual(picking.move_lines.stock_valuation_layer_ids.unit_cost, 10)
 
-    def test_02_purchase_eur(self):
-        purchase = self._create_purchase_order(self.currency_eur)
+    def test_02_purchase_currency_extra(self):
+        purchase = self._create_purchase_order(self.currency_extra)
         picking = purchase.picking_ids[0]
-        self.assertEqual(picking.purchase_currency_id, self.currency_eur)
+        self.assertEqual(picking.purchase_currency_id, self.currency_extra)
         self.assertEqual(picking.currency_rate_amount, 2.0)
         picking.move_ids_without_package.quantity_done = 1
         picking.button_validate()
