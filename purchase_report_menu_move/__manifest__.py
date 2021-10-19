8cb8ff883f68046a630b26e2563673d7b7ed0499
newtratip <tharathip.chaweewongphan@gmail.com>
2021-10-19 21:46:54 +0700

    [14.0][ADD] purchase_report_menu_move

---
+++ purchase_report_menu_move/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Purchase Report Menu Move",
+    "version": "14.0.1.0.0",
+    "author": "Ecosoft,Odoo Community Association (OCA)",
+    "category": "Purchase",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "depends": ["purchase"],
+    "data": ["report/purchase_report_views.xml"],
+    "auto_install": False,
+    "installable": True,
+    "maintainers": ["newtratip"],
+}
