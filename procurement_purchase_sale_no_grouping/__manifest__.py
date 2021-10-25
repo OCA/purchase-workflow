f607a81aa123ce4cc9858bf9599c4d32981208a1
sergio-teruel <sergio.teruel@tecnativa.com>
2021-10-25 22:05:18 +0200

    [MIG] procurement_purchase_sale_no_grouping: Migration to v13.0 TT31569

--- procurement_purchase_sale_no_grouping/__manifest__.py
+++ procurement_purchase_sale_no_grouping/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2021 Tecnativa - Carlos Dauden
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
 {
     "name": "Procurement Purchase Service No Grouping",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.0",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Procurements",
-    "depends": ["procurement_purchase_no_grouping", "sale_purchase",],
+    "depends": ["procurement_purchase_no_grouping", "sale_purchase"],
     "installable": True,
     "auto_install": True,
     "license": "AGPL-3",
