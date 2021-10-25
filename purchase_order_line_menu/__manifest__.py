0bb7e0f179ee6e439e8860c07c32a9040afc7a74
ChristianSantamaria <christiancameselle@hotmail.com>
2021-10-25 09:05:06 +0200

    [MIG] purchase_order_line_menu: Migration to 13.0

--- purchase_order_line_menu/__manifest__.py
+++ purchase_order_line_menu/__manifest__.py
@@ -3,14 +3,14 @@
 
 {
     "name": "Purchase Order Line Menu",
-    "version": "14.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "summary": "Adds Purchase Order Lines Menu",
     "website": "https://github.com/OCA/purchase-workflow",
     "license": "AGPL-3",
     "depends": ["purchase"],
     "category": "Inventory/Purchase",
-    "data": ["views/purchase_order_line_views.xml",],
+    "data": ["views/purchase_order_line_views.xml"],
     "installable": True,
     "maintainer": "dreispt",
     "development_status": "Beta",
