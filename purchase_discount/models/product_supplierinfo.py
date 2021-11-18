9c0cbf8b3a30f6156250749d9370266e8763b34c
moaadbourhim <moaad.bourhim@gmail.com>
2021-11-18 15:41:02 +0100

    [IMP] purchase_discount: black, isort, prettier

--- purchase_discount/models/product_supplierinfo.py
+++ purchase_discount/models/product_supplierinfo.py
@@ -12,7 +12,7 @@ class ProductSupplierInfo(models.Model):
 
     @api.onchange("name")
     def onchange_name(self):
-        """ Apply the default supplier discount of the selected supplier """
+        """Apply the default supplier discount of the selected supplier"""
         for supplierinfo in self.filtered("name"):
             supplierinfo.discount = supplierinfo.name.default_supplierinfo_discount
 
