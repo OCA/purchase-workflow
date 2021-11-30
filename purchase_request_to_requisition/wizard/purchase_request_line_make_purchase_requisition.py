f76c4af1f6f3a065913bed1be845e435f5ff6944
newtratip <tharathip.chaweewongphan@gmail.com>
2021-11-30 16:16:40 +0700

    Send analytic tag from purchase request to purchase requisition

--- purchase_request_to_requisition/wizard/purchase_request_line_make_purchase_requisition.py
+++ purchase_request_to_requisition/wizard/purchase_request_line_make_purchase_requisition.py
@@ -75,6 +75,7 @@ class PurchaseRequestLineMakePurchaseRequisition(models.TransientModel):
             "product_uom_id": item.product_uom_id.id,
             "purchase_request_lines": [(4, item.line_id.id)],
             "account_analytic_id": item.line_id.analytic_account_id.id or False,
+            "analytic_tag_ids": item.line_id.analytic_tag_ids.ids or False,
             "product_description_variants": item.name,
         }
 
@@ -85,6 +86,7 @@ class PurchaseRequestLineMakePurchaseRequisition(models.TransientModel):
             ("product_id", "=", item.product_id.id or False),
             ("product_uom_id", "=", item.product_uom_id.id or False),
             ("account_analytic_id", "=", item.line_id.analytic_account_id.id or False),
+            ("analytic_tag_ids", "=", item.line_id.analytic_tag_ids.ids or False),
         ]
         return vals
 
