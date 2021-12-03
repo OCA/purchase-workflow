f12516041d2d39a31f1b3649debf8fc56d8fe314
David Alonso <david.alonso@solvos.es>
2021-12-03 20:58:13 +0100

    [FIX] purchase_security: prevent access error
    When a Purchase User (own orders) create or edit an order, if changes
    Representative user to another and save it, an access error is raised.
    With this change, those users cannot change the default Representative,
    then orders are initially assigned to them, and no errors were fired.

--- purchase_security/__manifest__.py
+++ purchase_security/__manifest__.py
@@ -11,7 +11,7 @@
     "license": "AGPL-3",
     "depends": ["purchase"],
     "maintainers": ["joao-p-marques"],
-    "data": ["security/security.xml"],
+    "data": ["security/security.xml", "views/purchase_order_views.xml"],
     "installable": True,
     "auto_install": False,
 }
