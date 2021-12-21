30b0610a45eb4548e38808dba5c623df095586e5
Bernat Puig Font <bernat.puig@forgeflow.com>
2021-12-21 14:09:03 +0100

    [MIG] purchase_reception_notify: Migration to 15.0

--- purchase_reception_notify/hooks.py
+++ purchase_reception_notify/hooks.py
@@ -16,19 +16,18 @@ def post_init_hook(cr, registry):
         "existing purchase orders to assign the new subtype "
         "Purchase Receptions."
     )
-    with api.Environment.manage():
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        users = env["res.users"].search([])
-        followers = env["mail.followers"].search(
-            [
-                ("res_model", "=", "purchase.order"),
-                ("partner_id", "in", users.mapped("partner_id").ids),
-            ]
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    users = env["res.users"].search([])
+    followers = env["mail.followers"].search(
+        [
+            ("res_model", "=", "purchase.order"),
+            ("partner_id", "in", users.mapped("partner_id").ids),
+        ]
+    )
+    for follower in followers:
+        follower.subtype_ids += env.ref(
+            "purchase_reception_notify.mt_purchase_reception"
         )
-        for follower in followers:
-            follower.subtype_ids += env.ref(
-                "purchase_reception_notify.mt_purchase_reception"
-            )
     _logger.info(
         "Finalized task to update internal followers for "
         "existing purchase orders to assign the new subtype "
