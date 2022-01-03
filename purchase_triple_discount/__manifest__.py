16430c32ab498cfdf3fa488378c260298dc9055c
Christopher Ormaza <chris.ormaza@forgeflow.com>
2022-01-03 15:31:04 -0500

    [14.0][MIG] purchase_triple_discount

--- purchase_triple_discount/__manifest__.py
+++ purchase_triple_discount/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Purchase Order Triple Discount",
-    "version": "12.0.1.1.0",
+    "version": "14.0.1.0.0",
     "category": "Purchase Management",
     "author": "Tecnativa," "GRAP," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
