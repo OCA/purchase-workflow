d46a02ad337ffc40ced30d20d1b74b8c662c0128
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2022-01-18 16:52:18 +0530

    [MIG] purchase_advance_payment: Migration to 15.0

--- purchase_advance_payment/__manifest__.py
+++ purchase_advance_payment/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Purchase Advance Payment",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Forgeflow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Purchase",
