7d18e9622c0ab40f9e323506dac2864f5c4f55bb
Mateu Griful <mateu.griful@forgeflow.com>
2022-01-24 14:19:02 +0100

    [14.0][ADD] purchase_order_line_name_to_picking

---
+++ purchase_order_line_description_picking/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 ForgeFlow S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0).
+
+{
+    "name": "Purchase Order Line Name To Picking",
+    "version": "14.0.1.0.0",
+    "category": "Purchase Management",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "license": "AGPL-3",
+    "depends": ["purchase_stock"],
+    "installable": True,
+}
