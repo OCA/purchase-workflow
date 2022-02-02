51b81cfc87ad934379994354e0ed33d2ada34d87
SilvioC2C <silvio.gregorini@camptocamp.com>
2022-02-02 17:28:08 +0100

    [15.0][ADD] purchase_receipt_expectation

---
+++ purchase_receipt_expectation/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Purchase Receipt Expectation",
+    "version": "15.0.1.0.0",
+    "category": "Purchase Management",
+    "author": "Camptocamp SA, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "license": "AGPL-3",
+    "data": [
+        "views/purchase_order.xml",
+    ],
+    "depends": [
+        "purchase_stock",
+    ],
+    "installable": True,
+}
