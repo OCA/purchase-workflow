cc838b7b3b19e7c7901be868908763274c7c064b
CarlosRoca13 <carlos.roca@tecnativa.com>
2022-02-08 17:34:58 +0100

    [IMP] purchase_order_general_discount: black, isort, prettier

--- purchase_order_general_discount/models/res_company.py
+++ purchase_order_general_discount/models/res_company.py
@@ -7,11 +7,11 @@ class ResCompany(models.Model):
     _inherit = "res.company"
 
     purchase_general_discount_field = fields.Selection(
-        selection='_get_purchase_discount_fields',
-        string='Purchase Discount Field',
-        default='discount',
-        help='Set the purchase line discount field in wich the '
-             'discounts will be applied.',
+        selection="_get_purchase_discount_fields",
+        string="Purchase Discount Field",
+        default="discount",
+        help="Set the purchase line discount field in wich the "
+        "discounts will be applied.",
     )
 
     @api.model
@@ -19,14 +19,14 @@ class ResCompany(models.Model):
         """Extensible method to add possible discounts. We offer in advance
            the posibility of using purchase_triple_discount so no bridge
            module is needed """
-        discount_fields = [('discount', _('Discount'))]
-        purchase_line_fields = self.env['purchase.order.line']._fields.keys()
-        if 'discount2' in purchase_line_fields:
+        discount_fields = [("discount", _("Discount"))]
+        purchase_line_fields = self.env["purchase.order.line"]._fields.keys()
+        if "discount2" in purchase_line_fields:
             discount_fields += [
-                ('discount2', _('Discount 2')),
+                ("discount2", _("Discount 2")),
             ]
-        if 'discount3' in purchase_line_fields:
+        if "discount3" in purchase_line_fields:
             discount_fields += [
-                ('discount3', _('Discount 3')),
+                ("discount3", _("Discount 3")),
             ]
         return discount_fields
