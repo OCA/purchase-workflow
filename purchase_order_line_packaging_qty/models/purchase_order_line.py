8bdce098719d130db2f9e584a7910c27884538db
Iryna Vyshnevska <iryna.vyshnevska@camptocamp.com>
2022-02-08 21:22:33 +0200

    [FIX] purchase_order_line_packaging_qty:
    rename method and move to proper model

--- purchase_order_line_packaging_qty/models/purchase_order_line.py
+++ purchase_order_line_packaging_qty/models/purchase_order_line.py
@@ -108,3 +108,16 @@ class PurchaseOrderLine(models.Model):
                 },
             }
         return {}
+
+    @api.model
+    def _prepare_purchase_order_line_from_procurement(
+        self, product_id, product_qty, product_uom, company_id, values, po
+    ):
+        # For new PO lines we set the product packaging if present in
+        # the procurement values.
+        vals = super()._prepare_purchase_order_line_from_procurement(
+            product_id, product_qty, product_uom, company_id, values, po
+        )
+        if values.get("product_packaging_id"):
+            vals["product_packaging"] = values.get("product_packaging_id").id
+        return vals
