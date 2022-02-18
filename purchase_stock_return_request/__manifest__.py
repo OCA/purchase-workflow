085929ad0faa9a5cb0088c1b93f4976c9d805988
CarlosRoca13 <carlos.roca@tecnativa.com>
2022-02-18 13:17:10 +0100

    [MIG] purchase_stock_return_request: Migration to 13.0

--- purchase_stock_return_request/__manifest__.py
+++ purchase_stock_return_request/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Purchase Stock Return Request",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Stock",
     "website": "https://github.com/OCA/purchase-workflow",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
