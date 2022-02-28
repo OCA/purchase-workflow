3a22b4ea9adf88f25ea3a4f332732bb9fdc4fc33
CarlosRoca13 <carlos.roca@tecnativa.com>
2022-02-28 10:12:00 +0100

    [MIG] purchase_order_product_recommendation_secondary_unit: Migration to 13.0

--- purchase_order_product_recommendation_secondary_unit/__manifest__.py
+++ purchase_order_product_recommendation_secondary_unit/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Purchase Order Product Recommendation Secondary Unit",
     "summary": "Add secondary unit to recommend products wizard",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Purchases",
     "website": "https://github.com/OCA/purchase-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
