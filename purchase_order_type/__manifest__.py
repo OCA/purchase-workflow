6b0ef8069d1f8796c311367f30b5cddaf1b4b1a9
Vincent Van Rossem <vvrossem@gmail.com>
2022-03-21 15:08:31 +0100

    [MIG] purchase_order_type: Migration to version 15.0

--- purchase_order_type/__manifest__.py
+++ purchase_order_type/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Purchase Order Type",
-    "version": "14.0.1.0.2",
+    "version": "15.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Purchase Management",
