4522cdcd07891afe9cf3e12611dca2f42b0a1149
Michael Michot <michotm@gmail.com>
2022-04-06 10:17:15 +0200

    [IMP] last_purchase_price computation

--- purchase_last_price_info/models/product_product.py
+++ purchase_last_price_info/models/product_product.py
@@ -53,7 +53,10 @@ class ProductProduct(models.Model):
     @api.depends("last_purchase_line_id")
     def _compute_last_purchase_line_id_info(self):
         for item in self:
-            item.last_purchase_price = item.last_purchase_line_id.price_unit
+            item.last_purchase_price = (
+                item.last_purchase_line_id.price_subtotal
+                / item.last_purchase_line_id.product_qty
+            )
             item.last_purchase_date = item.last_purchase_line_id.date_order
             item.last_purchase_supplier_id = item.last_purchase_line_id.partner_id
             item.last_purchase_currency_id = item.last_purchase_line_id.currency_id
