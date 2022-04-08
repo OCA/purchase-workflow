fae75ad259b45531fccb94fb003512717d9a483a
Murtaza Mithaiwala <murtaza.m.serpentcs@gmail.com>
2022-04-08 09:48:30 +0530

    [15.0][ECH] Add the feature to set the Term Conditions based on Supplier.

--- purchase_default_terms_conditions/models/res_partner.py
+++ purchase_default_terms_conditions/models/res_partner.py
@@ -1,12 +1,12 @@
-# Copyright 2020 - TODAY, Marcel Savegnago - Escodoo
+# Copyright (C) 2022 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
 
 
-class ResCompany(models.Model):
+class ResPartner(models.Model):
 
-    _inherit = "res.company"
+    _inherit = "res.partner"
 
     purchase_note = fields.Text(
         string="Purchase Default Terms and Conditions", translate=True
