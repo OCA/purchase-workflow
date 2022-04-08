96cda7221c7c61ce59be5bfc348378d76943bbdf
Murtaza Mithaiwala <murtaza.m.serpentcs@gmail.com>
2022-04-08 09:39:46 +0530

    [15.0][MIG] purchase_default_terms_conditions.

--- purchase_default_terms_conditions/__manifest__.py
+++ purchase_default_terms_conditions/__manifest__.py
@@ -2,18 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Purchase Default Terms Conditions',
-    'summary': """
+    "name": "Purchase Default Terms Conditions",
+    "summary": """
         This module allows purchase default terms & conditions""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Escodoo,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/purchase-workflow',
-    'images': ['static/description/banner.png'],
-    'depends': [
-        'purchase',
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "images": ["static/description/banner.png"],
+    "depends": [
+        "purchase",
     ],
-    'data': [
-        'views/res_config_settings.xml',
+    "data": [
+        "views/res_config_settings.xml",
     ],
 }
