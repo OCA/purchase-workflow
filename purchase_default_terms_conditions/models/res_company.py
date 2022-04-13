e81576af33fb3f4631c4b71bcedef63729e12c17
Nikul-OSI <nchaudhary@opensourceintegrators.com>
2022-04-13 18:55:43 +0530

    [IMP] changed field Text to Html on note.

--- purchase_default_terms_conditions/models/res_company.py
+++ purchase_default_terms_conditions/models/res_company.py
@@ -8,6 +8,6 @@ class ResCompany(models.Model):
 
     _inherit = "res.company"
 
-    purchase_note = fields.Text(
+    purchase_note = fields.Html(
         string="Purchase Default Terms and Conditions", translate=True
     )
