bda238efbe17e2c72078b27c796a8d9e38e7daf0
i1 <ivan@cetmix.com>
2022-04-19 16:18:18 +0200

    [14.0][ADD] purchase_order_vendor_product

---
+++ purchase_order_vendor_product/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Purchase Order Vendor Products",
+    "version": "14.0.1.0.0",
+    "summary": "Show only products from the selected vendor in PO line",
+    "author": "Ooops, Cetmix, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Purchase",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "depends": ["purchase_stock"],
+    "external_dependencies": {},
+    "demo": [],
+    "data": [
+        "views/purchase_order.xml",
+    ],
+    "qweb": [],
+    "installable": True,
+    "application": False,
+}
