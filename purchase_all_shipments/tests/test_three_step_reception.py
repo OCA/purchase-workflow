2de32d63cb53e6b5e9d13611a34bf7aec2fc95b9
Joel Zilli <joz@adhoc.com.ar>
2022-05-03 12:19:13 -0300

    [MIG] purchase_all_shipments: Migration to 15.0

--- purchase_all_shipments/tests/test_three_step_reception.py
+++ purchase_all_shipments/tests/test_three_step_reception.py
@@ -10,12 +10,11 @@ class TestThreeStepReception(SavepointCase):
         super().setUpClass()
         cls.wh = cls.env.ref("stock.warehouse0")
         cls.po = cls.env.ref("purchase.purchase_order_1")
-        cls.po.location_id = cls.wh.wh_input_stock_loc_id
 
     def test_three_steps_generate_three_pickings(self):
         self.wh.reception_steps = "three_steps"
         self.po.button_confirm()
-        self.assertEqual(1, self.po.picking_count)
+        self.assertEqual(1, self.po.incoming_picking_count)
         self.assertEqual(3, self.po.all_picking_count)
 
     def test_action_view_all_pickings_one_step(self):
