daaccbb7a70f557b98d0bc30fa0aae1ea5811d83
Maksym Yankin <yankinmk@gmail.com>
2022-05-04 10:51:49 +0300

    [MIG] purchase_exception: Migration to 15.0

--- purchase_exception/tests/test_purchase_exception.py
+++ purchase_exception/tests/test_purchase_exception.py
@@ -9,44 +9,45 @@ from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
 
 class TestPurchaseException(TransactionCase):
-    def setUp(self):
-        super(TestPurchaseException, self).setUp()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         # Useful models
-        self.PurchaseOrder = self.env["purchase.order"]
-        self.PurchaseOrderLine = self.env["purchase.order.line"]
-        self.partner_id = self.env.ref("base.res_partner_1")
-        self.product_id_1 = self.env.ref("product.product_product_6")
-        self.product_id_2 = self.env.ref("product.product_product_7")
-        self.product_id_3 = self.env.ref("product.product_product_7")
-        self.date_planned = datetime.today().strftime(DEFAULT_SERVER_DATETIME_FORMAT)
-        self.purchase_exception_confirm = self.env["purchase.exception.confirm"]
-        self.exception_noemail = self.env.ref("purchase_exception.po_excep_no_email")
-        self.exception_qtycheck = self.env.ref("purchase_exception.pol_excep_qty_check")
-        self.po_vals = {
-            "partner_id": self.partner_id.id,
+        cls.PurchaseOrder = cls.env["purchase.order"]
+        cls.PurchaseOrderLine = cls.env["purchase.order.line"]
+        cls.partner_id = cls.env.ref("base.res_partner_1")
+        cls.product_id_1 = cls.env.ref("product.product_product_6")
+        cls.product_id_2 = cls.env.ref("product.product_product_7")
+        cls.product_id_3 = cls.env.ref("product.product_product_7")
+        cls.date_planned = datetime.today().strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+        cls.purchase_exception_confirm = cls.env["purchase.exception.confirm"]
+        cls.exception_noemail = cls.env.ref("purchase_exception.po_excep_no_email")
+        cls.exception_qtycheck = cls.env.ref("purchase_exception.pol_excep_qty_check")
+        cls.po_vals = {
+            "partner_id": cls.partner_id.id,
             "order_line": [
                 (
                     0,
                     0,
                     {
-                        "name": self.product_id_1.name,
-                        "product_id": self.product_id_1.id,
+                        "name": cls.product_id_1.name,
+                        "product_id": cls.product_id_1.id,
                         "product_qty": 5.0,
-                        "product_uom": self.product_id_1.uom_po_id.id,
+                        "product_uom": cls.product_id_1.uom_po_id.id,
                         "price_unit": 500.0,
-                        "date_planned": self.date_planned,
+                        "date_planned": cls.date_planned,
                     },
                 ),
                 (
                     0,
                     0,
                     {
-                        "name": self.product_id_2.name,
-                        "product_id": self.product_id_2.id,
+                        "name": cls.product_id_2.name,
+                        "product_id": cls.product_id_2.id,
                         "product_qty": 5.0,
-                        "product_uom": self.product_id_2.uom_po_id.id,
+                        "product_uom": cls.product_id_2.uom_po_id.id,
                         "price_unit": 250.0,
-                        "date_planned": self.date_planned,
+                        "date_planned": cls.date_planned,
                     },
                 ),
             ],
@@ -112,11 +113,9 @@ class TestPurchaseException(TransactionCase):
         # Simulation the opening of the wizard purchase_exception_confirm and
         # set ignore_exception to True
         po_except_confirm = self.purchase_exception_confirm.with_context(
-            {
-                "active_id": self.po.id,
-                "active_ids": [self.po.id],
-                "active_model": self.po._name,
-            }
+            active_id=self.po.id,
+            active_ids=[self.po.id],
+            active_model=self.po._name,
         ).create({"ignore": True})
         po_except_confirm.action_confirm()
         self.assertTrue(self.po.ignore_exception)
