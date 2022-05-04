c60ed73b61871c527786fc4b71d45f0faa9180b8
Maksym Yankin <yankinmk@gmail.com>
2022-05-04 10:16:08 +0300

    [MIG] purchase_analytic_global: Migration to 15.0

--- purchase_analytic_global/tests/test_purchase_analytic_global.py
+++ purchase_analytic_global/tests/test_purchase_analytic_global.py
@@ -2,58 +2,59 @@
 
 from datetime import date
 
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import Form, TransactionCase
 
 
 class TestPurchaseAnalyticGlobal(TransactionCase):
-    def setUp(self):
-        super(TestPurchaseAnalyticGlobal, self).setUp()
-        self.purchase_order_model = self.env["purchase.order"]
-        self.partner_model = self.env["res.partner"]
-        self.analytic_account_model = self.env["account.analytic.account"]
-        self.partner1 = self.partner_model.create({"name": "Partner1"})
-        self.partner2 = self.partner_model.create({"name": "Partner2"})
-        self.analytic_account1 = self.analytic_account_model.create(
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.purchase_order_model = cls.env["purchase.order"]
+        cls.partner_model = cls.env["res.partner"]
+        cls.analytic_account_model = cls.env["account.analytic.account"]
+        cls.partner1 = cls.partner_model.create({"name": "Partner1"})
+        cls.partner2 = cls.partner_model.create({"name": "Partner2"})
+        cls.analytic_account1 = cls.analytic_account_model.create(
             {"name": "Analytic Account 1"}
         )
-        self.analytic_account2 = self.analytic_account_model.create(
+        cls.analytic_account2 = cls.analytic_account_model.create(
             {"name": "Analytic Account 2"}
         )
-        self.product = self.env.ref("product.product_product_4")
-        self.purchase_order1 = self.purchase_order_model.create(
+        cls.product = cls.env.ref("product.product_product_4")
+        cls.purchase_order1 = cls.purchase_order_model.create(
             {
-                "partner_id": self.partner1.id,
-                "account_analytic_id": self.analytic_account1.id,
+                "partner_id": cls.partner1.id,
+                "account_analytic_id": cls.analytic_account1.id,
                 "order_line": [
                     (
                         0,
                         0,
                         {
-                            "product_id": self.product.id,
-                            "name": self.product.name,
+                            "product_id": cls.product.id,
+                            "name": cls.product.name,
                             "product_qty": 10,
                             "price_unit": 50,
-                            "product_uom": self.product.uom_id.id,
+                            "product_uom": cls.product.uom_id.id,
                             "date_planned": date.today(),
                         },
                     )
                 ],
             }
         )
-        self.purchase_order2 = self.purchase_order_model.create(
+        cls.purchase_order2 = cls.purchase_order_model.create(
             {
-                "partner_id": self.partner2.id,
+                "partner_id": cls.partner2.id,
                 "order_line": [
                     (
                         0,
                         0,
                         {
-                            "product_id": self.product.id,
-                            "name": self.product.name,
+                            "product_id": cls.product.id,
+                            "name": cls.product.name,
                             "product_qty": 5,
                             "price_unit": 40,
-                            "account_analytic_id": self.analytic_account2.id,
-                            "product_uom": self.product.uom_id.id,
+                            "account_analytic_id": cls.analytic_account2.id,
+                            "product_uom": cls.product.uom_id.id,
                             "date_planned": date.today(),
                         },
                     )
@@ -69,3 +70,14 @@ class TestPurchaseAnalyticGlobal(TransactionCase):
         self.assertEqual(
             self.purchase_order2.account_analytic_id, self.analytic_account2
         )
+        purchase_form = Form(self.purchase_order2)
+        with purchase_form.order_line.new() as line_form:
+            line_form.product_id = self.product
+            line_form.name = self.product.name
+            line_form.product_qty = 10
+            line_form.price_unit = 20
+            line_form.account_analytic_id = self.analytic_account1
+            line_form.product_uom = self.product.uom_id
+            line_form.date_planned = date.today()
+        purchase_form.save()
+        self.assertFalse(self.purchase_order2.account_analytic_id)
