7ca1c184e361386f556c19cf7a40fdf09da8d250
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-05-12 08:42:43 +0200

    [FIX] purchase_stock_secondary_unit: Proper license
    Must be AGPL, as it depends on AGPL.

--- purchase_stock_secondary_unit/__manifest__.py
+++ purchase_stock_secondary_unit/__manifest__.py
@@ -1,15 +1,15 @@
 # Copyright 2020 Jarsa Sistemas
 # Copyright 2021 Tecnativa - Sergio Teruel
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
     "name": "Purchase Stock Secondary Unit",
     "summary": "Get product quantities in a secondary unit",
-    "version": "13.0.1.0.1",
+    "version": "13.0.1.0.2",
     "development_status": "Beta",
     "category": "stock",
     "website": "https://github.com/OCA/purchase-workflow",
     "author": "Jarsa, Odoo Community Association (OCA)",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "application": False,
     "installable": True,
     "depends": ["purchase_order_secondary_unit", "stock_secondary_unit"],
