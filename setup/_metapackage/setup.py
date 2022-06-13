b42b0afbc52b1117d9959ab664d22d65542c5b47
Janire Olagibel <janire.olaguibel@bdo.es>
2022-06-13 10:48:28 +0200

    [MIG] purchase_force_invoiced black, isort, prettier

--- setup/_metapackage/setup.py
+++ setup/_metapackage/setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
         'odoo-addon-purchase_delivery_split_date>=15.0dev,<15.1dev',
         'odoo-addon-purchase_deposit>=15.0dev,<15.1dev',
         'odoo-addon-purchase_discount>=15.0dev,<15.1dev',
+        'odoo-addon-purchase_force_invoiced>=15.0dev,<15.1dev',
         'odoo-addon-purchase_order_line_menu>=15.0dev,<15.1dev',
         'odoo-addon-purchase_receipt_expectation>=15.0dev,<15.1dev',
         'odoo-addon-purchase_receipt_expectation_manual>=15.0dev,<15.1dev',
