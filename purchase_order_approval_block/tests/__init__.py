af300bd29e2f3743e0be967cb459799f489f830d
Denis Roussel <denis.roussel@acsone.eu>
2022-07-06 17:45:55 +0200

    [CHG] purchase_order_approval_block: Change to AGPL as depending on AGPL purchase_exception

--- purchase_order_approval_block/tests/__init__.py
+++ purchase_order_approval_block/tests/__init__.py
@@ -1,4 +1,4 @@
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import test_purchase_order_approval_block
 from . import test_po_approval_block_reason
