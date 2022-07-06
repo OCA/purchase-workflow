ba6ed1498913654b36793f2c870a93257c2a3373
Denis Roussel <denis.roussel@acsone.eu>
2022-07-06 17:45:55 +0200

    [CHG] purchase_order_approval_block: Change to AGPL as depending on AGPL purchase_exception

--- purchase_order_approval_block/wizard/__init__.py
+++ purchase_order_approval_block/wizard/__init__.py
@@ -1,3 +1,3 @@
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import purchase_exception_confirm
