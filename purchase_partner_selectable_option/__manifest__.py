4fcea5bffbbc53f1c74be7822368ae4e17ecb138
Cesar Andres Sanchez <cesar.sanchez@tecnativa.com>
2022-07-06 12:59:58 +0200

    [MIG] purchase_partner_selectable_option: Migration to 15.0

--- purchase_partner_selectable_option/__manifest__.py
+++ purchase_partner_selectable_option/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Purchase Partner Selectable Option",
-    "version": "13.0.1.1.0",
+    "version": "15.0.1.0.0",
     "category": "Purchase",
     "website": "https://github.com/OCA/purchase-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
