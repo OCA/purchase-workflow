1f596d35cfd9451657927630003a3c1c21b76ce4
Denis Roussel <denis.roussel@acsone.eu>
2022-07-06 17:45:55 +0200

    [CHG] purchase_order_approval_block: Change to AGPL as depending on AGPL purchase_exception

--- purchase_order_approval_block/__manifest__.py
+++ purchase_order_approval_block/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2017 ForgeFlow S.L.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Purchase Order Approval Block",
@@ -15,6 +15,6 @@
         "views/purchase_approval_block_reason_view.xml",
         "views/purchase_order_view.xml",
     ],
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "installable": True,
 }
