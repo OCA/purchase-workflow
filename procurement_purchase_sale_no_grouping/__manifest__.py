39f0ae5f323fce91deda7b8da808b2420e1ed783
Cesar Andres Sanchez <cesar.sanchez@tecnativa.com>
2022-07-12 10:49:56 +0200

    [MIG] procurement_purchase_sale_no_grouping: Migration to 15.0

--- procurement_purchase_sale_no_grouping/__manifest__.py
+++ procurement_purchase_sale_no_grouping/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Procurement Purchase Service No Grouping",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Procurements",
