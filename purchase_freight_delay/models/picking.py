c04e0cb51af20a2e6aa25efac7596152d8c92ff1
hparfr <raphael.reverdy@akretion.com>
2022-07-15 18:22:22 +0200

    purchase_freight_delay refactoring

--- purchase_freight_delay/models/picking.py
+++ purchase_freight_delay/models/picking.py
@@ -3,17 +3,29 @@
 
 
 from odoo import api, fields, models
-from .purchase import HELP_DURATION, HELP_DISPATCH
+
+from .purchase import HELP_DISPATCH, HELP_DURATION
 
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    freight_duration = fields.Integer(
-        store=True, readonly=True, copy=False, help=HELP_DURATION
-    )
-    dispatch_date = fields.Date(
-        copy=False,
-        readonly=True,
+    dispatch_date = fields.Datetime(
         help=HELP_DISPATCH,
     )
+    freight_duration = fields.Integer(
+        compute="_compute_freight_duration",
+        store=True,
+        readonly=True,
+        copy=False,
+        help=HELP_DURATION,
+    )
+
+    @api.depends("dispatch_date", "scheduled_date")
+    def _compute_freight_duration(self):
+        _set_freight_fields = self.env["purchase.order"]._set_freight_fields
+        for rec in self:
+            res = _set_freight_fields(
+                rec.scheduled_date, rec.dispatch_date, 0, "freight_duration"
+            )
+            rec.freight_duration = res["freight_duration"]
--- purchase_freight_delay/tests/test_module.py
+++ purchase_freight_delay/tests/test_module.py
@@ -1,8 +1,9 @@
 # © 2022 David BEAL @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from freezegun import freeze_time
 from datetime import timedelta
+
+from freezegun import freeze_time
 
 from odoo.tests import SavepointCase
 
@@ -18,109 +19,121 @@ class Test(SavepointCase):
         cls.product = cls.env.ref("product.product_product_9")
 
     def _create_order(self, freight=None):
-        order = self.env["purchase.order"].create(
-            {
-                "partner_id": self.partner.id,
-                "order_line": [
-                    (
-                        0,
-                        0,
-                        {
-                            "name": self.product.name,
-                            "product_id": self.product.id,
-                            "product_uom_qty": 1,
-                            "product_uom": self.product.uom_id.id,
-                        },
-                    )
-                ],
-            }
-        )
-        return order
+        values = {
+            "partner_id": self.partner.id,
+            "order_line": [
+                (
+                    0,
+                    0,
+                    {
+                        "name": self.product.name,
+                        "product_id": self.product.id,
+                        "product_uom_qty": 1,
+                        "product_uom": self.product.uom_id.id,
+                    },
+                )
+            ],
+        }
+
+        if freight:
+            values.update(
+                {
+                    "freight_change_policy": "duration",
+                    "receive_date": "2022-10-07",
+                    "dispatch_date": "2022-10-07",
+                }
+            )
+
+        return self.env["purchase.order"].create(values)
 
     def test_no_freight(self):
         with freeze_time(TIME):
             order = self._create_order()
             # dispatch_date initialized to date planned
-            self.assertEqual(order.date_planned.date(), order.dispatch_date)
+            self.assertEqual(order.receive_date, order.dispatch_date)
 
     def test_plane_freight(self):
         with freeze_time(TIME):
-            order = self._create_order()
+            order = self._create_order(freight=True)
+
+            self.assertEqual(order.freight_duration, 0)
+
             plane_freight = ref(self, "plane1")
+            order.freight_change_policy = "dispatch"
             order.freight_rule_id = plane_freight.id
-            vals = {"freight_rule_id": order.freight_rule_id.id}
-            vals = order.play_onchanges(vals, ["freight_rule_id", "freight_duration", "dispatch_date", "date_planned"])
-            print(vals)
             self.assertEqual(order.freight_rule_id, plane_freight)
             self.assertEqual(order.freight_duration, order.freight_rule_id.duration)
             self.check_planned_dispatch_date_with_duration(order)
 
     def test_reset_freight(self):
         with freeze_time(TIME):
-            order = self._create_order()
+            order = self._create_order(freight=True)
+            order.freight_change_policy = "dispatch"
             # Original date
-            self.assertEqual(order.date_planned.strftime("%Y-%m-%d"), "2022-10-07")
+            self.assertEqual(order.receive_date.strftime("%Y-%m-%d"), "2022-10-07")
             order.freight_rule_id = ref(self, "plane1").id
             self.check_planned_dispatch_date_with_duration(order)
             order.freight_rule_id = False
-            self.assertEqual(order.date_planned.strftime("%Y-%m-%d"), "2022-10-07")
+            self.assertEqual(order.receive_date.strftime("%Y-%m-%d"), "2022-10-07")
 
     def test_update_duration_dispatch(self):
         with freeze_time(TIME):
-            order = self._create_order()
-            self.assertEqual(order.date_planned.strftime("%Y-%m-%d"), "2022-10-07")
+            order = self._create_order(freight=True)
+            self.assertEqual(order.receive_date.strftime("%Y-%m-%d"), "2022-10-07")
             self.assertEqual(order.dispatch_date.strftime("%Y-%m-%d"), "2022-10-07")
-            order.freight_rule_id = ref(self, "plane1").id
-            self.assertEqual(order.freight_duration, 2)
-            self.assertEqual(order.freight_duration_policy, "dispatch")
-            self.assertEqual(order.dispatch_date.strftime("%Y-%m-%d"), "2022-10-05")
-            self.check_planned_dispatch_date_with_duration(order)
+            order.freight_change_policy = "dispatch"
             order.freight_duration = 4
             self.assertEqual(order.freight_duration, 4)
+            self.assertEqual(order.dispatch_date.strftime("%Y-%m-%d"), "2022-10-03")
+            self.check_planned_dispatch_date_with_duration(order)
+            order.freight_rule_id = ref(self, "plane1").id
+            self.assertEqual(order.freight_duration, 2)
+            self.assertEqual(order.dispatch_date.strftime("%Y-%m-%d"), "2022-10-05")
             self.check_planned_dispatch_date_with_duration(order)
 
     def test_update_duration_received(self):
         with freeze_time(TIME):
-            order = self._create_order()
-            self.assertEqual(order.date_planned.strftime("%Y-%m-%d"), "2022-10-07")
+            order = self._create_order(freight=True)
+            self.assertEqual(order.receive_date.strftime("%Y-%m-%d"), "2022-10-07")
+            order.freight_change_policy = "receive"
             order.freight_rule_id = ref(self, "plane1").id
+
             self.assertEqual(order.freight_duration, 2)
-            order.freight_duration_policy = "received"
             self.assertEqual(order.dispatch_date.strftime("%Y-%m-%d"), "2022-10-07")
-            self.assertEqual(order.date_planned.strftime("%Y-%m-%d"), "2022-10-09")
+            self.assertEqual(order.receive_date.strftime("%Y-%m-%d"), "2022-10-09")
             self.check_planned_dispatch_date_with_duration(order)
             order.freight_duration = 4
             self.assertEqual(order.dispatch_date.strftime("%Y-%m-%d"), "2022-10-07")
-            self.assertEqual(order.date_planned.strftime("%Y-%m-%d"), "2022-10-11")
+            self.assertEqual(order.receive_date.strftime("%Y-%m-%d"), "2022-10-11")
             self.check_planned_dispatch_date_with_duration(order)
 
-    def test_update_date_planned(self):
+    def test_update_receive_date(self):
         with freeze_time(TIME):
-            order = self._create_order()
+            order = self._create_order(freight=True)
+            order.freight_change_policy = "dispatch"
             order.freight_rule_id = ref(self, "plane1").id
-            order.date_planned = "2022-10-30"
+            order.receive_date = "2022-10-30"
             self.check_planned_dispatch_date_with_duration(order)
-            order.date_planned = "2022-10-07"
+            order.receive_date = "2022-10-07"
             self.check_planned_dispatch_date_with_duration(order)
             # order.dispatch_date = "2022-10-28"
             # self.check_planned_dispatch_date_with_duration(order)
 
     def check_planned_dispatch_date_with_duration(self, order):
-        "We check that freight_duration separates date_planned and dispatch_date"
+        "We check that freight_duration separates receive_date and dispatch_date"
         self.assertEqual(
             order.dispatch_date,
-            order.date_planned.date() - timedelta(days=order.freight_duration),
+            order.receive_date - timedelta(days=order.freight_duration),
         )
 
-    # def test_picking(self):
-    #     with freeze_time(TIME):
-    #         order = self._create_order()
-    #         order.freight_rule_id = self.freight.id
-    #         order.freight_duration = 99
-    #         order.button_confirm()
-    #         picking = order.picking_ids[0]
-    #         self.assertEqual(picking.freight_rule_id, order.freight_rule_id)
-    #         self.assertEqual(picking.freight_duration, order.freight_duration)
+    def test_picking(self):
+        with freeze_time(TIME):
+            order = self._create_order(freight=True)
+            order.freight_change_policy = "dispatch"
+            order.freight_duration = 99
+            order.button_confirm()
+            picking = order.picking_ids[0]
+            self.assertEqual(picking.freight_duration, order.freight_duration)
 
 
 def ref(self, idstring):
