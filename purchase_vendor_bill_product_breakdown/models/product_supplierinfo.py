03dcba7b6541011d4ad1461ce9df88d5b137333d
geomer198 <geomer198@gmail.com>
2022-08-17 15:36:28 +0300

    [FIX] Model product.supplierinfo : rename bill_components field to partner_bill_components

--- purchase_vendor_bill_product_breakdown/models/product_supplierinfo.py
+++ purchase_vendor_bill_product_breakdown/models/product_supplierinfo.py
@@ -5,7 +5,9 @@ class SupplierInfo(models.Model):
     _inherit = "product.supplierinfo"
 
     product_bill_components = fields.Boolean(related="product_tmpl_id.bill_components")
-    bill_components = fields.Boolean(related="name.bill_components")
+    partner_bill_components = bill_components = fields.Boolean(
+        related="name.bill_components"
+    )
     component_ids = fields.One2many(
         comodel_name="product.supplierinfo.component", inverse_name="supplierinfo_id"
     )
@@ -16,10 +18,10 @@ class SupplierInfo(models.Model):
     )
     price = fields.Float(compute="_compute_price", store=True, readonly=True)
 
-    @api.depends("component_ids", "bill_components")
+    @api.depends("component_ids", "partner_bill_components")
     def _compute_price(self):
         for rec in self.filtered(
-            lambda item: len(item.component_ids) > 0 and item.bill_components
+            lambda item: item.component_ids and item.partner_bill_components
         ):
             rec.price = sum(rec.component_ids.mapped("price_total"))
 
@@ -29,7 +31,7 @@ class SupplierInfo(models.Model):
             rec.product_variant_ids = rec.product_tmpl_id.product_variant_ids
 
     def action_open_component_view(self):
-        """Open view with product components"""
+        """Open product components view"""
         self.ensure_one()
         view_id = self.env.ref(
             "purchase_vendor_bill_product_breakdown.product_supplierinfo_component_form_view"
