8a7f148497bf6300917962fee661615128de1fe8
Kev-Roche <kevin.roche@akretion.com>
2022-09-13 16:12:00 +0200

    [ADD][14.0] Module: purchase_order_line_note

---
+++ purchase_order_line_note/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2022 Akretion (https://www.akretion.com).
+# @author Kévin Roche <kevin.roche@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Purchase Order Line Note",
+    "summary": "Note on purchase order line",
+    "version": "14.0.1.0.0",
+    "category": "Purchase",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "maintainers": ["Kev-Roche"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+        "views/purchase_order_line.xml",
+    ],
+}
