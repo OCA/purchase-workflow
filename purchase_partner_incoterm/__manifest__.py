ec600a1e93a0074856d1c0477418c06f6618ae36
Denis Leemann <denis.leemann@camptocamp.com>
2022-09-26 14:52:49 +0200

    [MIG] purchase_partner_incoterm: Migration to 15.0

--- purchase_partner_incoterm/__manifest__.py
+++ purchase_partner_incoterm/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2020 Camptocamp SA
+# Copyright 2020-2022 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Default purchase incoterm per partner",
     "summary": "Add a an incoterm field for supplier and use it on purchase order",
-    "version": "14.0.3.0.0",
+    "version": "15.0.3.0.0",
     "category": "Purchase",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
