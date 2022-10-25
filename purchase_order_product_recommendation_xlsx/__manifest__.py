52cf60440686b4cf30ea8554fb37f995dfb59cc0
sergio-teruel <sergio.teruel@tecnativa.com>
2022-10-25 15:55:55 +0200

    [MIG] purchase_order_product_recommendation_xlsx: Migration to v15.0

--- purchase_order_product_recommendation_xlsx/__manifest__.py
+++ purchase_order_product_recommendation_xlsx/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Purchase Order Product Recommendation XLSX",
     "summary": "Add a way to print recommended products for supplier",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Purchases",
     "website": "https://github.com/OCA/purchase-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
