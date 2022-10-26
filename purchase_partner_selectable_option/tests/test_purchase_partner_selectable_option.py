8f2d49d4b17c1b0cced9a6cc093159ddfb40df48
Víctor Martínez <victor.martinez@tecnativa.com>
2022-10-26 08:46:41 +0200

    [FIX] Change SavepointCase to TransactionCase

--- purchase_partner_selectable_option/tests/test_purchase_partner_selectable_option.py
+++ purchase_partner_selectable_option/tests/test_purchase_partner_selectable_option.py
@@ -5,7 +5,7 @@ from lxml import etree
 from odoo.tests import common
 
 
-class TestPurchasePartnerSelectableOption(common.SavepointCase):
+class TestPurchasePartnerSelectableOption(common.TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
