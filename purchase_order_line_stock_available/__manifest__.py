3c84f414a610d2390ed388fd38f53a02f2e9f676
Víctor Martínez <victor.martinez@tecnativa.com>
2022-10-27 14:03:56 +0200

    [MIG] purchase_order_line_stock_available: Migration to 15.0
    TT36980
    
    [UPD] Update purchase_order_line_stock_available.pot
    
    [UPD] README.rst
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: purchase-workflow-15.0/purchase-workflow-15.0-purchase_order_line_stock_available
    Translate-URL: https://translation.odoo-community.org/projects/purchase-workflow-15-0/purchase-workflow-15-0-purchase_order_line_stock_available/
    
    [UPD] README.rst
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: purchase-workflow-15.0/purchase-workflow-15.0-purchase_order_line_stock_available
    Translate-URL: https://translation.odoo-community.org/projects/purchase-workflow-15-0/purchase-workflow-15-0-purchase_order_line_stock_available/

--- purchase_order_line_stock_available/__manifest__.py
+++ purchase_order_line_stock_available/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Purchase order line stock available",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "AvanzOSC," "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
