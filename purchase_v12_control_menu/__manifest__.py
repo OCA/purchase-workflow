56db48f48d852a46add102421ba5bd5b015a4746
Malva <malva.prior@tecnativa.com>
2022-11-25 12:21:52 +0100

    [MIG] purchase_v12_control_menu:Migration to 15.0

--- purchase_v12_control_menu/__manifest__.py
+++ purchase_v12_control_menu/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Purchase Control Menu from v12",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Purchase",
     "author": "Tecnativa, Odoo S.A., Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
