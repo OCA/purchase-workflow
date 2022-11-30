c8ed7fc517ee59230948fc62e75b3650c2961518
Stefan Rijnhart <stefan@opener.am>
2022-11-30 08:34:30 +0100

    [FIX] purchase_request: increase coverage and fix bugs
    * Fix allocation when splitting moves
    * Fix incorrect field path in stock.move's _compute_purchase_request_ids
    * Clarify "split origin" test

--- purchase_request/tests/test_purchase_request_procurement.py
+++ purchase_request/tests/test_purchase_request_procurement.py
@@ -1,7 +1,7 @@
 # Copyright 2018-2019 ForgeFlow, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
 
-from odoo import SUPERUSER_ID, fields
+from odoo import fields
 from odoo.tests import common
 
 
@@ -14,24 +14,15 @@ class TestPurchaseRequestProcurement(common.TransactionCase):
         self.prl_model = self.env["purchase.request.line"]
         self.product_uom_model = self.env["uom.uom"]
         self.location = self.env.ref("stock.stock_location_stock")
+        self.customer_location = self.env.ref("stock.stock_location_customers")
 
         # Get required Model data
-        self.uom_unit_categ = self.env.ref("uom.product_uom_categ_unit")
         self.product_1 = self.env.ref("product.product_product_16")
         self.product_1.purchase_request = True
-        self.product_2 = self.env.ref("product.product_product_13")
-        self.uom_unit = self.env.ref("uom.product_uom_unit")
-
-        # Create UoM
-        self.uom_ten = self.product_uom_model.create(
-            {
-                "name": "Ten",
-                "category_id": self.uom_unit_categ.id,
-                "factor_inv": 10,
-                "uom_type": "bigger",
-            }
+        self.route_buy = self.env.ref("purchase_stock.route_warehouse0_buy")
+        self.rule_buy = self.route_buy.rule_ids.filtered(
+            lambda rule: rule.location_dest_id == self.location
         )
-
         # Create Supplier
         self.supplier = self.env["res.partner"].create(
             {"name": "Supplier", "is_company": True, "company_id": False}
@@ -54,63 +45,127 @@ class TestPurchaseRequestProcurement(common.TransactionCase):
             }
         )
 
-    def procurement_group_run(self, name, origin, product, qty):
-        values = {
-            "date_planned": fields.Datetime.now(),
-            "warehouse_id": self.env.ref("stock.warehouse0"),
-            "route_ids": self.env.ref("purchase_stock.route_warehouse0_buy"),
-            "company_id": self.env.ref("base.main_company"),
-        }
-        procurements = []
-        procurements.append(
-            self.env["procurement.group"].Procurement(
-                product,
-                qty,
-                product.uom_id,
-                self.location,
-                name,
-                origin,
-                self.env.company,
-                values,
-            )
+    def _procurement_group_run(self, origin, product, qty):
+        """Create an outgoing move and procure it"""
+        move = self.env["stock.move"].create(
+            {
+                "reservation_date": fields.Datetime.now(),
+                "location_dest_id": self.customer_location.id,
+                "location_id": self.location.id,
+                "name": product.name,
+                "origin": origin,
+                "procure_method": "make_to_order",
+                "product_id": product.id,
+                "product_uom": product.uom_id.id,
+                "product_uom_qty": qty,
+                "route_ids": [(4, self.route_buy.id)],
+            }
         )
-        return self.env["procurement.group"].run(procurements)
+        move._action_confirm()
+        return move
+
+    def test_orderpoint(self):
+        """Purchase request quantity is reflected in the orderpoint forecasted qty"""
+        qty = 5
+        orderpoint = self.env["stock.warehouse.orderpoint"].create(
+            {
+                "name": __name__,
+                "warehouse_id": self.env.ref("stock.warehouse0").id,
+                "location_id": self.location.id,
+                "product_id": self.product_1.id,
+                "product_min_qty": 1,
+                "product_max_qty": qty,
+            }
+        )
+        self.env["procurement.group"].run_scheduler()
+        self.assertEqual(
+            self.env["purchase.request"]
+            .search([("product_id", "=", self.product_1.id)])
+            .line_ids.product_qty,
+            qty,
+        )
+        self.assertEqual(orderpoint.qty_forecast, qty)
 
     def test_procure_purchase_request(self):
-        has_route = self.procurement_group_run(
-            "Test Purchase Request Procurement",
+        """A request line is created from a procured move"""
+        move = self._procurement_group_run(
             "Test Purchase Request Procurement",
             self.product_1,
             10,
         )
-        self.assertTrue(has_route)
-        self.env["procurement.group"].run_scheduler()
-        pr = self.env["purchase.request"].search(
-            [("origin", "=", "Test Purchase Request Procurement")]
-        )
+        self.assertTrue(move.created_purchase_request_line_id)
+        pr = move.created_purchase_request_line_id.request_id
         self.assertTrue(pr.to_approve_allowed)
         self.assertEqual(pr.origin, "Test Purchase Request Procurement")
-        prl = self.env["purchase.request.line"].search([("request_id", "=", pr.id)])
-        self.assertEqual(prl.request_id, pr)
-        # Test split(", ")
-        vals = {
-            "picking_type_id": self.env.ref("stock.picking_type_in").id,
-            "requested_by": SUPERUSER_ID,
-            "origin": "Test Origin",
-            "line_ids": [
-                [
-                    0,
-                    0,
-                    {
-                        "product_id": self.env.ref("product.product_product_13").id,
-                        "product_uom_id": self.env.ref("uom.product_uom_unit").id,
-                        "product_qty": 2.0,
-                    },
-                ]
-            ],
-        }
-        self.pr_model.create(vals)
-        self.procurement_group_run(
-            "Test Test, Split", "Test, Split", self.product_1, 10
+
+        # Now cancel the move. An activity is created on the request.
+        # Even if the activity type was deleted
+        activity = self.env.ref("mail.mail_activity_data_todo")
+        self.env["mail.activity"].search(
+            [("activity_type_id", "=", activity.id)]
+        ).unlink()
+        activity.unlink()
+        self.assertFalse(move.created_purchase_request_line_id.request_id.activity_ids)
+        move._action_cancel()
+        self.assertTrue(move.created_purchase_request_line_id.request_id.activity_ids)
+
+    def test_procure_purchase_request_with_fixed_group(self):
+        """Existing requests are reused depending on group settings.
+
+        Having a procurement rule with fixed group settings, existing
+        purchase requests are only matched during procurement
+        if the groups correspond.
+        """
+        group = self.env["procurement.group"].create({})
+        self.rule_buy.write(
+            {
+                "group_id": group.id,
+                "group_propagation_option": "fixed",
+            }
         )
-        self.procurement_group_run("Test Test, Split", False, self.product_1, 10)
+        move = self._procurement_group_run(
+            False,
+            self.product_1,
+            10,
+        )
+        pr = move.created_purchase_request_line_id.request_id
+        self.assertEqual(pr.group_id, group)
+
+        # A second procurement reuses the same request
+        move2 = self._procurement_group_run(
+            "Test Origin",
+            self.product_1,
+            10,
+        )
+        pr2 = move2.created_purchase_request_line_id.request_id
+        self.assertEqual(pr2, pr)
+
+        # Reset the group on the first purchase request
+        pr.group_id = self.env["procurement.group"].create({})
+
+        # Because of the group difference, the request is not reused
+        move3 = self._procurement_group_run("Test with group", self.product_1, 10)
+        pr3 = move3.created_purchase_request_line_id.request_id
+        self.assertEqual(pr3.group_id, group)
+        self.assertNotEqual(pr3, pr)
+
+    def test_origin(self):
+        """The purchase request origin reflects the origins of each procurement"""
+        move = self._procurement_group_run("Test Origin", self.product_1, 10)
+        pr = move.created_purchase_request_line_id.request_id
+        self.assertEqual(pr.origin, "Test Origin")
+
+        # A new procurement origin is added to the request origin
+        move2 = self._procurement_group_run("Test, Split", self.product_1, 10)
+        self.assertEqual(move2.created_purchase_request_line_id.request_id, pr)
+        self.assertEqual(pr.origin, "Test Origin, Test, Split")
+
+        # An empty procurement origin is not added to the request origin
+        move3 = self._procurement_group_run(False, self.product_1, 10)
+        self.assertEqual(move3.created_purchase_request_line_id.request_id, pr)
+        self.assertEqual(pr.origin, "Test Origin, Test, Split")
+
+        # An existing procurement origin is not added to the request origin
+        move4 = self._procurement_group_run("Split", self.product_1, 10)
+        self.assertEqual(move4.created_purchase_request_line_id.request_id, pr)
+        self.assertEqual(pr.origin, "Test Origin, Test, Split")
