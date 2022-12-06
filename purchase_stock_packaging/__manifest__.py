56d937be4328e213d894577463df0f420a89c7d7
Denis Roussel <denis.roussel@acsone.eu>
2022-12-06 16:51:21 +0100

    [ADD] purchase_stock_packaging

---
+++ purchase_stock_packaging/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Purchase Stock Packaging",
+    "summary": """
+        Allows to transmit the product packaging from the procurement values
+        to the generated purchase order line""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "maintainers": ["rousseldenis"],
+    "author": "ACSONE SA/NV,ForgeFlow,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "depends": [
+        "purchase_stock",
+    ],
+}
---
+++ purchase_stock_packaging/readme/CONTRIBUTORS.rst
@@ -0,0 +1,2 @@
+* Lois Rilo Antelo <lois.rilo@forgeflow.com>
+* Denis Roussel <denis.roussel@acsone.eu>
