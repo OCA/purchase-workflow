a7547243ba8bdddcce1475ec036ca8e071b0c25c
Alexis de Lattre <alexis.delattre@akretion.com>
2022-12-14 17:07:34 +0100

    [MIG] purchase_force_invoiced to v16
    Set force_invoiced field invisible on RFQs (state not in purchase or done)
    force_invoiced is now tracked in chatter

--- purchase_force_invoiced/tests/test_purchase_force_invoiced.py
+++ purchase_force_invoiced/tests/test_purchase_force_invoiced.py
@@ -3,9 +3,11 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
 from odoo import fields
+from odoo.tests import tagged
 from odoo.tests.common import TransactionCase
 
 
+@tagged("post_install", "-at_install")
 class TestPurchaseForceInvoiced(TransactionCase):
     def setUp(self):
         super(TestPurchaseForceInvoiced, self).setUp()
@@ -15,11 +17,7 @@ class TestPurchaseForceInvoiced(TransactionCase):
         self.account_invoice_line = self.env["account.move.line"]
         self.invoice_account = self.env["account.account"].search(
             [
-                (
-                    "user_type_id",
-                    "=",
-                    self.env.ref("account.data_account_type_revenue").id,
-                ),
+                ("account_type", "=", "expense"),
                 ("company_id", "=", self.env.company.id),
             ],
             limit=1,
@@ -51,32 +49,6 @@ class TestPurchaseForceInvoiced(TransactionCase):
             }
         )
         return product
-
-    def _create_invoice_from_purchase(self, purchase):
-        invoice = self.account_invoice_model.create(
-            {"partner_id": purchase.partner_id.id, "move_type": "in_invoice"}
-        )
-        invoice.write({"purchase_id": purchase.id})
-        invoice._onchange_purchase_auto_complete()
-
-        return invoice
-
-    def create_invoice_line(self, line, invoice):
-        vals = [
-            (
-                0,
-                0,
-                {
-                    "name": line.name,
-                    "product_id": line.product_id.id,
-                    "quantity": line.qty_received - line.qty_invoiced,
-                    "price_unit": line.price_unit,
-                    "account_id": self.invoice_account.id,
-                    "purchase_line_id": line.id,
-                },
-            )
-        ]
-        return invoice.update({"invoice_line_ids": vals})
 
     def test_purchase_order(self):
         po = self.purchase_order_model.create({"partner_id": self.customer.id})
@@ -113,13 +85,11 @@ class TestPurchaseForceInvoiced(TransactionCase):
             po.invoice_status, "to invoice", "The invoice status should be To Invoice"
         )
 
-        invoice = self._create_invoice_from_purchase(po)
-        self.create_invoice_line(pol1, invoice)
-        self.create_invoice_line(pol2, invoice)
+        action = po.action_create_invoice()
+        invoice = self.env["account.move"].browse(action["res_id"])
         self.assertEqual(
             po.invoice_status, "invoiced", "The invoice status should be Invoiced"
         )
-
         # Reduce the invoiced qty
         for line in pol2.invoice_lines:
             line.with_context(check_move_validity=False).unlink()
@@ -132,14 +102,16 @@ class TestPurchaseForceInvoiced(TransactionCase):
         self.assertEqual(
             po.invoice_status, "invoiced", "The invoice status should be Invoiced"
         )
-        invoice = self._create_invoice_from_purchase(po)
-        invoice_qty = sum(invoice.mapped("invoice_line_ids.quantity"))
-        self.assertEqual(invoice_qty, 0.0)
         # We remove the force invoiced.
         po.force_invoiced = False
         self.assertEqual(
             po.invoice_status, "to invoice", "The invoice status should be To Invoice"
         )
-        self.create_invoice_line(pol2, invoice)
-        invoice_qty = sum(invoice.mapped("invoice_line_ids.quantity"))
+        action = po.action_create_invoice()
+        invoice = self.env["account.move"].browse(action["res_id"])
+        invoice_qty = sum(
+            invoice.invoice_line_ids.filtered(
+                lambda x: x.product_id.id == self.service_2.id
+            ).mapped("quantity")
+        )
         self.assertEqual(invoice_qty, 2.0)
