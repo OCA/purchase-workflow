cf4d506d2a6a61b90a44c39f8a78d3920988c742
augusto-weiss <awe@adhoc.com.ar>
2023-01-12 08:38:49 -0300

    [MIG] purchase_all_shipments: Migration to 16.0

--- purchase_all_shipments/__manifest__.py
+++ purchase_all_shipments/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     "name": "Purchase All Shipments",
-    "version": "13.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Purchases",
