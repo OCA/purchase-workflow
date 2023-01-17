cf01f260176e8de56d6f86d1f60504cc943419ec
Ruchir Shukla <ruchir@bizzappdev.com>
2023-01-17 17:51:56 +0530

    [MIG][15.0]purchase_minimum_amount:Migration to 15.0

--- purchase_minimum_amount/tests/test_po_minimum_amount_block.py
+++ purchase_minimum_amount/tests/test_po_minimum_amount_block.py
@@ -40,7 +40,7 @@ class TestPoAmountBlock(TransactionCase):
     def _create_user(self, login, groups, company):
         """Create a user."""
         group_ids = [group.id for group in groups]
-        user = self.users_obj.with_context({"no_reset_password": True}).create(
+        user = self.users_obj.with_context(no_reset_password=True).create(
             {
                 "name": "Purchase User",
                 "login": login,
