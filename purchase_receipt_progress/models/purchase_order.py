03d747b656bbc738afbb2c156fbc69c99a2e0a46
ilo <dev@nomadiplus.com>
2023-01-25 09:15:20 -0300

    Removing deprecated code and fixing compute method

--- purchase_receipt_progress/models/purchase_order.py
+++ purchase_receipt_progress/models/purchase_order.py
@@ -1,73 +1,89 @@
 # Copyright 2023 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models, api
+from odoo import api, fields, models
 
 
 class PurchaseOrderReceivedByCategory(models.Model):
     _name = "purchase.order.received.by.category"
     _description = "Purchase Order Received by Category"
 
-    order_id = fields.Many2one('purchase.order', required=True, ondelete='cascade')
-    category_id = fields.Many2one('product.category', required=True)
-    product_qty = fields.Float(string='Received Quantity')
-    qty_received = fields.Float(string='To Receive Quantity')
-    to_receive = fields.Float(string='Received Percentage')
+    order_id = fields.Many2one("purchase.order")
+    category_id = fields.Many2one("product.category")
+    product_qty = fields.Float()
+    qty_received = fields.Float()
+    to_receive = fields.Float()
 
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    purchased_quantity_total = fields.Float(string='Total Quantity', compute='_compute_purchased_quantity_total')
-    delivery_status = fields.Float(compute='_compute_delivery_status')
-    received_by_category_ids = fields.One2many('purchase.order.received.by.category',
-                                               'order_id', string='Received by Category',
-                                               compute='_compute_received_by_category_ids')
+    purchased_quantity_total = fields.Float(
+        string="Total Quantity", compute="_compute_purchased_quantity_total"
+    )
+    delivery_status = fields.Float(compute="_compute_delivery_status")
+    received_by_category_ids = fields.One2many(
+        "purchase.order.received.by.category",
+        "order_id",
+        string="Received by Category",
+        compute="_compute_received_by_category_ids",
+    )
 
-    @api.depends('order_line.product_qty')
+    @api.depends("order_line.product_qty")
     def _compute_purchased_quantity_total(self):
         for order in self:
-            order.purchased_quantity_total = sum(order.mapped('order_line.product_qty'))
+            order.purchased_quantity_total = sum(order.mapped("order_line.product_qty"))
 
-    @api.depends('order_line.product_qty', 'order_line.qty_received')
+    @api.depends("order_line.product_qty", "order_line.qty_received")
     def _compute_delivery_status(self):
         for order in self:
-            if order.purchased_quantity_total:
-                order.delivery_status = sum(
-                    order.mapped('order_line.qty_received')) / order.purchased_quantity_total * 100
+            received_quantity = sum(order.mapped("order_line.qty_received"))
+            if order.purchased_quantity_total and received_quantity:
+                order.delivery_status = (
+                    received_quantity / order.purchased_quantity_total
+                )
             else:
                 order.delivery_status = 0
 
-    @api.depends('order_line.product_qty', 'order_line.qty_received')
+    @api.depends("order_line.product_qty", "order_line.qty_received")
     def _compute_received_by_category_ids(self):
         for order in self:
-            received_by_category_obj = self.env['purchase.order.received.by.category']
-            order.received_by_category_ids.unlink()
+            purchase_order_received_by_category_obj = self.env[
+                "purchase.order.received.by.category"
+            ]
+            order_id = (
+                order._origin.id if isinstance(order.id, models.NewId) else order.id
+            )
+            purchase_order_received_by_category_obj.search(
+                [("order_id", "=", order_id)]
+            ).unlink()
+            order.received_by_category_ids = False
             received_by_category_ids = []
-            for category in order.mapped('order_line.product_id.categ_id'):
-                lines = order.order_line.filtered(lambda l: l.product_id.categ_id == category)
+            for category in order.mapped("order_line.product_id.categ_id"):
+                lines = order.order_line.filtered(
+                    lambda l: l.product_id.categ_id == category
+                )
                 if lines:
-                    product_qty = sum(lines.mapped('product_qty'))
-                    qty_received = sum(lines.mapped('qty_received'))
-                    to_receive = sum(lines.mapped('to_receive'))
                     values = {
-                        'order_id': order.id,
-                        'category_id': category.id,
-                        'product_qty': product_qty,
-                        'qty_received': qty_received,
-                        'to_receive': to_receive,
+                        "order_id": order_id,
+                        "category_id": category.id,
+                        "product_qty": sum(lines.mapped("product_qty")),
+                        "qty_received": sum(lines.mapped("qty_received")),
+                        "to_receive": sum(lines.mapped("to_receive")),
                     }
-                    received_by_category_ids.append(received_by_category_obj.create(values).id)
+                    received_by_category_id = (
+                        purchase_order_received_by_category_obj.create(values)
+                    )
+                    received_by_category_ids.append((4, received_by_category_id.id))
             order.received_by_category_ids = received_by_category_ids
-
 
 
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
-    to_receive = fields.Float(compute='_compute_to_receive')
+    to_receive = fields.Float(compute="_compute_to_receive")
 
-    @api.depends('product_qty', 'qty_received')
+    @api.depends("product_qty", "qty_received")
     def _compute_to_receive(self):
         for line in self:
             line.to_receive = line.product_qty - line.qty_received
