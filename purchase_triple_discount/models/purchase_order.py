20aa268c5d8322bb821e5bb80afd19658ff4ce04
Aritz Olea <ao@landoo.es>
2023-01-27 09:48:37 +0100

    [IMP] purchase_triple_discount: when purchase line created from seller, sets discount2 and discount3
    Co-authored-by: ae-landoo <ae@landoo.es>
    Co-authored-by: legalsylvain <sylvain.legal@grap.coop>

--- purchase_triple_discount/models/purchase_order.py
+++ purchase_triple_discount/models/purchase_order.py
@@ -60,3 +60,15 @@ class PurchaseOrderLine(models.Model):
             }
         )
         return res
+
+    @api.model
+    def _prepare_purchase_order_line_from_seller(self, seller):
+        res = super()._prepare_purchase_order_line_from_seller(seller)
+        if not res:
+            return res
+        res.update(
+            {
+                "discount2": seller.discount2,
+                "discount3": seller.discount3,
+            }
+        )
