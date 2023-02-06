9b2f889a8d2ecfd35437bd0ff005789961f3a3e2
Saran440 <saranl@ecosoft.co.th>
2023-02-06 13:47:18 +0700

    [15.0][MIG] purchase_request_cancel_confirm

--- purchase_request_cancel_confirm/__manifest__.py
+++ purchase_request_cancel_confirm/__manifest__.py
@@ -3,8 +3,8 @@
 
 {
     "name": "Purchase Request Cancel Confirm",
-    "version": "14.0.1.0.1",
-    "author": "Ecosoft,Odoo Community Association (OCA)",
+    "version": "15.0.1.0.0",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
     "category": "Usability",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/purchase-workflow",
