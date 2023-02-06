067f8df4760a06262116cbe7598b1ead1824a54e
Saran440 <saranl@ecosoft.co.th>
2023-02-06 14:34:28 +0700

    [15.0][MIG] purchase_work_acceptance_tier_validation

--- purchase_work_acceptance_tier_validation/__manifest__.py
+++ purchase_work_acceptance_tier_validation/__manifest__.py
@@ -1,10 +1,11 @@
 # Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
     "name": "Purchase Work Acceptance Tier Validation",
     "summary": "Extends the functionality of Work Acceptance to "
     "support a tier validation process.",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Purchases",
     "website": "https://github.com/OCA/purchase-workflow",
     "author": "Ecosoft, Odoo Community Association (OCA)",
