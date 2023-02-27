760c08ea7c22ab84e9f07530e720b9794efa42fa
Ruchir Shukla <ruchir@bizzappdev.com>
2023-02-27 16:14:41 +0530

    [MIG][15.0]purchase_order_shipping_date: Migration to 15.0

--- purchase_order_shipping_date/__manifest__.py
+++ purchase_order_shipping_date/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Purchase Order Shipping Date",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "ooops404, Ashish Hirpara, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Purchase",
--- purchase_order_shipping_date/models/purchase_order.py
+++ purchase_order_shipping_date/models/purchase_order.py
@@ -5,5 +5,5 @@ class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
     shipping_date = fields.Datetime(
-        "Shipping Date", help="Shipping date promised by vendor.", copy=False
+        help="Shipping date promised by vendor.", copy=False
     )
