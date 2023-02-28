552bfd11aef335183752a519762ecfc9e14a815c
rolandojduartem <rolando@vauxoo.com>
2023-02-28 17:42:24 +0000

    [MIG] purchase_order_approved: Migration to 16.0

--- purchase_order_approved/__manifest__.py
+++ purchase_order_approved/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Purchase Order Approved",
     "summary": "Add a new state 'Approved' in purchase orders.",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Purchases",
     "website": "https://github.com/OCA/purchase-workflow",
     "author": "ForgeFlow, ACSONE SA/NV, Odoo Community Association (OCA)",
--- purchase_order_approved/readme/CONTRIBUTORS.rst
+++ purchase_order_approved/readme/CONTRIBUTORS.rst
@@ -2,3 +2,4 @@
 * Rattapong Chokmasermkul <rattapongc@ecosoft.co.th>
 * Laurent Mignon <laurent.mignon@acsone.eu>
 * Moaad Bourhim <moaad.bourhim@gmail.com>
+* Rolando Duarte <rolando@vauxoo.com>
