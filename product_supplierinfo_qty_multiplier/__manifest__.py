7e30d71bf6bca8a312366369e3fa7cb6ed47cfd3
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-03-03 12:11:53 +0100

    [MIG] Backport product_supplierinfo_qty_multiplier from 13.0 to 12.0

--- product_supplierinfo_qty_multiplier/__manifest__.py
+++ product_supplierinfo_qty_multiplier/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Product supplierinfo qty multiplier",
-    "version": "13.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Purchases",
     "website": "https://github.com/OCA/purchase-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
