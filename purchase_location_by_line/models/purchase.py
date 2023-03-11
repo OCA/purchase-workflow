6b9501292f74299661a2f75fc6703e029f1efda9
Cemal Bayram <hcbayram@gmail.com>
2023-03-11 13:19:27 +0300

    Picking type is set with null check

--- purchase_location_by_line/models/purchase.py
+++ purchase_location_by_line/models/purchase.py
@@ -24,6 +24,8 @@ class PurchaseOrderLine(models.Model):
         for key_element in key:
             if "location_dest_id" in key_element.keys():
                 vals["location_dest_id"] = key_element["location_dest_id"].id
+            if "picking_type_id" in key_element.keys():
+                vals["picking_type_id"] = key_element["picking_type_id"].id
         return vals
 
     @api.model
@@ -41,7 +43,8 @@ class PurchaseOrderLine(models.Model):
             default_picking_location_id
         )
         location = line.location_dest_id or default_picking_location
-        return key + ({"location_dest_id": location},)
+        picking_type = self.env['stock.picking.type'].search([('code', '=', 'incoming'), ('default_location_dest_id', '=', line.location_dest_id.id)]) or line.order_id.picking_type_id
+        return key + ({"location_dest_id": location, "picking_type_id": picking_type},)
 
     def _create_stock_moves(self, picking):
         res = super(PurchaseOrderLine, self)._create_stock_moves(picking)
@@ -55,4 +58,9 @@ class PurchaseOrderLine(models.Model):
                 line.move_ids.filtered(lambda m: m.state != "done").write(
                     {"location_dest_id": location.id}
                 )
+                picking_type = self.env['stock.picking.type'].search([('code', '=', 'incoming'), ('default_location_dest_id', '=', location.id)]) or line.order_id.picking_type_id
+                if picking_type:
+                    line.move_ids.filtered(lambda m: m.state != "done").write(
+                        {"picking_type_id": picking_type}
+                    )
         return res
