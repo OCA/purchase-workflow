b1c76d5f23261f8704befb37ba5a4dfadf3f88b1
dsolanki <dhara.solanki@initos.com>
2023-04-04 17:26:25 +0530

    [IMP] purchase_partner_incoterm: pre-commit stuff

--- purchase_partner_incoterm/__manifest__.py
+++ purchase_partner_incoterm/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Default purchase incoterm per partner",
     "summary": "Add a an incoterm field for supplier and use it on purchase order",
-    "version": "15.0.3.0.0",
+    "version": "16.0.1.0.0",
     "category": "Purchase",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
