5dd383edbc4d02330a2392dd046f855943cc088d
Urvisha-OSI <udesai@opensourceintegrators.com>
2023-04-04 12:34:54 +0530

    [IMP] purchase_reception_status: pre-commit stuff

--- purchase_reception_status/__manifest__.py
+++ purchase_reception_status/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Purchase Reception Status",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Purchases",
     "license": "AGPL-3",
     "summary": "Add reception status on purchase orders",
