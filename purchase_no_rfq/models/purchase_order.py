b60e2b65a4e1e449b9f4ce3dc2496f6772f24ace
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-04-19 16:33:32 +0200

    [MIG] purchase_no_rfq to v16.0 (from 12.0)

--- purchase_no_rfq/models/purchase_order.py
+++ purchase_no_rfq/models/purchase_order.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class PurchaseOrder(models.Model):
@@ -11,8 +11,8 @@ class PurchaseOrder(models.Model):
 
     state = fields.Selection(selection_add=[("draft", "Draft"), ("sent", "Sent")])
 
-    @api.multi
     def print_quotation(self):
-        self.write({"state": "sent"})
+        orders = self.filtered(lambda x: x.state == "draft")
+        orders.write({"state": "sent"})
         report = self.env.ref("purchase.action_report_purchase_order")
         return report.report_action(self)
