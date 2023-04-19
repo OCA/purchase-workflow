708ecce5086da062bd5f9c6632332ad37f11ef2d
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-04-19 10:28:37 +0200

    [IMP] purchase_no_rfq: black, isort, prettier

--- purchase_no_rfq/hooks.py
+++ purchase_no_rfq/hooks.py
@@ -2,14 +2,16 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, SUPERUSER_ID
+from odoo import SUPERUSER_ID, api
 
 
 def uninstall_hook(cr, registry):
     with api.Environment.manage():
         # Unhide menu item for request for quotation and restore sequence
         env = api.Environment(cr, SUPERUSER_ID, {})
-        env.ref("purchase.menu_purchase_rfq").write({
-            "groups_id": [(5,)],
-            "sequence": 0,
-        })
+        env.ref("purchase.menu_purchase_rfq").write(
+            {
+                "groups_id": [(5,)],
+                "sequence": 0,
+            }
+        )
