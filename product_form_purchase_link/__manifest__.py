14417e2c0ba650c97e92bd87a7993b2b4e867d48
aliciagarzo <alicia.garzo@factorlibre.com>
2023-06-05 17:26:13 +0200

    [MIG] product_form_purchase_link: Migration to 16.0

--- product_form_purchase_link/__manifest__.py
+++ product_form_purchase_link/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Product Form Purchase Link",
     "summary": """
         Add an option to display the purchases lines from product""",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "development_status": "Beta",
     "maintainers": ["rousseldenis"],
