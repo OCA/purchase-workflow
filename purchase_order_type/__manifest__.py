542d08794c20937288713d0ee53c2096abed2360
Christian Santamaria <christian.santamaria@solvos.es>
2023-06-06 11:08:16 +0200

    [MIG] purchase_order_type: Migration to 16.0

--- purchase_order_type/__manifest__.py
+++ purchase_order_type/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Purchase Order Type",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Purchase Management",
