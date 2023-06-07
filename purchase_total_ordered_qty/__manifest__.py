eceaf2c2f653a5e7ad6fe04e59b7db4569e12010
Alex Guardado <alex.guardado@factorlibre.com>
2023-06-07 10:17:55 +0200

    [MIG] purchase_total_ordered_qty: Migration to 16.0

--- purchase_total_ordered_qty/__manifest__.py
+++ purchase_total_ordered_qty/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Purchase - Total Ordered Qty",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Purchase Management",
     "author": "Camptocamp, Odoo Community Association (OCA), Italo Lopes",
     "website": "https://github.com/OCA/purchase-workflow",
