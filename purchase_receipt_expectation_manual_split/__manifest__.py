4fb15f710145691b7a9a74aad3f7da054bf1fe07
JanQubiQ <jan.tugores@qubiq.es>
2023-06-07 16:50:20 +0200

    [MIG] purchase_receipt_expectation_manual_split: update version module

--- purchase_receipt_expectation_manual_split/__manifest__.py
+++ purchase_receipt_expectation_manual_split/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2021 Camptocamp SA
+# Copyright 2023 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Purchase Receipt Expectation - Manual w/ Split",
-    "version": "15.0.2.0.0",
+    "version": "16.0.1.0.0",
     "category": "Purchase Management",
     "author": "Camptocamp SA, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
