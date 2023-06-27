5b2484bf47c1f9d60af3e5916f1cccece461da0b
OriolMForgeFlow <oriol.miranda@forgeflow.com>
2023-06-27 08:40:28 +0200

    [IMP] purchase_order_line_sequence: use correct sequence in moves when section or notes in purchase order

--- purchase_order_line_sequence/tests/test_po_lines_sequence.py
+++ purchase_order_line_sequence/tests/test_po_lines_sequence.py
@@ -134,6 +134,91 @@ class TestPurchaseOrder(common.TransactionCase):
             "The Sequence is not copied properly",
         )
 
+    def test_purchase_order_line_sequence_with_section_note(self):
+        """
+            Verify that the sequence is correctly assigned to the move associated
+            with the purchase order line it references.
+        """
+        po = self._create_purchase_order()
+        self.PurchaseOrderLine.create(
+            {
+                "name": "Section 1",
+                "display_type": "line_section",
+                "order_id": po.id,
+                "product_qty": 0,
+            }
+        )
+        self.PurchaseOrderLine.create(
+            {
+                "name": self.product_id_1.name,
+                "product_id": self.product_id_1.id,
+                "product_qty": 15.0,
+                "product_uom": self.product_id_1.uom_po_id.id,
+                "price_unit": 150.0,
+                "date_planned": datetime.today().strftime(
+                    DEFAULT_SERVER_DATETIME_FORMAT
+                ),
+                "order_id": po.id,
+            }
+        )
+        self.PurchaseOrderLine.create(
+            {
+                "name": "Note 1",
+                "display_type": "line_note",
+                "order_id": po.id,
+                "product_qty": 0,
+            }
+        )
+        self.PurchaseOrderLine.create(
+            {
+                "name": self.product_id_2.name,
+                "product_id": self.product_id_2.id,
+                "product_qty": 1.0,
+                "product_uom": self.product_id_2.uom_po_id.id,
+                "price_unit": 50.0,
+                "date_planned": datetime.today().strftime(
+                    DEFAULT_SERVER_DATETIME_FORMAT
+                ),
+                "order_id": po.id,
+            }
+        )
+        po.button_confirm()
+
+        moves = po.picking_ids[0].move_ids_without_package
+        self.assertNotEquals(len(po.order_line), len(moves))
+
+        for move in moves:
+            self.assertEqual(move.sequence, move.purchase_line_id.sequence)
+
+    def test_write_purchase_order_line(self):
+        po = self._create_purchase_order()
+        po.button_confirm()
+
+        po.write(
+            {
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": self.product_id_2.name,
+                            "product_id": self.product_id_2.id,
+                            "product_qty": 2,
+                            "product_uom": self.product_id_2.uom_id.id,
+                            "price_unit": 30,
+                            "date_planned": datetime.today().strftime(
+                                DEFAULT_SERVER_DATETIME_FORMAT
+                            ),
+                        },
+                    )
+                ]
+            }
+        )
+
+        moves = po.picking_ids[0].move_ids_without_package
+        for move in moves:
+            self.assertEqual(move.sequence, move.purchase_line_id.sequence)
+
     def test_invoice_sequence(self):
 
         po = self._create_purchase_order()
