dd3da0595eb6791e87534e27f8c039c8489f76e7
OriolMForgeFlow <oriol.miranda@forgeflow.com>
2023-06-30 13:36:19 +0200

    [IMP] purchase_order_line_sequence: module refactor, new sequence fields for orders and invoices

--- purchase_order_line_sequence/models/purchase.py
+++ purchase_order_line_sequence/models/purchase.py
@@ -1,5 +1,5 @@
 # Copyright 2017 Camptocamp SA - Damien Crier, Alexandre Fayolle
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 ForgeFlow S.L.
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -38,66 +38,20 @@ class PurchaseOrder(models.Model):
                     for ptype in order.order_line.mapped("product_id.type")
                 ]
             ):
-                pickings = order.picking_ids.filtered(
-                    lambda x: x.state not in ("done", "cancel")
-                )
-                if pickings:
-                    picking = pickings[0]
-                    order_lines = order.order_line.filtered(
-                        lambda l: l.product_id.type in ["product", "consu"]
-                    )
-                    for move, line in zip(
-                        sorted(picking.move_lines, key=lambda m: m.id), order_lines
-                    ):
-                        move.write({"sequence": line.sequence})
+                for picking in order.picking_ids:
+                    for move in picking.move_lines:
+                        if not move.purchase_line_id:
+                            continue
+                        move.write({"sequence": move.purchase_line_id.visible_sequence})
 
-    def _reset_sequence(self):
-        for rec in self:
-            current_sequence = 1
-            for line in rec.order_line:
-                line.sequence = current_sequence
-                current_sequence += 1
+    @api.model
+    def create(self, vals):
+        res = super(PurchaseOrder, self).create(vals)
+        self._update_moves_sequence()
+        return res
 
     def write(self, line_values):
         res = super(PurchaseOrder, self).write(line_values)
-        self._reset_sequence()
         if "order_line" in line_values:
             self._update_moves_sequence()
         return res
-
-    def copy(self, default=None):
-        return super(PurchaseOrder, self.with_context(keep_line_sequence=True)).copy(
-            default
-        )
-
-
-class PurchaseOrderLine(models.Model):
-    _inherit = "purchase.order.line"
-    _order = "sequence, id"
-
-    sequence = fields.Integer(
-        "Hidden Sequence",
-        help="Gives the sequence of the line when " "displaying the purchase order.",
-        default=9999,
-    )
-
-    sequence2 = fields.Integer(
-        "Sequence",
-        help="Displays the sequence of the line in " "the purchase order.",
-        related="sequence",
-        readonly=True,
-    )
-
-    def _prepare_stock_moves(self, picking):
-        res = super(PurchaseOrderLine, self)._prepare_stock_moves(picking)
-        for move, line in zip(res, self):
-            move.update(sequence=line.sequence)
-        return res
-
-    @api.model
-    def create(self, values):
-        line = super(PurchaseOrderLine, self).create(values)
-        # We do not reset the sequence when copying an entire purchase order
-        if not self.env.context.get("keep_line_sequence"):
-            line.order_id._reset_sequence()
-        return line
