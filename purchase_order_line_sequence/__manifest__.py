e55e504008bdc567ec2aacd101af6067ead5946d
OriolMForgeFlow <oriol.miranda@forgeflow.com>
2023-06-30 13:36:19 +0200

    [IMP] purchase_order_line_sequence: module refactor, new sequence fields for orders and invoices

--- purchase_order_line_sequence/__manifest__.py
+++ purchase_order_line_sequence/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2017 Camptocamp SA - Damien Crier, Alexandre Fayolle
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 ForgeFlow S.L.
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -10,7 +10,7 @@
     "version": "14.0.1.0.0",
     "category": "Purchase Management",
     "author": "Camptocamp, "
-    "Eficent, "
+    "ForgeFlow, "
     "Serpent CS, "
     "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
@@ -22,9 +22,10 @@
         "views/purchase_view.xml",
         "views/report_purchaseorder.xml",
         "views/report_purchasequotation.xml",
+        "views/account_move_view.xml",
+        "views/report_invoice.xml",
     ],
-    "post_init_hook": "post_init_hook",
-    "installable": False,
+    "installable": True,
     "auto_install": False,
     "license": "AGPL-3",
 }
--- purchase_order_line_sequence/models/purchase_line.py
+++ purchase_order_line_sequence/models/purchase_line.py
@@ -1,17 +1,33 @@
 # Copyright 2017 Camptocamp SA - Damien Crier, Alexandre Fayolle
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 ForgeFlow S.L.
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models
+from odoo import api, fields, models
 
 
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
+    _order = "sequence, id"
 
-    def _prepare_account_move_line(self, move=False):
-        self.ensure_one()
-        res = super(PurchaseOrderLine, self)._prepare_account_move_line(move)
-        res["sequence"] = self.sequence
+    sequence = fields.Integer(
+        "Hidden Sequence",
+        help="Gives the sequence of the line when " "displaying the purchase order.",
+        default=9999,
+    )
 
-        return res
+    visible_sequence = fields.Integer(
+        "Line Number",
+        help="Displays the sequence of the line in the purchase order.",
+        compute="_compute_visible_sequence",
+        store=True,
+    )
+
+    @api.depends("sequence", "order_id.order_line")
+    def _compute_visible_sequence(self):
+        for po in self.mapped("order_id"):
+            sequence = 1
+            order_lines = po.order_line.filtered(lambda l: not l.display_type)
+            for line in sorted(order_lines, key=lambda l: l.sequence):
+                line.visible_sequence = sequence
+                sequence += 1
