0d7f0659b33b0e7be53cd0689904e1787a944ce6
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-07-23 12:26:16 +0200

    [IMP] purchase_security: Computed and sortable team
    - Fine-tune security rules.
    - Put sequence on teams.
    - Edit teams on tree.
    - Convert purchase order team in computed writable.
    - Use new_test_user in tests + useless lines for shorten code.
    - Extra tests

--- purchase_security/models/purchase_order.py
+++ purchase_security/models/purchase_order.py
@@ -1,7 +1,8 @@
 # Copyright 2023 Tecnativa - Stefan Ungureanu
+# Copyright 2023 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PurchaseOrder(models.Model):
@@ -12,4 +13,23 @@ class PurchaseOrder(models.Model):
         string="Team",
         index=True,
         auto_join=True,
+        compute="_compute_team_id",
+        store=True,
+        readonly=False,
     )
+
+    @api.depends("user_id")
+    def _compute_team_id(self):
+        """When a user is assigned, the first team which the user belongs to is
+        assigned, and if no one, the first purchase team.
+        """
+        first_team = self.env["purchase.team"].search([], limit=1)
+        for record in self:
+            if record.user_id:
+                team = self.env["purchase.team"].search(
+                    [("user_ids", "=", record.user_id.id)], limit=1
+                )
+                if team:
+                    record.team_id = team.id
+                    continue
+            record.team_id = first_team.id
