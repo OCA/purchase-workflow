4ee1d3ce0b897f437e5d8aa5f0994409a423293f
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-07-23 12:26:16 +0200

    [IMP] purchase_security: Computed and sortable team
    - Fine-tune security rules.
    - Put sequence on teams.
    - Edit teams on tree.
    - Convert purchase order team in computed writable.
    - Use new_test_user in tests + useless lines for shorten code.
    - Extra tests
    
    [FIX] purchase_security: Fine-tune security rules
    
    - PO lines domain was not adjusted
    - Team rule doesn't need the second operand

--- purchase_security/models/purchase_team.py
+++ purchase_security/models/purchase_team.py
@@ -1,4 +1,5 @@
 # Copyright 2023 Tecnativa - Stefan Ungureanu
+# Copyright 2023 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
@@ -7,6 +8,8 @@ from odoo import fields, models
 class PurchaseTeam(models.Model):
     _name = "purchase.team"
     _description = "Purchase Team"
+    _order = "sequence,id"
 
-    name = fields.Char()
+    name = fields.Char(required=True)
+    sequence = fields.Integer(default=10)
     user_ids = fields.Many2many("res.users", string="Purchase Users")
