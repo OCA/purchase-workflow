eea37128630dfd0d3ea11a25feebdd15cb41c318
Emilio Pascual <emilio@moduon.team>
2023-08-14 08:46:44 +0200

    [16.0][IMP] purchase_order_line_menu: New views and stored fields
    @moduon MT-2028

--- purchase_order_line_menu/models/__init__.py
+++ purchase_order_line_menu/models/__init__.py
@@ -1,4 +1,3 @@
-# Copyright (C) 2021 Open Source Integrators
 # Copyright (C) 2023 Moduon Team
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from . import models
+from . import purchase_order_line
