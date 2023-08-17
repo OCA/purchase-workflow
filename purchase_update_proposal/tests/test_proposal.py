9c1cbec294bd9b9e502b117d71e60e7c4f4d1f34
Francois Poizat <francois.poizat@gmail.com>
2023-08-17 15:42:14 +0200

    FIX update test to self.assert*

--- purchase_update_proposal/tests/test_proposal.py
+++ purchase_update_proposal/tests/test_proposal.py
@@ -18,31 +18,31 @@ class Test(common.TransactionCase):
 
     def test_basics(self):
         order = self.order_basic
-        assert len(order.order_line) == 2
+        self.assertEqual(len(order.order_line), 2)
         order.order_line[0].button_update_proposal()
         order.order_line[1].button_update_proposal()
         order.order_line[0].button_update_proposal()
         prop1 = order.proposal_ids[0]
         prop2 = order.proposal_ids[1]
         prop3 = order.proposal_ids[2]
-        assert prop1.qty == order.order_line[0].product_qty
-        assert prop1.date is False
-        assert prop3.qty == order.order_line[0].product_qty
+        self.assertEqual(prop1.qty, order.order_line[0].product_qty)
+        self.assertEqual(prop1.date, False)
+        self.assertEqual(prop3.qty, order.order_line[0].product_qty)
         old1qty = prop1.qty
         prop1.write({"qty": prop1.qty - 1})
         prop2.write({"qty": prop2.qty - 1})
         prop3.write({"qty": 1})
         order.approve_proposal()
-        assert len(order.order_line) == 3
-        assert order.order_line[2].product_qty == 1
-        assert order.order_line[0].product_qty == old1qty - 1
+        self.assertEqual(len(order.order_line), 3)
+        self.assertEqual(order.order_line[2].product_qty, 1)
+        self.assertEqual(order.order_line[0].product_qty, old1qty - 1)
 
     def test_populate_complete_missing_lines(self):
         order = self.get_order_with_user(alternate_user=True)
         order.order_line[1].button_update_proposal()
         # Only missing lines should be added
         order.populate_all_purchase_lines()
-        assert order.proposal_count == len(order.order_line)
+        self.assertEqual(order.proposal_count, len(order.order_line))
 
     def test_mass_update_date(self):
         "Update all lines of proposal_ids"
@@ -50,10 +50,10 @@ class Test(common.TransactionCase):
         order.populate_all_purchase_lines()
         order.proposal_date = "2050-01-01"
         dates = [x.date for x in order.proposal_ids]
-        assert len(dates) == 2
+        self.assertEqual(len(dates), 2)
         dates = {x.date for x in order.proposal_ids}
-        assert len(dates) == 1
-        assert order.proposal_date == list(dates)[0]
+        self.assertEqual(len(dates), 1)
+        self.assertEqual(order.proposal_date, list(dates)[0])
 
     def test_proposal_workflow(self):
         # order with supplier
@@ -64,26 +64,26 @@ class Test(common.TransactionCase):
         prop = order_s.proposal_ids[0]
         prop.write({"qty": 10, "price_u": 2})
         order_s.submit_proposal()
-        assert order_s.proposal_updatable == "yes"
-        assert order.proposal_state == "submitted"
+        self.assertEqual(order_s.proposal_updatable, "yes")
+        self.assertEqual(order.proposal_state, "submitted")
         order.approve_proposal()
         # We check all is correctly written
-        assert order.state == "purchase"
-        assert order.order_line[0].product_qty == 10
-        assert order.order_line[0].price_unit == 2
+        self.assertEqual(order.state, "purchase")
+        self.assertEqual(order.order_line[0].product_qty, 10)
+        self.assertEqual(order.order_line[0].price_unit, 2)
 
     def test_duplicate_purchase_line(self):
         order = self.get_order_with_user()
         initial_qty = order.order_line[0].product_qty
         order.order_line[0].button_update_proposal()
         order.order_line[0].button_update_proposal()
-        assert len(order.proposal_ids) == 2
+        self.assertEqual(len(order.proposal_ids), 2)
         order.proposal_ids[0].write({"qty": order.proposal_ids[0].qty - 1})
         order.proposal_ids[1].write({"qty": 1})
         order.submit_proposal()
         order.approve_proposal()
-        assert order.order_line[0].product_qty == initial_qty - 1
-        assert order.order_line[2].product_qty == 1
+        self.assertEqual(order.order_line[0].product_qty, initial_qty - 1)
+        self.assertEqual(order.order_line[2].product_qty, 1)
 
     def test_check_purchase_line(self):
         order = self.get_order_with_user()
@@ -94,10 +94,10 @@ class Test(common.TransactionCase):
         order_s.proposal_ids[0].write({"qty": 2})
         order_s.proposal_ids[1].write({"qty": 5, "price_u": 3})
         order.approve_proposal()
-        assert order.order_line[0].product_qty == 2
-        assert order.order_line[2].product_qty == 5
-        assert order.order_line[2].price_unit == 3
-        assert order.order_line[2].taxes_id == order.order_line[0].taxes_id
+        self.assertEqual(order.order_line[0].product_qty, 2)
+        self.assertEqual(order.order_line[2].product_qty, 5)
+        self.assertEqual(order.order_line[2].price_unit, 3)
+        self.assertEqual(order.order_line[2].taxes_id, order.order_line[0].taxes_id)
 
     def test_supplier_should_not_update_forbidden_fields(self):
         order = self.get_order_with_user(alternate_user=True)
@@ -120,9 +120,9 @@ class Test(common.TransactionCase):
         order.proposal_ids[1].qty = 99
         order.submit_proposal()
         order.approve_proposal()
-        assert order.order_line[0].product_qty == 0
-        assert order.order_line[1].product_qty != 0
-        assert order.state != "cancel"
+        self.assertEqual(order.order_line[0].product_qty, 0)
+        self.assertEqual(order.order_line[1].product_qty, 99)
+        self.assertNotEqual(order.state, "cancel")
 
     def get_order_with_user(self, alternate_user=None):
         order = self.order_main
