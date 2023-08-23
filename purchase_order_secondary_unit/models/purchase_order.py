2c9cfc80b2db1130b1dc139474337449656250c0
Jairo Llopis <yajo.sk8@gmail.com>
2023-08-23 10:23:13 +0100

    [MIG] purchase_order_secondary_unit: migrate to v16
    Fix a bug when calling super's `_compute_product_qty()` was skipping the compute helper from the mixin.
    
    Fix buggy tests. These were asserting wrong behaviors.
    
    Upgrade views.
    
    @moduon MT-3483
    
    Co-authored-by: Andrea Cattalani <22261939+anddago78@users.noreply.github.com>

--- purchase_order_secondary_unit/models/purchase_order.py
+++ purchase_order_secondary_unit/models/purchase_order.py
@@ -15,18 +15,17 @@ class PurchaseOrderLine(models.Model):
         store=True, readonly=False, compute="_compute_product_qty", copy=True
     )
 
-    @api.depends("secondary_uom_qty", "secondary_uom_id")
+    @api.depends("product_packaging_qty", "secondary_uom_qty", "secondary_uom_id")
     def _compute_product_qty(self):
-        if hasattr(super(), "_compute_product_qty"):
-            return super()._compute_product_qty()
-        return self._compute_helper_target_field_qty()
+        self._compute_helper_target_field_qty()
+        return super()._compute_product_qty()
 
     @api.onchange("product_uom")
-    def onchange_product_uom_for_secondary(self):
+    def _onchange_product_uom_for_secondary(self):
         self._onchange_helper_product_uom_for_secondary()
 
     @api.onchange("product_id")
-    def onchange_product_id(self):
+    def _onchange_product_id(self):
         """If default purchases secondary unit set on product, put on secondary
         quantity 1 for being the default quantity. We override this method,
         that is the one that sets by default 1 on the other quantity with that
