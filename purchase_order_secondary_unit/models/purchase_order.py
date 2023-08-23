688e70f1f0f3187276c3bba32f9f3c81ff916bc1
Jairo Llopis <yajo.sk8@gmail.com>
2023-08-23 10:23:13 +0100

    [MIG] purchase_order_secondary_unit: Migration to v16

--- purchase_order_secondary_unit/models/purchase_order.py
+++ purchase_order_secondary_unit/models/purchase_order.py
@@ -20,7 +20,6 @@ class PurchaseOrderLine(models.Model):
     def _compute_product_qty(self):
         if hasattr(super(), "_compute_product_qty"):
             return super()._compute_product_qty()
-        return self._compute_helper_target_field_qty()
 
     @api.onchange("product_uom")
     def onchange_product_uom_for_secondary(self):
