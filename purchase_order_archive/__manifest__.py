c65505a56601f7b5b8f33ee3648994f93768f12d
Christihan Laurel <laurel@vauxoo.com>
2023-09-04 19:08:39 +0000

    [MIG] purchase_order_archive: Migration to 16.0

--- purchase_order_archive/__manifest__.py
+++ purchase_order_archive/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Onestein, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Purchases",
-    "version": "14.0.1.1.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["purchase"],
     "data": ["views/purchase_order.xml"],
