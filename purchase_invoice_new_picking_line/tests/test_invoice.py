260e7360cecd7e436b863ceb8feb96cc09358ae5
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-10-02 11:34:08 +0200

    [MIG] purchase_invoice_new_picking_line: Migration to 16.0
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- purchase_invoice_new_picking_line/tests/test_invoice.py
+++ purchase_invoice_new_picking_line/tests/test_invoice.py
@@ -2,74 +2,121 @@
 #   Robin Keunen <robin@coopiteasy.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+import datetime
 
-from odoo import tests
-from odoo.tests.common import Form
+from odoo import fields, tests
 
 
 class TestAccountInvoice(tests.common.TransactionCase):
-    def test_move_line_product_is_added_to_invoice(self):
-        purchase_order = self.browse_ref("purchase.purchase_order_4")
-        product = self.browse_ref("product.product_product_1")
-        supplier_loc = self.env.ref("stock.stock_location_suppliers")
-        stock_loc = self.env.ref("stock.warehouse0").lot_stock_id
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.purchase_order = cls.env.ref("purchase.purchase_order_4")
+        cls.product = cls.env.ref("product.product_product_1")
+        cls.supplier_loc = cls.env.ref("stock.stock_location_suppliers")
+        cls.stock_loc = cls.env.ref("stock.warehouse0").lot_stock_id
 
         # Confirm order
-        purchase_order.button_confirm()
-        picking = purchase_order.picking_ids
-        moves = picking.move_ids_without_package
-        self.assertEquals(len(purchase_order.order_line), len(moves))
+        cls.purchase_order.button_confirm()
+        cls.picking = cls.purchase_order.picking_ids
+        cls.moves = cls.picking.move_ids
 
-        moves[0].quantity_done = moves[0].product_qty
-        moves[1].quantity_done = moves[1].product_qty
-        moves[2].quantity_done = moves[2].product_qty - 1
+        cls.moves[0].quantity_done = cls.moves[0].product_qty
+        cls.moves[1].quantity_done = cls.moves[1].product_qty
+        cls.moves[2].quantity_done = cls.moves[2].product_qty - 1
 
-        new_move = self.env["stock.move"].create(
-            {
-                "picking_id": picking.id,
-                "name": product.name,
-                "product_id": product.id,
-                "product_uom": product.uom_id.id,
-                "quantity_done": 20,
-                "location_id": supplier_loc.id,
-                "location_dest_id": stock_loc.id,
-            }
-        )
-
-        wizard = picking.button_validate()
-        backorder_confirmation = self.env[wizard["res_model"]].browse(
-            wizard["res_id"]
+    def _validate_picking(self):
+        wizard = self.picking.button_validate()
+        backorder_confirmation = (
+            self.env[wizard["res_model"]].with_context(**wizard["context"]).create({})
         )
         backorder_confirmation.process_cancel_backorder()
 
-        res = purchase_order.with_context(
-            create_bill=True
-        ).action_view_invoice()
-        ctx = res.get("context")
-        f = Form(
-            self.env["account.invoice"].with_context(ctx),
-            view="account.invoice_supplier_form",
+    def _create_invoice(self):
+        res = self.purchase_order.action_create_invoice()
+        return self.env["account.move"].browse(res["res_id"])
+
+    def test_move_line_product_is_added_to_invoice(self):
+        new_move = self.env["stock.move"].create(
+            {
+                "picking_id": self.picking.id,
+                "name": self.product.name,
+                "product_id": self.product.id,
+                "product_uom": self.product.uom_id.id,
+                "quantity_done": 20,
+                "location_id": self.supplier_loc.id,
+                "location_dest_id": self.stock_loc.id,
+            }
         )
-        invoice = f.save()
+        self._validate_picking()
+
+        invoice = self._create_invoice()
 
         self.assertRecordValues(
             invoice.invoice_line_ids,
             [
                 {
-                    "product_id": moves[0].product_id.id,
-                    "quantity": moves[0].quantity_done,
+                    "product_id": self.moves[0].product_id.id,
+                    "quantity": self.moves[0].quantity_done,
                 },
                 {
-                    "product_id": moves[1].product_id.id,
-                    "quantity": moves[1].quantity_done,
+                    "product_id": self.moves[1].product_id.id,
+                    "quantity": self.moves[1].quantity_done,
                 },
                 {
-                    "product_id": moves[2].product_id.id,
-                    "quantity": moves[2].quantity_done,
+                    "product_id": self.moves[2].product_id.id,
+                    "quantity": self.moves[2].quantity_done,
                 },
                 {
                     "product_id": new_move.product_id.id,
                     "quantity": new_move.quantity_done,
+                    "price_unit": self.product.lst_price,
+                    "product_uom_id": new_move.product_uom.id,
+                    "display_type": "product",
+                },
+            ],
+        )
+
+    def test_with_supplierinfo(self):
+        """Same test, but the price is now defined on supplierinfo instead of on
+        the product.
+        """
+        self.env["product.supplierinfo"].create(
+            {
+                "product_id": self.product.id,
+                "product_tmpl_id": self.product.product_tmpl_id.id,
+                "partner_id": self.purchase_order.partner_id.id,
+                "price": 1,
+                "currency_id": self.purchase_order.currency_id.id,
+                "date_start": fields.Date.today() - datetime.timedelta(days=90),
+                "date_end": fields.Date.today() + datetime.timedelta(days=90),
+            }
+        )
+        new_move = self.env["stock.move"].create(
+            {
+                "picking_id": self.picking.id,
+                "name": self.product.name,
+                "product_id": self.product.id,
+                "product_uom": self.product.uom_id.id,
+                "quantity_done": 20,
+                "location_id": self.supplier_loc.id,
+                "location_dest_id": self.stock_loc.id,
+            }
+        )
+        self._validate_picking()
+
+        invoice = self._create_invoice()
+
+        self.assertRecordValues(
+            invoice.invoice_line_ids[-1],
+            [
+                {
+                    "product_id": new_move.product_id.id,
+                    "quantity": new_move.quantity_done,
+                    "price_unit": 1,
+                    "product_uom_id": new_move.product_uom.id,
+                    "display_type": "product",
                 },
             ],
         )
