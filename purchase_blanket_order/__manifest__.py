0779bf470a773c28cc24cd07791a660ffa5fa622
ThiagoMForgeFlow <thiago.mulero@forgeflow.com>
2023-10-11 10:51:55 +0200

    [IMP] purchase_blanket_order: avoid overwrite context in views inherited

--- purchase_blanket_order/__manifest__.py
+++ purchase_blanket_order/__manifest__.py
@@ -8,7 +8,11 @@
     "version": "14.0.1.4.0",
     "website": "https://github.com/OCA/purchase-workflow",
     "summary": "Purchase Blanket Orders",
-    "depends": ["purchase", "web_action_conditionable"],
+    "depends": [
+        "purchase",
+        "web_action_conditionable",
+        "base_view_inheritance_extension",
+    ],
     "data": [
         "security/ir.model.access.csv",
         "security/security.xml",
