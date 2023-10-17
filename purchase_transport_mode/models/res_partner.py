2e23b4d2ddd2aea727661db2daccb6681d743afc
Telmo Santos <telmo.santos@camptocamp.com>
2023-10-17 14:33:53 +0200

    [ADD] purchase_transport_mode

---
+++ purchase_transport_mode/models/res_partner.py
@@ -0,0 +1,10 @@
+# Copyright 2023 Camptocamp (<https://www.camptocamp.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    purchase_transport_mode_id = fields.Many2one("purchase.transport.mode")
---
+++ purchase_transport_mode/readme/USAGE.rst
@@ -0,0 +1,7 @@
+To use this module, you need to:
+
+#. In Purchase settings enable purchase transport mode validation
+#. Create a transport modes with contraints applied on PO  
+#. Select transport mode on PO
+
+If the transport requirements are not met, a message in yellow will be displayed at the top of the form.
