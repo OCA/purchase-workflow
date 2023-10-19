f50fcf267fad885b46f3bd7574756c3efd50513d
Aungkokolin1997 <lin@quartile.co>
2023-10-19 08:43:11 +0630

    [MIG] purchase_invoice_method: Migration to 16.0

--- purchase_invoice_method/__manifest__.py
+++ purchase_invoice_method/__manifest__.py
@@ -5,9 +5,9 @@
     "name": "Purchase Invoice Method",
     "summary": """
         Allow to force the invoice method of a purchase""",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
-    "author": "CreuBlanca,Odoo Community Association (OCA)",
+    "author": "CreuBlanca, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "depends": ["purchase"],
     "data": [
