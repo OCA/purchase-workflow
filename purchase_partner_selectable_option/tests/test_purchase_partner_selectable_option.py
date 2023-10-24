033ae50c09f52198772f8cda9d0ca3b02890e82d
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-24 08:51:43 +0200

    [IMP] purchase_partner_selectable_option: test performance improvement - Include context keys for avoiding mail operations overhead.

--- purchase_partner_selectable_option/tests/test_purchase_partner_selectable_option.py
+++ purchase_partner_selectable_option/tests/test_purchase_partner_selectable_option.py
@@ -4,11 +4,14 @@ from lxml import etree
 
 from odoo.tests import common
 
+from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+
 
 class TestPurchasePartnerSelectableOption(common.TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
 
     def test_sale_order(self):
         result = self.env["purchase.order"].get_view(
