d046daf64dc7e79524c002d8d1e3ce5c74c034ff
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-24 08:41:23 +0200

    [IMP] purchase_order_product_recommendation: test performance improvement - Include context keys for avoiding mail operations overhead.

--- purchase_order_product_recommendation/tests/test_recommendation.py
+++ purchase_order_product_recommendation/tests/test_recommendation.py
@@ -10,6 +10,16 @@ class RecommendationCase(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        # Remove this variable in v16 and put instead:
+        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+        DISABLED_MAIL_CONTEXT = {
+            "tracking_disable": True,
+            "mail_create_nolog": True,
+            "mail_create_nosubscribe": True,
+            "mail_notrack": True,
+            "no_reset_password": True,
+        }
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         cls.partner = cls.env["res.partner"].create({"name": "Mr. Odoo"})
         cls.category_obj = cls.env["product.category"]
         cls.categ1 = cls.category_obj.create({"name": "Test Cat 1"})
