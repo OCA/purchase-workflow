9a8fa548211596c2a11293abb08fcac34acd6af3
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-24 08:51:43 +0200

    [IMP] purchase_partner_selectable_option: test performance improvement - Include context keys for avoiding mail operations overhead.

--- purchase_partner_selectable_option/__manifest__.py
+++ purchase_partner_selectable_option/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Purchase Partner Selectable Option",
-    "version": "16.0.1.0.2",
+    "version": "16.0.1.0.3",
     "category": "Purchase",
     "website": "https://github.com/OCA/purchase-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
