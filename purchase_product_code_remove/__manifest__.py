adf95adb63c1c8628e64619594f2b84e0b2d44dd
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2023-11-23 17:44:20 -0600

    [ADD] purchase_product_code_remove: new module to remove default code in purchase order line description

---
+++ purchase_product_code_remove/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2023 Jarsa, (<https://www.jarsa.com>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.html).
+{
+    "name": "Purchase Product Code Remove",
+    "summary": "Remove product code from purchase order lines description",
+    "author": "Jarsa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "category": "Purchase Management",
+    "version": "15.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["purchase"],
+}
