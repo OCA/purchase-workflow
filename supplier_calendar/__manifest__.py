b8f7e32a4e2b5dda464bebe8789e7a8ed8783e38
DavidJForgeFlow <david.jimenez@forgeflow.com>
2023-12-15 12:37:05 +0100

    [MIG] supplier_calendar: Migration to 15.0

--- supplier_calendar/__manifest__.py
+++ supplier_calendar/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Supplier Calendar",
     "summary": "Supplier Calendar",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Purchase Management",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
