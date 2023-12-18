9fa556768f385792a8462b4419bd76dc2c8b4d3b
Wesley Oliveira <wesleygabrieldeoliveira@gmail.com>
2023-12-18 14:18:48 -0300

    [IMP] purchase_default_terms_conditions: fill the notes field in create method too

--- purchase_default_terms_conditions/models/purchase.py
+++ purchase_default_terms_conditions/models/purchase.py
@@ -19,3 +19,20 @@ class PurchaseOrder(models.Model):
         ):
             self.notes = self.company_id.purchase_note
         return super().onchange_partner_id()
+
+    @api.model
+    def create(self, vals):
+        order = super().create(vals)
+
+        if order.notes:
+            return order
+        elif order.partner_id.purchase_note:
+            order.notes = order.partner_id.purchase_note
+        elif (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("purchase.use_purchase_note")
+        ):
+            order.notes = order.company_id.purchase_note
+
+        return order
