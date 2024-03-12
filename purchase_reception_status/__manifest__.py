7d6d9374443de57f83e8326e0e7466b3261a364b
Rad0van <radovan@skolnik.info>
2024-03-12 11:45:25 +0100

    [MIG] purchase_reception_status: Migration to 17.0

--- purchase_reception_status/__manifest__.py
+++ purchase_reception_status/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Purchase Reception Status",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.1",
     "category": "Purchases",
     "license": "AGPL-3",
     "summary": "Add reception status on purchase orders",
