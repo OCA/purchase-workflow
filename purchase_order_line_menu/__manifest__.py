af52841acfe663acd01562d7e23b3b5500c19634
lawso017 <lawson.bj@gmail.com>
2024-04-24 08:13:40 -0400

    [MIG] purchase_order_line_menu: Migration to 17.0

--- purchase_order_line_menu/__manifest__.py
+++ purchase_order_line_menu/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Purchase Order Line Menu",
-    "version": "16.0.2.1.0",
+    "version": "17.0.1.0.0",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "summary": "Adds Purchase Order Lines Menu",
     "website": "https://github.com/OCA/purchase-workflow",
