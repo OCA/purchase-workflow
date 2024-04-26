407e7e27e08782a8d15f69f65e46e568834c191f
Ruchir Shukla <ruchir@bizzappdev.com>
2024-04-26 12:23:39 +0530

    [MIG][15.0] purchase_order_line_description_picking:Migration to 15.0

--- purchase_order_line_description_picking/tests/test_purchase_order_line_description_picking.py
+++ purchase_order_line_description_picking/tests/test_purchase_order_line_description_picking.py
@@ -4,7 +4,7 @@
 from odoo.tests import common
 
 
-class TestPurchaseOrderLineDescriptionPicking(common.SavepointCase):
+class TestPurchaseOrderLineDescriptionPicking(common.TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
