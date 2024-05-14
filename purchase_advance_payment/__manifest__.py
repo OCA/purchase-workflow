03cfd82042a2958fc50d40a9101b93027360ff51
Lois Rilo <lois.rilo@forgeflow.com>
2024-05-14 15:46:19 +0200

    [IMP] purchase_advance_payment: add dedicated menu for advance payments

--- purchase_advance_payment/__manifest__.py
+++ purchase_advance_payment/__manifest__.py
@@ -3,10 +3,11 @@
 
 {
     "name": "Purchase Advance Payment",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.1.0",
     "author": "Forgeflow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Purchase",
+    "maintainers": ["LoisRForgeFlow"],
     "license": "AGPL-3",
     "summary": "Allow to add advance payments on purchase orders",
     "depends": ["purchase"],
@@ -14,6 +15,7 @@
         "wizard/purchase_advance_payment_wizard_view.xml",
         "views/purchase_view.xml",
         "security/ir.model.access.csv",
+        "views/account_payment_views.xml",
     ],
     "installable": True,
 }
