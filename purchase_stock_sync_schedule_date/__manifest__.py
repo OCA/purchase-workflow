0a02653c1ce8ff3906686bd0f4cf3a11eb7db4ce
Mathieu <mathieu.delva@hotmail.com>
2024-05-27 14:34:06 +0200

    [ADD] purchase_stock_sync_schedule_date module

---
+++ purchase_stock_sync_schedule_date/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Akretion (https://www.akretion.com).
+# @author Mathieu Delva <mathieu.delva@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+
+{
+    "name": "Purchase Stock Sync Schedule Date",
+    "version": "14.0.1.1.0",
+    "author": "Akretion ,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "category": "Purchase",
+    "depends": ["purchase_stock"],
+    "data": [],
+    "installable": True,
+    "license": "AGPL-3",
+}
---
+++ purchase_stock_sync_schedule_date/models/purchase.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Akretion (https://www.akretion.com).
+# @author Mathieu Delva <mathieu.delva@akretion.com>
+
+from odoo import models
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    def _update_move_date_deadline(self, new_date):
+        res = super()._update_move_date_deadline(new_date)
+        moves_to_update = self.move_ids.filtered(lambda m: m.state not in ('done', 'cancel'))
+        if not moves_to_update:
+            moves_to_update = self.move_dest_ids.filtered(lambda m: m.state not in ('done', 'cancel'))
+        for move in moves_to_update:
+            move.date = new_date
+        return res
