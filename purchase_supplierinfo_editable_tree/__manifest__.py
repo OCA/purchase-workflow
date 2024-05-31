6eeda16f1445e2a9cb77ed8384cf10060ba3fd52
Victor Champonnois <victor@coopiteasy.be>
2024-05-31 16:32:39 +0200

    [ADD] purchase_supplierinfo_editable_tree

---
+++ purchase_supplierinfo_editable_tree/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Tecnativa - Ernesto Tejeda
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Supplierinfo Editable Tree",
+    "summary": "Set the supplierinfo tree view as editablee",
+    "version": "16.0.1.0.1",
+    "category": "Purchase",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "author": "Coop IT Easy SC, Odoo Community Association (OCA)",
+    "data": ["views/supplierinfo_views.xml"],
+    "license": "AGPL-3",
+    "depends": ["product"],
+}
---
+++ purchase_supplierinfo_editable_tree/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module sets the supplierinfo tree view as editable. This allows to quickly edit purchase prices.
+This is useful when we have to update the prices from a supplier, without having to do an export/import of supplierinfos.
