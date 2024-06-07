5118092da3229cf20a9725d1de8a9f2dec025f54
Akim Juillerat <akim.juillerat@camptocamp.com>
2024-06-07 17:43:44 +0200

    [MIG] purchase_order_line_receipt_status: Backport to 14.0

--- purchase_order_line_receipt_status/__manifest__.py
+++ purchase_order_line_receipt_status/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Purchase Reception Status",
     "summary": "Manage customizations on purchase order line",
-    "version": "16.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Camptocamp, Italo Lopes, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Purchase",
