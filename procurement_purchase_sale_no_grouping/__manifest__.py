8968f01ef4d968f8e5f8810120727117cb2d7e00
Raf Ven <raf.ven@dynapps.be>
2024-06-20 11:03:26 +0200

    [MIG] procurement_purchase_sale_no_grouping: Migration to 17.0

--- procurement_purchase_sale_no_grouping/__manifest__.py
+++ procurement_purchase_sale_no_grouping/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Procurement Purchase Service No Grouping",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Procurements",
