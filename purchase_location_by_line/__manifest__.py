fa5dd36d7b05d8671e3bec0aea04b9ef4913ef20
Deriman-Alonso <d.alonso@binhex.cloud>
2024-07-08 12:28:28 +0100

    [IMP] purchase_location_by_line: pre-commit auto fixes

--- purchase_location_by_line/__manifest__.py
+++ purchase_location_by_line/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Purchase Location by Line",
     "summary": "Allows to define a specific destination location on each PO line",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Purchase Management",
