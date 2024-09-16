7d891b1b6b003a48a1bcb88b623c41c1cf5af972
AaronHForgeFlow <aaron.henriquez@forgeflow.com>
2024-09-16 16:31:25 +0200

    [MIG] purchase_fully_received: Migration to v16

--- purchase_fully_received/tests/test_purchase_fully_received.py
+++ purchase_fully_received/tests/test_purchase_fully_received.py
@@ -17,6 +17,7 @@ class TestPurchaseOrderAutoLock(common.TransactionCase):
         self.partner_id = self.env.ref("base.res_partner_1")
         self.product_id_1 = self.env.ref("product.product_product_8")
         self.account_model = self.env["account.account"]
+        self.am_model = self.env["account.move"]
 
         self.product_id_1.write({"purchase_method": "purchase"})
         self.po_vals = {
@@ -50,7 +51,7 @@ class TestPurchaseOrderAutoLock(common.TransactionCase):
     def test_01_purchase_order_received_not_invoiced(self):
         """Test is marked as fully received and to invoice"""
         self.po.button_confirm()
-        self.po.picking_ids[0].move_lines.write({"quantity_done": 5})
+        self.po.picking_ids[0].move_line_ids.write({"qty_done": 5})
         self.po.picking_ids[0].button_validate()
         self.assertEqual(
             self.po.order_line[0].product_qty,
@@ -62,14 +63,15 @@ class TestPurchaseOrderAutoLock(common.TransactionCase):
     def test_02_purchase_order_received_invoiced_not_posted(self):
         """If the invoice is not posted is the same thing"""
         self.po.button_confirm()
-        self.po.picking_ids.move_lines.write({"quantity_done": 5})
+        self.po.picking_ids.move_line_ids.write({"qty_done": 5})
         self.po.picking_ids[0].button_validate()
-        move_form = Form(
-            self.env["account.move"].with_context(default_move_type="in_invoice")
+        move_form = Form(self.am_model.with_context(default_move_type="in_invoice"))
+        move_form.partner_id = self.po.partner_id
+        move_form.invoice_date = fields.Date().today()
+        move_form.purchase_vendor_bill_id = self.env["purchase.bill.union"].browse(
+            -self.po.id
         )
-        move_form.partner_id = self.supplier
         move_form.currency_id = self.currency_eur
-        move_form.purchase_id = self.po
         move_form.save()
         self.assertTrue(self.po.is_fully_shipped)
 
@@ -78,14 +80,15 @@ class TestPurchaseOrderAutoLock(common.TransactionCase):
         The invoiced and validated
         """
         self.po.button_confirm()
-        self.po.picking_ids.move_lines.write({"quantity_done": 5})
+        self.po.picking_ids.move_line_ids.write({"qty_done": 5})
         self.po.picking_ids[0].button_validate()
-        move_form = Form(
-            self.env["account.move"].with_context(default_move_type="in_invoice")
+        move_form = Form(self.am_model.with_context(default_move_type="in_invoice"))
+        move_form.partner_id = self.po.partner_id
+        move_form.invoice_date = fields.Date().today()
+        move_form.purchase_vendor_bill_id = self.env["purchase.bill.union"].browse(
+            -self.po.id
         )
-        move_form.partner_id = self.supplier
         move_form.currency_id = self.currency_eur
-        move_form.purchase_id = self.po
         move_form.invoice_date = datetime.now()
         invoice = move_form.save()
         invoice.action_post()
