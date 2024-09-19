fb1febfec750ac996d14f9aeb253a87a21cc3c46
Vincent Van Rossem <vincent.vanrossem@camptocamp.com>
2024-09-19 17:18:13 +0200

    [17.0][IMP] purchase_advance_payment: auto reconcile adv payments

--- purchase_advance_payment/models/res_config_settings.py
+++ purchase_advance_payment/models/res_config_settings.py
@@ -10,3 +10,6 @@ class ResConfigSettings(models.TransientModel):
     auto_post_purchase_advance_payments = fields.Boolean(
         config_parameter="purchase_advance_payment.auto_post_advance_payments",
     )
+    auto_reconcile_purchase_advance_payments = fields.Boolean(
+        config_parameter="purchase_advance_payment.auto_reconcile_advance_payments",
+    )
