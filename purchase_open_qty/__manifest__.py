be2286b37af8933d728bd80175ce62174232ab0d
Stefan Rijnhart <stefan@opener.amsterdam>
2024-10-10 11:26:38 +0200

    [MIG] purchase_open_qty: Migration to 18.0
    Move models and views into model specific files.

--- purchase_open_qty/__manifest__.py
+++ purchase_open_qty/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Purchase Open Qty",
     "summary": "Allows to identify the purchase orders that have quantities "
     "pending to invoice or to receive.",
-    "version": "17.0.2.0.0",
+    "version": "18.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Purchases",
@@ -14,7 +14,10 @@
         "purchase_stock",
         "purchase_order_line_menu",
     ],
-    "data": ["views/purchase_view.xml"],
+    "data": [
+        "views/purchase_order_line_views.xml",
+        "views/purchase_order_views.xml",
+    ],
     "pre_init_hook": "pre_init_hook",
     "license": "AGPL-3",
     "installable": True,
--- purchase_open_qty/models/purchase_order.py
+++ purchase_open_qty/models/purchase_order.py
@@ -6,42 +6,6 @@ from odoo import _, api, fields, models
 from odoo.tools import float_is_zero
 
 
-class PurchaseOrderLine(models.Model):
-    _inherit = "purchase.order.line"
-
-    @api.depends(
-        "move_ids.state",
-        "move_ids.product_uom",
-        "move_ids.product_uom_qty",
-        "product_qty",
-        "qty_received",
-    )
-    def _compute_qty_to_receive(self):
-        service_lines = self.filtered(lambda line: line.product_id.type == "service")
-        for line in self - service_lines:
-            total = 0.0
-            for move in line.move_ids.filtered(
-                lambda m: m.state not in ("cancel", "done")
-            ):
-                if move.product_uom != line.product_uom:
-                    total += move.product_uom._compute_quantity(
-                        move.product_uom_qty, line.product_uom
-                    )
-                else:
-                    total += move.product_uom_qty
-            line.qty_to_receive = total
-        for line in service_lines:
-            line.qty_to_receive = line.product_qty - line.qty_received
-
-    qty_to_receive = fields.Float(
-        compute="_compute_qty_to_receive",
-        digits="Product Unit of Measure",
-        copy=False,
-        string="Qty to Receive",
-        store=True,
-    )
-
-
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
