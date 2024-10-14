5139c2b319178807881cfd6e857ff6f92818c086
DavidJForgeFlow <david.jimenez@forgeflow.com>
2024-10-14 08:55:12 +0200

    [ADD] purchase_reception_status_line

---
+++ purchase_reception_status_line/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 ForgeFlow (http://www.akretion.com/)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Purchase Reception Status Line",
+    "version": "15.0.1.0.0",
+    "category": "Purchases",
+    "license": "AGPL-3",
+    "summary": "Add reception status on purchase order lines",
+    "author": "ForgeFlow,Odoo Community Association (OCA)",
+    "maintainers": ["DavidJForgeFlow"],
+    "website": "https://github.com/OCA/purchase-workflow",
+    "depends": ["purchase_reception_status"],
+    "data": ["views/purchase_order.xml"],
+    "installable": True,
+}
