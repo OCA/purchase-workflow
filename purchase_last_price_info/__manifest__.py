07d0000db4697e6e754af5bbb5157556343b7653
Stefan Rijnhart <stefan@opener.amsterdam>
2024-10-17 18:36:05 +0200

    [MIG] purchase_last_price_info: Migration to 18.0

--- purchase_last_price_info/__manifest__.py
+++ purchase_last_price_info/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Purchase Product Last Price Info",
-    "version": "17.0.1.0.2",
+    "version": "18.0.1.0.0",
     "category": "Purchase Management",
     "license": "AGPL-3",
     "author": "AvanzOSC, Tecnativa, Odoo Community Association (OCA)",
