caff005d93c59a5333b04f7afae66137e1134c6c
Stefan Rijnhart <stefan@opener.amsterdam>
2024-10-17 18:21:32 +0200

    [MIG] purchase_commercial_partner: Migration to 18.0

--- purchase_commercial_partner/__manifest__.py
+++ purchase_commercial_partner/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Purchase Commercial Partner",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Purchases",
     "license": "AGPL-3",
     "summary": "Add stored related field 'Commercial Supplier' on POs",
