18a2ec8985088f0df0d56be3f5da76ae3acaf6af
epanisset <emeric.panisset@camptocamp.com>
2024-10-22 12:33:42 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (45 of 45 strings)
    
    Translation: purchase-workflow-17.0/purchase-workflow-17.0-purchase_advance_payment
    Translate-URL: https://translation.odoo-community.org/projects/purchase-workflow-17-0/purchase-workflow-17-0-purchase_advance_payment/fr/

--- purchase_advance_payment/__manifest__.py
+++ purchase_advance_payment/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Purchase Advance Payment",
-    "version": "17.0.1.2.0",
+    "version": "17.0.1.3.0",
     "author": "Forgeflow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Purchase",
