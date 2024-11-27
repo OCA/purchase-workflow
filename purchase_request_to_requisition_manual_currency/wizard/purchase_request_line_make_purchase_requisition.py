5c757f7873f96494b2ff75821774b74196726ecd
Saran440 <saranl@ecosoft.co.th>
2024-11-27 10:54:13 +0700

    [FIX] get pr from items

--- purchase_request_to_requisition_manual_currency/wizard/purchase_request_line_make_purchase_requisition.py
+++ purchase_request_to_requisition_manual_currency/wizard/purchase_request_line_make_purchase_requisition.py
@@ -10,25 +10,11 @@ class PurchaseRequestLineMakePurchaseRequisition(models.TransientModel):
     @api.model
     def _prepare_purchase_requisition(self, item, picking_type_id, company_id):
         data = super()._prepare_purchase_requisition(item, picking_type_id, company_id)
-        model = self._context.get("active_model")
-        if model == "purchase.request":
-            active_ids = self._context.get("active_ids")
-            pr = self.env[model].browse(active_ids)
-            data.update(
-                {
-                    "manual_currency": pr.manual_currency,
-                    "type_currency": pr.type_currency,
-                    "manual_currency_rate": pr.manual_currency_rate,
-                }
-            )
-        elif model == "purchase.request.line":
-            active_id = self._context.get("active_id")
-            pr_line = self.env[model].browse(active_id)
-            data.update(
-                {
-                    "manual_currency": pr_line.request_id.manual_currency,
-                    "type_currency": pr_line.request_id.type_currency,
-                    "manual_currency_rate": pr_line.request_id.manual_currency_rate,
-                }
-            )
+        data.update(
+            {
+                "manual_currency": item.request_id.manual_currency,
+                "type_currency": item.request_id.type_currency,
+                "manual_currency_rate": item.request_id.manual_currency_rate,
+            }
+        )
         return data
