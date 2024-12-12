7935104e4e90fa140615a8428843dfa20f2b794b
Stefan Rijnhart <stefan@opener.amsterdam>
2024-12-12 16:19:19 +0100

    [MIG] purchase_order_type: Migration to 18.0

--- purchase_order_type/__manifest__.py
+++ purchase_order_type/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Purchase Order Type",
-    "version": "17.0.1.0.2",
+    "version": "18.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Purchase Management",
