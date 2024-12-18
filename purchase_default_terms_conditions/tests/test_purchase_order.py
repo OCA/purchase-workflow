ead6bac8495a8d419125d5dd2044417df3592db7
Bhavesh Heliconia <oca@heliconia.in>
2024-12-18 13:25:23 +0530

    [IMP]purchase_default_terms_conditions: improved test cases

--- purchase_default_terms_conditions/tests/test_purchase_order.py
+++ purchase_default_terms_conditions/tests/test_purchase_order.py
@@ -1,13 +1,13 @@
 # Copyright (C) 2022 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests import tagged
+from odoo.tests import Form, tagged
 
-from odoo.addons.account.tests.common import AccountTestInvoicingCommon
+from odoo.addons.base.tests.common import BaseCommon
 
 
 @tagged("post_install", "-at_install")
-class TestPurchase(AccountTestInvoicingCommon):
+class TestPurchase(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -26,39 +26,68 @@ class TestPurchase(AccountTestInvoicingCommon):
                 "taxes_id": False,
             }
         )
-        cls.partner_a.write(
-            {"purchase_note": "Purchase Default Terms and Conditions Partner"}
-        )
-
-    def test_onchange_partner_id(self):
-        purchase_order = (
-            self.env["purchase.order"]
-            .with_context(tracking_disable=True)
-            .sudo()
-            .create(
-                {
-                    "partner_id": self.partner_a.id,
-                }
-            )
-        )
-        purchase_order.onchange_partner_id()
-        PurchaseOrderLine = self.env["purchase.order.line"].with_context(
-            tracking_disable=True
-        )
-        PurchaseOrderLine.sudo().create(
+        cls.partner_a = cls.env["res.partner"].create(
             {
-                "name": self.product_order.name,
-                "product_id": self.product_order.id,
-                "product_qty": 10.0,
-                "product_uom": self.product_order.uom_id.id,
-                "price_unit": self.product_order.list_price,
-                "order_id": purchase_order.id,
-                "taxes_id": False,
+                "name": "partner_a",
+                "company_id": False,
+                "purchase_note": "Purchase Default Terms and Conditions Partner",
+            }
+        )
+        cls.company_id = cls.env["res.company"].create(
+            {
+                "name": "Test Company",
+                "country_id": cls.env.ref("base.be").id,
+                "purchase_note": "Company Default Terms and Conditions",
             }
         )
 
-        self.partner_a.write({"purchase_note": False})
+    def test_partner_note_with_form(self):
+        company_note = "Company Default Terms and Conditions"
+        self.company_id.purchase_note = company_note
 
-        self.env["ir.config_parameter"].set_param("purchase.use_purchase_note", "Test")
+        has_order_type = "order_type" in self.env["purchase.order"]._fields
 
+        # First case: Test with partner's purchase note
+        with Form(self.env["purchase.order"]) as purchase_form:
+            purchase_form.partner_id = self.partner_a
+            purchase_form.company_id = self.company_id
+
+            if has_order_type:
+                order_type = self.env.ref("purchase_order_type.po_type_regular")
+                if order_type.company_id and order_type.company_id != self.company_id:
+                    order_type.company_id = self.company_id
+                purchase_form.order_type = order_type
+
+            self.assertEqual(
+                purchase_form.notes.strip(),
+                self.partner_a.purchase_note,
+                "Notes should be set from partner's purchase note",
+            )
+
+        # Second case: Clear the partner's purchase note and test company purchase note
+        self.partner_a.purchase_note = False
+        self.env["ir.config_parameter"].sudo().set_param(
+            "purchase.use_purchase_note", "true"
+        )
+
+        purchase_order = self.env["purchase.order"].create(
+            {
+                "partner_id": self.partner_a.id,
+                "company_id": self.company_id.id,
+            }
+        )
         purchase_order.onchange_partner_id()
+
+        with Form(purchase_order) as purchase_form:
+            if has_order_type:
+                order_type = self.env.ref("purchase_order_type.po_type_regular")
+
+                if order_type.company_id and order_type.company_id != self.company_id:
+                    order_type.company_id = self.company_id
+                purchase_form.order_type = order_type
+
+            self.assertIn(
+                company_note,
+                purchase_form.notes,
+                "Notes should contain company purchase note when partner note is empty",
+            )
