5000f59d948084103c184642b48830436d207f85
Bhavesh Heliconia <oca@heliconia.in>
2024-12-19 12:49:39 +0530

    [MIG] purchase_order_type_dashboard: Migration to 18.0

--- purchase_order_type_dashboard/__manifest__.py
+++ purchase_order_type_dashboard/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Purchase Order Type Dashboard",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Solvos, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Purchase Management",
