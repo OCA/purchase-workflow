a70a9bbe85ddce8e2f6a293c00e9d429f3978d79
Maksym Yankin <yankinmk@gmail.com>
2025-01-21 18:53:19 +0200

    [IMP] purchase_exception: use BaseCommon as base class to reduce overhead from tracking

--- purchase_exception/tests/test_purchase_exception.py
+++ purchase_exception/tests/test_purchase_exception.py
@@ -4,11 +4,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from datetime import datetime
 
-from odoo.tests.common import TransactionCase
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
+from odoo.addons.base.tests.common import BaseCommon
 
-class TestPurchaseException(TransactionCase):
+
+class TestPurchaseException(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
