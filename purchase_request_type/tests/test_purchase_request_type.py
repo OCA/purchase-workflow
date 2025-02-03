4a274e75d4ae3dcefc67834ec180c8f7932b2a14
Saran440 <saranl@ecosoft.co.th>
2025-02-03 16:53:45 +0700

    [FIX] purchase_request_type: use sequence following company document

--- purchase_request_type/tests/test_purchase_request_type.py
+++ purchase_request_type/tests/test_purchase_request_type.py
@@ -28,6 +28,9 @@ class TestPurchaseRequestType(BaseCommon):
         # Picking Type
         cls.picking_type = cls.env.ref("stock.picking_type_in")
         cls.picking_type2 = cls.env.ref("stock.picking_type_internal")
+
+        # Add company in purchase type
+        cls.type2.company_id = cls.picking_type.company_id
         cls.type2.picking_type_id = cls.picking_type
         cls.company2 = cls.company_obj.create({"name": "company2"})
 
