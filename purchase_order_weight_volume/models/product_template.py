46005bfb0f03765d4ac1632195c553d775526551
Miro028 <mirotasevski10@gmail.com>
2025-02-05 13:56:39 +0100

    [18.0] [IMP] follow up BLA-207 - add total weight / Volume on the purchase order

--- purchase_order_weight_volume/models/product_template.py
+++ purchase_order_weight_volume/models/product_template.py
@@ -4,5 +4,5 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    weight = fields.Float(required=True)
+    weight = fields.Float(required=False)
     volume = fields.Float(required=False)
