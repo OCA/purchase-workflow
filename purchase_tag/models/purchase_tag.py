9a574a042a7719db1a40c163645fbb706eeeda20
abdessamad salim <abdessamad.salim@smile-maroc.com>
2025-02-13 12:11:38 +0100

    [MIG] purchase_tag: Migration to 18.0

--- purchase_tag/models/purchase_tag.py
+++ purchase_tag/models/purchase_tag.py
@@ -1,10 +1,6 @@
-# Copyright 2022 ForgeFlow S.L.
-#   (http://www.forgeflow.com)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from random import randint
 
-from odoo import _, api, fields, models
+from odoo import api, fields, models
 from odoo.exceptions import ValidationError
 
 
@@ -13,41 +9,37 @@ class PurchaseTag(models.Model):
     _description = "Purchase Tag"
     _parent_store = True
 
-    def _get_default_color(self):
-        return randint(1, 11)
-
+    # Fields
     name = fields.Char("Tag Name", required=True, translate=True)
     color = fields.Integer(default=lambda self: self._get_default_color())
     parent_id = fields.Many2one("purchase.tag", index=True, ondelete="cascade")
     child_ids = fields.One2many("purchase.tag", "parent_id")
-    parent_path = fields.Char(index=True, unaccent=False)
+    parent_path = fields.Char(index=True)
+    display_name = fields.Char(compute="_compute_display_name")
 
     _sql_constraints = [
         ("tag_name_uniq", "unique (name)", "Tag name already exists !"),
     ]
 
-    def name_get(self):
-        res = []
-        for tag in self:
-            names = []
-            current = tag
-            while current:
-                names.append(current.name)
-                current = current.parent_id
-            res.append((tag.id, " / ".join(reversed(names))))
-        return res
+    def _get_default_color(self):
+        return randint(1, 11)
+
+    @api.depends("name", "parent_id")
+    def _compute_display_name(self):
+        for rec in self:
+            if rec.parent_id and rec.parent_id.name:
+                rec.display_name = f"{rec.parent_id.name}/{rec.name}"
+            else:
+                rec.display_name = rec.name
 
     @api.model
-    def _name_search(
-        self, name="", domain=None, operator="ilike", limit=100, order=None
-    ):
-        if name:
-            domain = [("name", operator, name.split(" / ")[-1])] + list(domain or [])
-        return super()._name_search(
-            name=name, domain=domain, operator=operator, limit=limit, order=order
-        )
+    def _search_display_name(self, operator, value, limit=100):
+        if value:
+            domain = ["|", ("name", operator, value), ("parent_path", operator, value)]
+            return self.search(domain, limit=limit).ids
+        return super()._search_display_name(operator=operator, value=value)
 
     @api.constrains("parent_id")
     def _check_parent_recursion(self):
-        if not self._check_recursion("parent_id"):
-            raise ValidationError(_("Tags cannot be recursive."))
+        if self._has_cycle("parent_id"):
+            raise ValidationError(self.env._("Tags cannot be recursive."))
