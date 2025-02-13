4071942a283c6aa3579e7df6d1eb8c49d20e8300
nobuQuartile <nobu@quartile.co>
2025-02-13 05:14:38 +0000

    [FIX] purchase_order_revision: Allow non-admins to view revisions

--- purchase_order_revision/models/purchase_order.py
+++ purchase_order_revision/models/purchase_order.py
@@ -30,12 +30,11 @@ class PurchaseOrder(models.Model):
 
     def action_view_revisions(self):
         self.ensure_one()
-        action = self.env.ref("purchase.purchase_rfq")
-        result = action.read()[0]
-        result["domain"] = ["|", ("active", "=", False), ("active", "=", True)]
-        result["context"] = {
+        action = self.env["ir.actions.actions"]._for_xml_id("purchase.purchase_rfq")
+        action["domain"] = ["|", ("active", "=", False), ("active", "=", True)]
+        action["context"] = {
             "active_test": 0,
             "search_default_current_revision_id": self.id,
             "default_current_revision_id": self.id,
         }
-        return result
+        return action
