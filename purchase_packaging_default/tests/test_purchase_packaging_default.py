c92dd2abdcf52b51e2c54a472536d20817ae75e0
root <yassine.taleb@wesolved.com>
2025-02-27 17:23:37 +0000

    [18.0][MIG] fix: Migration to 18.0

--- purchase_packaging_default/tests/test_purchase_packaging_default.py
+++ purchase_packaging_default/tests/test_purchase_packaging_default.py
@@ -1,6 +1,7 @@
 # Copyright 2025 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo.tests import Form
+
 from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
 from odoo.addons.product.tests.common import ProductCommon
 
