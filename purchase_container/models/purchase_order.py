a252ae1e4927c4abe029d389ab2797126515feb5
Nayatec <olivier.nibart@akretion.com.br>
2025-04-08 17:55:54 +0200

    [ADD][14.0] purchase_container module

---
+++ purchase_container/models/purchase_order.py
@@ -0,0 +1,38 @@
+from odoo import api, fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    container_ids = fields.Many2many(
+        "purchase.container",
+        string="Containers",
+        compute="_compute_container_ids",
+        inverse="_set_container_ids",
+        copy=False,
+        store=True,
+        index=True,
+    )
+
+    @api.depends("picking_ids.container_id")
+    def _compute_container_ids(self):
+        for purchase in self:
+            if purchase.picking_ids:
+                purchase.container_ids = [
+                    (6, 0, 
+                    [c.id for c in purchase.picking_ids.container_id]
+                    )
+                ]
+
+    def _set_container_ids(self):
+        pass
+
+    def _prepare_picking(self):
+        # This function initialize the picking vals from the current order
+        vals = super()._prepare_picking()
+        # So if we have at least a container set on the order, we should
+        # initialize the picking container with one.
+        if self.container_ids:
+            # Arbitrarily, we take the first one by default
+            vals["container_id"] = self.container_ids[0].id
+        return vals
