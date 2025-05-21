52442ad98c96c042913bd6114f7dd72ae718b0eb
Joan Sisquella <joan.sisquella@forgeflow.com>
2025-05-21 14:57:49 +0200

    [IMP] purchase_reception_status_line: add over-received status and propagation logic for orders and lines

--- purchase_reception_status_line/__manifest__.py
+++ purchase_reception_status_line/__manifest__.py
@@ -11,6 +11,9 @@
     "maintainers": ["DavidJForgeFlow"],
     "website": "https://github.com/OCA/purchase-workflow",
     "depends": ["purchase_reception_status"],
-    "data": ["views/purchase_order.xml"],
+    "data": [
+        "views/purchase_order.xml",
+        "views/purchase_order_line_views.xml",
+    ],
     "installable": True,
 }
