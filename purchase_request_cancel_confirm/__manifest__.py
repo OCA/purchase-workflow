9ec9a16cb11df9bc88b0f76b3d2c93b4898817ea
NAKROB SAMAEL <nakrobs@ecosoft.co.th>
2025-06-04 14:57:52 +0700

    [MIG] purchase_request_cancel_confirm : Migration to 18.0

--- purchase_request_cancel_confirm/__manifest__.py
+++ purchase_request_cancel_confirm/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Purchase Request Cancel Confirm",
-    "version": "15.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "category": "Usability",
     "license": "AGPL-3",
