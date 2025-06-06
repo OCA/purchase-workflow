f81fe9146ffc3b97c8d4992927031380d3d65084
AEstLo <palestebanez1@hotmail.com>
2025-06-06 17:22:00 +0200

    [MIG] purchase_order_line_sequence: Migration to 18.0

--- purchase_order_line_sequence/__manifest__.py
+++ purchase_order_line_sequence/__manifest__.py
@@ -7,7 +7,7 @@
     "name": "Purchase Order Line Sequence",
     "summary": "Adds sequence to PO lines and propagates it to"
     "Invoice lines and Stock Moves",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Purchase Management",
     "author": "Camptocamp, "
     "ForgeFlow, "
