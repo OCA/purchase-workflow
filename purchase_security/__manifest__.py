d0a8c514bf4516b3762f8d146c3376aedfa80092
Stefan Rijnhart <stefan@opener.amsterdam>
2025-06-09 20:43:16 +0200

    [FIX] purchase_security: fix invalid res.users kanban view
    Fixes
    ```
    Error: Missing 'card' template.
    ```
    when opening the purchase team form view.
    
    [UPD] Update purchase_security.pot
    
    [BOT] post-merge updates
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: purchase-workflow-18.0/purchase-workflow-18.0-purchase_security
    Translate-URL: https://translation.odoo-community.org/projects/purchase-workflow-18-0/purchase-workflow-18-0-purchase_security/

--- purchase_security/__manifest__.py
+++ purchase_security/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Purchase Order security",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "category": "Purchase",
     "development_status": "Production/Stable",
     "author": "Tecnativa, Odoo Community Association (OCA)",
