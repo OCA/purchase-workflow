ea18967b66ec9613b943185d68f9512b6c1b4945
Lois Rilo <lois.rilo@forgeflow.com>
2025-09-01 18:11:24 +0200

    [UPD] purchase_date_planned_manual: Eficent -> ForgeFlow

--- purchase_date_planned_manual/tests/test_date_planned_manual.py
+++ purchase_date_planned_manual/tests/test_date_planned_manual.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-25 ForgeFlow S.L. (http://www.forgeflow.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from datetime import datetime, timedelta
