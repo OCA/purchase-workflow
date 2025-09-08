dbae2730411f451a42472100c50917167ba76cc7
Joan Sisquella <joan.sisquella@forgeflow.com>
2025-09-08 15:59:26 +0200

    [IMP] purchase_invoice_status_line: pre-commit stuff

--- purchase_invoice_status_line/models/purchase_order_line.py
+++ purchase_invoice_status_line/models/purchase_order_line.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Joan Marín
+# Copyright 2025 ForgeFlow (http://www.forgeflow.com/)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
