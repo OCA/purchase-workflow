2fd9d57627115a838a5375147191b106eee5b99c
Joan Sisquella <joan.sisquella@forgeflow.com>
2025-09-10 15:25:55 +0200

    [IMP] purchase_force_invoiced_quantity-rename: improve migration script

--- purchase_force_invoiced_quantity/migrations/18.0.1.1.0/pre-migration.py
+++ purchase_force_invoiced_quantity/migrations/18.0.1.1.0/pre-migration.py
@@ -3,56 +3,27 @@
 
 import logging
 
+from odoo.tools.sql import column_exists
+
 _logger = logging.getLogger(__name__)
 
 
 def migrate_force_invoiced_to_force_invoiced_qty(cr):
-    cr.execute(
-        """
-        SELECT column_name
-        FROM information_schema.columns
-        WHERE table_name = 'purchase_order_line'
-        AND column_name = 'force_invoiced'
-        """
-    )
-    old_field_exists = cr.fetchone()
-    cr.execute(
-        """
-        SELECT column_name
-        FROM information_schema.columns
-        WHERE table_name = 'purchase_order_line'
-        AND column_name = 'force_invoiced_qty'
-        """
-    )
-    new_field_exists = cr.fetchone() is not None
+    old_field_exists = column_exists(cr, "purchase_order_line", "force_invoiced")
+    new_field_exists = column_exists(cr, "purchase_order_line", "force_invoiced_qty")
     if old_field_exists and not new_field_exists:
         _logger.info(
-            "Migrating force_invoiced field to force_invoiced_qty "
-            "in purchase_order_line"
+            "Renaming force_invoiced field to force_invoiced_qty in purchase_order_line"
         )
         cr.execute(
-            """
-            ALTER TABLE purchase_order_line
-            ADD COLUMN IF NOT EXISTS force_invoiced_qty numeric DEFAULT 0.0
-            """
+            "ALTER TABLE purchase_order_line "
+            "RENAME COLUMN force_invoiced TO force_invoiced_qty"
         )
-        cr.execute(
-            """
-            UPDATE purchase_order_line
-            SET force_invoiced_qty = COALESCE(force_invoiced, 0.0)
-            WHERE force_invoiced IS NOT NULL
-            """
-        )
-        cr.execute(
-            "ALTER TABLE purchase_order_line DROP COLUMN IF EXISTS force_invoiced"
-        )
-        _logger.info("Successfully migrated force_invoiced to force_invoiced_qty")
-    elif new_field_exists:
+        _logger.info("Successfully renamed force_invoiced to force_invoiced_qty")
+    if new_field_exists:
         _logger.info("Field force_invoiced_qty already exists, skipping migration")
-    else:
-        _logger.info(
-            "No force_invoiced field found in purchase_order_line, skipping migration"
-        )
+    if not old_field_exists:
+        _logger.info("Field force_invoiced does not exist, skipping migration")
 
 
 def migrate(cr, version):
