2219f26bac195e9d93184426afe219bc063bc7c5
Diego Paradeda <diego.paradeda@kmee.com.br>
2025-11-24 14:37:08 -0300

    [MIG] product_supplierinfo_qty_multiplier: Migration to 18.0

--- product_supplierinfo_qty_multiplier/__manifest__.py
+++ product_supplierinfo_qty_multiplier/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Product supplierinfo qty multiplier",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Inventory/Purchase",
     "website": "https://github.com/OCA/purchase-workflow",
     "author": "Tecnativa,GRAP, Odoo Community Association (OCA)",
