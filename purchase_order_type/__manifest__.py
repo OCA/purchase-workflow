78612a46a888279fb7a1c5a15572f61a17d39965
mav-adhoc <mav@adhoc.com.ar>
2025-12-16 11:00:57 -0300

    [FIX]purchase_order_type: PO type in res_partner_view

--- purchase_order_type/__manifest__.py
+++ purchase_order_type/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Purchase Order Type",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.1.1",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Purchase Management",
