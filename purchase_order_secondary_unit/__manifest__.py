47b49fb86b85cf9fb473af1ab02bbd744be86fd2
Jochen De Bie <jochen.debie@dynapps.be>
2026-02-26 12:10:16 +0100

    [MIG] purchase_order_secondary_unit: Migration to 19.0

--- purchase_order_secondary_unit/__manifest__.py
+++ purchase_order_secondary_unit/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Purchase Order Secondary Unit",
     "summary": "Purchase product in a secondary unit",
-    "version": "18.0.1.2.3",
+    "version": "19.0.1.0.0",
     "development_status": "Beta",
     "category": "Purchase",
     "website": "https://github.com/OCA/purchase-workflow",
