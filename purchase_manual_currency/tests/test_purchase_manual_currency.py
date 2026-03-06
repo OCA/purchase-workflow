af6d0ebb5b0d7c70a9da1606236898408ebce12e
Saran440 <saranl@ecosoft.co.th>
2026-03-06 13:39:46 +0700

    [IMP] purchase_manual_currency: refactor to use standard currency_rate

--- purchase_manual_currency/tests/test_purchase_manual_currency.py
+++ purchase_manual_currency/tests/test_purchase_manual_currency.py
@@ -1,65 +1,186 @@
 # Copyright 2020 Ecosoft Co., Ltd. (http://ecosoft.co.th)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+from odoo import Command
 from odoo.exceptions import ValidationError
 from odoo.tests import Form, TransactionCase
 
 
 class TestPurchaseManualCurrency(TransactionCase):
-    def setUp(self):
-        super().setUp()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.main_currency = cls.env.ref("base.USD")
+        cls.eur = cls.env.ref("base.EUR")
+        cls.eur.write({"active": True})
 
-        self.currency_eur = self.env.ref("base.EUR")
-        self.currency_usd = self.env.ref("base.USD")
-        self.main_company = self.env.ref("base.main_company")
-        self.env.cr.execute(
-            """UPDATE res_company SET currency_id = %s
-            WHERE id = %s""",
-            (self.currency_eur.id, self.main_company.id),
+        cls.purchase_model = cls.env["purchase.order"]
+        cls.partner = cls.env.ref("base.res_partner_12")
+        cls.product = cls.env.ref("product.product_product_9")
+
+        # Fixed currency rate
+        cls.eur.rate_ids.unlink()
+        cls.env["res.currency.rate"].create(
+            {
+                "currency_id": cls.eur.id,
+                "rate": 5,
+                "create_date": "2010-01-01",
+                "write_date": "2010-01-01",
+            }
         )
-        self.purchase_order = self.env.ref("purchase.purchase_order_6").copy()
 
-    def test_01_purchase_manual_currency(self):
-        # Update purchase to company currency
-        self.purchase_order.currency_id = self.currency_eur
-        self.assertEqual(self.purchase_order.currency_id.name, "EUR")
+    def test_01_purchase_main_currency(self):
+        """Case1: Use main currency"""
+
+        po = self.purchase_model.create(
+            {
+                "partner_id": self.partner.id,
+                "order_line": [
+                    Command.create(
+                        {
+                            "product_id": self.product.id,
+                            "product_qty": 1,
+                            "price_unit": 100,
+                        }
+                    )
+                ],
+            }
+        )
+
+        self.assertEqual(po.currency_id.name, "USD")
         self.assertEqual(
-            self.purchase_order.order_line[0].price_subtotal,
-            self.purchase_order.order_line[0].subtotal_company_currency,
+            po.order_line.price_subtotal,
+            po.order_line.subtotal_company_currency,
         )
-        self.assertFalse(self.purchase_order.currency_diff)
+        self.assertEqual(po.currency_rate, 1)
 
-        # Activate Multi Currency Group
-        multi_currency_group = self.env.ref("base.group_multi_currency")
-        self.env.user.groups_id |= multi_currency_group
+    def test_02_purchase_manual_currency_company_rate(self):
+        """Case2: Use manual currency with company rate"""
 
-        # Change currency
-        self.assertEqual(self.purchase_order.manual_currency_rate, 0.0)
-        with Form(self.purchase_order) as p:
-            p.currency_id = self.currency_usd
+        po = self.purchase_model.create(
+            {
+                "partner_id": self.partner.id,
+                "order_line": [
+                    Command.create(
+                        {
+                            "product_id": self.product.id,
+                            "product_qty": 1,
+                            "price_unit": 100,
+                        }
+                    )
+                ],
+            }
+        )
+
+        self.assertEqual(po.currency_id.name, "USD")
+        with Form(po) as p:
+            p.currency_id = self.eur
             p.manual_currency = True
             p.type_currency = "company_rate"
-        # Manual currency rate will default following rate standard
-        self.assertNotEqual(self.purchase_order.manual_currency_rate, 0.0)
-        self.assertNotEqual(
-            self.purchase_order.order_line[0].price_subtotal,
-            self.purchase_order.order_line[0].subtotal_company_currency,
-        )
-        company_rate = self.purchase_order.manual_currency_rate
-        # Check type curreny -> company currency (USD -> EUR)
-        with Form(self.purchase_order) as p:
-            p.type_currency = "inverse_company_rate"
+        p.save()
+        self.assertEqual(po.currency_id.name, "EUR")
+        # Standard Currency
+        self.assertEqual(po.currency_rate, 5)
+        self.assertEqual(po.type_currency, "company_rate")
+
+        self.assertEqual(po.order_line.price_subtotal, 100)
+        self.assertEqual(po.order_line.subtotal_company_currency, 100 / 5)
+
+        # Rate for test is 5 EUR = 1 USD
+        # So, amount_total_cc = 100 / 5 = 20 USD
+        # amount_total = 100 EUR
+        self.assertEqual(po.amount_total_cc, 20.0)
+        self.assertEqual(po.amount_total, 100.0)
+
+        # Manual change currency rate to 2 EUR = 1 USD
+        with Form(po) as p:
+            p.currency_rate = 2
+        p.save()
+
+        self.assertEqual(po.currency_rate, 2)
+        self.assertEqual(po.type_currency, "company_rate")
+        # Rate for test is 2 EUR = 1 USD
+        # So, amount_total_cc = 100 / 2 = 50 USD
+        # amount_total = 100 EUR
         self.assertEqual(
-            round(self.purchase_order.manual_currency_rate, 8),
-            round(1 / company_rate, 8),
+            po.order_line.subtotal_company_currency, po.order_line.price_subtotal / 2
         )
-        # Check function refresh
-        currency_rate = self.purchase_order.manual_currency_rate
-        self.purchase_order.manual_currency_rate += 5.0
-        self.assertNotEqual(self.purchase_order.manual_currency_rate, currency_rate)
-        self.purchase_order.action_refresh_currency()
-        self.assertAlmostEqual(self.purchase_order.manual_currency_rate, currency_rate)
-        self.purchase_order.button_confirm()
-        self.assertEqual(self.purchase_order.state, "purchase")
-        with self.assertRaises(ValidationError):
-            self.purchase_order.action_refresh_currency()
+        self.assertEqual(po.amount_total_cc, 50.0)
+        self.assertEqual(po.amount_total, 100.0)
+
+        po.button_confirm()
+        self.assertEqual(po.state, "purchase")
+
+        with self.assertRaisesRegex(
+            ValidationError, "Rate currency can refresh state draft only."
+        ):
+            po.action_refresh_currency()
+        po.button_draft()
+        self.assertEqual(po.state, "draft")
+        po.action_refresh_currency()
+        self.assertEqual(po.currency_rate, 5)
+
+    def test_03_purchase_manual_currency_inverse_company_rate(self):
+        """Case2: Use manual currency with inverse_company rate"""
+
+        po = self.purchase_model.create(
+            {
+                "partner_id": self.partner.id,
+                "order_line": [
+                    Command.create(
+                        {
+                            "product_id": self.product.id,
+                            "product_qty": 1,
+                            "price_unit": 100,
+                        }
+                    )
+                ],
+            }
+        )
+
+        self.assertEqual(po.currency_id.name, "USD")
+        with Form(po) as p:
+            p.currency_id = self.eur
+            p.manual_currency = True
+            p.type_currency = "inverse_company_rate"
+        p.save()
+        self.assertEqual(po.currency_id.name, "EUR")
+
+        # Inverse, Currency Rate is 1 EUR = 0.2 USD
+        self.assertEqual(po.currency_rate, 0.2)
+        self.assertEqual(po.type_currency, "inverse_company_rate")
+        # Rate for test is 1 EUR = 0.2 USD
+        # So, amount_total_cc = 100 * 0.2 = 20 USD
+        # amount_total = 100 EUR
+        self.assertEqual(po.order_line.price_subtotal, 100)
+        self.assertEqual(po.order_line.subtotal_company_currency, 100 * 0.2)
+        self.assertEqual(po.amount_total_cc, 20.0)
+        self.assertEqual(po.amount_total, 100.0)
+
+        # Manual change currency rate to 2 EUR = 1 USD
+        with Form(po) as p:
+            p.currency_rate = 2
+        p.save()
+
+        self.assertEqual(po.currency_rate, 2)
+        self.assertEqual(po.type_currency, "inverse_company_rate")
+        # Rate for test is 2 EUR = 1 USD
+        # So, amount_total_cc = 100 * 2 = 200 USD
+        # amount_total = 100 EUR
+        self.assertEqual(
+            po.order_line.subtotal_company_currency, po.order_line.price_subtotal * 2
+        )
+        self.assertEqual(po.amount_total_cc, 200.0)
+        self.assertEqual(po.amount_total, 100.0)
+
+        po.button_confirm()
+        self.assertEqual(po.state, "purchase")
+
+        with self.assertRaisesRegex(
+            ValidationError, "Rate currency can refresh state draft only."
+        ):
+            po.action_refresh_currency()
+        po.button_draft()
+        self.assertEqual(po.state, "draft")
+        po.action_refresh_currency()
+        self.assertEqual(po.currency_rate, 0.2)
