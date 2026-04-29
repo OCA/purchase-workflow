ce1d8cf7cb6b914439d181e00b43bd869a7e2f80
Bhavesh Heliconia <oca@heliconia.in>
2026-04-29 16:43:37 +0530

    [IMP] purchase_order_line_stock_available: pre-commit auto fixes

--- purchase_order_line_stock_available/__manifest__.py
+++ purchase_order_line_stock_available/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Purchase order line stock available",
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
-    "author": "AvanzOSC," "Tecnativa," "Odoo Community Association (OCA)",
+    "author": "AvanzOSC,Tecnativa,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "depends": ["purchase_stock"],
     "category": "Purchases",
