c6896cce1884014844a03df4d121ff0147f06be8
Bhavesh Heliconia <oca@heliconia.in>
2026-05-01 15:24:36 +0530

    [MIG] purchase_request_substate: Migration to 19.0

--- purchase_request_substate/__manifest__.py
+++ purchase_request_substate/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Purchase Request Sub State",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Tools",
     "author": "Ecosoft,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
