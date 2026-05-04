b3f8c116d02a46137f458f13ce5c00b71ee24690
Bhavesh Heliconia <oca@heliconia.in>
2026-05-04 18:07:29 +0530

    [MIG] purchase_stock_packaging: Migration to 19.0

--- purchase_stock_packaging/tests/test_purchase_packaging.py
+++ purchase_stock_packaging/tests/test_purchase_packaging.py
@@ -1,72 +1,96 @@
 # Copyright 2022 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo.tests.common import TransactionCase
+from odoo import Command
+
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestPurchasePackaging(TransactionCase):
+class TestPurchasePackaging(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
         cls.line_obj = cls.env["purchase.order.line"]
-        cls.partner = cls.env.ref("base.res_partner_12")
-        cls.product = cls.env.ref("product.product_product_9")
-        cls.packaging = cls.env["product.packaging"].create(
-            {"name": "Test packaging", "product_id": cls.product.id, "qty": 5.0}
+        cls.product = cls.env["product.product"].create(
+            {
+                "name": "Test Product",
+                "type": "consu",
+                "purchase_ok": True,
+                "seller_ids": [
+                    Command.create(
+                        {
+                            "partner_id": cls.partner.id,
+                            "min_qty": 1,
+                            "price": 10.0,
+                        }
+                    )
+                ],
+            }
         )
-        cls.packaging_10 = cls.env["product.packaging"].create(
-            {"name": "Test packaging", "product_id": cls.product.id, "qty": 10.0}
+        cls.uom_unit = cls.env.ref("uom.product_uom_unit")
+        cls.packaging_uom = cls.env["uom.uom"].create(
+            {
+                "name": "Box of 6",
+                "relative_uom_id": cls.uom_unit.id,
+                "relative_factor": 6.0,
+            }
         )
-        cls.packaging_12 = cls.env["product.packaging"].create(
-            {"name": "Test packaging 12", "product_id": cls.product.id, "qty": 12.0}
-        )
-        cls.env.user.groups_id += cls.env.ref("product.group_stock_packaging")
-        cls.warehouse = cls.env.ref("stock.warehouse0")
+        cls.warehouse = cls.env["stock.warehouse"].search([], limit=1)
 
     def test_purchase_packaging_from_procurement(self):
-        # Check of packaging is well passed from procurement to purchase line
-        # and does not take default one
-        lines_before = self.line_obj.search([])
-        self.group = self.env["procurement.group"].create({"name": "Test"})
-        self.env["procurement.group"].run(
+        """Test if packaging UoM is passed from procurement to PO line"""
+        values = {
+            "warehouse_id": self.warehouse,
+            "packaging_uom_id": self.packaging_uom,
+            "route_ids": self.warehouse.reception_route_id,
+        }
+        self.env["stock.rule"].run(
             [
-                self.group.Procurement(
+                self.env["stock.rule"].Procurement(
                     self.product,
-                    20.0,
-                    self.product.uom_id,
+                    12.0,
+                    self.uom_unit,
                     self.warehouse.lot_stock_id,
-                    "Product",
-                    "Product",
-                    company_id=self.env.company,
-                    values={"product_packaging_id": self.packaging},
+                    "Test procurement",
+                    "TEST_PROC",
+                    self.env.company,
+                    values,
                 )
             ]
         )
-        lines_after = self.line_obj.search([]) - lines_before
-
-        self.assertTrue(lines_after.product_packaging_id)
-        self.assertEqual(lines_after.product_packaging_id, self.packaging)
+        po_line = self.line_obj.search(
+            [
+                ("product_id", "=", self.product.id),
+                ("order_id.origin", "ilike", "TEST_PROC"),
+            ]
+        )
+        self.assertTrue(po_line, "PO line should have been created")
+        self.assertEqual(po_line.product_uom_id, self.packaging_uom)
+        self.assertEqual(po_line.product_qty, 2.0)
 
     def test_purchase_packaging_from_move(self):
-        # Check of packaging is well passed from stock move to procurement,
-        # then to purchase line and does not take default one
-        lines_before = self.line_obj.search([])
-
-        self.move = self.env["stock.move"].create(
+        """Test if packaging UoM is passed from stock move to procurement to PO line"""
+        buy_route = self.env.ref("purchase_stock.route_warehouse0_buy")
+        self.product.route_ids = [Command.link(buy_route.id)]
+        move = self.env["stock.move"].create(
             {
-                "name": "Product Test",
+                "origin": "TEST_MOVE_ORIGIN",
                 "location_dest_id": self.env.ref("stock.stock_location_customers").id,
                 "location_id": self.warehouse.lot_stock_id.id,
                 "product_id": self.product.id,
-                "product_uom_qty": 20.0,
-                "product_packaging_id": self.packaging.id,
+                "product_uom_qty": 18.0,
+                "product_uom": self.uom_unit.id,
                 "procure_method": "make_to_order",
             }
         )
+        move.packaging_uom_id = self.packaging_uom
+        move._action_confirm()
+        po_line = self.line_obj.search(
+            [
+                ("product_id", "=", self.product.id),
+                ("order_id.origin", "ilike", "TEST_MOVE_ORIGIN"),
+            ]
+        )
 
-        self.move._action_confirm()
-
-        lines_after = self.line_obj.search([]) - lines_before
-
-        self.assertTrue(lines_after.product_packaging_id)
-        self.assertEqual(lines_after.product_packaging_id, self.packaging)
+        self.assertTrue(po_line, "PO line should have been created from move")
+        self.assertEqual(po_line.product_uom_id, self.packaging_uom)
+        self.assertEqual(po_line.product_qty, 3.0)  # 18 / 6
