e35c0354f23bfea4d906f334ddb7fbf3c060a949
Andrii9090-tecnativa <andrii.kompaniiets@tecnativa.com>
2026-05-07 14:44:12 +0200

    [IMP] product_main_seller: Add main_seller_id to product.product product and add to depends date_start, date_end

--- product_main_seller/hooks.py
+++ product_main_seller/hooks.py
@@ -36,3 +36,48 @@ def pre_init_hook(env):
         WHERE pt.id = first_supplierinfos.product_tmpl_id;
         """
     )
+    cr.execute(
+        """
+        ALTER TABLE product_product
+        ADD COLUMN IF NOT EXISTS main_seller_id integer;
+        """
+    )
+    cr.execute(
+        """
+        WITH ranked_supplierinfos AS (
+        SELECT
+            p.id AS product_id,
+            psi.partner_id,
+            ROW_NUMBER() OVER (
+                PARTITION BY p.id
+                ORDER BY
+                    CASE
+                        WHEN psi.product_id = p.id THEN 0
+                        ELSE 1
+                    END,
+                    psi.sequence,
+                    psi.min_qty DESC,
+                    psi.price,
+                    psi.id
+            ) AS row_number
+        FROM product_product p
+        JOIN product_supplierinfo psi
+            ON psi.product_tmpl_id = p.product_tmpl_id
+            AND (
+                psi.product_id = p.id
+                OR psi.product_id IS NULL
+            )
+        JOIN res_partner rp
+            ON rp.id = psi.partner_id
+            AND rp.active
+        WHERE
+            (psi.date_start IS NULL OR psi.date_start <= CURRENT_DATE)
+            AND (psi.date_end IS NULL OR psi.date_end >= CURRENT_DATE)
+        )
+        UPDATE product_product p
+        SET main_seller_id = ranked_supplierinfos.partner_id
+        FROM ranked_supplierinfos
+        WHERE ranked_supplierinfos.product_id = p.id
+        AND ranked_supplierinfos.row_number = 1;
+        """
+    )
--- product_main_seller/models/__init__.py
+++ product_main_seller/models/__init__.py
@@ -1,1 +1,3 @@
 from . import product_template
+from . import product_product
+from . import product_supplierinfo
