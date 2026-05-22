40f73b0072dd65dda9d3093f5ffeb219e1867a35
Paco Montés <f.montesdoria@gmail.com>
2026-05-22 08:38:36 +0200

    [18.0][MIG] purchase_line_product_image: Migration to 18.0

--- purchase_line_product_image/__manifest__.py
+++ purchase_line_product_image/__manifest__.py
@@ -1,19 +1,20 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Lucky Kurniawan <kurniawanluckyy@gmail.com>
+# Copyright 2026 TesseraTech Solutions S.L. <https://www.tesseratech.es>
+# Copyright 2026 Paco Montés <f.montesdoria@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Purchase Order Line Product Image",
     "summary": "Show Product Image at Purchase Order Line.",
-    "version": "10.0.1.0.0",
-    "author": "Lucky Kurniawan, "
-              "Odoo Community Association (OCA)",
+    "version": "18.0.1.0.0",
+    "author": "Lucky Kurniawan,"
+    "TesseraTech Solutions S.L.,"
+    "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Purchases",
-    "depends": ['purchase', 'web_tree_image'],
+    "depends": ["purchase"],
     "data": [
-        'views/purchase_order_views.xml',
+        "views/purchase_order_views.xml",
     ],
-    'qweb': ['static/src/xml/widget.xml', ],
     "license": "AGPL-3",
     "installable": True,
     "application": False,
--- purchase_line_product_image/models/purchase_order.py
+++ purchase_line_product_image/models/purchase_order.py
@@ -1,12 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Lucky Kurniawan <kurniawanluckyy@gmail.com>
+# Copyright 2026 TesseraTech Solutions S.L. <https://www.tesseratech.es>
+# Copyright 2026 Paco Montés <f.montesdoria@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
 
 
 class PurchaseOrderLine(models.Model):
-    _inherit = 'purchase.order.line'
+    _inherit = "purchase.order.line"
 
     product_image = fields.Binary(
-        related='product_id.product_tmpl_id.image_medium')
+        related="product_id.product_tmpl_id.image_1920",
+        readonly=True,
+    )
