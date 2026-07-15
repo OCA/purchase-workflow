0cf103de6de6e47be8a7a6ad072531d25b6b8fcb
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2026-07-15 16:13:11 +0200

    [MIG] account_fiscal_position_allowed_journal_sale_purchase: migration to version 19.0

--- account_fiscal_position_allowed_journal_sale_purchase/models/account_move.py
+++ account_fiscal_position_allowed_journal_sale_purchase/models/account_move.py
@@ -7,22 +7,15 @@ from odoo import api, models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    @api.onchange("partner_shipping_id", "company_id")
-    def _onchange_partner_shipping_id(self):
+    @api.depends("line_ids.purchase_line_id.order_id.fiscal_position_id")
+    def _compute_fiscal_position_id(self):  # pylint: disable=missing-return
+        """When both sale and purchase are installed, the fiscal position is
+        recomputed from the partner whenever a related field changes, which
+        would override the one set on the originating purchase order. Keep the
+        purchase order's fiscal position on the related vendor bill.
         """
-        Trigger the change of fiscal position when the shipping address is modified.
-        """
-        fiscal_position = self.fiscal_position_id
-        res = super()._onchange_partner_shipping_id()
-        if fiscal_position and fiscal_position != self.fiscal_position_id:
-            self.fiscal_position_id = fiscal_position
-        return res
-
-    @api.onchange("partner_id", "company_id")
-    def _onchange_partner_id(self):
-        fiscal_position = self.fiscal_position_id
-        res = super()._onchange_partner_id()
-        if fiscal_position:
-            self.fiscal_position_id = fiscal_position
-        self._onchange_fiscal_position_allowed_journal()
-        return res
+        super()._compute_fiscal_position_id()
+        for move in self:
+            purchase_order = move.line_ids.purchase_line_id.order_id[:1]
+            if purchase_order.fiscal_position_id:
+                move.fiscal_position_id = purchase_order.fiscal_position_id
