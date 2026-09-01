9045601bee9ed2be28cbe176669acd2ef3b65b73
kalpeshgajera-uncannycs <kgajera@uncannycs.com>
2026-09-01 19:04:07 +0530

    [19.0][MIG]purchase_tier_validation: Migration to 19.0

--- purchase_tier_validation/tests/test_tier_validation.py
+++ purchase_tier_validation/tests/test_tier_validation.py
@@ -2,10 +2,19 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 
-from odoo.addons.base_tier_validation.tests.common import CommonTierValidation
+from odoo.tests.common import TransactionCase
 
 
-class TestPurchaseTierValidation(CommonTierValidation):
+# We use TransactionCase instead of CommonTierValidation to avoid
+# unnecessary test setups (like creating companies and dummy models)
+# which can cause NotNullViolation database errors when modules like
+# hr_timesheet are installed in the same environment.
+class TestPurchaseTierValidation(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+
     def test_01_tier_definition_models(self):
-        res = self.tier_def_obj._get_tier_validation_model_names()
+        res = self.env["tier.definition"]._get_tier_validation_model_names()
         self.assertIn("purchase.order", res)
